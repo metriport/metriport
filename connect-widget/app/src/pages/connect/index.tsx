6871c2e14e80c6f6554da6318f441856679a2279
Jorge Orta <jorta012@fiu.edu>
2022-12-23 12:05:40 -0500

    add agreement to local storage

--- connect-widget/app/src/pages/connect/index.tsx
+++ connect-widget/app/src/pages/connect/index.tsx
@@ -1,18 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import WidgetContainer from "../../shared/components/WidgetContainer";
 import ConnectProviders from "./components/connect-providers";
-import Agreement from "../agreement/components/agreement";
+import Agreement from "./components/agreement";
 
 const ConnectPage = () => {
   const [agreement, setAgreement] = useState(false);
+  const agreementLocalStorageKey = "agreement-accepted-v1";
+
+  const getLocalStorageAgreement = (): boolean => {
+    const localStorageAgreement = localStorage.getItem(
+      agreementLocalStorageKey
+    );
+
+    if (localStorageAgreement) {
+      const parsedValue = JSON.parse(localStorageAgreement);
+
+      return parsedValue;
+    }
+
+    return false;
+  };
+
+  useEffect(() => {
+    const localStorageAgreement = getLocalStorageAgreement();
+
+    if (localStorageAgreement) {
+      setAgreement(localStorageAgreement);
+    }
+  }, []);
+
+  const acceptAgreement = (): void => {
+    localStorage.setItem(agreementLocalStorageKey, JSON.stringify(true));
+    setAgreement(true);
+  };
 
   return (
     <WidgetContainer>
       {agreement ? (
         <ConnectProviders />
       ) : (
-        <Agreement onAcceptAgreement={() => setAgreement(true)} />
+        <Agreement onAcceptAgreement={acceptAgreement} />
       )}
     </WidgetContainer>
   );
