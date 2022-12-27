602aee661f10cfff284c661fa87e217612d50c18
Jorge Orta <jorta012@fiu.edu>
2022-12-27 12:04:53 -0500

    111 add back garmin

--- connect-widget/app/src/pages/connect/components/connect-button.tsx
+++ connect-widget/app/src/pages/connect/components/connect-button.tsx
@@ -27,41 +27,18 @@ const ConnectButton = ({
   return (
     <Box>
       <Center>
-        {provider.name === "garmin" ? (
-          <ComingSoon btnWidth={btnWidth} />
-        ) : (
-          <Button
-            p={2}
-            w={btnWidth}
-            aria-label="Connect"
-            isDisabled={isConnected}
-            onClick={() => onRedirect(provider.name)}
-            isLoading={isLoading[provider.name]}
-          >
-            {content}
-          </Button>
-        )}
+        <Button
+          p={2}
+          w={btnWidth}
+          aria-label="Connect"
+          isDisabled={isConnected}
+          onClick={() => onRedirect(provider.name)}
+          isLoading={isLoading[provider.name]}
+        >
+          {content}
+        </Button>
       </Center>
     </Box>
-  );
-};
-
-const ComingSoon = ({ btnWidth }: { btnWidth: any }) => {
-  const garmin = useBreakpointValue({
-    base: <Text p="4">+</Text>,
-    sm: <Text p="4">Coming Soon!</Text>,
-  });
-
-  return (
-    <Button
-      border="1px"
-      p={2}
-      w={btnWidth}
-      variant={"hollow"}
-      isDisabled={true}
-    >
-      {garmin}
-    </Button>
   );
 };
 
