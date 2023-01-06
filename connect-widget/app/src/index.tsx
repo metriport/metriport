f6036b5b237931a7f0c2f8651975462390d2798a
Rafael Leite <rafael.leite@livechroma.ca>
2023-01-06 16:03:08 -0300

    Fire a new deploy
    #92

--- connect-widget/app/src/index.tsx
+++ connect-widget/app/src/index.tsx
@@ -7,7 +7,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
 root.render(
   <React.StrictMode>
     <App />
