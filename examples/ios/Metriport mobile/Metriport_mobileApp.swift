54db9503e9991b07770214e5d54c149dafde5652
Jorge Orta <jorta012@fiu.edu>
2023-01-08 13:44:52 -0500

    186 commit ios

---
+++ examples/ios/Metriport mobile/Metriport_mobileApp.swift
@@ -0,0 +1,92 @@
+//
+//  Metriport_mobileApp.swift
+//  Metriport mobile
+//
+//  Created by Jorge orta on 1/2/23.
+//
+
+import SwiftUI
+import Foundation
+import WebKit
+
+@main
+struct Metriport_mobileApp: App {
+    // Return the scene.
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var showWebView = false
+    
+    var webView = MetriportWidget(url: URL(string: "http://localhost:3001/?token=1bEFTi9Zt_ljmG88pHj4U&colorMode=dark")!)
+    
+    var body: some View {
+        VStack {
+          webView
+          HStack {
+            Button(action: {
+              self.webView.goBack()
+            }){
+              Image(systemName: "arrowtriangle.left.fill")
+                                .font(.title)
+                                .foregroundColor(.blue)
+                                .padding()
+            }
+            Spacer()
+            Button(action: {
+              self.webView.refresh()
+            }){
+              Image(systemName: "arrow.clockwise.circle.fill")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                    .padding()
+            }
+            Spacer()
+            Button(action: {
+              self.webView.goForward()
+            }){
+              Image(systemName: "arrowtriangle.right.fill")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                    .padding()
+            }
+          }
+        }
+      }
+}
+
+struct MetriportWidget: UIViewRepresentable {
+    
+    var url: URL
+    private var webView: WKWebView?
+    
+    public init(url: URL) {
+        self.webView = WKWebView()
+        self.url = url
+    }
+    
+    public func makeUIView(context: Context) -> WKWebView {
+        return webView!
+    }
+
+    public func updateUIView(_ webView: WKWebView, context: Context) {
+       let request = URLRequest(url: url)
+       webView.load(request)
+    }
+    
+    public func goBack(){
+        webView?.goBack()
+    }
+
+    public func goForward(){
+        webView?.goForward()
+    }
+    
+    public func refresh() {
+        webView?.reload()
+    }
+}
