93830805220f8a5c62a938c5cd27c425d2b142e3
Jorge Orta <jorta012@fiu.edu>
2023-01-08 13:12:31 -0500

    186 add examples for the android and ios widgets

---
+++ examples/android/app/src/main/java/com/example/metriport/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.metriport
+
+import android.os.Bundle
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // WebViewClient allows you to handle
+        // onPageFinished and override Url loading.
+        webView.webViewClient = WebViewClient()
+
+        // this will load the url of the website
+        webView.loadUrl("http://192.168.0.138:3001/?token=1bEFTi9Zt_ljmG88pHj4U&colorMode=dark")
+
+        webView.settings.domStorageEnabled = true;
+
+        // this will enable the javascript settings, it can also allow xss vulnerabilities
+        webView.settings.javaScriptEnabled = true
+
+        // if you want to enable zoom feature
+        webView.settings.setSupportZoom(true)
+
+
+    }
+
+    // if you press Back button this code will work
+    override fun onBackPressed() {
+        // if your webview can go back it will go back
+        if (webView.canGoBack())
+            webView.goBack()
+        // if your webview cannot go back
+        // it will exit the application
+        else
+            super.onBackPressed()
+    }
+}
