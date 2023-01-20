b4b962f880b223b982fcc1ae6bfaba4db8fc8147
Jorge Orta <jorta012@fiu.edu>
2023-01-20 11:40:20 -0500

    fix(apple): add lowercase number

--- api/app/src/errors/metriport-error.ts
+++ api/app/src/errors/metriport-error.ts
@@ -1,7 +1,7 @@
 import status from 'http-status';
 
 export default abstract class MetriportError extends Error {
-  status: Number = status.INTERNAL_SERVER_ERROR;
+  status: number = status.INTERNAL_SERVER_ERROR;
   constructor(message: string) {
     super(message);
   }
