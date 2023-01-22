9dd28d9b521d0456c50a482487f382c4ac37b186
Rafael Leite <rafael@metriport.com>
2023-01-22 14:06:19 -0600

    build: add missing scripts
    Ref #143

--- infra/test/infrastructure.test.ts
+++ infra/test/infrastructure.test.ts
@@ -1,3 +1,5 @@
+import { test } from "@jest/globals";
+
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
 // import * as Infrastructure from '../lib/infrastructure-stack';
