54b996c6c928045fb45e8206014e7102337b47be
Jorge Orta <jorta012@fiu.edu>
2023-01-25 20:06:44 -0500

    fix(api): add back report usage

--- api/app/src/routes/index.ts
+++ api/app/src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Application } from "express";
 import connect from "./connect";
 import { processAPIKey } from "./middlewares/auth";
-// import  } from "./middlewares/usage";
+import { reportUsage } from "./middlewares/usage";
 import nutrition from "./nutrition";
 import activity from "./activity";
 import body from "./body";
@@ -20,12 +20,12 @@ export default (app: Application) => {
 
   // routes with API key auth
   app.use("/settings", processAPIKey, settings);
-  app.use("/activity", processAPIKey, activity);
-  app.use("/body", processAPIKey, body);
-  app.use("/biometrics", processAPIKey, biometrics);
-  app.use("/nutrition", processAPIKey, nutrition);
-  app.use("/sleep", processAPIKey, sleep);
-  app.use("/user", processAPIKey, user);
+  app.use("/activity", processAPIKey, reportUsage, activity);
+  app.use("/body", processAPIKey, reportUsage, body);
+  app.use("/biometrics", processAPIKey, reportUsage, biometrics);
+  app.use("/nutrition", processAPIKey, reportUsage, nutrition);
+  app.use("/sleep", processAPIKey, reportUsage, sleep);
+  app.use("/user", processAPIKey, reportUsage, user);
 
   // routes with session token auth
   app.use("/connect", connect);
