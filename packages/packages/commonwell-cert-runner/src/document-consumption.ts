75a70011562ea03395b07b159a91599709318ae8
Rafael Leite <rafael@metriport.ai>
2023-01-30 22:50:23 -0600

    chore: remove leftovers
    Ref #227

--- packages/packages/commonwell-cert-runner/src/document-consumption.ts
+++ packages/packages/commonwell-cert-runner/src/document-consumption.ts
@@ -35,13 +35,6 @@ export async function queryDocuments(
   // E1: Document Query
   console.log(`>>> E1c: Query for documents using FHIR (REST)`);
 
-  console.log(`... Search for Patient...`, {
-    firstName: docPerson.details.name[0].given[0],
-    lastName: docPerson.details.name[0].family[0],
-    DOB: docPerson.details.birthDate,
-    gender: docPerson.details.gender.code,
-    zipCode: docPerson.details.address[0].zip,
-  });
   const respPatient = await commonWell.searchPatient(
     queryMeta,
     docPerson.details.name[0].given[0],
@@ -65,26 +58,18 @@ export async function queryDocuments(
     }
     const patient = embeddedPatients[0];
     patientId = getIdTrailingSlash(patient);
-    console.log(`... patientId: ${patientId}`);
 
-    console.log(`... Search for Person using patient's demographics...`, { patientId: patientId });
     const respPerson = await commonWell.searchPersonByPatientDemo(queryMeta, patientId);
     console.log(respPerson);
     personId = getPersonIdFromSearchByPatientDemo(respPerson);
-    console.log(`... personId: ${personId}`);
 
     //
   } else {
     // OTHERWISE ADD ONE
-    console.log(`... Did not find a patient, creating person/patient...`);
-
-    console.log(`... Enroll a Person with a Strong ID...`);
     const respPerson = await commonWell.enrollPerson(queryMeta, docPerson);
     console.log(respPerson);
     personId = getId(respPerson);
-    console.log(`... personId: ${personId}`);
 
-    console.log(`... Register a new Patient...`);
     const respPatientCreate = await commonWell.registerPatient(queryMeta, docPatient);
     console.log(respPatientCreate);
     patientId = getIdTrailingSlash(respPatientCreate);
@@ -92,9 +77,7 @@ export async function queryDocuments(
     const patientStrongId = patientStrongIds
       ? patientStrongIds.find(id => id.system === metriportSystem)
       : undefined;
-    console.log(`... patientId: ${patientId}`);
 
-    console.log(`... Link a Patient to a Person upgrading from LOLA 1 to LOLA 2...`);
     const patientLink = respPatientCreate._links.self.href;
     const respLink = await commonWell.patientLink(
       queryMeta,
@@ -106,14 +89,12 @@ export async function queryDocuments(
   }
 
   if (!personId) throw new Error(`[E1c] personId is undefined before calling getPatientsLinks()`);
-  console.log(`... Get Network links...`);
   const respLinks = await commonWell.getPatientsLinks(queryMeta, patientId);
   console.log(respLinks);
   const allLinks = respLinks._embedded.networkLink;
   const lola1Links = allLinks.filter(isLOLA1);
   console.log(`Found ${allLinks.length} network links, ${lola1Links.length} are LOLA 1`);
   for (const link of lola1Links) {
-    console.log(`... Upgrade link from LOLA 1 to LOLA 2...`);
     const respUpgradeLink = await commonWell.upgradeOrDowngradePatientLink(
       queryMeta,
       link._links.upgrade.href
@@ -125,7 +106,6 @@ export async function queryDocuments(
   if (!patientIdForDocQuery) {
     throw new Error(`[E1c] Could not determine patient ID for document query`);
   }
-  console.log(`... patientIdForDocQuery: ${patientIdForDocQuery}`);
 
   console.log(`>>> [E1c] Querying for docs...`);
   const respDocQuery = await commonWell.queryDocument(queryMeta, patientIdForDocQuery);
@@ -141,7 +121,6 @@ export async function retrieveDocument(
   // E2: Document Retrieve
   console.log(`>>> E2c: Retrieve documents using FHIR (REST)`);
 
-  console.log(`... FILE: ${JSON.stringify(doc, undefined, 2)}`);
   const fileName = `./commonwell_${nanoid()}.file`;
   const outputStream = fs.createWriteStream(fileName, {
     // the default is UTF-8, avoid changing the encoding if we don't know the file we're downloading
