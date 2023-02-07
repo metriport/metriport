2c6c476fd48a12120bfe6a5fd756c5bfa22677da
Jorge Orta <jorta012@fiu.edu>
2023-02-07 16:18:57 -0500

    feat(packages): create react native widget
    Ref. metriport/metriport-internal#325

--- examples/react-native/App.tsx
+++ examples/react-native/App.tsx
@@ -1,57 +1,29 @@
-import React, { useState } from "react";
-import { Modal, StyleSheet, Text, Pressable, View } from "react-native";
+import * as React from "react";
+
+import { StyleSheet, Platform } from "react-native";
+import { MetriportWidget } from "@metriport/react-native-sdk";
 import { WebView } from "react-native-webview";
 
-const App = () => {
-  const [modalVisible, setModalVisible] = useState(false);
+export default function App() {
+  if (Platform.OS === "ios") {
+    return (
+      <MetriportWidget
+        token="CONNECT_SESSION_TOKEN"
+        clientApiKey="CLIENT_API_KEY"
+        sandbox={false}
+        style={styles.box}
+      />
+    );
+  }
 
   return (
-    <View style={styles.centeredView}>
-      <Modal
-        animationType='slide'
-        presentationStyle='formSheet'
-        visible={modalVisible}
-      >
-        <Pressable
-          style={{ backgroundColor: "#748df0", height: 40, alignItems: "center", justifyContent: 'center' }}
-          onPress={() => setModalVisible(false)}
-        >
-          <Text style={styles.textStyle}>CLOSE</Text>
-        </Pressable>
-        <WebView
-          source={{ uri: "https://connect.metriport.com/?token=demo" }}
-        />
-      </Modal>
-      <Pressable
-        style={[styles.button, styles.buttonOpen]}
-        onPress={() => setModalVisible(true)}
-      >
-        <Text style={styles.textStyle}>Show Metriport Widget</Text>
-      </Pressable>
-    </View>
+    <WebView source={{ uri: "https://connect.metriport.com/?token=demo" }} />
   );
-};
+}
 
 const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2
-  },
-  buttonOpen: {
-    backgroundColor: "#748df0"
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
+  box: {
+    width: "100%",
+    height: "100%",
   },
 });
-
-export default App;
---
+++ packages/packages/react-native-sdk/README.md
@@ -0,0 +1,31 @@
+# @metriport/react-native-sdk
+
+a package to consume metriport
+
+## Installation
+
+```sh
+npm install @metriport/react-native-sdk
+```
+
+## Usage
+
+```js
+import { MetriportWidget } from "@metriport/react-native-sdk";
+
+// ...
+
+<MetriportWidget token="CONNECT_TOKEN" clientApiKey="CLIENT_API_KEY" sandbox={false} />;
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
