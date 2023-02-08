e9f1d880e649772d9760d893a94a062d02da61dc
Jorge Orta <jorta012@fiu.edu>
2023-02-07 22:18:31 -0500

    feat(packages): combine apple and android in package
    Ref. metriport/metriport-internal#325

--- examples/react-native/App.tsx
+++ examples/react-native/App.tsx
@@ -1,23 +1,16 @@
 import * as React from "react";
 
-import { StyleSheet, Platform } from "react-native";
+import { StyleSheet } from "react-native";
 import { MetriportWidget } from "@metriport/react-native-sdk";
-import { WebView } from "react-native-webview";
 
 export default function App() {
-  if (Platform.OS === "ios") {
-    return (
-      <MetriportWidget
-        token="CONNECT_SESSION_TOKEN"
-        clientApiKey="CLIENT_API_KEY"
-        sandbox={false}
-        style={styles.box}
-      />
-    );
-  }
-
   return (
-    <WebView source={{ uri: "https://connect.metriport.com/?token=demo" }} />
+    <MetriportWidget
+      token="CONNECT_TOKEN"
+      clientApiKey="CLIENT_API_KEY"
+      sandbox={false}
+      style={styles.box}
+    />
   );
 }
 
--- packages/packages/react-native-sdk/src/index.tsx
+++ packages/packages/react-native-sdk/src/index.tsx
@@ -1,7 +1,8 @@
 import { requireNativeComponent, UIManager, Platform, ViewStyle } from "react-native";
+import { WebView, WebViewProps } from "react-native-webview";
 
 const LINKING_ERROR =
-  `The package 'metriport-react-native' doesn't seem to be linked. Make sure: \n\n` +
+  `The package '@metriport/react-native-sdk' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: "" }) +
   "- You rebuilt the app after installing the package\n" +
   "- You are not using Expo Go\n";
@@ -20,9 +21,19 @@ type MetriportWidgetProps = {
 
 const ComponentName = "MetriportWidget";
 
-export const MetriportWidget =
+const NativeWidget =
   UIManager.getViewManagerConfig(ComponentName) != null
     ? requireNativeComponent<MetriportWidgetProps>(ComponentName)
     : () => {
         throw new Error(LINKING_ERROR);
       };
+
+export const MetriportWidget = (props: MetriportWidgetProps & WebViewProps) => {
+  if (Platform.OS === "ios") {
+    return <NativeWidget {...props} />;
+  }
+
+  const url = props.url ? props.url : "https://connect.metriport.com";
+
+  return <WebView source={{ uri: `${url}?token=${props.token}` }} {...props} />;
+};
