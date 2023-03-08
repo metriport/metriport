036a7b360dd25df94797242a588b9064b29e6916
Rafael Leite <rafael@metriport.ai>
2023-03-08 16:36:29 -0600

    refactor(api): re-add returning:true to Patient updates
    Ref. metriport/metriport-internal#369

--- api/app/src/command/medical/patient/update-patient.ts
+++ api/app/src/command/medical/patient/update-patient.ts
@@ -27,17 +27,17 @@ export const updatePatient = async (patient: PatientUpdate): Promise<Patient> =>
   data.address = sanitized.address;
   data.contact = sanitized.contact;
 
-  const [count] = await Patient.update(
+  const [count, rows] = await Patient.update(
     {
       data,
     },
-    { where: { id, cxId } }
+    { where: { id, cxId }, returning: true }
   );
   if (count < 1) throw new NotFoundError();
   // TODO #156 Send this to Sentry
   if (count > 1) console.error(`Updated ${count} patients for id ${id} and cxId ${cxId}`);
 
-  return updatedPatient;
+  return rows[0];
 };
 
 // TODO #369 move this to a CW specific command
@@ -60,15 +60,15 @@ export const setCommonwellId = async ({
     COMMONWELL: new PatientDataCommonwell(commonwellPersonId, commonwellPatientId),
   };
 
-  const [count] = await Patient.update(
+  const [count, rows] = await Patient.update(
     {
       data,
     },
-    { where: { id: patientId, cxId } }
+    { where: { id: patientId, cxId }, returning: true }
   );
   if (count < 1) throw new NotFoundError();
   // TODO #156 Send this to Sentry
   if (count > 1) console.error(`Updated ${count} patients for id ${patientId} and cxId ${cxId}`);
 
-  return updatedPatient;
+  return rows[0];
 };
