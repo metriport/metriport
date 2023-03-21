516af472aba73fcf504bcf98c3425d93a22628ed
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-03-21 14:05:04 -0400

    fix(api): update sleep to handle samples
    Ref. metriport/metriport-internal#423

--- api/app/src/mappings/google/sleep.ts
+++ api/app/src/mappings/google/sleep.ts
@@ -1,9 +1,15 @@
 import { Sleep } from "@metriport/api";
 import dayjs from "dayjs";
 import convert from "convert-units";
+import { sortBy } from "lodash";
 
 import { PROVIDER_GOOGLE } from "../../shared/constants";
 import { GoogleSleep, sourceIdSleep } from "./models/sleep";
+
+type SleepSession = {
+  startTime: string;
+  endTime: string;
+};
 
 export const mapToSleep = (googleSleep: GoogleSleep, date: string): Sleep => {
   const metadata = {
@@ -17,16 +23,36 @@ export const mapToSleep = (googleSleep: GoogleSleep, date: string): Sleep => {
 
   googleSleep.bucket[0].dataset.forEach(data => {
     if (data.point.length) {
-      const startTimeNanos = Number(data.point[0].startTimeNanos);
-      const endTimeNanos = Number(data.point[0].endTimeNanos);
+      const sortedDates = sortBy(data.point, "startTimeNanos");
+
+      const sleepSession = sortedDates.reduce((acc: SleepSession[], curr) => {
+        if (acc.length) {
+          const lastItem = acc[acc.length - 1];
+          const currDate = dayjs(convert(Number(curr.startTimeNanos)).from("ns").to("ms"));
+
+          const timeDifference = currDate.diff(lastItem.endTime, "minutes");
+
+          if (timeDifference >= 30) {
+            return acc;
+          }
+        }
+
+        const startTimeNanos = Number(curr.startTimeNanos);
+        const endTimeNanos = Number(curr.endTimeNanos);
+
+        acc.push({
+          startTime: dayjs(convert(startTimeNanos).from("ns").to("ms")).format(
+            "YYYY-MM-DDTHH:mm:ssZ"
+          ),
+          endTime: dayjs(convert(endTimeNanos).from("ns").to("ms")).format("YYYY-MM-DDTHH:mm:ssZ"),
+        });
+
+        return acc;
+      }, []);
 
       if (data.dataSourceId === sourceIdSleep) {
-        sleep.start_time = dayjs(convert(startTimeNanos).from("ns").to("ms")).format(
-          "YYYY-MM-DDTHH:mm:ssZ"
-        );
-        sleep.end_time = dayjs(convert(endTimeNanos).from("ns").to("ms")).format(
-          "YYYY-MM-DDTHH:mm:ssZ"
-        );
+        sleep.start_time = sleepSession[0].startTime;
+        sleep.end_time = sleepSession[sleepSession.length - 1].endTime;
       }
     }
   });
