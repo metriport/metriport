a323fbd6788ad76e959cdf89b3495e643aa1e562
Rafael Leite <rafael@metriport.ai>
2023-03-22 10:42:34 -0500

    refactor(sdk): match address schema on api and sdk
    Ref. metriport/metriport-internal#410

--- api/app/src/routes/medical/schemas/address.ts
+++ api/app/src/routes/medical/schemas/address.ts
@@ -1,11 +1,14 @@
 import { z } from "zod";
+import { USState } from "../../../shared/geographic-locations";
 import { optionalString } from "./shared";
+
+export const usStateSchema = z.nativeEnum(USState);
 
 export const addressSchema = z.object({
   addressLine1: z.string().min(1),
   addressLine2: optionalString(z.string()),
   city: z.string().min(1),
-  state: z.string().min(1),
+  state: usStateSchema,
   zip: z.string().length(5),
   country: z.string().min(1).default("USA"),
 });
