43bb397cc2229d6e9b59d7cc94b41a4306b8b564
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-03-23 17:03:59 -0400

    fix(api): fix address schemas
    Ref. metriport/metriport-internal#475

--- api/app/src/routes/medical/schemas/address.ts
+++ api/app/src/routes/medical/schemas/address.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 import { optionalString } from "./shared";
 
 export const addressSchema = z.object({
-  addressLine1: z.string().min(1),
+  addressLine1: z.string(),
   addressLine2: optionalString(z.string()),
-  city: z.string().min(1),
-  state: z.string().min(1),
+  city: z.string(),
+  state: z.string(),
   zip: z.string().length(5),
-  country: z.string().min(1).default("USA"),
+  country: z.string().default("USA"),
 });
--- packages/packages/api/src/medical/models/common/address.ts
+++ packages/packages/api/src/medical/models/common/address.ts
@@ -2,12 +2,12 @@ import { z } from "zod";
 import { usStateSchema } from "./us-data";
 
 export const addressSchema = z.object({
-  addressLine1: z.string(),
+  addressLine1: z.string().optional(),
   addressLine2: z.string().optional().nullable(),
-  city: z.string(),
-  state: usStateSchema,
+  city: z.string().optional(),
+  state: usStateSchema.optional(),
   zip: z.string(),
-  country: z.string(),
+  country: z.string().optional(),
 });
 
 export type Address = z.infer<typeof addressSchema>;
