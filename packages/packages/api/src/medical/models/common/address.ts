1bcb1a2553a4640b83ae2600b0b391dd362b4686
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-03-23 17:42:04 -0400

    fix(api): sandbox paylaod
    Ref. metriport/metriport-internal#475

--- packages/packages/api/src/medical/models/common/address.ts
+++ packages/packages/api/src/medical/models/common/address.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { usStateSchema } from "./us-data";
 
 export const addressSchema = z.object({
-  addressLine1: z.string(),
+  addressLine1: z.string().min(1),
   addressLine2: z.string().optional().nullable(),
   city: z.string().min(1),
   state: usStateSchema,
