b0e5bb018139d3113e7580048ef018fd1edd2861
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-03-23 17:08:19 -0400

    fix(api): fix address schemas
    Ref. metriport/metriport-internal#475

--- api/app/src/routes/medical/schemas/address.ts
+++ api/app/src/routes/medical/schemas/address.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 import { optionalString } from "./shared";
 
 export const addressSchema = z.object({
-  addressLine1: z.string(),
+  addressLine1: z.string().min(1),
   addressLine2: optionalString(z.string()),
-  city: z.string(),
-  state: z.string(),
+  city: z.string().min(1),
+  state: z.string().min(1),
   zip: z.string().length(5),
-  country: z.string().default("USA"),
+  country: z.string().min(1).default("USA"),
 });
