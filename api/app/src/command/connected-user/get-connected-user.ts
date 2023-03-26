d59eec4e9ed68d8ee14ab3c4c4bd72a8a5829cf7
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-03-26 12:39:54 -0400

    fix(api): remove leftover
    Ref. metriport/metriport-internal#423

--- api/app/src/command/connected-user/get-connected-user.ts
+++ api/app/src/command/connected-user/get-connected-user.ts
@@ -52,12 +52,3 @@ export const getConnectedUsers = async ({
     },
   });
 };
-
-// {
-//   "google": {
-//       "token": "{\"access_token\":\"ya29.a0Ael9sCN33baX4EAXCNu5S-YXTSEMzk6nXmObAkDN4DuT3vjbG53hGRaRawEBCfxN9GrBitj8itLULwoLW5KaD0Xw0yquc4_hlgLzFWA1emaVEUMqcKCHsAwhBYoAzTtgHx8J7WG3A5JXbFHHgMzzVSTRhMlW_VOoaCgYKATESARISFQF4udJhm6tjDebOo13V-YKpQrGx-w0167\",\"expires_in\":3599,\"scope\":\"https://www.googleapis.com/auth/fitness.body.read https://www.googleapis.com/auth/fitness.blood_glucose.read https://www.googleapis.com/auth/fitness.location.read https://www.googleapis.com/auth/fitness.body_temperature.read https://www.googleapis.com/auth/fitness.nutrition.read https://www.googleapis.com/auth/fitness.heart_rate.read https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.blood_pressure.read https://www.googleapis.com/auth/fitness.oxygen_saturation.read https://www.googleapis.com/auth/fitness.sleep.read\",\"token_type\":\"Bearer\",\"expires_at\":\"2023-03-25T17:06:13.038Z\",\"refresh_token\":\"1//06lNjtK0C287TCgYIARAAGAYSNwF-L9Ir8lNG5iRG-m8nqCR--R338_8F5jEaKiTThShVd6jOpvU4EmLlEmkMqIg5rNW-GLqHSdU\"}"
-//   },
-//   "withings": {
-//       "token": "{\"userid\":33686487,\"access_token\":\"7ad8cde2d17591cfd1285a7a9c3c67cabd40d077\",\"refresh_token\":\"dba253e7c44db665bf3bcd9fc796d456eba53872\",\"scope\":\"user.activity,user.metrics\",\"expires_in\":10800,\"token_type\":\"Bearer\",\"expires_at\":1679771174}"
-//   }
-// }
