d0d5cf8e5c0bbeaae8e8ff6d616598ad42e39c4a
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-03-28 12:48:33 -0400

    feat(api): update import from posthog
    Ref. metriport/metriport-internal#468

--- api/app/src/shared/analytics.ts
+++ api/app/src/shared/analytics.ts
@@ -1,6 +1,20 @@
 import { PostHog } from "posthog-node";
-import { EventMessageV1 } from "posthog-node/src/types";
+// import { EventMessageV1 } from "posthog-node/src/types";
 import { Config } from "./config";
+
+export interface IdentifyMessageV1 {
+  distinctId: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  properties?: Record<string | number, any>;
+}
+
+// TEMPORARY FIX - CANT EXPORT THE TYPE FROM MODULE
+export interface EventMessageV1 extends IdentifyMessageV1 {
+  event: string;
+  groups?: Record<string, string | number>; // Mapping of group type to group id
+  sendFeatureFlags?: boolean;
+  timestamp?: Date;
+}
 
 const postApiKey = Config.getPostHogApiKey();
 
