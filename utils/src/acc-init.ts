6fadc5f9a4c6c000ac49bdc747ebcfe2f0699ee0
Rafael Leite <2132564+leite08@users.noreply.github.com>
2023-03-30 10:55:59 -0500

    build(util): add script to initialize customer account
    - add script to initialize customer account from Cognito users
    
    Ref. metriport/metriport-internal#366

---
+++ utils/src/acc-init.ts
@@ -0,0 +1,90 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+// Keep dotenv import and config before everything else
+import {
+  CognitoIdentityProviderClient,
+  CognitoIdentityProviderClientConfig,
+  ListUsersCommand,
+  ListUsersCommandInput,
+} from "@aws-sdk/client-cognito-identity-provider"; // ES Modules import
+import axios from "axios";
+import { uniq } from "lodash";
+
+/**
+ * Initializes customers' accounts based on their Cognito users.
+ *
+ * Requires env vars as indicated at start of this function.
+ * Its suggested to set those on a .env file so they are not stored on
+ * your shell history.
+ *
+ * Created as part of #366
+ */
+async function main() {
+  const accessKeyId = getEnvVarOrFail("ACCESS_KEY");
+  const secretAccessKey = getEnvVarOrFail("SECRET_KEY");
+  const userPoolId = getEnvVarOrFail("USER_POOL_ID");
+  const region = getEnvVarOrFail("REGION");
+  const apiAddress = getEnvVarOrFail("API_ADRESS");
+
+  console.log(`Getting users...`);
+  const users = await getUsers(accessKeyId, secretAccessKey, userPoolId, region);
+
+  console.log(`Users from Cognito: ${users.length}`);
+  console.log(JSON.stringify(users, null, 2));
+
+  console.log(`Initializing the accoung of ${users.length} users...`);
+  for (const userId of users) {
+    await axios.post(`${apiAddress}/internal/init`, null, { params: { cxId: userId } });
+  }
+  console.log(`Done`);
+}
+
+async function getUsers(
+  accessKeyId: string,
+  secretAccessKey: string,
+  userPoolId: string,
+  region: string
+): Promise<string[]> {
+  const config: CognitoIdentityProviderClientConfig = {
+    region,
+    credentials: {
+      accessKeyId,
+      secretAccessKey,
+    },
+  };
+  const client = new CognitoIdentityProviderClient(config);
+  const input: ListUsersCommandInput = {
+    UserPoolId: userPoolId,
+    AttributesToGet: ["email_verified"],
+    Limit: Number("5"),
+    Filter: 'cognito:user_status = "CONFIRMED"',
+  };
+  const command = new ListUsersCommand(input);
+
+  const users: string[] = [];
+  let response = await client.send(command);
+  users.push(...(response.Users ?? []).flatMap(u => (u.Username ? u.Username : [])));
+  while (response.PaginationToken) {
+    const inputInternal = {
+      ...input,
+      PaginationToken: response.PaginationToken,
+    };
+    const commandInternal = new ListUsersCommand(inputInternal);
+    response = await client.send(commandInternal);
+    users.push(...(response.Users ?? []).flatMap(u => (u.Username ? u.Username : [])));
+  }
+  return uniq(users);
+}
+
+function getEnvVar(varName: string): string | undefined {
+  return process.env[varName];
+}
+function getEnvVarOrFail(varName: string): string {
+  const value = getEnvVar(varName);
+  if (!value || value.trim().length < 1) {
+    throw new Error(`Missing ${varName} env var`);
+  }
+  return value;
+}
+
+main();
