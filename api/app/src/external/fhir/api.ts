ed87ee410630efc55cb98c1eadcf417bd237a4b6
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-04-13 16:31:16 -0400

    feat(api): remove hard coded medplum
    Ref. metriport/metriport-internal#521

--- api/app/src/external/fhir/api.ts
+++ api/app/src/external/fhir/api.ts
@@ -1,8 +1,9 @@
 import { Config } from "../../shared/config";
-import { MedplumClient } from "../medplum";
+import { MedplumClient } from "@medplum/core";
 
 const fhirUrl = Config.getFHIRServerUrl();
 
 export const api = new MedplumClient({
   baseUrl: fhirUrl,
+  fhirUrlPath: "/fhir",
 });
