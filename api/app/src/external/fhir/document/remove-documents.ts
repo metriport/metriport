0cf6d8767402a79a1a156f02e262da9d488a7899
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-04-19 09:09:56 -0400

    feat(api): remove, remove documents
    Ref. metriport/metriport-internal#521

--- api/app/src/external/fhir/document/remove-documents.ts
+++
@@ -1,9 +0,0 @@
-// import { DocumentReference } from "@medplum/fhirtypes";
-// import { getDocuments } from "./get-documents";
-// import { api } from "../api";
-
-// export const removeDocuments = async (
-//   patientId: string
-// ): Promise<DocumentReference[] | undefined> => {
-//   return getDocuments(`patient=${patientId}&`);
-// };
