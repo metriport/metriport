f4b6859a27e9d00b3085800f991baf2315468e07
Rafael Leite <2132564+leite08@users.noreply.github.com>
2023-04-24 13:57:02 -0500

    build: setup infra alarms to Slack
    - add alarms to API service's ECS
    - point all api-stack alarms to Slack
    - split api-stack's API ECS code into its own file
    
    Ref. metriport/metriport-internal#135

---
+++ infra/lib/alarm-slack-chatbot.ts
@@ -0,0 +1,55 @@
+import { LoggingLevel, SlackChannelConfiguration } from "aws-cdk-lib/aws-chatbot";
+import {
+  Effect,
+  ManagedPolicy,
+  PolicyDocument,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
+import { RetentionDays } from "aws-cdk-lib/aws-logs";
+import { ITopic } from "aws-cdk-lib/aws-sns";
+import { Construct } from "constructs";
+
+type SlackBotProps = {
+  readonly configName: string;
+  readonly workspaceId: string;
+  readonly channelId: string;
+  readonly topics: ReadonlyArray<ITopic>;
+};
+
+export class AlarmSlackBot {
+  public static addSlackChannelConfig(
+    scope: Construct,
+    props: SlackBotProps
+  ): SlackChannelConfiguration {
+    const { configName, channelId, workspaceId, topics } = props;
+
+    const role = new Role(scope, "SlackBotRole", {
+      assumedBy: new ServicePrincipal("chatbot.amazonaws.com"),
+      description: "Role for AWS ChatBot",
+      managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName("ReadOnlyAccess")],
+      inlinePolicies: {
+        CloudWatchPolicy: new PolicyDocument({
+          statements: [
+            new PolicyStatement({
+              effect: Effect.ALLOW,
+              resources: ["*"],
+              actions: ["cloudwatch:Describe*", "cloudwatch:Get*", "cloudwatch:List*"],
+            }),
+          ],
+        }),
+      },
+    });
+
+    return new SlackChannelConfiguration(scope, "SlackChannelConfiguration", {
+      slackChannelConfigurationName: configName,
+      slackWorkspaceId: workspaceId,
+      slackChannelId: channelId,
+      notificationTopics: [...topics],
+      role,
+      loggingLevel: LoggingLevel.INFO,
+      logRetention: RetentionDays.ONE_MONTH,
+    });
+  }
+}
