5df4b44dc1acf1fac41882720a21815421836864
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-05-30 17:52:13 -0400

    feat(api): gitignore node modules
    Ref. metriport/metriport-internal#726

--- api/app/src/command/medical/document/document-download.ts
+++ api/app/src/command/medical/document/document-download.ts
@@ -1,9 +1,44 @@
 import { makeLambdaClient } from "../../../external/aws/lambda";
+import { makeS3Client } from "../../../external/aws/s3";
 import { Config } from "../../../shared/config";
 
 const lambdaClient = makeLambdaClient();
+const s3client = makeS3Client();
 
 export const downloadDocument = async ({
+  fileName,
+  conversionType,
+}: {
+  fileName: string;
+  conversionType?: string;
+}): Promise<string> => {
+  if (conversionType === "html") {
+    const url = await getConversionUrl({ fileName, conversionType });
+    return url;
+  } else if (conversionType === "pdf") {
+    const url = await getConversionUrl({ fileName, conversionType });
+    return url;
+  } else {
+    const url = await getSignedURL({ fileName });
+    return url;
+  }
+};
+
+const getConversionUrl = async ({
+  fileName,
+  conversionType,
+}: {
+  fileName: string;
+  conversionType?: string;
+}): Promise<boolean> => {
+  const convertedFileName = fileName.concat(`.${conversionType}`);
+  const obj = await doesObjExist({ fileName: convertedFileName });
+
+  if (obj) return await getSignedURL({ fileName: convertedFileName });
+  else return await convertDoc({ fileName, conversionType });
+};
+
+const convertDoc = async ({
   fileName,
   conversionType,
 }: {
@@ -21,4 +56,30 @@ export const downloadDocument = async ({
   if (result.Payload === undefined) throw new Error("Payload is undefined");
 
   return result.Payload.toString();
+};
+
+const doesObjExist = async ({ fileName }: { fileName: string }): Promise<boolean> => {
+  try {
+    await s3client
+      .getObject({
+        Bucket: Config.getMedicalDocumentsBucketName(),
+        Key: fileName,
+      })
+      .promise();
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+const getSignedURL = async ({ fileName }: { fileName: string }): Promise<string> => {
+  const seconds = 60;
+
+  const url = s3client.getSignedUrl("getObject", {
+    Bucket: Config.getMedicalDocumentsBucketName(),
+    Key: fileName,
+    Expires: seconds,
+  });
+
+  return url;
 };
