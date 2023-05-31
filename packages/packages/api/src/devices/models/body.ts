c6d1613d358d62479b8f1dae9ac2e258b66fd478
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-05-31 16:00:57 -0400

    feat(api): add weight samples
    Ref. metriport/metriport-internal#380

--- packages/packages/api/src/devices/models/body.ts
+++ packages/packages/api/src/devices/models/body.ts
@@ -1,5 +1,5 @@
 import { MetriportData } from "./metriport-data";
-
+import { Sample } from "./common/sample";
 export interface Body extends MetriportData {
   body_fat_pct?: number;
   height_cm?: number;
@@ -8,4 +8,5 @@ export interface Body extends MetriportData {
   muscle_mass_kg?: number;
   lean_mass_kg?: number;
   max_possible_heart_rate_bpm?: number;
+  weight_samples_kg?: Sample[];
 }
--- packages/packages/api/src/devices/models/common/sample.ts
+++ packages/packages/api/src/devices/models/common/sample.ts
@@ -1,4 +1,7 @@
+import { SourceInfo } from "./source-info";
+
 export interface Sample {
   time: string;
   value: number;
+  data_source?: SourceInfo;
 }
