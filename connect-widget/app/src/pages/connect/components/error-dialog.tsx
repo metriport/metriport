b56cc7e12be5dc5e538a40318bb70812c988cec2
Ramil Garipov <garipovramil93@hotmail.ca>
2023-06-05 16:44:19 -0700

    feat(widget): docs link for no token
    refs. metriport/metriport#429

--- connect-widget/app/src/pages/connect/components/error-dialog.tsx
+++ connect-widget/app/src/pages/connect/components/error-dialog.tsx
@@ -17,16 +17,57 @@ export interface ErrorDialogProps {
   show: boolean;
   title?: string;
   message?: string;
+  link?: string;
+  linkText?: string;
+  remainingText?: string;
   onClose?: () => void;
 }
 
-const ErrorDialog = ({ show, title, message, onClose: closed }: ErrorDialogProps) => {
+const parseMessage = (msg: string) => {
+  console.log(msg);
+  const regex = /(.+?)\s*\[(.+?)\]\((.+?)\)\s*(.+)/;
+  const match = msg.match(regex);
+
+  if (match) {
+    const msg = match[1].trim();
+    const linkText = match[2].trim();
+    const link = match[3].trim();
+    const remainingText = match[4].trim();
+    return { msg, linkText, link, remainingText };
+  }
+  return null;
+};
+
+const ErrorDialog = ({
+  show,
+  title,
+  message,
+  link,
+  linkText,
+  remainingText,
+  onClose: closed,
+}: ErrorDialogProps) => {
   const { isOpen, onClose } = useDisclosure({ isOpen: show });
   const closeButtonRef = useRef(null);
-  const [{ actualTitle, actualMsg }, setActual] = useState({
-    actualTitle: title,
-    actualMsg: message,
-  });
+
+  if (message) {
+    const parsedMsg = parseMessage(message);
+    if (parsedMsg) {
+      message = parsedMsg?.msg;
+      link = parsedMsg?.link;
+      linkText = parsedMsg?.linkText;
+      remainingText = parsedMsg?.remainingText;
+    }
+  }
+
+  const [{ actualTitle, actualMsg, actualLink, actualLinkText, actualRemainingText }, setActual] =
+    useState({
+      actualTitle: title,
+      actualMsg: message,
+      actualLink: link,
+      actualLinkText: linkText,
+      actualRemainingText: remainingText,
+    });
 
   useEffect(() => {
     isOpen && capture.message("Error dialog displayed", { extra: { show, title, message } });
@@ -36,6 +77,9 @@ const ErrorDialog = ({ show, title, message, onClose: closed }: ErrorDialogProps
     setActual({
       actualTitle: title ?? "Ooops...",
       actualMsg: message ?? DEFAULT_ERROR_MESSAGE,
+      actualLink: link ?? undefined,
+      actualLinkText: linkText ?? undefined,
+      actualRemainingText: remainingText ?? "",
     });
   }, [title, message]);
 
@@ -51,7 +95,16 @@ const ErrorDialog = ({ show, title, message, onClose: closed }: ErrorDialogProps
           <AlertDialogHeader fontSize="lg" fontWeight="bold">
             {actualTitle}
           </AlertDialogHeader>
-          <AlertDialogBody>{actualMsg}</AlertDialogBody>
+          {actualLink && (
+            <AlertDialogBody>
+              {actualMsg}{" "}
+              <a href={actualLink}>
+                <u>{actualLinkText}</u>
+              </a>{" "}
+              {actualRemainingText}
+            </AlertDialogBody>
+          )}
+          {!actualLink && <AlertDialogBody>{actualMsg}</AlertDialogBody>}
           <AlertDialogFooter>
             <Button ref={closeButtonRef} onClick={close} ml={3}>
               OK
