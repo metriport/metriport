eee879c08c8ad1ce81890ca1dbc147af674f3a46
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-06-05 11:18:39 -0400

    fix(api): fix zof bugs and update docs
    Ref. metriport/metriport-internal#753

--- api/app/src/mappings/withings/models/measurements.ts
+++ api/app/src/mappings/withings/models/measurements.ts
@@ -41,8 +41,8 @@ export type WithingsMeasurementGrp = z.infer<typeof withingsMeasurementGrp>;
 
 // https://developer.withings.com/api-reference/#operation/measure-getmeas
 export const withingsMeasurementResp = z.object({
-  updatetime: z.number(),
-  timezone: z.string(),
+  updatetime: z.number().nullable().optional(),
+  timezone: z.string().nullable().optional(),
   measuregrps: z.array(withingsMeasurementGrp),
   more: z.number().nullable().optional(),
   offset: z.number().nullable().optional(),
