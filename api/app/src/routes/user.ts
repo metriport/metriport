6226d10066df0581b192b8c948c7fa6800bdc076
Rafael Leite <2132564+leite08@users.noreply.github.com>
2023-06-16 10:35:34 -0500

    fix(api): use proper ID validation on /connect/token
    Ref. metriport/metriport-internal#785

--- api/app/src/routes/user.ts
+++ api/app/src/routes/user.ts
@@ -95,8 +95,6 @@ router.post(
 router.get(
   "/connect/token",
   asyncHandler(async (req: Request, res: Response) => {
-    // validate required query params
-    if (!req.query.userId) throw new BadRequestError();
     const userId = getUserIdFrom("query", req).orFail();
     const cxId = getCxIdOrFail(req);
 
