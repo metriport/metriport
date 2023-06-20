64af12d75bb5bd0e6962fd82fa2facf42fef7dcc
Rafael Leite <2132564+leite08@users.noreply.github.com>
2023-06-20 00:09:37 -0500

    refactor(sdk): only download files not on S3 + FHIR server
    Ref. metriport/metriport-internal#785

---
+++ api/app/src/external/fhir/shared/paginated.ts
@@ -0,0 +1,12 @@
+import { ExtractResource, ResourceType } from "@medplum/fhirtypes";
+
+export const getAllPages = async <K extends ExtractResource<ResourceType>>(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  searchFunction: <W extends K>() => AsyncGenerator<K[]>
+) => {
+  const pages: K[] = [];
+  for await (const page of searchFunction<K>()) {
+    pages.push(...page);
+  }
+  return pages;
+};
