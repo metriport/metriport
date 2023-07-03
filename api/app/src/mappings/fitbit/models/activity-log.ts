937cc5c543f635fc6697a6d9cd92513ec1ddb335
Goncharo <dima@metriport.ai>
2023-07-03 12:47:48 -0700

    fix(api): patch fitbit activity data model
    refs. metriport/metriport#626

--- api/app/src/mappings/fitbit/models/activity-log.ts
+++ api/app/src/mappings/fitbit/models/activity-log.ts
@@ -88,7 +88,7 @@ export const fitbitActivityLogResp = z.array(
 export type FitbitActivityLogs = z.infer<typeof fitbitActivityLogResp>;
 
 export interface HeartRateZone {
-  caloriesOut: number;
+  caloriesOut?: number;
   max: number;
   min: number;
   minutes: number;
