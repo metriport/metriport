bfda88b6d2fae1af407f23a895305c8a170f9fda
Goncharo <dima@metriport.ai>
2023-07-08 12:35:08 -0700

    fix(api): remove s3 url from lambda
    refs. metriport/metriport-internal#407

--- api/lambdas/commonwell/index.js
+++ api/lambdas/commonwell/index.js
@@ -3,7 +3,7 @@ exports.handler = async event => {
   const response = {
     statusCode: 301,
     headers: {
-      Location: "https://tmp-407.s3.us-east-2.amazonaws.com/doc-contrib-payload",
+      Location: "https://S3_URL",
     },
   };
 
