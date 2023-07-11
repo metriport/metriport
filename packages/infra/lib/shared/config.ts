ad7ce44252f2bdc6cf4c3c7515ad4c14491563a8
Rafael Leite <2132564+leite08@users.noreply.github.com>
2023-07-11 18:09:06 -0500

    build: infra using JS files + fix symlink compilation
    Ref. metriport/metriport-internal#237

--- packages/infra/lib/shared/config.ts
+++ packages/infra/lib/shared/config.ts
@@ -23,7 +23,7 @@ export async function initConfig(node: Node): Promise<EnvConfig> {
       `Context variable missing on CDK command. Pass in as "-c env=XXX". Valid values are: ${validVals}`
     );
   }
-  const configPath = `../../config/${env}.ts`;
+  const configPath = `../../config/${env}`;
   const configModule = await import(configPath);
   if (!configModule || !configModule.default) {
     throw new Error(`Ensure config is defined, could not fine file ${configPath}`);
