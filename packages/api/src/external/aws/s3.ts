bb212a5062017f30bc3b2e0dc4e7ad581133ac12
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-07-25 08:04:08 -0400

    fix(api): add aws credentials
    Ref. metriport/metriport#672

--- packages/api/src/external/aws/s3.ts
+++ packages/api/src/external/aws/s3.ts
@@ -2,5 +2,10 @@ import * as AWS from "aws-sdk";
 import { Config } from "../../shared/config";
 
 export function makeS3Client() {
-  return new AWS.S3({ signatureVersion: "v4", region: Config.getAWSRegion() });
+  return new AWS.S3({
+    signatureVersion: "v4",
+    region: Config.getAWSRegion(),
+    accessKeyId: process.env["ACCESS_KEY"],
+    secretAccessKey: process.env["SECRET_KEY"],
+  });
 }
