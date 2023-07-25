9e553ae22ed6e230797d7b1ccc77e95f0d8d803c
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-07-25 17:15:43 -0400

    fix(api): remove the patient doc query seed script
    Ref. metriport/metriport#672

--- packages/api/src/external/aws/s3.ts
+++ packages/api/src/external/aws/s3.ts
@@ -2,10 +2,5 @@ import * as AWS from "aws-sdk";
 import { Config } from "../../shared/config";
 
 export function makeS3Client() {
-  return new AWS.S3({
-    signatureVersion: "v4",
-    region: Config.getAWSRegion(),
-    accessKeyId: process.env["ACCESS_KEY"],
-    secretAccessKey: process.env["SECRET_KEY"],
-  });
+  return new AWS.S3({ signatureVersion: "v4", region: Config.getAWSRegion() });
 }
