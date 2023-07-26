d60d63494a1eafd8867b2612f849d1b98da54035
Rafael Leite <2132564+leite08@users.noreply.github.com>
2023-07-26 15:35:29 -0500

    refactor(api): add doc ID mapping (internal/external)
    Ref. metriport/metriport-internal#699

---
+++ packages/api/src/models/medical/docref-mapping.ts
@@ -0,0 +1,36 @@
+import { DataTypes, Sequelize } from "sequelize";
+import { DocRefMapping } from "../../domain/medical/docref-mapping";
+import { MedicalDataSource } from "../../external";
+import { BaseModel, ModelSetup } from "../_default";
+
+export class DocRefMappingModel extends BaseModel<DocRefMappingModel> implements DocRefMapping {
+  static NAME = "docref_mapping";
+  declare externalId: string;
+  declare cxId: string;
+  declare patientId: string;
+  declare source: MedicalDataSource;
+
+  static setup: ModelSetup = (sequelize: Sequelize) => {
+    DocRefMappingModel.init(
+      {
+        ...BaseModel.attributes(),
+        cxId: {
+          type: DataTypes.UUID,
+        },
+        patientId: {
+          type: DataTypes.STRING,
+        },
+        source: {
+          type: DataTypes.STRING,
+        },
+        externalId: {
+          type: DataTypes.STRING,
+        },
+      },
+      {
+        ...BaseModel.modelOptions(sequelize),
+        tableName: DocRefMappingModel.NAME,
+      }
+    );
+  };
+}
