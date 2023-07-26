7e571119804daab6fa01255519bced99b214bab2
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-07-26 18:04:15 -0400

    fix(api): remove doc contribution url env
    Ref. metriport/metriport#672

--- packages/api/src/shared/config.ts
+++ packages/api/src/shared/config.ts
@@ -216,8 +216,4 @@ export class Config {
   static getConvertDocLambdaName(): string | undefined {
     return getEnvVar("CONVERT_DOC_LAMBDA_NAME");
   }
-
-  static getDocContributionUrl(): string | undefined {
-    return getEnvVar("DOCUMENT_CONTRIBUTION_URL");
-  }
 }
