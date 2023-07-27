043225ed5d0cf177c6b7fae1d7658e26f2fd069a
Ramil Garipov <garipovramil93@hotmail.ca>
2023-07-27 13:33:03 -0700

    feat(widget): included uri decoding and a tip for the devs in the docs
    refs. metriport/metriport#711

--- packages/connect-widget/src/shared/localStorage/redirect-url.ts
+++ packages/connect-widget/src/shared/localStorage/redirect-url.ts
@@ -2,12 +2,12 @@ import Constants from "../constants";
 
 export const storeRedirectUrl = (url: string | null) => {
   if (url && localStorage) {
-    localStorage.setItem(Constants.SUCCESS_REDIRECT_URL_PARAM, url);
+    localStorage.setItem(Constants.SUCCESS_REDIRECT_URL_PARAM, decodeURI(url));
   }
 };
 
 export const storeFailRedirectUrl = (url: string | null) => {
   if (url && localStorage) {
-    localStorage.setItem(Constants.FAILURE_REDIRECT_URL_PARAM, url);
+    localStorage.setItem(Constants.FAILURE_REDIRECT_URL_PARAM, decodeURI(url));
   }
 };
