88de052b6b9dd0ea5dd4fbbea58e743620288f5d
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-07-28 14:17:38 -0400

    feat(api): commit before raf pr merge
    Ref. metriport/metriport-internal#672

---
+++ packages/utils/src/initialize-cw-test-env/ollie/index.ts
@@ -0,0 +1,75 @@
+import { Doc } from "..";
+
+export const ollieDocs: Doc[] = [
+  {
+    index: 2,
+    description: "Ollie Brown 2 test",
+    fileName: "ollie2.xml",
+  },
+  {
+    index: 3,
+    description: "Ollie Brown 3 test",
+    fileName: "ollie3.xml",
+  },
+  {
+    index: 4,
+    description: "Ollie Brown 4 test",
+    fileName: "ollie4.xml",
+  },
+  {
+    index: 5,
+    description: "Ollie Brown 5 test",
+    fileName: "ollie5.xml",
+  },
+  {
+    index: 6,
+    description: "Ollie Brown 6 test",
+    fileName: "ollie6.xml",
+  },
+  {
+    index: 7,
+
+    description: "Ollie Brown 7 test",
+    fileName: "ollie7.xml",
+  },
+  {
+    index: 8,
+    description: "Ollie Brown 8 test",
+    fileName: "ollie8.xml",
+  },
+  {
+    index: 9,
+    description: "Ollie Brown 9 test",
+    fileName: "ollie9.xml",
+  },
+  {
+    index: 10,
+    description: "Ollie Brown 10 test",
+    fileName: "ollie10.xml",
+  },
+  {
+    index: 11,
+    description: "Ollie Brown 11 test",
+    fileName: "ollie11.pdf",
+  },
+  {
+    index: 12,
+    description: "Ollie Brown 12 test",
+    fileName: "ollie12.pdf",
+  },
+  {
+    index: 13,
+    description: "Ollie Brown 13 test",
+    fileName: "ollie13.pdf",
+  },
+  {
+    index: 14,
+    description: "Ollie Brown 14 test",
+    fileName: "ollie14.jpeg",
+  },
+  {
+    index: 16,
+    description: "Ollie Brown 16 test",
+    fileName: "ollie16.jpeg",
+  },
+];
