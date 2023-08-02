7fa778e18c2e119fb93679cbd393282a2f06d36c
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-08-02 16:55:01 -0400

    feat(ops): add unit test infra and a demonstration test to sdk
    Ref. metriport/metriport#537

---
+++ packages/api-sdk/src/devices/client/__tests__/metriport.test.ts
@@ -0,0 +1,28 @@
+import { mocked } from "jest-mock";
+import { MetriportDevicesApi } from "../metriport";
+import axios from "axios";
+
+jest.mock("axios");
+
+describe("getMetriportUserId", () => {
+  const userId = "1";
+  const apiKey = "FAKE_KEY";
+  const mockedAxios = mocked(axios, true);
+
+  it("returns the userId", async () => {
+    const metriportClient = new MetriportDevicesApi(apiKey);
+
+    metriportClient["api"] = mockedAxios;
+
+    jest.spyOn(metriportClient, "getMetriportUserId");
+
+    mockedAxios.post.mockResolvedValueOnce({ data: { userId } });
+
+    const result = await metriportClient.getMetriportUserId(userId);
+
+    expect(mockedAxios.post).toHaveBeenCalledTimes(1);
+    expect(axios.post).toHaveBeenCalledWith(`/user`, null, { params: { appUserId: userId } });
+    expect(metriportClient.getMetriportUserId).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(userId);
+  });
+});
