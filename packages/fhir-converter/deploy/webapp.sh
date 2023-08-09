fd3860f060ababb5b498968fc940620eb9c701e6
Rafael Leite <2132564+leite08@users.noreply.github.com>
2023-08-08 19:46:37 -0500

    chore: testing intellicode w/o fhir converter
    Ref. metriport/metriport-internal#941

--- packages/fhir-converter/deploy/webapp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# symlink for web app persistence
-mkdir -p /home/fhirconvertertemplates
-ln -snf /home/fhirconvertertemplates /usr/src/app/src/service-templates
-
-npm start
--- packages/fhir-converter/test/e2e-test/regression-test/config/index.js
+++
@@ -1,9 +0,0 @@
-// -------------------------------------------------------------------------------------------------
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
-// -------------------------------------------------------------------------------------------------
-
-const cdaCases = require("./testcases-cda")();
-const hl7v2Cases = require("./testcases-hl7v2")();
-
-module.exports = { cdaCases, hl7v2Cases };
