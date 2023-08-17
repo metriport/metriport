b629449a8dd00fc27964b51d8851f738050bd44f
Ramil Garipov <garipovramil93@hotmail.ca>
2023-08-17 08:58:22 -0700

    feat(dapi): added energy expenditure to fitbit mapping
    refs. metriport/metriport#803

--- packages/api/src/mappings/fitbit/activity.ts
+++ packages/api/src/mappings/fitbit/activity.ts
@@ -37,6 +37,13 @@ export const mapToActivity = (fitbitActiveLogs: FitbitActivityLogs, date: string
       },
     };
 
+    if (fitbitActivityLog.calories) {
+      activityLog.energy_expenditure = {
+        ...activityLog.energy_expenditure,
+        active_kcal: fitbitActivityLog.calories,
+      };
+    }
+
     if (fitbitActivityLog.activityLevel && fitbitActivityLog.activityLevel.length) {
       const level = fitbitActivityLog.activityLevel;
       activityLog.durations = {
