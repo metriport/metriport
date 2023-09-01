018b1430dba8cf23c9e2572229d1f5a498b3f7b3
Ramil Garipov <garipovramil93@hotmail.ca>
2023-08-31 23:18:59 -0700

    feat(mapi): included more metadata to doc refs
    refs. metriport/metriport#846

--- packages/api/src/external/commonwell/document/shared.ts
+++ packages/api/src/external/commonwell/document/shared.ts
@@ -1,6 +1,7 @@
-import { Document } from "@metriport/commonwell-sdk";
+import { Document, DocumentContent, HumanName } from "@metriport/commonwell-sdk";
 import { contentType, extension } from "mime-types";
 import { Patient } from "../../../models/medical/patient";
+import { OrganizationDTO } from "../../../routes/medical/dtos/organizationDTO";
 
 export const sandboxSleepTime = 5000;
 
@@ -16,6 +17,14 @@ export type CWDocumentWithMetriportData = DocumentWithMetriportId & {
     location: string;
     fileSize: number | undefined;
   };
+};
+
+export type DocumentReferenceExtras = {
+  organization?: Partial<OrganizationDTO>;
+  practitioner?: {
+    name: HumanName;
+  };
+  facilityType?: string;
 };
 
 export function getFileName(patient: Patient, doc: Document): string {
@@ -38,4 +47,47 @@ export function getFileExtension(value: string | undefined): string {
   if (!value || !contentType(value)) return "";
   const fileExtension = extension(value);
   return fileExtension ? `.${fileExtension}` : "";
+}
+
+export function getExtraDocRefInfo(content: DocumentContent | undefined): DocumentReferenceExtras {
+  const extraInfo: DocumentReferenceExtras = {};
+  const contained = content?.contained;
+  const context = content?.context;
+
+  if (contained) {
+    contained.forEach(item => {
+      const containedName = item.name;
+      if (item.resourceType === "Organization") {
+        if (typeof containedName === "string") {
+          extraInfo.organization = {
+            name: containedName,
+          };
+        }
+      } else if (item.resourceType === "Practitioner" && containedName) {
+        if (Array.isArray(containedName)) {
+          const name = containedName[0];
+          if (name) addPractitionerName(extraInfo, name);
+        } else if (typeof containedName === "object") {
+          addPractitionerName(extraInfo, containedName);
+        }
+      }
+    });
+  }
+
+  if (context?.facilityType?.text) {
+    extraInfo.facilityType = context.facilityType.text;
+  }
+  return extraInfo;
+}
+
+export function addPractitionerName(extraInfo: DocumentReferenceExtras, name: HumanName): void {
+  if (name && name.given?.length && name.given[0] != "") {
+    extraInfo.practitioner = {
+      name: {
+        given: name.given,
+        family: name.family,
+        prefix: name.prefix?.length && name.prefix[0] !== "" ? name.prefix : undefined,
+      },
+    };
+  }
 }
