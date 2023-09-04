86130675ce4f90d0985bdc6e0bc0554f2782fee6
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-09-04 17:11:45 -0400

    fix(api): build works locally
    Ref. metriport/metriport-internal#187

--- packages/api/src/external/fhir/__tests__/delete-organization.ts
+++ packages/api/src/external/fhir/__tests__/delete-organization.ts
@@ -2,7 +2,7 @@ import { makeFhirApi } from "../api/api-factory";
 import { ResourceType } from "../shared";
 
 /**
- * For E2E testing locally and staging.
+ * For E2E testing locally and staging
  */
 export const deleteOrgFromFHIRServer = async (cxId: string, id: string) => {
   await makeFhirApi(cxId).deleteResource(ResourceType.Organization, id);
