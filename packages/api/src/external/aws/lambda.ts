a7b3057bf543e74d791c7bd7b89e2df847215bfe
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-09-04 14:29:54 -0400

    fix(api): adjust aws env styles
    Ref. metriport/metriport-internal#187

--- packages/api/src/external/aws/lambda.ts
+++ packages/api/src/external/aws/lambda.ts
@@ -2,8 +2,5 @@ import * as AWS from "aws-sdk";
 import { Config } from "../../shared/config";
 
 export function makeLambdaClient() {
-  return new AWS.Lambda({
-    signatureVersion: "v4",
-    region: Config.getAWSRegion(),
-  });
+  return new AWS.Lambda({ signatureVersion: "v4", region: Config.getAWSRegion() });
 }
