873da7e285bf16707a79957ba98eaaf540c02d3d
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-09-05 09:15:46 -0400

    fix(api): comment out cognito api url
    Ref. metriport/metriport-internal#187

--- packages/api/src/routes/__tests__/shared.ts
+++ packages/api/src/routes/__tests__/shared.ts
@@ -3,8 +3,7 @@ dotenv.config();
 // Keep dotenv import and config before everything else
 import Axios from "axios";
 import { customAlphabet } from "nanoid";
-import { getEnvVarOrFail, getEnvVar } from "../../shared/config";
-import { Config } from "../../shared/config";
+import { getEnvVarOrFail } from "../../shared/config";
 
 export const nanoid = customAlphabet("1234567890abcdef", 10);
 
@@ -12,22 +11,24 @@ export const testApiKey = getEnvVarOrFail("TEST_API_KEY");
 
 export const baseURL = getEnvVarOrFail("API_URL");
 
-// export const internalUrl = getEnvVarOrFail("INTERNAL_API_URL");
-
-// Used locally
-export const cognitoApiUrl = getEnvVar("STAGING_INTERNAL_API_URL");
-
-export function getCognitoBaseURL(): string | undefined {
-  const isStaging = Config.isProdEnv();
-  const isDev = !Config.isCloudEnv();
-
-  return isStaging ? baseURL : isDev ? cognitoApiUrl || undefined : undefined;
-}
-
-export const apiCognito = Axios.create({ baseURL: getCognitoBaseURL() });
-// export const apiInternal = Axios.create({ baseURL: internalUrl });
 export const api = Axios.create({
   timeout: 10_000,
   baseURL: baseURL,
   headers: { "x-api-key": testApiKey, "Content-Type": "application/json" },
 });
+
+// TODO: #1022 - TO BE USED WHEN ABLE TO HIT INTERNAL IN GITHUB RUNNER
+// export const internalUrl = getEnvVarOrFail("INTERNAL_API_URL");
+
+// Used locally
+// export const cognitoApiUrl = getEnvVar("STAGING_INTERNAL_API_URL");
+
+// export function getCognitoBaseURL(): string | undefined {
+//   const isStaging = Config.isProdEnv();
+//   const isDev = !Config.isCloudEnv();
+
+//   return isStaging ? baseURL : isDev ? cognitoApiUrl || undefined : undefined;
+// }
+
+// export const apiCognito = Axios.create({ baseURL: getCognitoBaseURL() });
+// export const apiInternal = Axios.create({ baseURL: internalUrl });
