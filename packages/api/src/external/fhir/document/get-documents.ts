b3f0a8411992f240417a5cd2acc3693b4b4e2402
Goncharo <dima@metriport.ai>
2023-09-14 22:22:27 -0700

    fix(api): skip extension check when listing docs on sandbox mode
    refs. metriport/metriport#895

--- packages/api/src/external/fhir/document/get-documents.ts
+++ packages/api/src/external/fhir/document/get-documents.ts
@@ -1,4 +1,5 @@
 import { DocumentReference } from "@medplum/fhirtypes";
+import { Config } from "../../../shared/config";
 import { capture } from "../../../shared/notifications";
 import { isCommonwellExtension } from "../../commonwell/extension";
 import { makeFhirApi } from "../api/api-factory";
@@ -43,6 +44,9 @@ export const getDocuments = async ({
 };
 
 function checkExtensions(doc: DocumentReference) {
+  // skip this check for sandbox as we don't have extensions in the sandbox doc refs
+  // to be removed in #895
+  if (Config.isSandbox()) return true;
   const extensions = doc.extension;
   if (!extensions) return false;
   const metriport = extensions.find(isMetriportExtension);
