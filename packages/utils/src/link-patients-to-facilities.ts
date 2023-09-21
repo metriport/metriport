610de3533e939963d543ed65cc56689e8ec8f84b
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-09-20 22:06:59 -0400

    fix(infra): reset script for review
    Ref. metriport/metriport-internal#799

--- packages/utils/src/link-patients-to-facilities.ts
+++ packages/utils/src/link-patients-to-facilities.ts
@@ -1,20 +1,14 @@
 import * as fs from "fs";
 import * as dotenv from "dotenv";
 import { chunk } from "lodash";
-import { MetriportMedicalApi, Patient } from "@metriport/api-sdk";
 import { getEnvVarOrFail } from "./shared/env";
 import axios from "axios";
 
 dotenv.config();
 // Keep dotenv import and config before everything else
 
-const apiKey = getEnvVarOrFail("API_KEY");
-const apiUrl = getEnvVarOrFail("API_URL");
+const apiLbUrl = getEnvVarOrFail("API_LB_URL");
 const CQ_ORG_CHUNK_SIZE = 50;
-
-const metriportAPI = new MetriportMedicalApi(apiKey, {
-  baseAddress: apiUrl,
-});
 
 type SimpleOrg = {
   Id: string;
@@ -22,32 +16,17 @@ type SimpleOrg = {
   States: string[];
 };
 
-const patientIdsArr: Patient[] = [];
 const cqOrgsList = fs.readFileSync("./cq-org-list.json", "utf8");
 const basePortalUrl = "https://portal.commonwellalliance.org";
+
+const cxId = "";
 const orgOid = "";
 const cookie = "";
 
 // If it fails, change the index to the last one that was successful
-const downloadProgressIndex = 45;
+const downloadProgressIndex = 0;
 
 async function main() {
-  let patients: Patient[] = [];
-
-  if (patients.length === 0) {
-    const facilities = await metriportAPI.listFacilities();
-
-    for (const facility of facilities) {
-      const patientsList = await metriportAPI.listPatients(facility.id);
-
-      patientsList.forEach(patient => {
-        patients.push(patient);
-      });
-    }
-  } else {
-    patients = patientIdsArr;
-  }
-
   const orgs: SimpleOrg[] = JSON.parse(cqOrgsList);
 
   const chunks = chunk(orgs, CQ_ORG_CHUNK_SIZE);
@@ -89,47 +68,11 @@ async function main() {
       throw error;
     }
 
-    const patientsToUpdate = getPatientsToUpdate(patients, orgChunk);
+    await axios.post(`${apiLbUrl}/internal/patient/update-all?cxId=${cxId}`);
 
-    const newPatients = await Promise.all(
-      patientsToUpdate.map(async patient => {
-        return await metriportAPI.updatePatient(patient, patient.facilityIds[0]);
-      })
-    );
-
-    patients = newPatients;
-
-    await sleep(10000);
+    await sleep(5000);
   }
 }
-
-const getPatientsToUpdate = (patients: Patient[], orgs: SimpleOrg[]): Patient[] => {
-  const orgStates = orgs.map(org => org.States);
-
-  const filteredPatients = patients.filter(patient => {
-    let patientStates: string[] = [];
-
-    if (Array.isArray(patient.address)) {
-      const patientWithValidStates = patient.address.reduce((acc: string[], address) => {
-        if (address.state) {
-          return [...acc, address.state];
-        }
-
-        return acc;
-      }, []);
-
-      patientStates = patientWithValidStates;
-    } else {
-      if (patient.address.state) patientStates = [patient.address.state];
-    }
-
-    return orgStates.some(states => {
-      return states.some(state => patientStates.includes(state));
-    });
-  });
-
-  return filteredPatients;
-};
 
 main();
 
