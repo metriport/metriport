b1d38fda9a9c4ecd755019fffbd4a95be1c894c2
Rafael Leite <2132564+leite08@users.noreply.github.com>
2023-10-01 13:25:29 -0500

    build: allow API to RW to DLQ +
    - allow API to RW to DLQ
    - handle erros from getMessagesFromQueue
    
    Ref: metriport/metriport-internal#1092

--- packages/infra/lib/api-service.ts
+++ packages/infra/lib/api-service.ts
@@ -156,7 +156,7 @@ export function createAPIService(
     });
   sidechainFHIRConverterDLQ &&
     provideAccessToQueue({
-      accessType: "send",
+      accessType: "both",
       queue: sidechainFHIRConverterDLQ,
       resource: fargateService.service.taskDefinition.taskRole,
     });
