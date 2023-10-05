e80b0348534463731c60ba2b74f0a4fea291f56e
Ramil Garipov <garipovramil93@hotmail.ca>
2023-10-05 15:24:52 -0700

    feat(sdk): optional attributes on PatientDTO
    refs. metriport/metriport#1088

--- packages/api-sdk/src/medical/models/patientDTO.ts
+++ packages/api-sdk/src/medical/models/patientDTO.ts
@@ -1,21 +1,21 @@
 export type PatientDTO = {
   id: string;
-  eTag: string | undefined;
+  eTag?: string | undefined;
   firstName: string;
   lastName: string;
   dob: string;
   genderAtBirth: string;
-  personalIdentifiers: PersonalIdentifier | undefined;
+  personalIdentifiers?: PersonalIdentifier | undefined;
   facilityIds: string[];
   address: Address | Address[];
-  contact: Contact | Contact[] | undefined;
+  contact?: Contact | Contact[] | undefined;
 };
 
 type Address = {
-  addressLine1: string | undefined;
-  addressLine2: string | undefined;
-  city: string | undefined;
-  state: string | undefined;
+  addressLine1?: string | undefined;
+  addressLine2?: string | undefined;
+  city?: string | undefined;
+  state?: string | undefined;
   zip: string;
   country: string;
 };
@@ -25,19 +25,19 @@ type PersonalIdentifier = {
   period:
     | {
         start: string;
-        end: string | undefined;
+        end?: string | undefined;
       }
     | {
-        start: string;
-        end: string | undefined;
+        start?: string | undefined;
+        end: string;
       }
     | undefined;
-  assigner: string | undefined;
-  type: string | undefined;
-  state: string | undefined;
+  assigner?: string | undefined;
+  type?: string | undefined;
+  state?: string | undefined;
 };
 
 type Contact = {
-  phone: string | undefined;
-  email: string | undefined;
+  phone?: string | undefined;
+  email?: string | undefined;
 };
