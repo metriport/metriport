dc6a75f1d79c001cb41d9e7f3498a0a0350f20e5
Ramil Garipov <garipovramil93@hotmail.ca>
2023-10-05 13:32:48 -0700

    feat(sdk): patientDTO for sdk
    refs. metriport/metriport#1088

--- packages/api-sdk/src/medical/models/common/address.ts
+++ packages/api-sdk/src/medical/models/common/address.ts
@@ -17,4 +17,18 @@ export const addressSchema = z.object({
   country: defaultOptionalString.default("USA"), // here for backwards compatibility, we'll ignore this and always default to USA
 });
 
+export const addressSchemaDTO = z.object({
+  addressLine1: defaultOptionalString,
+  addressLine2: defaultOptionalString,
+  city: defaultOptionalString,
+  state: usStateSchema.or(defaultOptionalString),
+  zip: z.coerce
+    .string()
+    .transform(zipStr => stripNonNumericChars(zipStr))
+    .refine(zip => zip.length === zipLength, {
+      message: `Zip must be a string consisting of ${zipLength} numbers.`,
+    }),
+  country: defaultOptionalString.default("USA"), // here for backwards compatibility, we'll ignore this and always default to USA
+});
+
 export type Address = z.infer<typeof addressSchema>;
