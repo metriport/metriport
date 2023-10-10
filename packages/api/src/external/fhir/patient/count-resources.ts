b79525e517d76272fc8383baeda8df4eed1a2195
Rafael Leite <2132564+leite08@users.noreply.github.com>
2023-10-10 17:24:18 -0500

    build: internal endpoint to count fhir resources per cx
    Ref: metriport/metriport-internal#1050

--- packages/api/src/external/fhir/patient/count-resources.ts
+++ packages/api/src/external/fhir/patient/count-resources.ts
@@ -12,48 +12,26 @@ export type ResourceCount = {
   };
 };
 
-export async function countResourcesPerPatient({
+export type CountResourcesParams = {
+  patient: Pick<Patient, "cxId"> & Partial<Pick<Patient, "id" | "cxId">>;
+  resources?: ResourceTypeForConsolidation[];
+  dateFrom?: string;
+  dateTo?: string;
+};
+
+const summaryCount = "&_summary=count";
+
+export async function countResources({
   patient,
   resources = [],
   dateFrom,
   dateTo,
-}: {
-  patient: Pick<Patient, "id" | "cxId">;
-  resources?: ResourceTypeForConsolidation[];
-  dateFrom?: string;
-  dateTo?: string;
-}): Promise<ResourceCount> {
-  const { log } = Util.out(
-    `countResourcesPerPatient - cxId ${patient.cxId}, patientId ${patient.id}`
-  );
-  const fhir = makeFhirApi(patient.cxId);
+}: CountResourcesParams): Promise<ResourceCount> {
+  const { log } = Util.out(`countResources - cxId ${patient.cxId}, patientId ${patient.id}`);
 
-  const {
-    resourcesByPatient,
-    resourcesBySubject,
-    dateFilter: fullDateQuery,
-  } = getPatientFilter({
-    resources,
-    dateFrom,
-    dateTo,
-  });
-
-  const summaryCount = "&_summary=count";
-
-  const result = await Promise.allSettled([
-    ...resourcesByPatient.map(async resource => {
-      const dateFilter = fullDateQueryForResource(fullDateQuery, resource);
-      const filter = `patient=${patient.id}${dateFilter}${summaryCount}`;
-      const res = await fhir.search(resource, filter);
-      return { resourceType: resource, count: res.total ?? 0 };
-    }),
-    ...resourcesBySubject.map(async resource => {
-      const dateFilter = fullDateQueryForResource(fullDateQuery, resource);
-      const filter = `subject=${patient.id}${dateFilter}${summaryCount}`;
-      const res = await fhir.search(resource, filter);
-      return { resourceType: resource, count: res.total ?? 0 };
-    }),
-  ]);
+  const result = patient.id
+    ? await getResourceCountByPatient({ patient, resources, dateFrom, dateTo })
+    : await getResourceCountByCustomer({ cxId: patient.cxId, resources, dateFrom, dateTo });
   const succeeded = result.flatMap(r => (r.status === "fulfilled" ? r.value : []));
   const failed = result.flatMap(r => (r.status === "rejected" ? r.reason : []));
 
@@ -72,7 +50,7 @@ export async function countResourcesPerPatient({
     );
     capture.message(`Failed to count FHIR resources`, {
       extra: {
-        context: `countResourcesPerPatient`,
+        context: `countResources`,
         patientId: patient.id,
         succeeded: succeeded.length,
         failed: failed.length,
@@ -86,4 +64,70 @@ export async function countResourcesPerPatient({
     total,
     resources: countPerResource,
   };
+}
+
+async function getResourceCountByPatient({
+  patient,
+  resources = [],
+  dateFrom,
+  dateTo,
+}: CountResourcesParams) {
+  const fhir = makeFhirApi(patient.cxId);
+
+  const {
+    resourcesByPatient,
+    resourcesBySubject,
+    dateFilter: fullDateQuery,
+  } = getPatientFilter({
+    resources,
+    dateFrom,
+    dateTo,
+  });
+
+  const result = await Promise.allSettled([
+    ...resourcesByPatient.map(async resource => {
+      const dateFilter = fullDateQueryForResource(fullDateQuery, resource);
+      const filter = `patient=${patient.id}${dateFilter}${summaryCount}`;
+      const res = await fhir.search(resource, filter);
+      return { resourceType: resource, count: res.total ?? 0 };
+    }),
+    ...resourcesBySubject.map(async resource => {
+      const dateFilter = fullDateQueryForResource(fullDateQuery, resource);
+      const filter = `subject=${patient.id}${dateFilter}${summaryCount}`;
+      const res = await fhir.search(resource, filter);
+      return { resourceType: resource, count: res.total ?? 0 };
+    }),
+  ]);
+
+  return result;
+}
+
+async function getResourceCountByCustomer({
+  cxId,
+  resources = [],
+  dateFrom,
+  dateTo,
+}: Omit<CountResourcesParams, "patient"> & { cxId: string }) {
+  const fhir = makeFhirApi(cxId);
+
+  const {
+    resourcesByPatient,
+    resourcesBySubject,
+    dateFilter: fullDateQuery,
+  } = getPatientFilter({
+    resources,
+    dateFrom,
+    dateTo,
+  });
+
+  const result = await Promise.allSettled([
+    ...[...resourcesByPatient, ...resourcesBySubject].map(async resource => {
+      const dateFilter = fullDateQueryForResource(fullDateQuery, resource);
+      const filter = `${dateFilter}${summaryCount}`;
+      const res = await fhir.search(resource, filter);
+      return { resourceType: resource, count: res.total ?? 0 };
+    }),
+  ]);
+
+  return result;
 }
