54944fddd2165ab78ada9e2710a1fa581e485ce9
Rafael Leite <2132564+leite08@users.noreply.github.com>
2023-10-10 20:05:21 -0500

    build: ingest 15 doc in parallel on OS +
    - ingest 15 doc in parallel on OS
    - adjust e2e test env on connect widget
    
    Ref: metriport/metriport-internal#1050

--- packages/connect-widget/jest.config.ts
+++ packages/connect-widget/jest.config.ts
@@ -8,7 +8,7 @@ const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
   verbose: true,
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/", "/build/"],
   testMatch: isE2E
     ? ["**/__tests__/**/(*.)+(spec|test).e2e.[jt]s?(x)"]
     : ["**/__tests__/**/(*.)+(spec|test).[jt]s?(x)"],
