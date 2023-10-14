8b1e83b22794fcd97e9723299a1b78f35b7dd26d
Ramil Garipov <garipovramil93@hotmail.ca>
2023-10-13 18:47:07 -0700

    feat(mapi): changed the lambda code to use aws.s3 functions
    refs. metriport/metriport#960

--- packages/lambdas/src/uploaded-document-processor.ts
+++ packages/lambdas/src/uploaded-document-processor.ts
@@ -1,51 +1,53 @@
-import * as Sentry from "@sentry/serverless";
+// import * as Sentry from "@sentry/serverless";
 import status from "http-status";
-import { capture } from "./shared/capture";
+// import { capture } from "./shared/capture";
 import { getEnvOrFail } from "./shared/env";
-import { SQSClient } from "@metriport/core/external/aws/sqs";
+import { S3Event } from "aws-lambda";
+import { makeS3Client } from "@metriport/core/external/aws/s3";
 // import { getFileInfoFromS3 } from "./document-downloader";
 
 // Keep this as early on the file as possible
-capture.init();
+// capture.init();
 
-const bucketName = getEnvOrFail("MEDICAL_DOCUMENTS_BUCKET_NAME");
+// const bucketName = getEnvOrFail("MEDICAL_DOCUMENTS_BUCKET_NAME");
 const region = getEnvOrFail("AWS_REGION");
+const s3 = makeS3Client(region);
 
 const buildResponse = (status: number, body?: unknown) => ({
   statusCode: status,
   body,
 });
 
-export const handler = Sentry.AWSLambda.wrapHandler(async event => {
-  console.log(
-    `Triggered the upload document process lambda! Bucket to watch: ${bucketName} Type of event: `,
-    typeof event
-  );
-  console.log("Event is:", JSON.stringify(event));
-
+export const handler = async (event: S3Event) => {
+  // Get the object from the event and show its content type
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+  const newKey = `devs.metrpiport.com/ramil/${removeSuffix(key, "_upload")}`;
+  const params = {
+    Bucket: bucket,
+    CopySource: encodeURI(`${bucket}/${key}`),
+    Key: newKey,
+  };
   try {
-    if (event.Records && event.Records.length) {
-      const s3Event = event.Records[0].s3;
-      const bucket = s3Event.bucket.name;
-      const key = s3Event.object.key;
-      console.log(`Bucket: ${bucket} Key: ${key}`);
-    } else {
-      console.log("No records found in event.");
-    }
+    console.log("Bucket name is:", bucket, "\nAnd key is", key);
+    s3.copyObject(params, (err, data) => {
+      if (err) console.log("Err copying the file:", err);
+      console.log("Data is:", data);
+    });
+    return buildResponse(status.NOT_FOUND);
   } catch (err) {
-    console.log("Something went wrong with the event.", err);
+    console.log(err);
+    const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
+    console.log(message);
+    throw new Error(message);
   }
+};
 
-  try {
-    const sqs = new SQSClient({ region });
-    console.log("SQS client created successfully.", sqs);
-  } catch (err) {
-    console.log("Couldn't start an sqs client.", err);
-  }
-
-  return buildResponse(status.NOT_FOUND);
-});
-
+function removeSuffix(key: string, arg1: string) {
+  const newFileName = key.indexOf(arg1) > -1 ? key.replace(arg1, "") : key;
+  console.log("New file name will be", newFileName);
+  return newFileName;
+}
 // from getFileInfoFromS3
 
 // const head = await s3Client
