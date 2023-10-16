a050edd5ff37dbbd2f3690f3176390eab5309b9f
Ramil Garipov <garipovramil93@hotmail.ca>
2023-10-16 14:39:47 -0700

    fix(mapi): sandbox patient document query should now return all documents
    refs. metriport/metriport-internal#1155

--- packages/api/src/external/commonwell/document/document-query-sandbox.ts
+++ packages/api/src/external/commonwell/document/document-query-sandbox.ts
@@ -14,6 +14,7 @@ import { Util } from "../../../shared/util";
 import { convertCDAToFHIR, isConvertible } from "../../fhir-converter/converter";
 import { upsertDocumentToFHIRServer } from "../../fhir/document/save-document-reference";
 import { getFileExtension, sandboxSleepTime } from "./shared";
+import { getDocuments } from "../../fhir/document/get-documents";
 
 const randomDates = [
   "2023-06-15",
@@ -60,15 +61,18 @@ export async function sandboxGetDocRefsAndUpsert({
   const entries = patientData.docRefs;
   log(`Got ${entries.length} doc refs`);
 
-  const convertibleDocs = patientData.docRefs
-    .map(entry => {
-      return {
-        ...entry,
-        content: { mimeType: entry.docRef.content?.[0]?.attachment?.contentType },
-      };
-    })
-    .filter(doc => isConvertible(doc.content?.mimeType));
+  const docsWithContent = entries.map(entry => {
+    return {
+      ...entry,
+      content: { mimeType: entry.docRef.content?.[0]?.attachment?.contentType },
+    };
+  });
+
+  const convertibleDocs = docsWithContent.filter(doc => isConvertible(doc.content?.mimeType));
   const convertibleDocCount = convertibleDocs.length;
+
+  const existingFhirDocs = await getDocuments({ cxId: organization.cxId, patientId: patient.id });
+  const existingTitles = existingFhirDocs.map(d => d.content?.[0]?.attachment?.title);
 
   // set initial download/convert totals
   await appendDocQueryProgress({
@@ -88,39 +92,44 @@ export async function sandboxGetDocRefsAndUpsert({
     requestId,
   });
 
-  for (const entry of convertibleDocs) {
-    const prevDocId = entry.docRef.id;
-    entry.docRef.id = uuidv7();
-    try {
-      await convertCDAToFHIR({
-        patient,
-        document: {
-          id: entry.docRef.id,
-          content: { mimeType: entry.docRef.content?.[0]?.attachment?.contentType },
-        },
-        s3FileName: entry.s3Info.key,
-        s3BucketName: entry.s3Info.bucket,
-        requestId,
-      });
+  for (const entry of docsWithContent) {
+    const fileTitle = entry.docRef.content?.[0]?.attachment?.title;
+    if (!existingTitles.includes(fileTitle)) {
+      const prevDocId = entry.docRef.id;
+      entry.docRef.id = uuidv7();
+      try {
+        if (convertibleDocs.find(d => d.docRef.id === entry.docRef.id)) {
+          await convertCDAToFHIR({
+            patient,
+            document: {
+              id: entry.docRef.id,
+              content: { mimeType: entry.docRef.content?.[0]?.attachment?.contentType },
+            },
+            s3FileName: entry.s3Info.key,
+            s3BucketName: entry.s3Info.bucket,
+            requestId,
+          });
+        }
 
-      const contained = entry.docRef.contained ?? [];
-      const containsPatient = contained.filter(c => c.resourceType === "Patient").length > 0;
-      if (!containsPatient) {
-        contained.push({
-          resourceType: "Patient",
-          id: patient.id,
-        });
+        const contained = entry.docRef.contained ?? [];
+        const containsPatient = contained.filter(c => c.resourceType === "Patient").length > 0;
+        if (!containsPatient) {
+          contained.push({
+            resourceType: "Patient",
+            id: patient.id,
+          });
+        }
+        entry.docRef.subject = {
+          type: "Patient",
+          reference: `Patient/${patient.id}`,
+        };
+
+        entry.docRef = addSandboxFields(entry.docRef);
+        entry.docRef.contained = contained;
+        await upsertDocumentToFHIRServer(patient.cxId, entry.docRef);
+      } catch (err) {
+        log(`Error w/ file docId ${entry.docRef.id}, prevDocId ${prevDocId}: ${err}`);
       }
-      entry.docRef.subject = {
-        type: "Patient",
-        reference: `Patient/${patient.id}`,
-      };
-
-      entry.docRef = addSandboxFields(entry.docRef);
-      entry.docRef.contained = contained;
-      await upsertDocumentToFHIRServer(patient.cxId, entry.docRef);
-    } catch (err) {
-      log(`Error w/ file docId ${entry.docRef.id}, prevDocId ${prevDocId}: ${err}`);
     }
   }
 
