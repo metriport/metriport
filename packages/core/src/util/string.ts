d13ef886a2a7776d6dc543904e2981408e87f57f
Rafael Leite <2132564+leite08@users.noreply.github.com>
2023-10-25 20:13:28 -0500

    build: initial version of CW Session Mgmt
    Ref: metriport/metriport-internal#1195

--- packages/core/src/util/string.ts
+++ packages/core/src/util/string.ts
@@ -1,3 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export function sizeInBytes(str: string) {
   return new Blob([str]).size;
+}
+
+/**
+ * Returns a string representation of the parameter. If its an object, its stringified.
+ * If its a string, number, boolean, bigint or function, its converted to string.
+ * If its undefined or null, it returns undefined or null.
+ */
+export function safeStringify(
+  v: any,
+  replacer?: (key: string, value: any) => any,
+  space?: string | number
+): string | undefined | null {
+  if (v == undefined) return v;
+  return typeof v === "object" ? JSON.stringify(v, replacer, space) : String(v);
 }
