329f9aeb1113859245e4940040779dd94cd1f82e
Rafael Leite <2132564+leite08@users.noreply.github.com>
2023-10-29 19:58:19 -0500

    build: fix lambda deploy script
    Ref: metriport/metriport-internal#1195

---
+++ packages/lambdas/scripts/prep-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+main() {
+   # Clean so we don't have test files on the node_modules that will be copied to the layer
+   npm run clean
+
+   npm ci --omit=dev --ignore-scripts --no-fund 
+
+   ./scripts/build-shared-layer.sh 
+
+   # Reinstall with dev dependencies so the compilation works - the regular code imports from layers,
+   # so part of the code is available without being installed through `npm install`.
+   npm ci --ignore-scripts --no-fund 
+
+   # Rebuild so ./dist is available to CDK through packages/infra
+   npm run build:cloud
+}
+
+main
