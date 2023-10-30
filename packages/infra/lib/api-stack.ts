46b1a86c0e79a8a2972c558e7279fdaee93398ae
Goncharo <dima@metriport.ai>
2023-10-30 01:27:05 -0700

    chore: increate fhir to medical record lambda memory to 4gb
    refs. metriport/metriport-internal#1213

--- packages/infra/lib/api-stack.ts
+++ packages/infra/lib/api-stack.ts
@@ -1059,7 +1059,7 @@ export class APIStack extends Stack {
         ...(sentryDsn ? { SENTRY_DSN: sentryDsn } : {}),
       },
       layers: [...lambdaLayers, chromiumLayer],
-      memory: 1024,
+      memory: 4096,
       timeout: lambdaTimeout,
       vpc,
       alarmSnsAction: alarmAction,
