f5092e848fc80a201d62d1520f5b2bfb346f44d5
Ramil Garipov <garipovramil93@hotmail.ca>
2023-11-13 11:56:18 -0800

    feat(ihe-gateway): not found for missing oid
    refs. metriport/metriport-internal#1251

--- packages/ihe-gateway-sdk/src/common/shared.ts
+++ packages/ihe-gateway-sdk/src/common/shared.ts
@@ -1,15 +1,16 @@
 import BadRequestError from "@metriport/core/util/error/bad-request";
+import NotFoundError from "@metriport/core/util/error/not-found";
 
 const oidRegex = /^(urn:oid:)?([0-9]+(\.[0-9]+)*)$/;
 
 export function normalizeOid(oid: string | undefined): string {
   if (!oid) {
-    throw new BadRequestError("OID must be present");
+    throw new NotFoundError("OID must be present");
   }
 
   const match = oidRegex.exec(oid);
   if (match && match[2]) {
-    return match[2]; // Extracts the OID part, whether it's plain or in a URN format
+    return match[2];
   }
 
   throw new BadRequestError(
