2db66894d6ac789efc564dd276c08113c8306e94
Ramil Garipov <garipovramil93@hotmail.ca>
2023-11-16 11:36:46 -0800

    feat(shared): upd tsdoc and deps
    refs. metriport/metriport-internal#1251

--- packages/ihe-gateway-sdk/src/client/ihe-gateway.ts
+++ packages/ihe-gateway-sdk/src/client/ihe-gateway.ts
@@ -1,5 +1,5 @@
 import { Patient } from "@medplum/fhirtypes";
-import { uuidv7 } from "@metriport/core/util/uuid-v7";
+import { v4 as uuidv4 } from "uuid";
 import { NPIStringArray, npiStringArraySchema } from "../models/shared";
 import { XCPDGateways, XCPDRequest, xcpdGatewaysSchema } from "../models/xcpd-request";
 
@@ -12,9 +12,10 @@ export class IHEGateway {
    * @param principalCareProviderNPIs The list of NPIs of the practitioners associated with the patient.
    * @param requestId Optional. Unique ID for the request. If not provided, one will be created.
    *
-   * @throws ZodError if organization OIDs or principalCareProviderNPIs don't meet their respective criteria.
-   *
    * @returns an XCPD request to be used with an IHE Gateway.
+   *
+   * @throws {@link ZodError}
+   * Thrown if organization OIDs or principalCareProviderNPIs don't meet their respective criteria.
    */
   createXCPDRequest({
     patient,
@@ -30,7 +31,7 @@ export class IHEGateway {
     requestId?: string;
   }): XCPDRequest {
     const xcpdRequest: XCPDRequest = {
-      id: requestId ?? uuidv7(),
+      id: requestId ?? uuidv4(), // #1263 TODO: need to change this to UUIDv7
       cxId,
       xcpdGateways: xcpdGatewaysSchema.parse(xcpdGateways),
       timestamp: new Date().toISOString(),
