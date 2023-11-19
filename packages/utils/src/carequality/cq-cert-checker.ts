f2e1adf4bc8e230bf030251a7b2b25ff9ed94a90
jonahkaye <70026960+jonahkaye@users.noreply.github.com>
2023-11-18 21:04:01 -0500

    feat(infra): cq cert stack (#1190)
    * feat(infra): ihe gateway cert added to ihe stack
    
    Refs: #1189

---
+++ packages/utils/src/carequality/cq-cert-checker.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import * as https from "https";
+import * as fs from "fs";
+
+/**
+ * Script to test the Certificate issued by Carequality
+ * Run with ts-node run-cert.ts
+ */
+
+const app = express();
+
+// Read the private key and the chain of certificates
+const privateKey = fs.readFileSync("<path_to_your_private_key>/decrypted_private_key.key", "utf8");
+const certificateChain = fs.readFileSync("<path_to_your_chain>/chained_no_root.pem", "utf8");
+
+const credentials = { key: privateKey, cert: certificateChain };
+
+// Create HTTPS server
+https.createServer(credentials, app).listen(3000, () => {
+  console.log("Express server listening on port 3000 with HTTPS");
+});
