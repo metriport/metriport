0473ca11c8fb4c1bf82aa76e8625697780a31cf7
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-11-22 09:17:23 -0500

    feat(cq-runner): cq runner base setup
    Ref. metriport/metriport#1279

---
+++ packages/cq-cert-runner/README.md
@@ -0,0 +1,45 @@
+# `cq-cert-runner`
+
+CQ Certification Runner by Metriport Inc.
+
+Tool to run through CQ certification test cases.
+
+## Install
+
+To install the program, execute the following command on your terminal:
+
+`npm i -g @metriport/cq-cert-runner`
+
+Note: you may have to run the command with `sudo`.
+
+## Usage
+
+## Options
+
+## Development
+
+`npm run build`: builds the package
+
+`npm start`: runs the local code pointing to `./.env`
+
+(optionally) `npm run install-local`: installs the package globally from the local source
+
+```
+            ,▄,
+          ▄▓███▌
+      ▄▀╙   ▀▓▀    ²▄
+    ▄└               ╙▌
+  ,▀                   ╨▄
+  ▌                     ║
+                         ▌
+                         ▌
+,▓██▄                 ╔███▄
+╙███▌                 ▀███▀
+    ▀▄
+      ▀╗▄         ,▄
+         '╙▀▀▀▀▀╙''
+
+
+      by Metriport Inc.
+
+```
---
+++ packages/cq-cert-runner/src/index.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+import * as dotenv from "dotenv";
+
+function metriportBanner(): string {
+  return `
+            ,▄,
+          ▄▓███▌
+      ▄▀╙   ▀▓▀    ²▄
+    ▄└               ╙▌
+  ,▀                   ╨▄
+  ▌                     ║
+                         ▌
+                         ▌
+,▓██▄                 ╔███▄
+╙███▌                 ▀███▀
+    ▀▄
+      ▀╗▄         ,▄
+         '╙▀▀▀▀▀╙''
+
+        Metriport Inc.
+
+    CommonWell Cert Runner
+      `;
+}
+
+export const program = new Command();
+program
+  .name("cq-cert-runner")
+  .description("Tool to run through CQ certification test cases.")
+  .requiredOption(
+    `--env-file <file-path>`,
+    `Absolute path to the .env file containing required config. Example required file contents:
+"
+    `
+  )
+  .addHelpText("before", metriportBanner())
+  .showHelpAfterError()
+  .version("1.0.0");
+
+async function main() {
+  console.log(metriportBanner());
+  program.parse();
+  const options = program.opts();
+  dotenv.config({ path: options.envFile });
+}
+
+main();
