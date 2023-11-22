e3d3964dc2f484e32ce2fb47f9d0e59989b50b5c
Jonah Kaye <jonah.arturo@gmail.com>
2023-11-22 11:35:44 -0500

    feat(sdk): java tests move and first test rewritten
    Refs: #1212

---
+++ packages/sdks/java/tester-java/src/main/java/com/metriport/test/Main.java
@@ -0,0 +1,14 @@
+package com.metriport.test;
+import io.github.cdimascio.dotenv.Dotenv;
+import com.metriport.api.Metriport;
+import com.metriport.api.resources.commons.types.Address;
+import com.metriport.api.resources.commons.types.UsState;
+import com.metriport.api.resources.medical.organization.types.OrganizationCreate;
+import com.metriport.api.resources.medical.organization.types.OrgType;
+
+
+public class Main {
+    public static void main(String[] args) {
+      GetOrg.main(args);
+    }
+}
