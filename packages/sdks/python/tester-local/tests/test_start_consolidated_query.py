46d8f9c3f416d21acaee268c2ebd248884572427
Jonah Kaye <jonah.arturo@gmail.com>
2023-11-24 14:01:14 -0500

    feat(sdks): testing changes complete. waiting on bug resolution and fhir import
    Refs: #1212

--- packages/sdks/python/tester-local/tests/test_start_consolidated_query.py
+++ packages/sdks/python/tester-local/tests/test_start_consolidated_query.py
@@ -6,31 +6,25 @@ from generated.client import Metriport
 
 load_dotenv()
 
-def get_env_var_or_fail(key):
-    value = os.getenv(key)
-    if value is None:
-        raise Exception(f"{key} not found")
-    return value
+api_key = os.environ.get("METRIPORT_API_KEY")
+patient_id = os.environ.get("PATIENT_ID")
+base_url = os.environ.get("BASE_URL")
 
 def test_start_consolidated_query():
-    base_url = get_env_var_or_fail("BASE_URL")
-    api_key = get_env_var_or_fail("METRIPORT_API_KEY")
-    patient_id = get_env_var_or_fail("PATIENT_ID")
-
     client = Metriport(api_key=api_key, base_url=base_url)
 
     print("Calling get_consolidated_query_status...")
     query_status = client.medical.fhir.get_consolidated_query_status(id=patient_id)
     print(f"queryStatus: {query_status}")
 
-    print("Calling start_consolidated_query...")
-    response = client.medical.fhir.start_consolidated_query(
-        id=patient_id,
-        resources="DocumentReference,Appointment",
-        date_from="2021-03-01",
-        date_to="2023-04-23"
-    )
-    print(f"response: {json.dumps(response.dict(), indent=2)}")
+    # print("Calling start_consolidated_query...")
+    # response = client.medical.fhir.start_consolidated_query(
+    #     id=patient_id,
+    #     resources="DocumentReference,Appointment",
+    #     date_from="2021-03-01",
+    #     date_to="2023-04-23"
+    # )
+    # print(f"response: {json.dumps(response.dict(), indent=2)}")
 
     print("Now, calling get_consolidated_query_status...")
     query_status = client.medical.fhir.get_consolidated_query_status(id=patient_id)
@@ -43,5 +37,10 @@ def test_start_consolidated_query():
     query_status = client.medical.fhir.get_consolidated_query_status(id=patient_id)
     print(f"queryStatus: {json.dumps(query_status.dict(), indent=2)}")
 
-if __name__ == "__main__":
-    main()
+    print("Counting...")
+    count = client.medical.fhir.count_patient_data(
+        id=patient_id,
+    )
+    print(f"count: {json.dumps(count.dict(), indent=2)}")
+
+
