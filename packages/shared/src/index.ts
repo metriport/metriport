90e573e1882c951e33c298c4b143ef5bfc1a0781
Rafael Leite <2132564+leite08@users.noreply.github.com>
2023-12-02 14:41:15 -0600

    build: don't error when no CW data and EC is on
    Ref: metriport/metriport-internal#1195

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,6 @@
 export { optionalDateSchema } from "./common/date";
 export { emptyFunction } from "./common/general";
 export { normalizeOid } from "./common/normalize-oid";
-export { executeWithRetries } from "./common/retry";
+export { executeWithRetries, executeWithRetriesOrFail } from "./common/retry";
 export { sleep } from "./common/sleep";
 export { validateNPI } from "./common/validate-npi";
