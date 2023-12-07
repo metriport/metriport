5ae9e8bb9d08559da9a6d10ca1e280cd569676cb
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-12-06 19:20:24 -0500

    fix(ihe-gateway-sdk): update imports
    Ref. metriport/metriport#1279

--- packages/ihe-gateway-sdk/src/index.ts
+++ packages/ihe-gateway-sdk/src/index.ts
@@ -9,27 +9,22 @@ export {
 export {
   DocumentQueryRequest,
   DocumentQueryResponse,
-  documentQueryRequestSchema,
   documentQueryResponseSchema,
 } from "./models/document-query";
 export {
   DocumentRetrievalRequest,
-  documentRetrievalRequestSchema,
   DocumentRetrievalResponse,
   documentRetrievalResponseSchema,
 } from "./models/document-retrieval";
 export {
   documentReference,
-  baseRequestSchema,
   baseResponseSchema,
   npiStringSchema,
   NPIStringArray,
   npiStringArraySchema,
   oidStringSchema,
-  samlAttributes,
   SamlAttributes,
   DocumentReference,
   issue,
   operationOutcome,
-  OperationOutcome,
 } from "./models/shared";
