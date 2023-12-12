2dfad08538af8f40a192a33834f5ede60fb5da17
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-12-12 18:24:16 -0500

    fix(api): nullable to nullsih
    Ref. metriport/metriport-internal#1333

--- packages/ihe-gateway-sdk/src/models/shared.ts
+++ packages/ihe-gateway-sdk/src/models/shared.ts
@@ -39,10 +39,10 @@ export const documentReference = z.object({
   homeCommunityId: z.string(),
   urn: z.string(),
   repositoryUniqueId: z.string(),
-  contentType: z.string().nullable(),
-  uri: z.string().nullable(),
-  creation: z.string().nullable(),
-  title: z.string().nullable(),
+  contentType: z.string().nullish(),
+  uri: z.string().nullish(),
+  creation: z.string().nullish(),
+  title: z.string().nullish(),
 });
 
 export type DocumentReference = z.infer<typeof documentReference>;
@@ -67,5 +67,5 @@ export const baseResponseSchema = z.object({
   xcpdPatientId: z.object({ id: z.string(), system: z.string() }).optional(),
   gateway: z.object({ oid: z.string(), url: z.string() }),
   patientId: z.string(),
-  operationOutcome: operationOutcome.optional(),
+  operationOutcome: operationOutcome.nullish(),
 });
