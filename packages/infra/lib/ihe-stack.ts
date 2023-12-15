fa94d6d71532eacc4ed00f7774e8b974d03f91d3
Jonah Kaye <jonah.arturo@gmail.com>
2023-12-15 12:10:01 -0500

    feat(infra): nested stacks bc of 500 resource limit
    Refs: #1356

--- packages/infra/lib/ihe-stack.ts
+++ packages/infra/lib/ihe-stack.ts
@@ -1,4 +1,4 @@
-import { CfnOutput, StackProps } from "aws-cdk-lib";
+import { NestedStack, NestedStackProps, CfnOutput } from "aws-cdk-lib";
 import * as apig from "aws-cdk-lib/aws-apigateway";
 import * as cert from "aws-cdk-lib/aws-certificatemanager";
 import { SnsAction } from "aws-cdk-lib/aws-cloudwatch-actions";
@@ -10,7 +10,7 @@ import { EnvConfig } from "../config/env-config";
 import { createLambda } from "./shared/lambda";
 import { LambdaLayers } from "./shared/lambda-layers";
 
-interface IHEStackProps extends StackProps {
+interface IHEStackProps extends NestedStackProps {
   config: EnvConfig;
   vpc: ec2.IVpc;
   alarmAction: SnsAction | undefined;
@@ -18,133 +18,136 @@ interface IHEStackProps extends StackProps {
   publicZone: r53.IHostedZone;
 }
 
-export function createIHEStack(stack: Construct, props: IHEStackProps) {
-  //-------------------------------------------
-  // API Gateway
-  //-------------------------------------------
-  if (!props.config.iheGateway?.subdomain) {
-    throw new Error("Must define subdomainmain if building the IHE stack!");
+export class IHEStack extends NestedStack {
+  constructor(scope: Construct, id: string, props: IHEStackProps) {
+    super(scope, id, props);
+    //-------------------------------------------
+    // API Gateway
+    //-------------------------------------------
+    if (!props.config.iheGateway?.subdomain) {
+      throw new Error("Must define subdomainmain if building the IHE stack!");
+    }
+
+    if (!props.config.iheGateway?.certArn) {
+      throw new Error("Must define cert arn if building the IHE stack!");
+    }
+
+    // Create the API Gateway
+    const api = new apig.RestApi(this, "IHEAPIGateway", {
+      description: "Metriport IHE Gateway",
+      defaultCorsPreflightOptions: {
+        allowOrigins: ["*"],
+        allowHeaders: ["*"],
+      },
+    });
+
+    // get the certificate form ACM
+    const certificate = cert.Certificate.fromCertificateArn(
+      this,
+      "IHECertificate",
+      props.config.iheGateway.certArn
+    );
+
+    // add domain cert + record
+    const iheApiUrl = `${props.config.iheGateway?.subdomain}.${props.config.domain}`;
+    api.addDomainName("IHEAPIDomain", {
+      domainName: iheApiUrl,
+      certificate: certificate,
+      securityPolicy: apig.SecurityPolicy.TLS_1_2,
+    });
+    new r53.ARecord(this, "IHEAPIDomainRecord", {
+      recordName: iheApiUrl,
+      zone: props.publicZone,
+      target: r53.RecordTarget.fromAlias(new r53_targets.ApiGateway(api)),
+    });
+
+    const iheLambda = createLambda({
+      stack: this,
+      name: "IHE",
+      entry: "ihe",
+      layers: [props.lambdaLayers.shared],
+      envType: props.config.environmentType,
+      envVars: {
+        ...(props.config.lambdasSentryDSN ? { SENTRY_DSN: props.config.lambdasSentryDSN } : {}),
+      },
+      vpc: props.vpc,
+      alarmSnsAction: props.alarmAction,
+    });
+
+    const proxy = new apig.ProxyResource(this, `IHE/Proxy`, {
+      parent: api.root,
+      anyMethod: false,
+      defaultCorsPreflightOptions: { allowOrigins: ["*"] },
+    });
+    proxy.addMethod("ANY", new apig.LambdaIntegration(iheLambda), {
+      requestParameters: {
+        "method.request.path.proxy": true,
+      },
+    });
+
+    // Create lambdas
+    const iti38Lambda = createLambda({
+      stack: this,
+      name: "ITI38",
+      entry: "iti38",
+      layers: [props.lambdaLayers.shared],
+      envType: props.config.environmentType,
+      envVars: {
+        ...(props.config.lambdasSentryDSN ? { SENTRY_DSN: props.config.lambdasSentryDSN } : {}),
+      },
+      vpc: props.vpc,
+      alarmSnsAction: props.alarmAction,
+    });
+
+    const iti39Lambda = createLambda({
+      stack: this,
+      name: "ITI39",
+      entry: "iti39",
+      layers: [props.lambdaLayers.shared],
+      envType: props.config.environmentType,
+      envVars: {
+        ...(props.config.lambdasSentryDSN ? { SENTRY_DSN: props.config.lambdasSentryDSN } : {}),
+      },
+      vpc: props.vpc,
+      alarmSnsAction: props.alarmAction,
+    });
+
+    const iti55Lambda = createLambda({
+      stack: this,
+      name: "ITI55",
+      entry: "iti55",
+      layers: [props.lambdaLayers.shared],
+      envType: props.config.environmentType,
+      envVars: {
+        ...(props.config.lambdasSentryDSN ? { SENTRY_DSN: props.config.lambdasSentryDSN } : {}),
+      },
+      vpc: props.vpc,
+      alarmSnsAction: props.alarmAction,
+    });
+
+    // Create resources for each lambda directly under the API root
+    const iti38Resource = api.root.addResource("ITI38");
+    const iti39Resource = api.root.addResource("ITI39");
+    const iti55Resource = api.root.addResource("ITI55");
+
+    // Add methods for each resource
+    iti38Resource.addMethod("ANY", new apig.LambdaIntegration(iti38Lambda));
+    iti39Resource.addMethod("ANY", new apig.LambdaIntegration(iti39Lambda));
+    iti55Resource.addMethod("ANY", new apig.LambdaIntegration(iti55Lambda));
+    //-------------------------------------------
+    // Output
+    //-------------------------------------------
+    new CfnOutput(this, "IHEAPIGatewayUrl", {
+      description: "IHE API Gateway URL",
+      value: api.url,
+    });
+    new CfnOutput(this, "IHEAPIGatewayID", {
+      description: "IHE API Gateway ID",
+      value: api.restApiId,
+    });
+    new CfnOutput(this, "IHEAPIGatewayRootResourceID", {
+      description: "IHE API Gateway Root Resource ID",
+      value: api.root.resourceId,
+    });
   }
-
-  if (!props.config.iheGateway?.certArn) {
-    throw new Error("Must define cert arn if building the IHE stack!");
-  }
-
-  // Create the API Gateway
-  const api = new apig.RestApi(stack, "IHEAPIGateway", {
-    description: "Metriport IHE Gateway",
-    defaultCorsPreflightOptions: {
-      allowOrigins: ["*"],
-      allowHeaders: ["*"],
-    },
-  });
-
-  // get the certificate form ACM
-  const certificate = cert.Certificate.fromCertificateArn(
-    stack,
-    "IHECertificate",
-    props.config.iheGateway.certArn
-  );
-
-  // add domain cert + record
-  const iheApiUrl = `${props.config.iheGateway?.subdomain}.${props.config.domain}`;
-  api.addDomainName("IHEAPIDomain", {
-    domainName: iheApiUrl,
-    certificate: certificate,
-    securityPolicy: apig.SecurityPolicy.TLS_1_2,
-  });
-  new r53.ARecord(stack, "IHEAPIDomainRecord", {
-    recordName: iheApiUrl,
-    zone: props.publicZone,
-    target: r53.RecordTarget.fromAlias(new r53_targets.ApiGateway(api)),
-  });
-
-  const iheLambda = createLambda({
-    stack: stack,
-    name: "IHE",
-    entry: "ihe",
-    layers: [props.lambdaLayers.shared],
-    envType: props.config.environmentType,
-    envVars: {
-      ...(props.config.lambdasSentryDSN ? { SENTRY_DSN: props.config.lambdasSentryDSN } : {}),
-    },
-    vpc: props.vpc,
-    alarmSnsAction: props.alarmAction,
-  });
-
-  const proxy = new apig.ProxyResource(stack, `IHE/Proxy`, {
-    parent: api.root,
-    anyMethod: false,
-    defaultCorsPreflightOptions: { allowOrigins: ["*"] },
-  });
-  proxy.addMethod("ANY", new apig.LambdaIntegration(iheLambda), {
-    requestParameters: {
-      "method.request.path.proxy": true,
-    },
-  });
-
-  // Create lambdas
-  const iti38Lambda = createLambda({
-    stack: stack,
-    name: "ITI38",
-    entry: "iti38",
-    layers: [props.lambdaLayers.shared],
-    envType: props.config.environmentType,
-    envVars: {
-      ...(props.config.lambdasSentryDSN ? { SENTRY_DSN: props.config.lambdasSentryDSN } : {}),
-    },
-    vpc: props.vpc,
-    alarmSnsAction: props.alarmAction,
-  });
-
-  const iti39Lambda = createLambda({
-    stack: stack,
-    name: "ITI39",
-    entry: "iti39",
-    layers: [props.lambdaLayers.shared],
-    envType: props.config.environmentType,
-    envVars: {
-      ...(props.config.lambdasSentryDSN ? { SENTRY_DSN: props.config.lambdasSentryDSN } : {}),
-    },
-    vpc: props.vpc,
-    alarmSnsAction: props.alarmAction,
-  });
-
-  const iti55Lambda = createLambda({
-    stack: stack,
-    name: "ITI55",
-    entry: "iti55",
-    layers: [props.lambdaLayers.shared],
-    envType: props.config.environmentType,
-    envVars: {
-      ...(props.config.lambdasSentryDSN ? { SENTRY_DSN: props.config.lambdasSentryDSN } : {}),
-    },
-    vpc: props.vpc,
-    alarmSnsAction: props.alarmAction,
-  });
-
-  // Create resources for each lambda directly under the API root
-  const iti38Resource = api.root.addResource("ITI38");
-  const iti39Resource = api.root.addResource("ITI39");
-  const iti55Resource = api.root.addResource("ITI55");
-
-  // Add methods for each resource
-  iti38Resource.addMethod("ANY", new apig.LambdaIntegration(iti38Lambda));
-  iti39Resource.addMethod("ANY", new apig.LambdaIntegration(iti39Lambda));
-  iti55Resource.addMethod("ANY", new apig.LambdaIntegration(iti55Lambda));
-  //-------------------------------------------
-  // Output
-  //-------------------------------------------
-  new CfnOutput(stack, "IHEAPIGatewayUrl", {
-    description: "IHE API Gateway URL",
-    value: api.url,
-  });
-  new CfnOutput(stack, "IHEAPIGatewayID", {
-    description: "IHE API Gateway ID",
-    value: api.restApiId,
-  });
-  new CfnOutput(stack, "IHEAPIGatewayRootResourceID", {
-    description: "IHE API Gateway Root Resource ID",
-    value: api.root.resourceId,
-  });
 }
