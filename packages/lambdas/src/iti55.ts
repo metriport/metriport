367506a388399f26c9740bcd39335bcbfa256c1f
Jonah Kaye <jonah.arturo@gmail.com>
2023-12-20 00:09:39 -0500

    feat(mpi): updating import
    Refs: #574

--- packages/lambdas/src/iti55.ts
+++ packages/lambdas/src/iti55.ts
@@ -1,6 +1,6 @@
 import * as Sentry from "@sentry/serverless";
 import { PatientDiscoveryRequestIncoming } from "@metriport/ihe-gateway-sdk";
-import { processIncomingRequest } from "@metriport/core/external/carequality/process-incoming-iti-55";
+import { processIncomingRequest } from "@metriport/core/external/carequality/iti55/process-incoming-iti-55";
 // Function to construct error response
 
 export const handler = Sentry.AWSLambda.wrapHandler(
