7860262484a9f8021ba526a4fcb4e85cb9e85cd4
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-01-05 10:34:31 -0600

    build: install tsdoc eslint plugin on api
    Ref: metriport/metriport-internal#1402

--- packages/api/.eslintrc.js
+++ packages/api/.eslintrc.js
@@ -1,7 +1,10 @@
 /*global module*/
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "eslint-plugin-tsdoc"],
   root: true,
+  rules: {
+    'tsdoc/syntax': 'warn'
+  }
 };
