2865f97b81d67a097dda7694a33e3872af8e9ad1
Jonah Kaye <jonah.arturo@gmail.com>
2024-01-06 21:05:12 -0500

    feat(pd): fix lambda response
    Refs: #574

--- packages/lambdas/src/patient-discovery.ts
+++ packages/lambdas/src/patient-discovery.ts
@@ -3,13 +3,18 @@ import { MPIMetriportAPI } from "@metriport/core/mpi/patient-mpi-metriport-api";
 import { getEnvVarOrFail } from "@metriport/core/util/env-var";
 import { baseRequestSchema, PatientDiscoveryRequestIncoming } from "@metriport/ihe-gateway-sdk";
 import * as Sentry from "@sentry/serverless";
+import { APIGatewayProxyEvent } from "aws-lambda";
 
 // comment for diff...
 const apiUrl = getEnvVarOrFail("API_URL");
 const mpi = new MPIMetriportAPI(apiUrl);
 
-export const handler = Sentry.AWSLambda.wrapHandler(
-  async (payload: PatientDiscoveryRequestIncoming) => {
+export const handler = Sentry.AWSLambda.wrapHandler(async (event: APIGatewayProxyEvent) => {
+  if (!event.body) {
+    return buildResponse(400, "Request body is missing");
+  }
+  try {
+    const payload: PatientDiscoveryRequestIncoming = JSON.parse(event.body);
     const baseRequest = baseRequestSchema.parse({
       id: payload.id,
       timestamp: payload.timestamp,
@@ -22,6 +27,16 @@ export const handler = Sentry.AWSLambda.wrapHandler(
       patientResource: payload.patientResource,
     };
 
-    return await processIncomingRequest(fullRequest, mpi);
+    const result = await processIncomingRequest(fullRequest, mpi);
+    return buildResponse(200, result);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    return buildResponse(400, err.message);
   }
-);
+});
+
+const buildResponse = (status: number, body?: unknown) => ({
+  statusCode: status,
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(body),
+});
