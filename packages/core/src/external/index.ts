5882f7b811ccb814d2e01380e90e3a1a87a6bc7e
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2024-01-18 21:22:40 -0500

    fix(api): remove unused code
    Ref250etriport/metriport-internal#799

--- packages/core/src/external/index.ts
+++ packages/core/src/external/index.ts
@@ -9,14 +9,3 @@ export function isMedicalDataSource(s?: string | null): s is MedicalDataSource {
   const list = Object.values(MedicalDataSource) as string[];
   return list.includes(s);
 }
-
-export const HL7OID = "2.16.840.1.113883";
-
-type ValidMedicalDataSourceOid = Exclude<
-  MedicalDataSource,
-  MedicalDataSource.ALL | MedicalDataSource.CAREQUALITY
->;
-
-export const MedicalDataSourceOid: Record<ValidMedicalDataSourceOid, string> = {
-  [MedicalDataSource.COMMONWELL]: `${HL7OID}.3.3330`,
-};
