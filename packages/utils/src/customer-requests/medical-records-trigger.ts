bae6302b91fc769918ff69fa2bf1c497ec8c23e7
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-01-23 15:12:04 -0600

    build: use axios instead of SDK to trigger MR
    Ref: metriport/metriport-internal#1441

--- packages/utils/src/customer-requests/medical-records-trigger.ts
+++ packages/utils/src/customer-requests/medical-records-trigger.ts
@@ -1,9 +1,10 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 // keep that ^ on top
-import { MetriportMedicalApi } from "@metriport/api-sdk";
 import { getEnvVarOrFail } from "@metriport/core/util/env-var";
 import { sleep } from "@metriport/shared";
+import axios, { CreateAxiosDefaults } from "axios";
+import base64url from "base64url";
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
 import isBetween from "dayjs/plugin/isBetween";
@@ -17,39 +18,37 @@ dayjs.extend(isBetween);
  * To run this script, set the env vars, the date range, system/exam code, and the list of patient IDs below.
  */
 
-// Make sure to set the patient IDs below
+const cxId = "";
 const patientIds: string[] = [];
 
 // The date range is inclusive on both ends
 const fromDate = undefined;
 const toDate = undefined;
 
-const resources = undefined;
+const resources: string[] | undefined = undefined;
 const conversionType = "pdf";
 
 const timeBetweenPatients = 5_000;
 
 const apiUrl = getEnvVarOrFail("API_URL");
-// TODO update these to use `getCxData()` instead
-const apiKey = getEnvVarOrFail("API_KEY");
 
-const metriportAPI = new MetriportMedicalApi(apiKey, {
-  baseAddress: apiUrl,
+const axiosConfig: CreateAxiosDefaults = {
   timeout: 120_000,
-});
+  baseURL: apiUrl,
+  headers: {
+    "x-api-key": base64url.encode(`metriport:${cxId}`),
+  },
+};
+const api = axios.create(axiosConfig);
 
 async function main() {
   console.log(`########################## Running... - started at ${new Date().toISOString()}`);
   const startedAt = Date.now();
 
   for (const patientId of patientIds) {
-    await metriportAPI.startConsolidatedQuery(
-      patientId,
-      resources,
-      fromDate,
-      toDate,
-      conversionType
-    );
+    await api.post(`/medical/v1/patient/${patientId}/consolidated/query`, undefined, {
+      params: { resources: resources && resources.join(","), fromDate, toDate, conversionType },
+    });
     await sleep(timeBetweenPatients);
   }
 
