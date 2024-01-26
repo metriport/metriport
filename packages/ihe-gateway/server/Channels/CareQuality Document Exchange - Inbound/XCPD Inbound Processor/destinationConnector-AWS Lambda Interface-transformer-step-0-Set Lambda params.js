57929cc38ab67d66062856a57fabd06949bbb389
Ramil Garipov <garipovramil93@hotmail.ca>
2024-01-25 17:54:51 -0800

    feat(ihe-gateway): fixed a typo
    refs. metriport/metriport-internal#1452

--- packages/ihe-gateway/server/Channels/CareQuality Document Exchange - Inbound/XCPD Inbound Processor/destinationConnector-AWS Lambda Interface-transformer-step-0-Set Lambda params.js
+++ packages/ihe-gateway/server/Channels/CareQuality Document Exchange - Inbound/XCPD Inbound Processor/destinationConnector-AWS Lambda Interface-transformer-step-0-Set Lambda params.js
@@ -1,5 +1,5 @@
 // Set the parameters required for the AWS Lambda
 var inboundXCPDUrl = Config.getInboundXcpdUrl();
 
-channelMap.put("inboundXCPDUrl", inboundXCPDUrl);
+channelMap.put("URL", inboundXCPDUrl);
 channelMap.put("DEST_ID", "d" + connectorMessage.getDestinationIdMap().get("AWS Lambda Interface"));
