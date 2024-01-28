ef4cae063270b9b607d3762013bcacde3533008a
Jonah Kaye <jonah.arturo@gmail.com>
2024-01-27 22:54:47 -0500

    feat(qual): removing react native sdk
    Refs: #1212

--- packages/react-native-sdk/__tests__/react-native.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const reactNative = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(reactNative(), 'Hello from reactNative');
-console.info("reactNative tests passed");
--- packages/react-native-sdk/src/__tests__/index.test.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-it.todo('write a test');
