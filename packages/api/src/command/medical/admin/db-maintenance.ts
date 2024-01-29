99d90068970310846fb6a4a679d727121d40975a
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-01-29 16:35:21 -0600

    build: response of dbMaintenance with clearer structure
    Ref: metriport/metriport-internal#1040

--- packages/api/src/command/medical/admin/db-maintenance.ts
+++ packages/api/src/command/medical/admin/db-maintenance.ts
@@ -4,21 +4,30 @@ import { WebhookRequest } from "../../../models/webhook-request";
 
 const DAYS_TO_KEEP_WEBHOOK_REQUESTS = 30;
 
+export type WHRequestsCleanupResult = {
+  removed: number;
+  remaining: number;
+};
+
 /**
  * ADMIN ONLY, NOT TO BE CALLED BY REGULAR CODE/USERS.
  */
-export async function dbMaintenance() {
-  const whRequestsRemoved = await WebhookRequest.destroy({
+export async function dbMaintenance(): Promise<{ webhookRequests: WHRequestsCleanupResult }> {
+  const webhookRequests = await cleanupWHRequests();
+  return { webhookRequests };
+}
+
+async function cleanupWHRequests(): Promise<WHRequestsCleanupResult> {
+  const removed = await WebhookRequest.destroy({
     where: {
       createdAt: {
         [Op.lt]: dayjs().subtract(DAYS_TO_KEEP_WEBHOOK_REQUESTS, "days").toDate(),
       },
     },
   });
-  const whRequestsRemaining = await WebhookRequest.count();
-
+  const remaining = await WebhookRequest.count();
   return {
-    whRequestsRemoved,
-    whRequestsRemaining,
+    removed,
+    remaining,
   };
 }
