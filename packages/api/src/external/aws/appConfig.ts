2cce0165150e79cc2f288b186d8558dc8d629cab
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-02-09 20:31:36 -0600

    build: fail to start API if FFs fail
    Ref. metriport/metriport-internal#1437

--- packages/api/src/external/aws/appConfig.ts
+++ packages/api/src/external/aws/appConfig.ts
@@ -1,10 +1,60 @@
 import { FeatureFlagDatastore, getFeatureFlagValue } from "@metriport/core/external/aws/appConfig";
+import { MetriportError } from "@metriport/core/util/error/metriport-error";
 import { capture } from "@metriport/core/util/notifications";
 import { errorToString } from "@metriport/shared/common/error";
 import { Config } from "../../shared/config";
 import { Util } from "../../shared/util";
 
-const log = Util.log(`App Config`);
+const log = Util.log(`App Config - FF`);
+
+const listOfFeatureFlags: Array<keyof FeatureFlagDatastore> = [
+  "cxsWithEnhancedCoverageFeatureFlag",
+  "cxsWithCQDirectFeatureFlag",
+  "cxsWithADHDMRFeatureFlag",
+  "cxsWithIncreasedSandboxLimitFeatureFlag",
+];
+
+/**
+ * Go through all Feature Flags to make sure they are accessible.
+ */
+export async function initFeatureFlags() {
+  if (Config.isDev()) {
+    log(`Skipping initializing Feature Flags - Develop/Local env`);
+    return;
+  }
+  const res = await Promise.allSettled(
+    listOfFeatureFlags.map(ff =>
+      getFeatureFlagValueLocal(ff).catch(initFeatureFlagsErrorHandling(ff))
+    )
+  );
+  const failed = res.flatMap(r => (r.status === "rejected" ? r.reason : []));
+  if (failed.length > 0) {
+    throw new MetriportError(`Failed to initialize Feature Flags`, undefined, {
+      failed: failed.map(f => f.reason).join("; "),
+    });
+  }
+  log(`Feature Flags initialized.`);
+}
+
+function initFeatureFlagsErrorHandling(featureFlagName: keyof FeatureFlagDatastore) {
+  return (error: unknown) => {
+    const msg = `Failed to get Feature Flag Value`;
+    const extra = { featureFlagName };
+    log(`${msg} - ${JSON.stringify(extra)} - ${errorToString(error)}`);
+    capture.error(msg, { extra: { ...extra, error } });
+    throw error;
+  };
+}
+
+function getFeatureFlagValueLocal(featureFlagName: keyof FeatureFlagDatastore) {
+  return getFeatureFlagValue(
+    Config.getAWSRegion(),
+    Config.getAppConfigAppId(),
+    Config.getAppConfigConfigId(),
+    Config.getEnvType(),
+    featureFlagName
+  );
+}
 
 /**
  * Returns the list of customers that are enabled for the given feature flag.
@@ -15,13 +65,7 @@ async function getCxsWithFeatureFlagEnabled(
   featureFlagName: keyof FeatureFlagDatastore
 ): Promise<string[]> {
   try {
-    const featureFlag = await getFeatureFlagValue(
-      Config.getAWSRegion(),
-      Config.getAppConfigAppId(),
-      Config.getAppConfigConfigId(),
-      Config.getEnvType(),
-      featureFlagName
-    );
+    const featureFlag = await getFeatureFlagValueLocal(featureFlagName);
     if (featureFlag.enabled) {
       if (featureFlag.cxIds) return featureFlag.cxIds;
       if (featureFlag.cxIdsAndLimits) return featureFlag.cxIdsAndLimits;
