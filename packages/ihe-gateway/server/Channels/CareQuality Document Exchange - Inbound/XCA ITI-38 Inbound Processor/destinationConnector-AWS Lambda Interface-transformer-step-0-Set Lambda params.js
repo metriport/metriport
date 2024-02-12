1997e8edf3468d37cf82edf63f5a69ee85d6fb47
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-02-12 17:48:12 -0600

    Revert "Revert "Merge pull request #1521 from metriport/1452-mirth-env-vars""
    This reverts commit 00856e85f2710a12dcf0082db517eb9a5027afd9.

--- packages/ihe-gateway/server/Channels/CareQuality Document Exchange - Inbound/XCA ITI-38 Inbound Processor/destinationConnector-AWS Lambda Interface-transformer-step-0-Set Lambda params.js
+++ packages/ihe-gateway/server/Channels/CareQuality Document Exchange - Inbound/XCA ITI-38 Inbound Processor/destinationConnector-AWS Lambda Interface-transformer-step-0-Set Lambda params.js
@@ -1,4 +1,3 @@
 // Set the parameters required for the AWS Lambda
-channelMap.put('URL', configurationMap.get('INBOUND_PATIENT_DISCOVERY_URL'));
-
-channelMap.put('DEST_ID', 'd' + connectorMessage.getDestinationIdMap().get('AWS Lambda Interface'));
+channelMap.put("URL", Config.getInboundDocumentQueryURL());
+channelMap.put("DEST_ID", "d" + connectorMessage.getDestinationIdMap().get("AWS Lambda Interface"));
