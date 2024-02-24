93b13f60da77247440fb27239db28be37309dd24
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2024-02-23 21:31:46 -0500

    fix(ihe): make url dynamic
    Ref. metriport/metriport-internal#1317

--- packages/ihe-gateway/server/Channels/CareQuality Document Exchange - Outbound/XCA - DQ App Interface/DeployScript.js
+++ packages/ihe-gateway/server/Channels/CareQuality Document Exchange - Outbound/XCA - DQ App Interface/DeployScript.js
@@ -4,4 +4,14 @@
 // Store for the XCA ITI-38 and ITI-39 Processors
 globalMap.put('XCADQAPPINTERFACE', channelId);
 
+const baseAddress = globalMap.get("API_BASE_ADDRESS");
+if (!baseAddress) {
+  logger.error("XCA - DQ App Interface: Failed start, missing API_BASE_ADDRESS");
+  ChannelUtil.stopChannel(channelId);
+  return;
+}
+
+const destinationURL = baseAddress + "/internal/carequality/document-query/response";
+globalChannelMap.put("URL", destinationURL);
+
 return;
