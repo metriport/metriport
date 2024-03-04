2917fa3cce4105d5affbdebbebce4b9f7b46d1b4
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2024-03-04 17:56:06 -0500

    fix(ihe): stringify operation outcome
    Ref. metriport/metriport-internal#1350

--- packages/ihe-gateway/server/Channels/CareQuality Document Exchange - Outbound/XCPD ITI-55 Processor/destinationConnector-XCPD Endpoint-responseTransformer-step-5-Case 5: Application Error.js
+++ packages/ihe-gateway/server/Channels/CareQuality Document Exchange - Outbound/XCPD ITI-55 Processor/destinationConnector-XCPD Endpoint-responseTransformer-step-5-Case 5: Application Error.js
@@ -35,7 +35,7 @@ if (['AE','AR'].indexOf(ack.toString()) > -1 || ['AE','QE'].indexOf(queryRespons
 		operationOutcome.issue.push(issue);		
 	}
   channelMap.put('OPERATION_OUTCOME', operationOutcome);
-	var _response = getXCPD55ResponseTemplate(channelMap.get('REQUEST'), operationOutcome);
+	var _response = getXCPD55ResponseTemplate(channelMap.get('REQUEST'), JSON.stringify(operationOutcome));
 	
 	// Send the response back to the app
 	var result = router.routeMessageByChannelId(globalMap.get('XCPDAPPINTERFACE'), JSON.stringify(_response));
