827ad1bf840d3a5977ebd2d1bf4a31c090289e2e
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-03-06 21:54:38 -0600

    build: channel dependencies mapped for startup + log on channel deploy
    Ref. metriport/metriport-internal#1350

--- packages/ihe-gateway/server/Channels/CareQuality Document Exchange - Outbound/XCPD Bulk Interface/DeployScript.js
+++ packages/ihe-gateway/server/Channels/CareQuality Document Exchange - Outbound/XCPD Bulk Interface/DeployScript.js
@@ -1,3 +1,6 @@
 // This script executes once when the channel is deployed
 // You only have access to the globalMap and globalChannelMap here to persist data
+
+logger.info("Deploying " + channelName);
+
 return;
