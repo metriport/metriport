f7099e38c3e286020318e9ee01cad607c10b1b1a
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-03-06 21:52:19 -0600

    build: pull-from-server also waits for server to be online
    Ref. metriport/metriport-internal#1350

--- packages/ihe-gateway/scripts/pull-from-server.sh
+++ packages/ihe-gateway/scripts/pull-from-server.sh
@@ -18,6 +18,40 @@ if [ -z "${IHE_GW_FULL_BACKUP_LOCATION}" ]; then
   exit 1
 fi
 
+isApiAvailable() {
+  local checkApiResult=$(curl -s --header "X-Requested-With: push-to-server" -u $IHE_GW_USER:$IHE_GW_PASSWORD -w '%{response_code}' -o /dev/null "$IHE_GW_URL/server/jvm")
+  if [[ $checkApiResult -lt 100 ]]; then
+    return 1 # not ready
+  elif [[ $checkApiResult -ge 300 ]]; then
+    echo "[config] Failed login to server, trying up to $MAX_LOGIN_ATTEMPTS times - Result: $checkApiResult" >/dev/stderr
+    if [ $INCORRECT_LOGIN_ATTEMPTS -ge $MAX_LOGIN_ATTEMPTS ]; then
+      echo "[config] Too many incorrect login attempts, stopping..."
+      cleanup
+      exit 1
+    fi
+    INCORRECT_LOGIN_ATTEMPTS=$((INCORRECT_LOGIN_ATTEMPTS + 1))
+  fi
+  return 0
+}
+
+waitServerOnline() {
+  echo "[config] Waiting for the server to start... (${IHE_GW_URL})"
+  until curl -s -f -o /dev/null $IHE_GW_URL; do
+    sleep 1
+  done
+  until isApiAvailable; do
+    sleep 1
+  done
+  sleep 2
+}
+
+###################################################################################################
+#
+# MAIN LOGIC
+#
+###################################################################################################
+waitServerOnline
+
 # clear out the server directory because mirthsync doesn't remove files that are no longer on the server
 rm -rf ./server/Channels
 rm -rf ./server/CodeTemplates
@@ -30,3 +64,5 @@ rm -rf ./server/GlobalScripts
 ./scripts/mirthsync.sh -s $IHE_GW_URL -u $IHE_GW_USER -p $IHE_GW_PASSWORD -i -t $IHE_GW_FULL_BACKUP_LOCATION --include-configuration-map -m backup -f pull
 
 mv $IHE_GW_FULL_BACKUP_LOCATION/FullBackup.xml $IHE_GW_FULL_BACKUP_LOCATION/FullBackup-$ENV_TYPE.xml
+
+echo "[config] Done."
