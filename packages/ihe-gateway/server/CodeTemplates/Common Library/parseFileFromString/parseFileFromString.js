08498fc9a3621aa6fc71fb8ed2711101389de9c6
Jonah Kaye <jonah.arturo@gmail.com>
2024-03-10 19:20:46 -0400

    feat(mime): mime type patch
    Refs: #1350

--- packages/ihe-gateway/server/CodeTemplates/Common Library/parseFileFromString/parseFileFromString.js
+++ packages/ihe-gateway/server/CodeTemplates/Common Library/parseFileFromString/parseFileFromString.js
@@ -12,8 +12,6 @@
  * @return {ParsedFile} returns the file type, extension and decoded string and bytes
  */
 function parseFileFromString(fileAsString) {
-	// logger.info("[parseFileFromString] fileAsString: " + fileAsString);
-  let decodedString = null;
 	// logger.info("[parseFileFromString] decodedString: " + decodedString);
   let decodedBytes = null;
   // We don't know upfront if the file is base64 encoded, so we try to decode it and if it fails
@@ -21,19 +19,15 @@ function parseFileFromString(fileAsString) {
   try {
     const byteDecoder = java.util.Base64.getDecoder();
     decodedBytes = byteDecoder.decode(fileAsString);
-    const decoded = decodeBase64(fileAsString.toString());
-    decodedString = new Packages.java.lang.String(decoded);
   } catch (ex) {
     // intentionally left empty
   }
-  if (!decodedString || !decodedBytes) return;
+  if (!decodedBytes) return;
 
-	// logger.info("[parseFileFromString] post decodedString: " + decodedString);
-
-  const type = detectFileType(decodedString);
+  const decodedString = new Packages.java.lang.String(decodedBytes).toString();
+  const type = detectFileType(decodedBytes, decodedString);
   const mimeType = type[0];
   const extension = type[1];
-	// logger.info("[parseFileFromString] mimeType: " + mimeType + "; extension: " + extension);
 
   if (mimeType === XML_TXT_MIME_TYPE || mimeType === XML_APP_MIME_TYPE) {
     // If the file is XML, it might contain an attachment (nonXMLBody) that we need to extract
