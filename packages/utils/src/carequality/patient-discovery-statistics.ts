91cee0c48a0adae70185136321231a4cf924f0c3
Ramil Garipov <garipovramil93@hotmail.ca>
2024-03-11 18:22:18 -0700

    feat(api): fixed erroneous file path
    refs. metriport/metriport-internal#1350

--- packages/utils/src/carequality/patient-discovery-statistics.ts
+++ packages/utils/src/carequality/patient-discovery-statistics.ts
@@ -1,14 +1,14 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 // keep that ^ on top
-import { getXcpdStatisticsForPatient } from "@metriport/core/external/carequality/pd/get-statistics";
+import { getXcpdStatisticsForPatient } from "@metriport/core/external/carequality/pd/get-xcpd-statistics";
 import { getEnvVarOrFail } from "../../../api/src/shared/config";
 
 const apiUrl = getEnvVarOrFail("API_URL");
 const cxId = getEnvVarOrFail("CX_ID");
 const sqlDBCreds = getEnvVarOrFail("DB_CREDS");
 const patientId = "";
-const dateString = ""; // RECOMMENDED to use in production due to the large amount of data generated on a daily basis
+const dateString = "";
 
 async function main() {
   const xcpdResultsString = await getXcpdStatisticsForPatient(
