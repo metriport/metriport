394f153ccc8ea906630485b561531602796ca690
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2024-03-12 18:59:01 -0400

    fix(ihe): send commit
    Ref. metriport/metriport-internal#1350

--- packages/api/src/external/carequality/command/update-patient-discovery-status.ts
+++ packages/api/src/external/carequality/command/update-patient-discovery-status.ts
@@ -46,7 +46,7 @@ export async function updatePatientDiscoveryStatus({
       const scheduledDocQueryRequestId = getCQData(externalData)?.scheduledDocQueryRequestId;
 
       if (scheduledDocQueryRequestId) {
-        // TODO: define the data to be updated on the patient in setDocQueryProgress, so we could call it here too.
+        // TODO: define the data to be updated on the patient in setDocQueryProgress so we could call it here too.
         await setDocQueryProgress({
           patient,
           requestId: scheduledDocQueryRequestId,
