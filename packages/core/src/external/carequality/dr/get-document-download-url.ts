45053b0a7eceba6d6e9fa4a3f245358aad10c746
Jonah Kaye <jonah.arturo@gmail.com>
2024-03-18 10:18:37 -0400

    feat(inbound): fixing unique doc id
    Refs: #1613

--- packages/core/src/external/carequality/dr/get-document-download-url.ts
+++ packages/core/src/external/carequality/dr/get-document-download-url.ts
@@ -17,48 +17,51 @@ export async function buildDocumentReferences(
 ): Promise<DocumentReference[]> {
   validateBasePayload(payload);
 
-  const documentIds = extractDocumentIds(payload);
+  const [documentIds, uniqueIds] = extractDocumentIds(payload);
   if (documentIds.length === 0) {
     throw new XDSRegistryError("Valid Document ID is not defined");
   }
 
-  return await retrieveDocumentReferences(documentIds);
+  return await retrieveDocumentReferences(documentIds, uniqueIds);
 }
 
-async function retrieveDocumentReferences(documentIds: string[]): Promise<DocumentReference[]> {
+async function retrieveDocumentReferences(
+  documentIds: string[],
+  uniqueIds: string[]
+): Promise<DocumentReference[]> {
   const s3Utils = new S3Utils(region);
   const documentReferences: DocumentReference[] = [];
 
   // TODO consider making this more robust, so if one fails we still return the rest
-  for (const id of documentIds) {
-    const { size, contentType, eTag } = await s3Utils.getFileInfoFromS3(
-      id,
-      medicalDocumentsBucketName
-    );
-    if (!eTag) {
-      const message = `Failed to retrieve ETag for document`;
+  documentIds.forEach(async (id, index) => {
+    const { size, contentType } = await s3Utils.getFileInfoFromS3(id, medicalDocumentsBucketName);
+    const uniqueId = uniqueIds[index];
+    if (!uniqueId) {
+      const message = `Failed to retrieve uniqueId for document`;
       console.log(`${message}: ${id}`);
       throw new XDSRegistryError("Failed to retrieve Document");
     }
     const documentReference: DocumentReference = {
       homeCommunityId: METRIPORT_HOME_COMMUNITY_ID,
       repositoryUniqueId: METRIPORT_REPOSITORY_UNIQUE_ID,
-      docUniqueId: eTag,
+      docUniqueId: uniqueId,
       contentType: contentType,
       size: size,
       urn: id,
     };
     documentReferences.push(documentReference);
-  }
+  });
 
   return documentReferences;
 }
 
-function extractDocumentIds(payload: InboundDocumentRetrievalReq): string[] {
+function extractDocumentIds(payload: InboundDocumentRetrievalReq): [string[], string[]] {
   const documentIds: string[] = [];
+  const uniqueIds: string[] = [];
 
   for (const documentReference of payload.documentReference) {
+    uniqueIds.push(documentReference.docUniqueId);
     documentIds.push(extractDocumentUniqueId(documentReference.docUniqueId));
   }
-  return documentIds;
+  return [documentIds, uniqueIds];
 }
