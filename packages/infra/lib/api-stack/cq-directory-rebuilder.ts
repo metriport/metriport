48e9b6732cb3b6909bc1588e5ae7a7619915bf21
Ramil Garipov <garipovramil93@hotmail.ca>
2024-03-19 01:15:22 -0700

    feat(lambda): added a scheduled lambda to rebuild cq directory
    refs. metriport/metriport-internal#1350

---
+++ packages/infra/lib/api-stack/cq-directory-rebuilder.ts
@@ -0,0 +1,72 @@
+import { Duration } from "aws-cdk-lib";
+import { SnsAction } from "aws-cdk-lib/aws-cloudwatch-actions";
+import { IVpc } from "aws-cdk-lib/aws-ec2";
+import { Function as Lambda, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import { EnvConfig } from "../../config/env-config";
+import { getConfig } from "../shared/config";
+import { LambdaLayers } from "../shared/lambda-layers";
+import { createScheduledLambda } from "../shared/lambda-scheduled";
+
+export type CqDirectoryRebuilderProps = {
+  stack: Construct;
+  lambdaLayers: LambdaLayers;
+  vpc: IVpc;
+  apiAddress: string;
+  alarmSnsAction?: SnsAction;
+};
+
+function getSettings(
+  props: CqDirectoryRebuilderProps,
+  config: NonNullable<EnvConfig["cqDirectoryRebuilder"]>
+) {
+  return {
+    ...props,
+    name: "ScheduledCqDirectoryRebuilder",
+    lambdaMemory: 256,
+    lambdaTimeout: Duration.seconds(900), // How long can the lambda run for, max is 900 seconds (15 minutes)
+    runtime: Runtime.NODEJS_18_X,
+    scheduleExpression: config.scheduleExpressions, // See: https://docs.aws.amazon.com/lambda/latest/dg/services-cloudwatchevents-expressions.html
+    url: `http://${props.apiAddress}/internal/carequality/directory/rebuild`,
+    httpTimeout: Duration.seconds(3),
+  };
+}
+
+export function createCqDirectoryRebuilder(props: CqDirectoryRebuilderProps): Lambda | undefined {
+  const config = getConfig();
+  if (!config.docQueryChecker) return;
+
+  const {
+    stack,
+    lambdaLayers,
+    vpc,
+    runtime,
+    alarmSnsAction,
+    name,
+    lambdaMemory,
+    lambdaTimeout,
+    scheduleExpression,
+    url,
+    httpTimeout,
+  } = getSettings(props, config.docQueryChecker);
+
+  const lambda = createScheduledLambda({
+    stack,
+    layers: [lambdaLayers.shared],
+    name,
+    vpc,
+    scheduleExpression,
+    url,
+    runtime,
+    memory: lambdaMemory,
+    timeout: lambdaTimeout,
+    alarmSnsAction,
+    envType: config.environmentType,
+    envVars: {
+      TIMEOUT_MILLIS: String(httpTimeout.toMilliseconds()),
+      ...(config.lambdasSentryDSN ? { SENTRY_DSN: config.lambdasSentryDSN } : {}),
+    },
+  });
+
+  return lambda;
+}
