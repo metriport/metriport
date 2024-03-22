fafc93f23fec337f9529f51d8f7a36ff719ae9e9
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-03-22 15:27:24 -0500

    build: wip draft of changes WIP
    Ref. metriport/metriport-internal#1634

--- packages/api/src/__tests__/e2e/mapi/mapi.test.e2e.ts
+++ packages/api/src/__tests__/e2e/mapi/mapi.test.e2e.ts
@@ -1,10 +1,12 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-
+// Keep dotenv import and config before everything else
 import { faker } from "@faker-js/faker";
 import { Organization as FhirOrg } from "@medplum/fhirtypes";
 import { Facility, Organization, PatientDTO } from "@metriport/api-sdk";
 import { Organization as CWOrganization } from "@metriport/commonwell-sdk";
+import { executeWithRetries } from "@metriport/shared";
+import { areDocumentsProcessing } from "../../../command/medical/document/document-status";
 import * as cwCommands from "../../../external/commonwell/__tests__";
 import { Config } from "../../../shared/config";
 import { retryFunction } from "../../../shared/retry";
@@ -14,13 +16,14 @@ import { createFacility, validateFacility } from "./facility";
 import { validateCWOrg, validateFhirOrg, validateLocalOrg } from "./organization";
 import { createPatient, validateFhirPatient, validateLocalPatient } from "./patient";
 import { fhirApi, fhirHeaders, medicalApi } from "./shared";
-import { areDocumentsProcessing } from "../../../command/medical/document/document-status";
 
 const maxRetries = 4;
 
 jest.setTimeout(30000);
 
-// NEVER TO BE RUN IN PRODUCTION
+console.log(`Config.getEnvType(): ${Config.getEnvType()}`);
+
+// NEVER TO BE RUN IN PRODUCTION - really?
 if (Config.isStaging() || Config.isDev()) {
   describe("MAPI E2E Tests", () => {
     let facility: Facility;
@@ -28,13 +31,12 @@ if (Config.isStaging() || Config.isDev()) {
 
     it("gets an organization", async () => {
       const org = await medicalApi.getOrganization();
-
       expect(org).toBeTruthy();
 
       if (org) {
         const fhirOrg = await fhirApi.readResource("Organization", org.id, fhirHeaders);
 
-        const cwOrg = await retryFunction<CWOrganization | undefined>(
+        const cwOrg = await executeWithRetries<CWOrganization | undefined>(
           async () => await cwCommands.organization.getOne(org.oid),
           maxRetries,
           3000
@@ -48,84 +50,66 @@ if (Config.isStaging() || Config.isDev()) {
 
     it("updates an organization", async () => {
       const org = await medicalApi.getOrganization();
-
       expect(org).toBeTruthy();
+      if (!org) throw new Error(`Unreachable code on test 'updates an organization'`);
 
-      if (org) {
-        const newName = faker.word.noun();
+      const newName = faker.word.noun();
+      const updateOrg: Organization = {
+        ...org,
+        name: newName,
+      };
+      const updatedOrg = await medicalApi.updateOrganization(updateOrg);
 
-        const updateOrg: Organization = {
-          ...org,
-          name: newName,
-        };
+      fhirApi.invalidateAll();
+      const fhirOrg: FhirOrg = await fhirApi.readResource(
+        "Organization",
+        updatedOrg.id,
+        fhirHeaders
+      );
 
-        const updatedOrg = await medicalApi.updateOrganization(updateOrg);
+      const cwOrg: CWOrganization | undefined = await retryFunction<CWOrganization | undefined>(
+        async () => await cwCommands.organization.getOne(updatedOrg.oid),
+        maxRetries,
+        3000,
+        result => result?.name === newName
+      );
 
-        fhirApi.invalidateAll();
-        const fhirOrg: FhirOrg = await fhirApi.readResource(
-          "Organization",
-          updatedOrg.id,
-          fhirHeaders
-        );
-
-        const cwOrg: CWOrganization | undefined = await retryFunction<CWOrganization | undefined>(
-          async () => await cwCommands.organization.getOne(updatedOrg.oid),
-          maxRetries,
-          3000,
-          result => result?.name === newName
-        );
-
-        validateLocalOrg(updatedOrg, updateOrg);
-        validateFhirOrg(fhirOrg, updateOrg);
-        validateCWOrg(cwOrg, updateOrg);
-      }
+      validateLocalOrg(updatedOrg, updateOrg);
+      validateFhirOrg(fhirOrg, updateOrg);
+      validateCWOrg(cwOrg, updateOrg);
     });
 
     it("create a facility", async () => {
       facility = await medicalApi.createFacility(createFacility);
-
       validateFacility(facility);
     });
 
     it("updates a facility", async () => {
       const newName = faker.word.noun();
-
       const updateFacility: Facility = {
         ...facility,
         name: newName,
       };
-
       facility = await medicalApi.updateFacility(updateFacility);
-
       validateFacility(facility, updateFacility);
       expect(facility.name).toEqual(newName);
     });
 
     it("gets one facility", async () => {
       const getFacility = await medicalApi.getFacility(facility.id);
-
       validateFacility(getFacility);
     });
 
     it("creates a patient", async () => {
       patient = await medicalApi.createPatient(createPatient, facility.id);
-
       const fhirPatient = await fhirApi.readResource("Patient", patient.id, fhirHeaders);
-
       validateLocalPatient(patient);
       validateFhirPatient(fhirPatient, patient);
-
-      // Creating a CW patient is done in the background need to await so we can query docs
-      await Util.sleep(10000);
     });
 
-    it("creates consolidated data for patient", async () => {
-      const payload = createConsolidated(patient.id);
-      const consolidated = await medicalApi.createPatientConsolidated(patient.id, payload);
-      const count = await medicalApi.countPatientConsolidated(patient.id);
-
-      expect(count.total).toEqual(payload.entry?.length);
-      expect(consolidated).toBeTruthy();
+    it("waits after creating a patient", async () => {
+      // Creating a CW and CQ patient is done in the background need to await so we can query docs
+      await Util.sleep(10000);
     });
 
     it("triggers a document query for the created patient", async () => {
@@ -146,26 +130,39 @@ if (Config.isStaging() || Config.isDev()) {
       expect(documents.length).toEqual(2);
     });
 
-    it("deletes a patient's consolidated data", async () => {
-      // when moved to internal will updated
-      const consolidated = await medicalApi.getPatientConsolidated(patient.id);
+    it("contains expected data on FHIR server", async () => {
+      // TODO implement this
+      // do we need a dedicated one for MR's data or does it come from consolidated?
+    });
 
-      if (consolidated && consolidated.entry) {
-        for (const docEntry of consolidated.entry) {
-          if (docEntry.resource && docEntry.resource.id) {
-            await fhirApi.deleteResource(
-              docEntry.resource.resourceType,
-              docEntry.resource.id,
-              fhirHeaders
-            );
-          }
-        }
-      }
-
+    it("creates consolidated data for patient", async () => {
+      const payload = createConsolidated(patient.id);
+      const consolidated = await medicalApi.createPatientConsolidated(patient.id, payload);
       const count = await medicalApi.countPatientConsolidated(patient.id);
 
-      expect(count.total).toEqual(0);
+      expect(count.total).toEqual(payload.entry?.length);
+      expect(consolidated).toBeTruthy();
     });
+
+    // if we keep this only to FHIR, should we move this to the proper folder?
+    // it("deletes a patient's consolidated data", async () => {
+    //   // when moved to internal will updated
+    //   const consolidated = await medicalApi.getPatientConsolidated(patient.id);
+
+    //   if (consolidated && consolidated.entry) {
+    //     for (const docEntry of consolidated.entry) {
+    //       if (docEntry.resource && docEntry.resource.id) {
+    //         await fhirApi.deleteResource(
+    //           docEntry.resource.resourceType,
+    //           docEntry.resource.id,
+    //           fhirHeaders
+    //         );
+    //       }
+    //     }
+    //   }
+    //   const count = await medicalApi.countPatientConsolidated(patient.id);
+    //   expect(count.total).toEqual(0);
+    // });
   });
 } else {
   describe("Prod Tests", () => {
