1f699ecd7769e602519e591e6b58eabc62400c49
Jonah Kaye <jonah.arturo@gmail.com>
2024-04-08 18:02:32 -0500

    feat(saml): refactoring to simplify
    Refs: #1667

--- packages/core/src/external/carequality/ihe-gateway-v2/process-xcpd-response.ts
+++ packages/core/src/external/carequality/ihe-gateway-v2/process-xcpd-response.ts
@@ -7,6 +7,166 @@ import {
 } from "@metriport/ihe-gateway-sdk";
 import { normalizeGender } from "./utils";
 
+function handleHTTPErrorResponse({
+  httpError,
+  outboundRequest,
+  gateway,
+}: {
+  httpError: string;
+  outboundRequest: OutboundPatientDiscoveryReq;
+  gateway: XCPDGateway;
+}): OutboundPatientDiscoveryResp {
+  const operationOutcome: OperationOutcome = {
+    resourceType: "OperationOutcome",
+    id: outboundRequest.id,
+    issue: [
+      {
+        severity: "error",
+        code: "http-error",
+        details: {
+          text: httpError,
+        },
+      },
+    ],
+  };
+  return {
+    id: outboundRequest.id,
+    timestamp: outboundRequest.timestamp,
+    responseTimestamp: new Date().toISOString(),
+    gateway: gateway,
+    patientId: outboundRequest.patientId || "",
+    patientMatch: null,
+    operationOutcome: operationOutcome,
+  };
+}
+
+function handlePatientMatchResponse({
+  jsonObj,
+  outboundRequest,
+  gateway,
+}: {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  jsonObj: any;
+  outboundRequest: OutboundPatientDiscoveryReq;
+  gateway: XCPDGateway;
+}): OutboundPatientDiscoveryResp {
+  const subject1 =
+    jsonObj["soap:Envelope"]["soap:Body"]["PRPA_IN201306UV02"]["controlActProcess"]["subject"][
+      "registrationEvent"
+    ]["subject1"];
+
+  const addr = subject1["patient"]["patientPerson"]["addr"];
+  const addresses = [
+    {
+      line: [addr["streetAddressLine"]["#text"] || addr["streetAddressLine"]],
+      city: addr["city"]["#text"] || addr["city"],
+      state: addr["state"]["#text"] || addr["state"],
+      postalCode: addr["postalCode"]["#text"] || addr["postalCode"],
+      country: addr["country"]["#text"] || addr["country"],
+    },
+  ];
+
+  const patientResource = {
+    name: [
+      {
+        given: [subject1["patient"]["patientPerson"]["name"]["given"]],
+        family: subject1["patient"]["patientPerson"]["name"]["family"],
+      },
+    ],
+    gender: normalizeGender(
+      subject1["patient"]["patientPerson"]["administrativeGenderCode"]["@_code"]
+    ),
+    birthDate: subject1["patient"]["patientPerson"]["birthTime"]["@_value"],
+    address: addresses,
+  };
+
+  const response: OutboundPatientDiscoveryResp = {
+    id: outboundRequest.id,
+    timestamp: outboundRequest.timestamp,
+    responseTimestamp: new Date().toISOString(),
+    externalGatewayPatient: {
+      id: subject1["patient"]["id"]["@_extension"].toString(),
+      system: subject1["patient"]["id"]["@_root"].toString(),
+    },
+    gateway: gateway,
+    patientId: outboundRequest.patientId,
+    patientMatch: true,
+    gatewayHomeCommunityId: outboundRequest.samlAttributes.homeCommunityId,
+    patientResource: patientResource,
+  };
+
+  return response;
+}
+
+function handlePatientErrorResponse({
+  jsonObj,
+  outboundRequest,
+  gateway,
+}: {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  jsonObj: any;
+  outboundRequest: OutboundPatientDiscoveryReq;
+  gateway: XCPDGateway;
+}): OutboundPatientDiscoveryResp {
+  const acknowledgementDetail =
+    jsonObj["soap:Envelope"]["soap:Body"]["PRPA_IN201306UV02"]["acknowledgement"][
+      "acknowledgementDetail"
+    ];
+  const issue = {
+    severity: "error",
+    code: acknowledgementDetail["code"]["@_code"],
+    details: {
+      text: acknowledgementDetail["text"]["#text"] || acknowledgementDetail["text"],
+    },
+  };
+  const operationOutcome: OperationOutcome = {
+    resourceType: "OperationOutcome",
+    id: outboundRequest.id,
+    issue: [issue],
+  };
+  const response: OutboundPatientDiscoveryResp = {
+    id: outboundRequest.id,
+    timestamp: outboundRequest.timestamp,
+    responseTimestamp: new Date().toISOString(),
+    gateway: gateway,
+    patientId: outboundRequest.patientId || "",
+    patientMatch: null,
+    operationOutcome: operationOutcome,
+  };
+  return response;
+}
+
+function handlePatientNoMatchResponse({
+  outboundRequest,
+  gateway,
+}: {
+  outboundRequest: OutboundPatientDiscoveryReq;
+  gateway: XCPDGateway;
+}): OutboundPatientDiscoveryResp {
+  const issue = {
+    severity: "information",
+    code: "not-found",
+    details: {
+      text: "NF",
+    },
+  };
+  const operationOutcome: OperationOutcome = {
+    resourceType: "OperationOutcome",
+    id: outboundRequest.id,
+    issue: [issue],
+  };
+  const response: OutboundPatientDiscoveryResp = {
+    id: outboundRequest.id,
+    timestamp: outboundRequest.timestamp,
+    responseTimestamp: new Date().toISOString(),
+    gateway: gateway,
+    patientId: outboundRequest.patientId || "",
+    patientMatch: false,
+    operationOutcome: operationOutcome,
+  };
+  return response;
+}
+
 export function processXCPDResponse({
   xmlStringOrError,
   outboundRequest,
@@ -17,28 +177,11 @@ export function processXCPDResponse({
   gateway: XCPDGateway;
 }): OutboundPatientDiscoveryResp {
   if (typeof xmlStringOrError === "object" && xmlStringOrError.error) {
-    const operationOutcome: OperationOutcome = {
-      resourceType: "OperationOutcome",
-      id: outboundRequest.id,
-      issue: [
-        {
-          severity: "error",
-          code: "http-error",
-          details: {
-            text: xmlStringOrError.error,
-          },
-        },
-      ],
-    };
-    return {
-      id: outboundRequest.id,
-      timestamp: outboundRequest.timestamp,
-      responseTimestamp: new Date().toISOString(),
-      gateway: gateway,
-      patientId: outboundRequest.patientId || "",
-      patientMatch: null,
-      operationOutcome: operationOutcome,
-    };
+    return handleHTTPErrorResponse({
+      httpError: xmlStringOrError.error,
+      outboundRequest,
+      gateway,
+    });
   } else {
     const parser = new XMLParser({
       ignoreAttributes: false,
@@ -61,105 +204,22 @@ export function processXCPDResponse({
       ]["@_code"];
 
     if (ack === "AA" && queryResponseCode === "OK") {
-      const subject1 =
-        jsonObj["soap:Envelope"]["soap:Body"]["PRPA_IN201306UV02"]["controlActProcess"]["subject"][
-          "registrationEvent"
-        ]["subject1"];
-
-      const addr = subject1["patient"]["patientPerson"]["addr"];
-      const addresses = [
-        {
-          line: [addr["streetAddressLine"]["#text"] || addr["streetAddressLine"]],
-          city: addr["city"]["#text"] || addr["city"],
-          state: addr["state"]["#text"] || addr["state"],
-          postalCode: addr["postalCode"]["#text"] || addr["postalCode"],
-          country: addr["country"]["#text"] || addr["country"],
-        },
-      ];
-
-      const patientResource = {
-        name: [
-          {
-            given: [subject1["patient"]["patientPerson"]["name"]["given"]],
-            family: subject1["patient"]["patientPerson"]["name"]["family"],
-          },
-        ],
-        gender: normalizeGender(
-          subject1["patient"]["patientPerson"]["administrativeGenderCode"]["@_code"]
-        ),
-        birthDate: subject1["patient"]["patientPerson"]["birthTime"]["@_value"],
-        address: addresses,
-      };
-
-      const response: OutboundPatientDiscoveryResp = {
-        id: outboundRequest.id,
-        timestamp: outboundRequest.timestamp,
-        responseTimestamp: new Date().toISOString(),
-        externalGatewayPatient: {
-          id: subject1["patient"]["id"]["@_extension"].toString(),
-          system: subject1["patient"]["id"]["@_root"].toString(),
-        },
-        gateway: gateway,
-        patientId: outboundRequest.patientId,
-        patientMatch: true,
-        gatewayHomeCommunityId: outboundRequest.samlAttributes.homeCommunityId,
-        patientResource: patientResource,
-      };
-
-      return response;
+      return handlePatientMatchResponse({
+        jsonObj,
+        outboundRequest,
+        gateway,
+      });
+    } else if (ack === "AE" || queryResponseCode === "AE") {
+      return handlePatientErrorResponse({
+        jsonObj,
+        outboundRequest,
+        gateway,
+      });
     } else {
-      const operationOutcome: OperationOutcome = {
-        resourceType: "OperationOutcome",
-        id: outboundRequest.id,
-        issue: [],
-      };
-      if (ack === "AE" || queryResponseCode === "AE") {
-        try {
-          const acknowledgementDetail =
-            jsonObj["soap:Envelope"]["soap:Body"]["PRPA_IN201306UV02"]["acknowledgement"][
-              "acknowledgementDetail"
-            ];
-          const issue = {
-            severity: "error",
-            code: acknowledgementDetail["code"]["@_code"],
-            details: {
-              text: acknowledgementDetail["text"]["#text"] || acknowledgementDetail["text"],
-            },
-          };
-          operationOutcome.issue.push(issue);
-        } catch (error) {
-          // Handle error if acknowledgementDetail is not found
-        }
-        const response: OutboundPatientDiscoveryResp = {
-          id: outboundRequest.id,
-          timestamp: outboundRequest.timestamp,
-          responseTimestamp: new Date().toISOString(),
-          gateway: gateway,
-          patientId: outboundRequest.patientId || "",
-          patientMatch: null,
-          operationOutcome: operationOutcome,
-        };
-        return response;
-      } else {
-        const issue = {
-          severity: "information",
-          code: "not-found",
-          details: {
-            text: "NF",
-          },
-        };
-        operationOutcome.issue.push(issue);
-        const response: OutboundPatientDiscoveryResp = {
-          id: outboundRequest.id,
-          timestamp: outboundRequest.timestamp,
-          responseTimestamp: new Date().toISOString(),
-          gateway: gateway,
-          patientId: outboundRequest.patientId || "",
-          patientMatch: false,
-          operationOutcome: operationOutcome,
-        };
-        return response;
-      }
+      return handlePatientNoMatchResponse({
+        outboundRequest,
+        gateway,
+      });
     }
   }
 }
