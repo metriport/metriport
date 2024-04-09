4c038b9715affc06c92636cc40a144b1ed494f88
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-04-09 11:53:49 -0500

    build: add download docs and consolidated util scripts
    Ref. metriport/metriport-internal#1040

--- packages/utils/src/customer-requests/get-consolidated.ts
+++ packages/utils/src/customer-requests/get-consolidated.ts
@@ -9,36 +9,33 @@ import axios from "axios";
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
 import fs from "fs";
-import https from "https";
 import { getCxData } from "../shared/get-cx-data";
 import { getFolderNameForOrg } from "./folder";
 
 dayjs.extend(duration);
 
 /**
- * Utility to generate Medical Records for a subset of a customer's patients.
+ * Utility to get and store consolidated data for a subset of a customer's patients.
  *
  * This will:
- *    - create a new folder in the "runs" dir for the customer, with "MR-Summaries" as prefix
- *    - get each patient's consolidated data as PDF
- *    - store each PDF in the "MR-Summaries"
+ *    - create a new folder in the "runs" dir for the customer, with "consolidated" as prefix
+ *    - get each patient's consolidated data as JSON
+ *    - store those on the local filesystem
  *
- * Update the respective env variables and run `npm run create-medical-records`
+ * Update the respective env variables and run `npm run get-consolidated`
  */
 
 /**
- * List of patients to generate Medical Records for.
+ * List of patients to get consolidated data for.
  */
 const patientIds: string[] = [];
-
-const conversionType = "pdf";
 
 const apiUrl = getEnvVarOrFail("API_URL");
 const cxId = getEnvVarOrFail("CX_ID");
 
 const endpointUrl = `${apiUrl}/internal/patient/consolidated`;
 
-const getDirName = (orgName: string) => `./runs/MR-Summaries/${getFolderNameForOrg(orgName)}`;
+const getDirName = (orgName: string) => `./runs/consolidated/${getFolderNameForOrg(orgName)}`;
 
 async function main() {
   console.log(
@@ -57,19 +54,19 @@ async function main() {
     const log = (msg: string) => console.log(`${new Date().toISOString()} [${patientId}] ${msg}`);
     const patientStartedAt = Date.now();
     try {
-      log(`>>> Generating MR...`);
-      const url = await getMedicalRecordURL(patientId);
-      if (!url) {
-        log(`No Medical Record URL, skipping...`);
+      log(`>>> Getting data...`);
+      const data = await getData(patientId);
+      if (!data) {
+        log(`No data returned, skipping...`);
         continue;
       }
-      const timeToMakeMR = Date.now() - patientStartedAt;
-      log(`... Got MR URL in ${timeToMakeMR} ms, downloading the MR file...`);
-      await downloadFile(url, patientId, dirName);
+      const timeToGetData = Date.now() - patientStartedAt;
+      log(`... Got data in ${timeToGetData} ms, storing it local...`);
+      storeData(data, patientId, dirName);
 
       log("... Patient is done.");
     } catch (error) {
-      log(`Error downloading MR: ${errorToString(error)}`);
+      log(`Error downloading data: ${errorToString(error)}`);
     }
   }
   const duration = Date.now() - startedAt;
@@ -77,29 +74,21 @@ async function main() {
   console.log(`>>> Done all patients in ${Date.now() - startedAt} ms / ${durationMin} min`);
 }
 
-async function getMedicalRecordURL(patientId: string): Promise<string | undefined> {
+async function getData(patientId: string): Promise<Bundle<DocumentReference> | undefined> {
   const params = new URLSearchParams({
     patientId,
     cxId,
-    conversionType,
   });
   const resp = await axios.get(`${endpointUrl}?${params}`);
+  if (!resp.data) return undefined;
   const bundle = resp.data.bundle as Bundle<DocumentReference>;
-  return bundle.entry?.[0]?.resource?.content?.[0]?.attachment?.url;
+  return bundle;
 }
 
-async function downloadFile(url: string, patientId: string, dirName: string) {
-  return new Promise(resolve => {
-    https.get(url, res => {
-      const fileStream = fs.createWriteStream(`./${dirName}/${patientId}.pdf`);
-      res.pipe(fileStream);
-
-      fileStream.on("finish", () => {
-        fileStream.close();
-        resolve("success");
-      });
-    });
-  });
+function storeData(data: Bundle<DocumentReference>, patientId: string, dirName: string): void {
+  const extension = "json";
+  const filename = `./${dirName}/${patientId}.${extension}`;
+  fs.writeFileSync(filename, JSON.stringify(data, null, 2));
 }
 
 main();
