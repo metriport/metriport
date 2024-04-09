6c518431ec90787556f0bfe2f0265a49fb9462d5
Jonah Kaye <jonah.arturo@gmail.com>
2024-04-09 13:21:46 -0500

    feat(girth): saml edge case with empty signatures
    Refs: #1667

---
+++ packages/core/src/external/saml/__tests__/saml-verify.test.ts
@@ -0,0 +1,37 @@
+import { verifySaml } from "../security/verify";
+import fs from "fs";
+import path from "path";
+import { IHE_STAGING_CERT } from "./constants";
+
+const xmlString = fs.readFileSync(path.join(__dirname, "./valid-saml.xml"), "utf8");
+describe("SAML Verification Edge Cases", () => {
+  it("should return true when verifying SAML signature with valid public key in XML", () => {
+    const publicCert = IHE_STAGING_CERT;
+
+    const result = verifySaml({ xmlString, publicCert });
+
+    expect(result).toBe(true);
+  });
+  it("should return false when verifying SAML signature with invalid public key in XML", () => {
+    const publicCert = "invalid cert";
+
+    const result = verifySaml({ xmlString, publicCert });
+
+    expect(result).toBe(false);
+  });
+
+  it("should return false when verifying SAML signature with invalid XML", () => {
+    const invalidXmlString = "invalid xml";
+
+    const result = verifySaml({ xmlString: invalidXmlString, publicCert: IHE_STAGING_CERT });
+
+    expect(result).toBe(false);
+  });
+  it("should return false when verifying SAML signature with empty xml", () => {
+    const emptyXmlString = "";
+
+    const result = verifySaml({ xmlString: emptyXmlString, publicCert: IHE_STAGING_CERT });
+
+    expect(result).toBe(false);
+  });
+});
