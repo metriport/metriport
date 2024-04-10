799218fc45880321959334f9403f28aa2e3e8104
Jonah Kaye <jonah.arturo@gmail.com>
2024-04-10 01:38:50 -0500

    feat(saml): test cert naming nit
    Refs: #1667

--- packages/core/src/external/saml/__tests__/saml-full-signing.test.ts
+++ packages/core/src/external/saml/__tests__/saml-full-signing.test.ts
@@ -3,57 +3,45 @@ import { createITI38SoapEnvelope } from "../xca/iti38-envelope";
 import { createITI39SoapEnvelope } from "../xca/iti39-envelope";
 import { verifySaml } from "../security/verify";
 import { signTimestamp, signEnvelope } from "../security/sign";
-import {
-  iti55BodyData,
-  iti38BodyData,
-  iti39BodyData,
-  IHE_STAGING_CERT,
-  IHE_STAGING_KEY,
-} from "./constants";
+import { iti55BodyData, iti38BodyData, iti39BodyData, TEST_CERT, TEST_KEY } from "./constants";
 
 describe("Full Saml Envelope Signing", () => {
   it("should sign and verify the XCPD SOAP envelope successfully", () => {
     const soapEnvelope = createITI5SoapEnvelope({
       bodyData: iti55BodyData,
-      publicCert: IHE_STAGING_CERT,
+      publicCert: TEST_CERT,
     });
 
-    const signedWithTimestamp = signTimestamp({ xml: soapEnvelope, privateKey: IHE_STAGING_KEY });
-    expect(
-      verifySaml({ xmlString: signedWithTimestamp, publicCert: IHE_STAGING_CERT })
-    ).toBeTruthy();
+    const signedWithTimestamp = signTimestamp({ xml: soapEnvelope, privateKey: TEST_KEY });
+    expect(verifySaml({ xmlString: signedWithTimestamp, publicCert: TEST_CERT })).toBeTruthy();
 
-    const signedEnvelope = signEnvelope({ xml: signedWithTimestamp, privateKey: IHE_STAGING_KEY });
-    expect(verifySaml({ xmlString: signedEnvelope, publicCert: IHE_STAGING_CERT })).toBeTruthy();
+    const signedEnvelope = signEnvelope({ xml: signedWithTimestamp, privateKey: TEST_KEY });
+    expect(verifySaml({ xmlString: signedEnvelope, publicCert: TEST_CERT })).toBeTruthy();
   });
 
   it("should sign and verify the ITI38 envelope successfully", () => {
     const soapEnvelope = createITI38SoapEnvelope({
       bodyData: iti38BodyData,
-      publicCert: IHE_STAGING_CERT,
+      publicCert: TEST_CERT,
     });
 
-    const signedWithTimestamp = signTimestamp({ xml: soapEnvelope, privateKey: IHE_STAGING_KEY });
-    expect(
-      verifySaml({ xmlString: signedWithTimestamp, publicCert: IHE_STAGING_CERT })
-    ).toBeTruthy();
+    const signedWithTimestamp = signTimestamp({ xml: soapEnvelope, privateKey: TEST_KEY });
+    expect(verifySaml({ xmlString: signedWithTimestamp, publicCert: TEST_CERT })).toBeTruthy();
 
-    const signedEnvelope = signEnvelope({ xml: signedWithTimestamp, privateKey: IHE_STAGING_KEY });
-    expect(verifySaml({ xmlString: signedEnvelope, publicCert: IHE_STAGING_CERT })).toBeTruthy();
+    const signedEnvelope = signEnvelope({ xml: signedWithTimestamp, privateKey: TEST_KEY });
+    expect(verifySaml({ xmlString: signedEnvelope, publicCert: TEST_CERT })).toBeTruthy();
   });
 
   it("should sign and verify the ITI39 envelope successfully", () => {
     const soapEnvelope = createITI39SoapEnvelope({
       bodyData: iti39BodyData,
-      publicCert: IHE_STAGING_CERT,
+      publicCert: TEST_CERT,
     });
 
-    const signedWithTimestamp = signTimestamp({ xml: soapEnvelope, privateKey: IHE_STAGING_KEY });
-    expect(
-      verifySaml({ xmlString: signedWithTimestamp, publicCert: IHE_STAGING_CERT })
-    ).toBeTruthy();
+    const signedWithTimestamp = signTimestamp({ xml: soapEnvelope, privateKey: TEST_KEY });
+    expect(verifySaml({ xmlString: signedWithTimestamp, publicCert: TEST_CERT })).toBeTruthy();
 
-    const signedEnvelope = signEnvelope({ xml: signedWithTimestamp, privateKey: IHE_STAGING_KEY });
-    expect(verifySaml({ xmlString: signedEnvelope, publicCert: IHE_STAGING_CERT })).toBeTruthy();
+    const signedEnvelope = signEnvelope({ xml: signedWithTimestamp, privateKey: TEST_KEY });
+    expect(verifySaml({ xmlString: signedEnvelope, publicCert: TEST_CERT })).toBeTruthy();
   });
 });
--- packages/core/src/external/saml/__tests__/saml-verify.test.ts
+++ packages/core/src/external/saml/__tests__/saml-verify.test.ts
@@ -1,12 +1,12 @@
 import { verifySaml } from "../security/verify";
 import fs from "fs";
 import path from "path";
-import { IHE_STAGING_CERT } from "./constants";
+import { TEST_CERT } from "./constants";
 
 const xmlString = fs.readFileSync(path.join(__dirname, "./valid-saml.xml"), "utf8");
 describe("SAML Verification Edge Cases", () => {
   it("should return true when verifying SAML signature with valid public key in XML", () => {
-    const publicCert = IHE_STAGING_CERT;
+    const publicCert = TEST_CERT;
 
     const result = verifySaml({ xmlString, publicCert });
 
@@ -23,14 +23,14 @@ describe("SAML Verification Edge Cases", () => {
   it("should return false when verifying SAML signature with invalid XML", () => {
     const invalidXmlString = "invalid xml";
 
-    const result = verifySaml({ xmlString: invalidXmlString, publicCert: IHE_STAGING_CERT });
+    const result = verifySaml({ xmlString: invalidXmlString, publicCert: TEST_CERT });
 
     expect(result).toBe(false);
   });
   it("should return false when verifying SAML signature with empty xml", () => {
     const emptyXmlString = "";
 
-    const result = verifySaml({ xmlString: emptyXmlString, publicCert: IHE_STAGING_CERT });
+    const result = verifySaml({ xmlString: emptyXmlString, publicCert: TEST_CERT });
 
     expect(result).toBe(false);
   });
