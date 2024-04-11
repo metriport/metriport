370ba86c33dbc3762473ccb3d31efe100c351ab6
Jonah Kaye <jonah.arturo@gmail.com>
2024-04-10 19:36:10 -0500

    feat(girth): dq lambda nits
    Refs: #1667

--- packages/lambdas/src/girth-outbound-document-query.ts
+++ packages/lambdas/src/girth-outbound-document-query.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 import * as Sentry from "@sentry/serverless";
 import { getSecret } from "@aws-lambda-powertools/parameters/secrets";
-import { OutboundDocumentQueryResp } from "@metriport/ihe-gateway-sdk";
+import {
+  OutboundDocumentQueryResp,
+  outboundDocumentQueryReqSchema,
+} from "@metriport/ihe-gateway-sdk";
 import { createAndSignBulkDQRequests } from "@metriport/core/external/saml/xca/iti38-envelope";
 import {
   processDQResponse,
@@ -10,11 +13,11 @@ import {
 import { sendSignedRequests } from "@metriport/core/external/carequality/ihe-gateway-v2/saml-client";
 import { getEnvVarOrFail } from "@metriport/core/util/env-var";
 import { Config } from "@metriport/core/util/config";
+import { capture } from "./shared/capture";
 
 const apiUrl = getEnvVarOrFail("API_URL");
-const patientDiscoveryUrl = `${apiUrl}/internal/carequality/patient-discovery/response`;
+const documentQueryResponseUrl = `${apiUrl}/internal/carequality/document-query/response`;
 
-// get secrets
 const privateKeySecretName = Config.getCQOrgPrivateKey();
 const privateKeyPasswordSecretName = Config.getCQOrgPrivateKeyPassword();
 const publicCertSecretName = Config.getCQOrgCertificate();
@@ -38,6 +41,20 @@ export const handler = Sentry.AWSLambda.wrapHandler(async (event: GirthDQRequest
     typeof certChain !== "string"
   ) {
     throw new Error("Failed to get secrets or one of the secrets is not a string.");
+  }
+
+  const xcpdRequest = outboundDocumentQueryReqSchema.safeParse(dqRequestsGirth);
+  if (!xcpdRequest.success) {
+    const msg = `Invalid request: ${xcpdRequest.error}`;
+    capture.error(msg, {
+      extra: {
+        context: `lambda.girth-outbound-patient-discovery`,
+        error: xcpdRequest.error,
+        patientId,
+        cxId,
+      },
+    });
+    throw Error;
   }
 
   const signedRequests = createAndSignBulkDQRequests({
@@ -73,6 +90,7 @@ export const handler = Sentry.AWSLambda.wrapHandler(async (event: GirthDQRequest
     }
   );
 
-  // send results to internal endpoint
-  await axios.post(patientDiscoveryUrl, results);
+  for (const result of results) {
+    await axios.post(documentQueryResponseUrl, result);
+  }
 });
