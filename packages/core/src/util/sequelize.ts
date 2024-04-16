4e22cc3f335acecc6fc0125442bab89ee60f3e7c
Jonah Kaye <jonah.arturo@gmail.com>
2024-04-15 20:41:27 -0400

    feat(girth): merging dev to branch
    Refs: #1667

--- packages/core/src/util/sequelize.ts
+++ packages/core/src/util/sequelize.ts
@@ -9,12 +9,52 @@ export const dbCredsSchema = z.object({
   port: z.number(),
   engine: z.literal("postgres"),
 });
+export type DbCreds = z.infer<typeof dbCredsSchema>;
+
+export const dbReadReplicaEndpointSchema = z.object({
+  host: z.string(),
+  port: z.number(),
+});
+export type DbReadReplicaEndpoint = z.infer<typeof dbReadReplicaEndpointSchema>;
+
+/**
+ * This function is used to initialize the readonly DB pool for queries that require the read replica.
+ *
+ * Note that this is a workaround while we don't have https://github.com/metriport/metriport-internal/issues/1174
+ * in place.
+ */
+export function initReadonlyDbPool(
+  dbCreds: string,
+  dbReadReplicaEndpoint: string,
+  poolOptions?: PoolOptions,
+  logging?: boolean
+) {
+  const dbCredsRaw = JSON.parse(dbCreds);
+  const parsedDbCreds = dbCredsSchema.parse(dbCredsRaw);
+
+  const dbReadReplicaEndpointRaw = JSON.parse(dbReadReplicaEndpoint);
+  const parsedDbReadReplicaEndpoint = dbReadReplicaEndpointSchema.parse(dbReadReplicaEndpointRaw);
+
+  parsedDbCreds.host = parsedDbReadReplicaEndpoint.host;
+  parsedDbCreds.port = parsedDbReadReplicaEndpoint.port;
+
+  return initDbPoolFromCreds(parsedDbCreds, poolOptions, logging);
+}
 
 /**
  * This function is used to initialize the DB pool for raw queries that can't rely on Models.
  */
-export function initDBPool(
-  dbCreds: string,
+export function initDbPool(dbCreds: string, poolOptions?: PoolOptions, logging?: boolean) {
+  const sqlDBCreds = JSON.parse(dbCreds);
+  const parsedDbCreds = dbCredsSchema.parse(sqlDBCreds);
+  return initDbPoolFromCreds(parsedDbCreds, poolOptions, logging);
+}
+
+/**
+ * This function is used to initialize the DB pool for raw queries that can't rely on Models.
+ */
+function initDbPoolFromCreds(
+  dbCreds: DbCreds,
   poolOptions: PoolOptions = {
     max: 5,
     min: 1,
@@ -23,20 +63,12 @@ export function initDBPool(
   },
   logging = false
 ) {
-  const sqlDBCreds = JSON.parse(dbCreds);
-  const parsedDbCreds = dbCredsSchema.parse(sqlDBCreds);
-
-  const sequelize = new Sequelize(
-    parsedDbCreds.dbname,
-    parsedDbCreds.username,
-    parsedDbCreds.password,
-    {
-      host: parsedDbCreds.host,
-      port: parsedDbCreds.port,
-      dialect: parsedDbCreds.engine,
-      pool: poolOptions,
-      logging,
-    }
-  );
+  const sequelize = new Sequelize(dbCreds.dbname, dbCreds.username, dbCreds.password, {
+    host: dbCreds.host,
+    port: dbCreds.port,
+    dialect: dbCreds.engine,
+    pool: poolOptions,
+    logging,
+  });
   return sequelize;
 }
