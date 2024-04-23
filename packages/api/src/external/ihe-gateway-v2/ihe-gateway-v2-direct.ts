a8a24e6a1ccdf8ce36fbf021eef40dd969d31ada
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2024-04-23 18:50:32 -0400

    fix(api): update ihegw direct
    Ref. metriport/metriport-internal#1667

--- packages/api/src/external/ihe-gateway-v2/ihe-gateway-v2-direct.ts
+++ packages/api/src/external/ihe-gateway-v2/ihe-gateway-v2-direct.ts
@@ -1,12 +1,21 @@
 import { getSecret } from "@aws-lambda-powertools/parameters/secrets";
-import { OutboundPatientDiscoveryReq } from "@metriport/ihe-gateway-sdk";
+import {
+  OutboundPatientDiscoveryReq,
+  OutboundDocumentQueryReq,
+  isSuccessfulOutboundDocQueryResponse,
+  OutboundDocumentRetrievalReq,
+} from "@metriport/ihe-gateway-sdk";
 import { uuidv7 } from "@metriport/core/util/uuid-v7";
 import { IHEGatewayV2 } from "@metriport/core/external/carequality/ihe-gateway-v2/ihe-gateway-v2";
-import { createSignSendProcessXCPDRequest } from "@metriport/core/external/carequality/ihe-gateway-v2/ihe-gateway-v2-logic";
+import {
+  createSignSendProcessXCPDRequest,
+  createSignSendProcessDQRequests,
+} from "@metriport/core/external/carequality/ihe-gateway-v2/ihe-gateway-v2-logic";
 import { emptyFunction } from "@metriport/shared";
 import { createOutboundPatientDiscoveryResp } from "../carequality/command/outbound-resp/create-outbound-patient-discovery-resp";
 import { processPostRespOutboundPatientDiscoveryResps } from "../carequality/process-subsequent-outbound-patient-discovery-resps";
-import { getPDResultStatus } from "../carequality/ihe-result";
+import { createOutboundDocumentQueryResp } from "../carequality/command/outbound-resp/create-outbound-document-query-resp";
+import { getPDResultStatus, getDQResultStatus } from "../carequality/ihe-result";
 import { Config } from "../../shared/config";
 
 export class IHEGatewayV2Direct extends IHEGatewayV2 {
@@ -70,5 +79,75 @@ export class IHEGatewayV2Direct extends IHEGatewayV2 {
         }).catch(emptyFunction);
       }
     }
+  }
+  async startDocumentQueryGatewayV2({
+    dqRequestsGatewayV2,
+    patientId,
+    cxId,
+  }: {
+    dqRequestsGatewayV2: OutboundDocumentQueryReq[];
+    patientId: string;
+    cxId: string;
+  }): Promise<void> {
+    // MAKE REUSABLE
+    const privateKeySecretName = Config.getCQOrgPrivateKey();
+    const privateKeyPasswordSecretName = Config.getCQOrgPrivateKeyPassword();
+    const publicCertSecretName = Config.getCQOrgCertificate();
+    const certChainSecretName = Config.getCQOrgCertificateIntermediate();
+
+    const privateKey = await getSecret(privateKeySecretName);
+    const privateKeyPassword = await getSecret(privateKeyPasswordSecretName);
+    const publicCert = await getSecret(publicCertSecretName);
+    const certChain = await getSecret(certChainSecretName);
+
+    if (
+      !privateKey ||
+      typeof privateKey !== "string" ||
+      !privateKeyPassword ||
+      typeof privateKeyPassword !== "string" ||
+      !publicCert ||
+      typeof publicCert !== "string" ||
+      !certChain ||
+      typeof certChain !== "string"
+    ) {
+      throw new Error("One or more required secrets are undefined.");
+    }
+
+    const results = await createSignSendProcessDQRequests({
+      dqRequestsGatewayV2,
+      publicCert: publicCert,
+      privateKey: privateKey,
+      privateKeyPassword: privateKeyPassword,
+      certChain: certChain,
+      patientId,
+      cxId,
+    });
+
+    let status = "failure";
+    for (const result of results) {
+      if (isSuccessfulOutboundDocQueryResponse(result)) {
+        status = getDQResultStatus({
+          docRefLength: result.documentReference?.length,
+        });
+      }
+      await createOutboundDocumentQueryResp({
+        id: uuidv7(),
+        requestId: result.id,
+        patientId: result.patientId,
+        status,
+        response: result,
+      });
+    }
+  }
+  async startDocumentRetrievalGatewayV2({
+    drRequestsGatewayV2,
+    patientId,
+    cxId,
+  }: {
+    drRequestsGatewayV2: OutboundDocumentRetrievalReq[];
+    patientId: string;
+    cxId: string;
+  }): Promise<void> {
+    throw new Error(`Method not implemented. ${drRequestsGatewayV2}, ${patientId}, ${cxId}`);
   }
 }
