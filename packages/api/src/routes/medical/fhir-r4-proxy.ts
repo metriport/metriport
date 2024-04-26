4e7b474f422c0194ce0a4965aeaf3e57e3ad91e8
Thomas Yopes <thomasyopes@Thomass-MacBook-Pro.local>
2024-04-26 09:30:51 -0700

    refactor(api): replacing solution with string-replace solution
    Ref: #1943

--- packages/api/src/routes/medical/fhir-r4-proxy.ts
+++ packages/api/src/routes/medical/fhir-r4-proxy.ts
@@ -5,24 +5,6 @@ import { Config } from "../../shared/config";
 import { Util } from "../../shared/util";
 import { asyncHandler, getCxIdOrFail } from "../util";
 
-export type fhirLink = {
-  relation: "self" | "next" | "previous";
-  url: string;
-};
-
-export type fhirEntry = {
-  fullUrl: string;
-  resource: {
-    // TODO: Incomplete type
-    resourceType: string;
-    id: string;
-  };
-  search: {
-    // TODO: Incomplete type
-    mode: string;
-  };
-};
-
 const { log } = Util.out(`FHIR-R4-PROXY`);
 
 const fhirRouter = (fhirServerUrl: string) =>
@@ -37,50 +19,9 @@ const fhirRouter = (fhirServerUrl: string) =>
     },
     //eslint-disable-next-line @typescript-eslint/no-unused-vars
     userResDecorator: function (proxyRes, proxyResData, userReq, userRes) {
-      const data = JSON.parse(proxyResData.toString("utf8"));
-      // v1 handle only known use case `/fhir/R4/{resource}`
-      if (data["resourceType"] === "Bundle" && "type" in data && data["type"] === "searchset") {
-        if ("link" in data) {
-          data["link"].forEach((link: fhirLink) => {
-            const defaultPaginationQueryParams = {
-              _getpages: data["id"],
-              _getpagesoffset: "0",
-              _count: "20",
-              _pretty: "true",
-              _bundletype: "searchset",
-            };
-            let queryParams = link.url.includes("?")
-              ? link.url
-                  .split("?")[1]
-                  .split("&")
-                  .reduce((a, v) => ({ ...a, [v.split("=")[0]]: v.split("=")[1] }), {})
-              : {};
-            queryParams = {
-              ...defaultPaginationQueryParams,
-              ...queryParams,
-            };
-            link.url =
-              Config.getApiUrl() +
-              userReq.baseUrl +
-              "?" +
-              Object.entries(queryParams)
-                .map(entry => `${entry[0]}=${entry[1]}`)
-                .join("&");
-          });
-        }
-        if ("entry" in data) {
-          data["entry"].forEach((entry: fhirEntry) => {
-            entry.fullUrl =
-              Config.getApiUrl() +
-              userReq.baseUrl +
-              "/" +
-              entry.resource.resourceType +
-              "/" +
-              entry.resource.id;
-          });
-        }
-      }
-      return JSON.stringify(data);
+      const data: string = proxyResData.toString("utf8");
+      const apiUrl = Config.getApiUrl();
+      return data.replaceAll(`${apiUrl}/oauth/fhir/${userReq.cxId}`, `${apiUrl}${userReq.baseUrl}`);
     },
   });
 
