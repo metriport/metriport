39690c5e78d22a1bf28a7c3de2ccff7adde13230
Jonah Kaye <jonah.arturo@gmail.com>
2024-04-26 08:28:12 -0700

    feat(patch): removing powertools from api
    Refs: #1667

--- packages/api/src/external/ihe-gateway-v2/ihe-gateway-v2-direct.ts
+++ packages/api/src/external/ihe-gateway-v2/ihe-gateway-v2-direct.ts
@@ -1,4 +1,3 @@
-import { getSecret } from "@aws-lambda-powertools/parameters/secrets";
 import { OutboundPatientDiscoveryReq } from "@metriport/ihe-gateway-sdk";
 import { uuidv7 } from "@metriport/core/util/uuid-v7";
 import { IHEGatewayV2 } from "@metriport/core/external/carequality/ihe-gateway-v2/ihe-gateway-v2";
@@ -22,15 +21,11 @@ export class IHEGatewayV2Direct extends IHEGatewayV2 {
     patientId: string;
     cxId: string;
   }): Promise<void> {
-    const privateKeySecretName = Config.getCQOrgPrivateKey();
-    const privateKeyPasswordSecretName = Config.getCQOrgPrivateKeyPassword();
-    const publicCertSecretName = Config.getCQOrgCertificate();
-    const certChainSecretName = Config.getCQOrgCertificateIntermediate();
+    const privateKey = Config.getCQOrgPrivateKey();
+    const privateKeyPassword = Config.getCQOrgPrivateKeyPassword();
+    const publicCert = Config.getCQOrgCertificate();
+    const certChain = Config.getCQOrgCertificateIntermediate();
 
-    const privateKey = await getSecret(privateKeySecretName);
-    const privateKeyPassword = await getSecret(privateKeyPasswordSecretName);
-    const publicCert = await getSecret(publicCertSecretName);
-    const certChain = await getSecret(certChainSecretName);
     if (
       !privateKey ||
       typeof privateKey !== "string" ||
