f3adb44b4d08cd60e279a8b0284cc28d0e1b6be1
Ramil Garipov <garipovramil93@hotmail.ca>
2024-05-06 11:08:12 -0700

    fix(api): minor fixes
    refs. metriport/metriport-internal#1603

--- packages/core/src/fhir-to-cda/cda-templates/clinical-document/custodian.ts
+++ packages/core/src/fhir-to-cda/cda-templates/clinical-document/custodian.ts
@@ -2,7 +2,7 @@ import { metriportOrganization } from "@metriport/shared";
 import { buildRepresentedOrganization } from "../commons";
 import { CDACustodian } from "../../cda-types/shared-types";
 
-export function buildCustodian(): CDACustodian | undefined {
+export function buildCustodian(): CDACustodian {
   const custodian = {
     assignedCustodian: {
       representedCustodianOrganization: buildRepresentedOrganization(metriportOrganization),
