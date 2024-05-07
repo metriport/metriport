27b90884d3ef8c595b06bd47a8c63bd3d9c288ae
Ramil Garipov <garipovramil93@hotmail.ca>
2024-05-07 11:16:31 -0700

    fix(fhir-to-cda): removed duplicate file size check
    refs. metriport/metriport-internal#1603

--- packages/core/src/shareback/cda-uploader.ts
+++ packages/core/src/shareback/cda-uploader.ts
@@ -1,13 +1,10 @@
 import { Organization } from "@medplum/fhirtypes";
 import { errorToString } from "@metriport/shared";
 import { createUploadFilePath, createUploadMetadataFilePath } from "../domain/document/upload";
-import { MAXIMUM_UPLOAD_FILE_SIZE } from "../external/aws/lambda-logic/document-uploader";
 import { S3Utils } from "../external/aws/s3";
-import BadRequestError from "../util/error/bad-request";
 import { MetriportError } from "../util/error/metriport-error";
 import { out } from "../util/log";
 import { XML_APP_MIME_TYPE } from "../util/mime";
-import { capture } from "../util/notifications";
 import { sizeInBytes } from "../util/string";
 import { createAndUploadDocumentMetadataFile } from "./create-and-upload-extrinsic-object";
 
@@ -30,8 +27,6 @@ export async function cdaDocumentUploaderHandler({
 }): Promise<void> {
   const { log } = out(`CDA Upload - cxId: ${cxId} - patientId: ${patientId}`);
   const fileSize = sizeInBytes(cdaBundle);
-  checkFileSizeRestrictions(fileSize, log);
-
   const s3Utils = new S3Utils(region);
   const metadataFileName = createUploadMetadataFilePath(cxId, patientId, docId);
   const destinationKey = createUploadFilePath(cxId, patientId, `${docId}.xml`);
@@ -72,15 +67,5 @@ export async function cdaDocumentUploaderHandler({
       medicalDocumentsBucket,
       destinationKey,
     });
-  }
-}
-
-function checkFileSizeRestrictions(fileSize: number, log: typeof console.log): void {
-  if (fileSize > MAXIMUM_UPLOAD_FILE_SIZE) {
-    const msg = `Uploaded file size exceeds the maximum allowed size of ${MAXIMUM_UPLOAD_FILE_SIZE} bytes`;
-    log(`${msg} - error ${fileSize}`);
-    const error = new BadRequestError(msg);
-    capture.message(msg, { extra: { error }, level: "info" });
-    throw error;
   }
 }
