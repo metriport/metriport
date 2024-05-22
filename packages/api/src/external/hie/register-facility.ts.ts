6fed643623dda99834244faf85a01c6ae26382ee
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2024-05-22 12:35:21 -0400

    feat(api): call hies async
    Ref. metriport/metriport-internal#1785

--- packages/api/src/external/hie/register-facility.ts.ts
+++ packages/api/src/external/hie/register-facility.ts.ts
@@ -43,10 +43,10 @@ export async function registerFacilityWithinHIEs(
   );
 
   // CAREQUALITY
-  await createOrUpdateInCq(cmdFacility, cxOrg, cqOboData, coordinates);
+  createOrUpdateInCq(cmdFacility, cxOrg, cqOboData, coordinates);
 
   // COMMONWELL
-  await createOrUpdateInCw(cmdFacility, facility.cwFacilityName, cxOrg, cxId);
+  createOrUpdateInCw(cmdFacility, facility.cwFacilityName, cxOrg, cxId);
 
   return cmdFacility;
 }
