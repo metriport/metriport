3a220e6d3a6cd803ac1c34c0e30af8bab7012344
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2024-05-23 09:15:33 -0400

    feat(api): sign off
    Ref. metriport/metriport-internal#1669
    
    Signed-off-by: Jorge Orta <36802074+Orta21@users.noreply.github.com>

--- packages/api/src/command/medical/document/document-conversion-status.ts
+++ packages/api/src/command/medical/document/document-conversion-status.ts
@@ -97,7 +97,6 @@ export async function calculateDocumentConversionStatus({
     });
 
     const conversionStatus = expectedPatient.data.documentQueryProgress?.convert?.status;
-
     if (conversionStatus === "completed") {
       processPatientDocumentRequest(
         cxId,
