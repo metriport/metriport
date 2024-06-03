0ce8f82eaa018bb60be687a0145902e72daa7478
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-06-03 16:38:06 -0500

    feat(api): add endpoint sync facility w/ HIEs
    Ref. metriport/metriport-internal#1839
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/api/src/routes/medical/internal-facility.ts
+++ packages/api/src/routes/medical/internal-facility.ts
@@ -1,14 +1,21 @@
+import { executeAsynchronously } from "@metriport/core/util/concurrency";
 import { Request, Response } from "express";
 import Router from "express-promise-router";
 import httpStatus from "http-status";
+import { z } from "zod";
+import { FacilityRegister } from "../../domain/medical/facility";
+import { registerFacilityWithinHIEs } from "../../external/hie/facility/register-facility";
+import { syncFacilityWithinHIEs } from "../../external/hie/facility/sync-facility";
 import { requestLogger } from "../helpers/request-logger";
-import { facilityOboDetailsSchema } from "./schemas/facility";
 import { getUUIDFrom } from "../schemas/uuid";
 import { asyncHandler } from "../util";
-import { registerFacilityWithinHIEs } from "../../external/hie/register-facility.ts";
-import { FacilityRegister } from "../../domain/medical/facility";
+import { facilityOboDetailsSchema } from "./schemas/facility";
 
 const router = Router();
+
+const facilityIdsSchema = z.object({
+  facilityIds: z.string().array(),
+});
 
 /** ---------------------------------------------------------------------------
  *
@@ -53,6 +60,32 @@ router.put(
     const facility = await registerFacilityWithinHIEs(cxId, facilityUpdate);
 
     return res.status(httpStatus.OK).json(facility);
+  })
+);
+
+/** ---------------------------------------------------------------------------
+ *
+ * POST /internal/facility/sync-with-hies
+ *
+ * Uses the existing data on the DB to create or update the facilities at the HIEs.
+ *
+ * @return The updated facility.
+ */
+router.post(
+  "/sync-with-hies",
+  requestLogger,
+  asyncHandler(async (req: Request, res: Response) => {
+    const cxId = getUUIDFrom("query", req, "cxId").orFail();
+    const { facilityIds } = facilityIdsSchema.parse(req.body);
+
+    await executeAsynchronously(
+      facilityIds,
+      async facilityId => {
+        syncFacilityWithinHIEs(cxId, facilityId);
+      },
+      { numberOfParallelExecutions: 5 }
+    );
+    return res.status(httpStatus.OK).json({ message: "Facilities synchronized with HIEs" });
   })
 );
 
