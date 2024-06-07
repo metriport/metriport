cb5d334f9816b774dd7397ca407eeb57eb6d283a
Thomas Yopes <thomasyopes@Thomass-MacBook-Pro.local>
2024-06-07 11:31:03 -0700

    feat(api): add indexes
    Ref: #1394
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MacBook-Pro.local>

--- packages/api/src/sequelize/migrations/2024-06-04_00_create-cw-patient-data.ts
+++ packages/api/src/sequelize/migrations/2024-06-04_00_create-cw-patient-data.ts
@@ -3,6 +3,12 @@ import type { Migration } from "..";
 import * as shared from "../migrations-shared";
 
 const tableName = "cw_patient_data";
+const cwTableName = tableName;
+const cwIndexToCreate = "cw_patient_data_id_index";
+const cwIndexToCreateFieldName = "id";
+const cqTableName = "cq_patient_data";
+const cqIndexToCreate = "cq_patient_data_id_index";
+const cqIndexToCreateFieldName = "id";
 
 // Use 'Promise.all' when changes are independent of each other
 export const up: Migration = async ({ context: queryInterface }) => {
@@ -28,11 +34,23 @@ export const up: Migration = async ({ context: queryInterface }) => {
       },
       { transaction, addVersion: true }
     );
+    await queryInterface.addIndex(cwTableName, {
+      name: cwIndexToCreate,
+      fields: [cwIndexToCreateFieldName],
+      transaction,
+    });
+    await queryInterface.addIndex(cqTableName, {
+      name: cqIndexToCreate,
+      fields: [cqIndexToCreateFieldName],
+      transaction,
+    });
   });
 };
 
 export const down: Migration = ({ context: queryInterface }) => {
   return queryInterface.sequelize.transaction(async transaction => {
+    await queryInterface.removeIndex(cqTableName, cqIndexToCreate, { transaction });
+    await queryInterface.removeIndex(cwTableName, cwIndexToCreate, { transaction });
     await queryInterface.dropTable(tableName, { transaction });
   });
 };
