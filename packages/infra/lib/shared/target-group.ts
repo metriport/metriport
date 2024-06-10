c9ee9a43df59ce8bc549dd6e0deed5e5643dd4bd
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-06-10 13:59:14 -0500

    build: remove alarms on TargetGroup reponse time
    Ref. metriport/metriport-internal#1040
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/infra/lib/shared/target-group.ts
+++ packages/infra/lib/shared/target-group.ts
@@ -1,4 +1,3 @@
-import { Duration } from "aws-cdk-lib";
 import { ComparisonOperator, TreatMissingData } from "aws-cdk-lib/aws-cloudwatch";
 import { SnsAction } from "aws-cdk-lib/aws-cloudwatch-actions";
 import { ApplicationTargetGroup, HttpCodeTarget } from "aws-cdk-lib/aws-elasticloadbalancingv2";
@@ -24,16 +23,6 @@ export function addDefaultMetricsToTargetGroup({
     metric: targetGroup.metrics.unhealthyHostCount(),
     alarmName: `${name}_UnhealthyRequestCount`,
     threshold: 1,
-    evaluationPeriods: 1,
-    comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
-    treatMissingData: TreatMissingData.NOT_BREACHING,
-    alarmAction,
-  });
-  addAlarmToMetric({
-    scope,
-    metric: targetGroup.metrics.targetResponseTime(),
-    alarmName: `${name}_TargetResponseTime`,
-    threshold: Duration.seconds(29).toSeconds(),
     evaluationPeriods: 1,
     comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
     treatMissingData: TreatMissingData.NOT_BREACHING,
