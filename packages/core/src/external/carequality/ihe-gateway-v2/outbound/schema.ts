f6e157894cf4b3b7a6cdd9c99e4c6bccc9bc829d
Jonah Kaye <jonah.arturo@gmail.com>
2024-06-11 15:30:58 -0400

    feat(ihe): filtering empty strings to undefined
    Refs: #1667
    Signed-off-by: Jonah Kaye <jonah.arturo@gmail.com>

--- packages/core/src/external/carequality/ihe-gateway-v2/outbound/schema.ts
+++ packages/core/src/external/carequality/ihe-gateway-v2/outbound/schema.ts
@@ -1,11 +1,17 @@
 import { z } from "zod";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const emptyToUndefined = (v: any) =>
+  typeof v === "string" && (v.length < 1 || v == undefined) ? undefined : v;
+
 export const schemaOrEmpty = <T extends z.ZodTypeAny>(schema: T) =>
-  z.union([schema, z.literal("")]);
+  z.union([schema, z.literal("")]).transform(emptyToUndefined);
 export const schemaOrArray = <T extends z.ZodTypeAny>(schema: T) =>
   z.union([schema, z.array(schema)]);
+
 export const schemaOrArrayOrEmpty = <T extends z.ZodTypeAny>(schema: T) =>
-  z.union([schema, z.array(schema), z.literal("")]);
+  z.union([schema, z.array(schema), z.literal("")]).transform(emptyToUndefined);
+
 export const textSchema = z.union([
   z.string(),
   z.object({
