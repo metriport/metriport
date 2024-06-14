8ad941c0fc03e38697304420fe722c78b6d0b9cb
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2024-06-14 10:12:06 -0400

    fix(api): remove logs
    Ref. metriport/metriport#1040
    
    Signed-off-by: Jorge Orta <36802074+Orta21@users.noreply.github.com>

--- packages/api/src/external/carequality/gateway/index.ts
+++ packages/api/src/external/carequality/gateway/index.ts
@@ -36,8 +36,6 @@ export async function gatherXCPDGateways(patient: Patient): Promise<Gateways> {
 
   const isEpicEnabled = await isEpicEnabledForCx(patient.cxId);
 
-  console.log("isEpicEnabled", isEpicEnabled);
-
   const nearbyOrgsWithUrls = await searchCQDirectoriesAroundPatientAddresses({
     patient,
     mustHaveXcpdLink: true,
@@ -53,9 +51,6 @@ export async function gatherXCPDGateways(patient: Patient): Promise<Gateways> {
   const allOrgsWithBasics = filteredOrgs.map(toBasicOrgAttributes);
   const orgsToSearch = filterCQOrgsToSearch(allOrgsWithBasics);
   const { v1Gateways, v2Gateways } = await cqOrgsToXCPDGateways(orgsToSearch, patient.cxId);
-
-  console.log("v1Gateways", v1Gateways.length, v1Gateways);
-  console.log("v2Gateways", v2Gateways.length, v2Gateways);
 
   return {
     v1Gateways,
