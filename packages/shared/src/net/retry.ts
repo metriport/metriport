0dc46e5d2246f5c0f013fc3429b10f2597609758
Jonah Kaye <jonah.arturo@gmail.com>
2024-06-20 09:27:39 -0400

    feat(retry): 3x initial delay on 429s
    Refs: #1667
    Signed-off-by: Jonah Kaye <jonah.arturo@gmail.com>

--- packages/shared/src/net/retry.ts
+++ packages/shared/src/net/retry.ts
@@ -3,8 +3,13 @@ import {
   defaultOptions as defaultRetryWithBackoffOptions,
   executeWithRetries,
   ExecuteWithRetriesOptions,
+  GetTimeToWaitParams,
+  defaultGetTimeToWait,
 } from "../common/retry";
 import { NetworkError, networkTimeoutErrors } from "./error";
+
+const tooManyRequestsStatus = 429;
+const tooManyRequestsMultiplier = 3;
 
 export type ExecuteWithNetworkRetriesOptions = Omit<
   ExecuteWithRetriesOptions<unknown>,
@@ -25,9 +30,51 @@ const defaultOptions: ExecuteWithNetworkRetriesOptions = {
     "ECONNREFUSED", // (Connection refused): No connection could be made because the target machine actively refused it. This usually results from trying to connect to a service that is inactive on the foreign host.
     "ECONNRESET", //  (Connection reset by peer): A connection was forcibly closed by a peer. This normally results from a loss of the connection on the remote socket due to a timeout or reboot. Commonly encountered via the http and net modules.
   ],
-  httpStatusCodesToRetry: [429], // 429 Too Many Requests
+  httpStatusCodesToRetry: [tooManyRequestsStatus], // 429 Too Many Requests
   retryOnTimeout: false,
 };
+
+function getHttpStatusFromError(error: unknown): number | undefined {
+  return axios.isAxiosError(error) ? error.response?.status : undefined;
+}
+
+function getHttpCodeFromError(error: unknown): string | undefined {
+  return axios.isAxiosError(error) ? error.code : undefined;
+}
+
+/**
+ * Custom getTimeToWait function for network retries. Has special handling for 429 Too Many Requests. Else regular backoff.
+ * @param initialDelay The initial delay in milliseconds.
+ * @param backoffMultiplier The backoff multiplier.
+ * @param attempt The current attempt number.
+ * @param maxDelay The maximum delay in milliseconds.
+ * @param error The error that occurred.
+ * @returns The time to wait before the next retry.
+ */
+function networkGetTimeToWait({
+  initialDelay,
+  backoffMultiplier,
+  attempt,
+  maxDelay,
+  error,
+}: GetTimeToWaitParams) {
+  const status = getHttpStatusFromError(error);
+  if (status === tooManyRequestsStatus) {
+    return defaultGetTimeToWait({
+      initialDelay: initialDelay * tooManyRequestsMultiplier,
+      backoffMultiplier,
+      attempt,
+      maxDelay,
+    });
+  } else {
+    return defaultGetTimeToWait({
+      initialDelay,
+      backoffMultiplier,
+      attempt,
+      maxDelay,
+    });
+  }
+}
 
 /**
  * Executes a function with retries and backoff with jitter. If the function throws a network
@@ -63,13 +110,14 @@ export async function executeWithNetworkRetries<T>(
   return executeWithRetries(fn, {
     ...actualOptions,
     shouldRetry: (_, error: unknown) => {
-      const networkCode = axios.isAxiosError(error) ? error.code : undefined;
-      const networkStatus = axios.isAxiosError(error) ? error.response?.status : undefined;
+      const networkCode = getHttpCodeFromError(error);
+      const networkStatus = getHttpStatusFromError(error);
       return (
         (networkCode && codesAsString.includes(networkCode)) ||
         (networkStatus && httpStatusCodesToRetry.includes(networkStatus)) ||
         false
       );
     },
+    getTimeToWait: networkGetTimeToWait,
   });
 }
