d5ecf28b8517e9bca5dcfbc4deddfc49cef461bb
Jonah Kaye <jonah.arturo@gmail.com>
2024-06-21 11:21:24 -0400

    feat(ihe): updating dq retries again
    Refs: #1667
    Signed-off-by: Jonah Kaye <jonah.arturo@gmail.com>

--- packages/core/src/external/carequality/ihe-gateway-v2/saml/saml-client.ts
+++ packages/core/src/external/carequality/ihe-gateway-v2/saml/saml-client.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 import * as AWS from "aws-sdk";
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
+import { NetworkError } from "@metriport/shared";
 import { SamlCertsAndKeys } from "./security/types";
 import { Config } from "../../../../util/config";
 import { log as getLog, out } from "../../../../util/log";
@@ -23,6 +24,20 @@ const { log } = out("Saml Client");
 const httpTimeoutPatientDiscovery = dayjs.duration({ seconds: 60 });
 const httpTimeoutDocumentQuery = dayjs.duration({ seconds: 120 });
 const httpTimeoutDocumentRetrieve = dayjs.duration({ seconds: 120 });
+
+const httpCodesToRetry: NetworkError[] = [
+  "ECONNREFUSED",
+  "ECONNRESET",
+  "ETIMEDOUT",
+  "ECONNABORTED",
+];
+
+const httpCodesToRetryPatientDiscovery: NetworkError[] = [...httpCodesToRetry];
+
+const httpCodesToRetryDocumentQuery: NetworkError[] = [...httpCodesToRetry, "ERR_BAD_RESPONSE"];
+
+const httpCodesToRetryDocumentRetrieve: NetworkError[] = [...httpCodesToRetry, "ERR_BAD_RESPONSE"];
+
 const initialDelay = dayjs.duration({ seconds: 3 });
 const maxPayloadSize = Infinity;
 let rejectUnauthorized = true;
@@ -102,13 +117,15 @@ export async function sendSignedXml({
           "Cache-Control": "no-cache",
         },
         httpsAgent: agent,
+        maxBodyLength: maxPayloadSize,
+        maxContentLength: maxPayloadSize,
       });
     },
     {
       initialDelay: initialDelay.asMilliseconds(),
       maxAttempts: isDq ? 4 : 3,
       //TODO: This introduces retry on timeout without needing to specify the http Code: https://github.com/metriport/metriport/pull/2285. Remove once PR is merged
-      httpCodesToRetry: ["ECONNREFUSED", "ECONNRESET", "ETIMEDOUT", "ECONNABORTED"],
+      httpCodesToRetry: isDq ? httpCodesToRetryDocumentQuery : httpCodesToRetryPatientDiscovery,
     }
   );
 
@@ -161,13 +178,7 @@ export async function sendSignedXmlMtom({
       initialDelay: initialDelay.asMilliseconds(),
       maxAttempts: 4,
       //TODO: This introduces retry on timeout without needing to specify the http Code: https://github.com/metriport/metriport/pull/2285. Remove once PR is merged
-      httpCodesToRetry: [
-        "ECONNREFUSED",
-        "ECONNRESET",
-        "ETIMEDOUT",
-        "ECONNABORTED",
-        "ERR_BAD_RESPONSE",
-      ],
+      httpCodesToRetry: httpCodesToRetryDocumentRetrieve,
       log: logger,
     }
   );
