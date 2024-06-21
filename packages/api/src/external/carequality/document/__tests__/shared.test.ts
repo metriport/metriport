b6d344dfb68f12353aa561090ba7119ab22217b6
Jonah Kaye <jonah.arturo@gmail.com>
2024-06-21 08:47:46 -0400

    feat(ihe): test
    Refs: #1667
    Signed-off-by: Jonah Kaye <jonah.arturo@gmail.com>

--- packages/api/src/external/carequality/document/__tests__/shared.test.ts
+++ packages/api/src/external/carequality/document/__tests__/shared.test.ts
@@ -2,7 +2,7 @@ import {
   makeDocumentReferenceWithMetriportId,
   makeDocumentReference,
 } from "./make-document-reference-with-metriport-id";
-import { containsMetriportId } from "../shared";
+import { containsMetriportId, containsDuplicateMetriportId } from "../shared";
 import { faker } from "@faker-js/faker";
 
 describe("filterDocRefsWithMetriportId", () => {
@@ -20,5 +20,19 @@ describe("filterDocRefsWithMetriportId", () => {
     expect(filteredDocRefs.length).toBe(2);
     expect(filteredDocRefs[0].metriportId).toBe(metriportId1);
     expect(filteredDocRefs[1].metriportId).toBe(metriportId2);
+  });
+
+  it("should filter out identical docRefs", async () => {
+    const seenMetriportIds = new Set<string>();
+    const docRefsWithMetriportId = [
+      makeDocumentReferenceWithMetriportId({ metriportId: "123" }),
+      makeDocumentReferenceWithMetriportId({ metriportId: "123" }),
+    ];
+
+    const deduplicatedDocRefsWithMetriportId = docRefsWithMetriportId.filter(
+      docRef => !containsDuplicateMetriportId(docRef, seenMetriportIds)
+    );
+
+    expect(deduplicatedDocRefsWithMetriportId.length).toBe(1);
   });
 });
