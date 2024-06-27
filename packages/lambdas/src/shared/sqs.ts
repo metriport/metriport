bd38096f96f5db8d1868e7115528f48b6d5ccc41
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-06-27 18:05:10 -0500

    refactor(lambda): remove unused SQSUtils code
    Ref. metriport/metriport-internal#1040
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/lambdas/src/shared/sqs.ts
+++ packages/lambdas/src/shared/sqs.ts
@@ -1,8 +1,7 @@
-import { SQSMessageAttributes, SQSRecord } from "aws-lambda";
+import { SQSMessageAttributes } from "aws-lambda";
 import * as AWS from "aws-sdk";
 import { SQS } from "aws-sdk";
 import { MessageBodyAttributeMap } from "aws-sdk/clients/sqs";
-import { capture } from "./capture";
 
 export class SQSUtils {
   public readonly _sqs: SQS;
@@ -17,58 +16,6 @@ export class SQSUtils {
 
   get sqs(): SQS {
     return this._sqs;
-  }
-
-  async sendToDLQ(message: SQSRecord) {
-    if (!this.dlqURL) throw new Error(`Missing dlqURL`);
-    await this.dequeue(message);
-    const sendParams: AWS.SQS.SendMessageRequest = {
-      MessageBody: message.body,
-      QueueUrl: this.dlqURL,
-      MessageAttributes: SQSUtils.attributesToSend(message.messageAttributes),
-    };
-    try {
-      console.log(`Sending message to DLQ: ${JSON.stringify(sendParams)}`);
-      await this.sqs.sendMessage(sendParams).promise();
-    } catch (err) {
-      console.log(`Failed to send message to queue: `, message, err);
-      capture.error(err, {
-        extra: { message, sendParams, context: "sendToDLQ" },
-      });
-    }
-  }
-
-  async reEnqueue(message: SQSRecord) {
-    await this.dequeue(message);
-    const sendParams = {
-      MessageBody: message.body,
-      QueueUrl: this.sourceQueueURL,
-      MessageAttributes: SQSUtils.attributesToSend(message.messageAttributes),
-      DelaySeconds: this.delayWhenRetryingSeconds, // wait at least that long before retrying
-    };
-    try {
-      await this.sqs.sendMessage(sendParams).promise();
-    } catch (err) {
-      console.log(`Failed to re-enqueue message: `, message, err);
-      capture.error(err, {
-        extra: { message, sendParams, context: "reEnqueue" },
-      });
-    }
-  }
-
-  async dequeue(message: SQSRecord) {
-    const deleteParams = {
-      QueueUrl: this.sourceQueueURL,
-      ReceiptHandle: message.receiptHandle,
-    };
-    try {
-      await this.sqs.deleteMessage(deleteParams).promise();
-    } catch (err) {
-      console.log(`Failed to remove message from queue: `, message, err);
-      capture.error(err, {
-        extra: { message, deleteParams, context: "dequeue" },
-      });
-    }
   }
 
   static attributesToSend(inboundMessageAttribs: SQSMessageAttributes): MessageBodyAttributeMap {
