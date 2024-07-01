66d7f8eada05f04d4bf35bb471660ea442edc7d6
Ramil Garipov <garipovramil93@hotmail.ca>
2024-07-01 08:06:27 -0700

    fix(core): improved MR summary gen to include missing obs data
    refs. metriport/metriport-internal#2302
    
    Signed-off-by: Ramil Garipov <garipovramil93@hotmail.ca>

--- packages/core/src/external/aws/lambda-logic/bundle-to-html.ts
+++ packages/core/src/external/aws/lambda-logic/bundle-to-html.ts
@@ -6,6 +6,7 @@ import {
   Coverage,
   DiagnosticReport,
   Encounter,
+  Extension,
   FamilyMemberHistory,
   Immunization,
   Location,
@@ -22,6 +23,7 @@ import {
 } from "@medplum/fhirtypes";
 import dayjs from "dayjs";
 import { uniqWith } from "lodash";
+import { BASE_64_EXTENSION_URL } from "../../fhir/shared/extensions/base-64-extension";
 
 const ISO_DATE = "YYYY-MM-DD";
 
@@ -588,8 +590,8 @@ function createDiagnosticReportsSection(
     false
   );
 
-  const hasNonAWEreports = nonAWEreports.length > 0;
   const reportsFromB64 = createDiagnosticReportsWithB64Notes(diagnosticReports, observations);
+  const noReportsPlaceholder = `<table><tbody><tr><td>No reports found</td></tr></tbody></table>`;
   return `
     <div id="reports" class="section">
       <div class="section-title">
@@ -597,15 +599,25 @@ function createDiagnosticReportsSection(
         <a href="#mr-header">&#x25B2; Back to Top</a>
       </div>
       <div class="section-content">
-        ${
-          hasNonAWEreports
-            ? nonAWEreports
-            : `<table><tbody><tr><td>No reports found</td></tr></tbody></table>`
-        }
-        ${reportsFromB64}
+        ${combineStrings(nonAWEreports, reportsFromB64, noReportsPlaceholder)}
       </div>
     </div>
   `;
+}
+
+function combineStrings(
+  stringA: string | undefined,
+  stringB: string | undefined,
+  placeholder = "default"
+) {
+  if (stringA && stringA.length > 0 && stringB && stringB.length > 0) {
+    return stringA + stringB;
+  } else if (stringA && stringA.length > 0) {
+    return stringA;
+  } else if (stringB && stringB.length > 0) {
+    return stringB;
+  }
+  return placeholder;
 }
 
 function createDiagnosticReportsWithB64Notes(
@@ -625,36 +637,49 @@ function createDiagnosticReportsWithB64Notes(
       return dayjs(timeA).isBefore(dayjs(timeB)) ? -1 : 1;
     })
     .map(report => {
-      const time = report.effectiveDateTime ?? report.effectivePeriod?.start;
+      let time = report.effectiveDateTime ?? report.effectivePeriod?.start;
+
       const obsRefs = report.result?.flatMap(obsRef => obsRef.reference || []);
       const notes =
         obsRefs
           ?.flatMap(obsRef => {
             const obsRefId = obsRef?.split("/")[1];
             if (!obsRefId) return;
-            const valueString = mappedObservations[obsRefId]?.valueString ?? "";
-            const cleanedUp = valueString.replace(/&#x3D;/g, "").trim();
-            let resString;
-            try {
-              resString = Buffer.from(cleanedUp, "base64").toString("utf-8");
-            } catch {
-              resString = cleanedUp;
+            const referencedObs = mappedObservations[obsRefId];
+            if (!time) {
+              time = referencedObs?.effectiveDateTime ?? referencedObs?.effectivePeriod?.start;
             }
-            return resString.replace(/root/g, "div").replace(/<\/content>/g, "</content><br/>");
+            const valueString = referencedObs?.valueString ?? "";
+            const cleanedUp = removeJunk(valueString);
+            const obsExtensions = referencedObs?.extension;
+            const resString = containsB64(obsExtensions)
+              ? Buffer.from(cleanedUp, "base64").toString("utf-8")
+              : cleanedUp;
+
+            return cleanUp(resString);
           })
           ?.join("<br/>") ?? "";
 
-      console.log(notes);
-      return { time, notes };
+      const formattedTime = dayjs(time).format(ISO_DATE);
+      return { time: formattedTime, notes };
     });
 
-  return `
-    ${
-      timedNotes
-        ? timedNotes.map(note => createDiagnosticTable(note)).join("")
-        : `<table><tbody><tr><td>No reports found</td></tr></tbody></table>`
-    }
-  `;
+  return `${timedNotes.map(note => createDiagnosticTable(note)).join("")}`;
+}
+
+function removeJunk(valueString: string): string {
+  return valueString.replace(/&#x3D;/g, "").trim();
+}
+
+function containsB64(extension: Extension[] | undefined) {
+  return extension?.some(ext => ext.url === BASE_64_EXTENSION_URL);
+}
+
+function cleanUp(valueString: string): string {
+  const htmlString = valueString.replace(/root/g, "div").replace(/<\/content>/g, "</content><br/>");
+  const noId = htmlString.replace(/<ID>.*?<\/ID>/g, "");
+  console.log(noId);
+  return noId;
 }
 
 function createDiagnosticTable(timedNote: { time: string | undefined; notes: string }) {
---
+++ packages/core/src/external/fhir/shared/extensions/base-64-extension.ts
@@ -0,0 +1,8 @@
+import { BASE_EXTENSION_URL } from "./base-extension";
+
+export const BASE_64_EXTENSION_URL = `${BASE_EXTENSION_URL}/is-base-64-encoded.json`;
+
+export const BASE_64_EXTENSION = {
+  url: BASE_64_EXTENSION_URL,
+  valueBoolean: true,
+};
