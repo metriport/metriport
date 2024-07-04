8c5c6cf21c820e920bf1026845706b66828a2fb8
Ramil Garipov <garipovramil93@hotmail.ca>
2024-07-04 10:14:00 -0700

    fix(core): adding checks for other b64 encoded mime types
    refs. metriport/metriport-internal#1925
    
    Signed-off-by: Ramil Garipov <garipovramil93@hotmail.ca>

--- packages/core/src/external/cda/remove-b64.ts
+++ packages/core/src/external/cda/remove-b64.ts
@@ -1,17 +1,16 @@
 import { toArray } from "@metriport/shared";
 import { XMLBuilder, XMLParser } from "fast-xml-parser";
-import { OCTET_MIME_TYPE, PDF_MIME_TYPE } from "../../util/mime";
+import { BASE64_ENCODED_MIME_TYPES } from "../../util/mime";
 
 const notesTemplateId = "2.16.840.1.113883.10.20.22.2.65";
 const b64Representation = "B64";
 
-const parser = new XMLParser({
-  ignoreAttributes: false,
-  attributeNamePrefix: "@_",
-  removeNSPrefix: true,
-});
-
 export function removeBase64PdfEntries(payloadRaw: string): string {
+  const parser = new XMLParser({
+    ignoreAttributes: false,
+    attributeNamePrefix: "@_",
+    removeNSPrefix: true,
+  });
   const json = parser.parse(payloadRaw);
 
   let removedEntry = 0;
@@ -28,10 +27,11 @@ export function removeBase64PdfEntries(payloadRaw: string): string {
         if (comp.section.entry) {
           //eslint-disable-next-line @typescript-eslint/no-explicit-any
           comp.section.entry = toArray(comp.section.entry).filter((entry: any) => {
-            const mediaType = entry.act?.text?.["@_mediaType"]?.toLowerCase();
+            const mediaType = entry.act?.text?.["@_mediaType"]?.trim().toLowerCase();
             if (
-              (mediaType === PDF_MIME_TYPE || mediaType === OCTET_MIME_TYPE) &&
-              entry.act.text["@_representation"]?.toLowerCase() === b64Representation.toLowerCase()
+              BASE64_ENCODED_MIME_TYPES.includes(mediaType) &&
+              entry.act.text["@_representation"]?.trim().toLowerCase() ===
+                b64Representation.toLowerCase()
             ) {
               removedEntry++;
               return false;
