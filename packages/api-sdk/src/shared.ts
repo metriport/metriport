7732da0e9a02da541ba1c978a3aaa5c838250c08
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-07-05 19:28:23 -0500

    refactor(sdk): option to use JWT w/ SDK
    Ref. metriport/metriport-internal#1935
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/api-sdk/src/shared.ts
+++ packages/api-sdk/src/shared.ts
@@ -4,6 +4,7 @@ import { z, ZodString } from "zod";
 export const BASE_ADDRESS = "https://api.metriport.com";
 export const BASE_ADDRESS_SANDBOX = "https://api.sandbox.metriport.com";
 export const API_KEY_HEADER = "x-api-key";
+export const CX_ID_HEADER = "x-cx-id";
 export const DEFAULT_AXIOS_TIMEOUT_MILLIS = 20_000;
 
 export const emptyStringToUndefined = (v: string | undefined | null) =>
