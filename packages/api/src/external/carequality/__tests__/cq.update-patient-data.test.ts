7f1096cad74f8771fb116caffbc38e8836e0f4cf
Thomas Yopes <thomasyopes@Thomass-MacBook-Pro.local>
2024-07-18 05:56:52 -0700

    feat(api): test improvements
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MacBook-Pro.local>

--- packages/api/src/external/carequality/__tests__/cq.update-patient-data.test.ts
+++ packages/api/src/external/carequality/__tests__/cq.update-patient-data.test.ts
@@ -2,11 +2,14 @@
 import { CQPatientDataModel } from "../models/cq-patient-data";
 import { LinkDemographicsHistory } from "@metriport/core/domain/patient-demographics";
 import { makeCqDataLink, makeLinksHistory, makeCqPatientData } from "./cq-patient-data";
+import { makePatient } from "../../../domain/medical/__tests__/patient";
 import { mockStartTransaction } from "../../../models/__tests__/transaction";
 import { CQLink } from "../cq-patient-data";
 import { createOrUpdateCQPatientData } from "../command/cq-patient-data/create-cq-data";
+import { updateCQPatientData } from "../command/cq-patient-data/update-cq-data";
 
 let cqPatientModel_findOne: jest.SpyInstance;
+let cqPatientModel_create: jest.SpyInstance;
 let cqPatientModel_update: jest.SpyInstance;
 
 const mockUpdate = jest.fn();
@@ -14,6 +17,9 @@ const mockUpdate = jest.fn();
 beforeEach(() => {
   mockStartTransaction();
   cqPatientModel_findOne = jest.spyOn(CQPatientDataModel, "findOne");
+  cqPatientModel_create = jest
+    .spyOn(CQPatientDataModel, "create")
+    .mockImplementation(async () => [1]);
   cqPatientModel_update = mockUpdate.mockImplementation(async () => [1]);
 });
 
@@ -21,29 +27,87 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
+const checkPatientCreateeWith = ({
+  newLinks,
+  newLinksDemographicsHistory,
+}: {
+  newLinks: CQLink[];
+  newLinksDemographicsHistory?: LinkDemographicsHistory;
+}) => {
+  expect(cqPatientModel_create).toHaveBeenCalledWith(
+    expect.objectContaining({
+      data: expect.objectContaining({
+        links: expect.objectContaining(newLinks),
+        ...(newLinksDemographicsHistory && {
+          linkDemographicsHistory: expect.objectContaining(newLinksDemographicsHistory),
+        }),
+      }),
+    })
+  );
+};
+
 const checkPatientUpdateWith = ({
   newLinks,
   newLinksDemographicsHistory,
 }: {
   newLinks: CQLink[];
-  newLinksDemographicsHistory: LinkDemographicsHistory;
+  newLinksDemographicsHistory?: LinkDemographicsHistory;
 }) => {
   expect(cqPatientModel_update).toHaveBeenCalledWith(
     expect.objectContaining({
       data: expect.objectContaining({
         links: expect.objectContaining(newLinks),
-        linkDemographicsHistory: expect.objectContaining(newLinksDemographicsHistory),
+        ...(newLinksDemographicsHistory && {
+          linkDemographicsHistory: expect.objectContaining(newLinksDemographicsHistory),
+        }),
       }),
     }),
     expect.anything()
   );
 };
 
-describe("update cq data", () => {
-  it("update patient with no link demographics", async () => {
+describe("create or update cq data", () => {
+  it("create patient data (links only)", async () => {
+    const patient = makePatient();
+    cqPatientModel_findOne.mockResolvedValueOnce(undefined);
+    const newLinks = [makeCqDataLink()];
+    await createOrUpdateCQPatientData({
+      id: patient.id,
+      cxId: patient.cxId,
+      cqLinks: newLinks,
+    });
+    checkPatientCreateeWith({
+      newLinks,
+    });
+  });
+  it("create patient data (links and links demographics)", async () => {
+    const patient = makePatient();
+    cqPatientModel_findOne.mockResolvedValueOnce(undefined);
+    const newLinks = [makeCqDataLink()];
+    const newLinksDemographicsHistory = makeLinksHistory();
+    const requestId = Object.keys(newLinksDemographicsHistory)[0];
+    const linksDemographics = Object.values(newLinksDemographicsHistory)[0];
+    await createOrUpdateCQPatientData({
+      id: patient.id,
+      cxId: patient.cxId,
+      cqLinks: newLinks,
+      requestLinksDemographics: {
+        requestId,
+        linksDemographics,
+      },
+    });
+    checkPatientCreateeWith({
+      newLinks,
+      newLinksDemographicsHistory,
+    });
+  });
+  it("update patient with existing initial link demographics (createOrUpdate)", async () => {
+    const existingCqLinks = [makeCqDataLink()];
+    const existingLinksDemographicsHistory = makeLinksHistory();
     const cqPatientData = makeCqPatientData({
       data: {
-        links: [],
+        links: existingCqLinks,
+        linkDemographicsHistory: existingLinksDemographicsHistory,
       },
     });
     cqPatientModel_findOne.mockResolvedValueOnce({
@@ -64,11 +128,14 @@ describe("update cq data", () => {
       },
     });
     checkPatientUpdateWith({
-      newLinks,
-      newLinksDemographicsHistory,
+      newLinks: [...existingCqLinks, ...newLinks],
+      newLinksDemographicsHistory: {
+        ...existingLinksDemographicsHistory,
+        ...newLinksDemographicsHistory,
+      },
     });
   });
-  it("update patient with existing initial link demographics", async () => {
+  it("update patient with existing initial link demographics (update)", async () => {
     const existingCqLinks = [makeCqDataLink()];
     const existingLinksDemographicsHistory = makeLinksHistory();
     const cqPatientData = makeCqPatientData({
@@ -85,7 +152,7 @@ describe("update cq data", () => {
     const newLinksDemographicsHistory = makeLinksHistory();
     const requestId = Object.keys(newLinksDemographicsHistory)[0];
     const linksDemographics = Object.values(newLinksDemographicsHistory)[0];
-    await createOrUpdateCQPatientData({
+    await updateCQPatientData({
       id: cqPatientData.id,
       cxId: cqPatientData.cxId,
       cqLinks: newLinks,
--- packages/api/src/external/commonwell/__tests__/cw.update-patient-data.test.ts
+++ packages/api/src/external/commonwell/__tests__/cw.update-patient-data.test.ts
@@ -2,24 +2,49 @@
 import { CwPatientDataModel } from "../models/cw-patient-data";
 import { LinkDemographicsHistory } from "@metriport/core/domain/patient-demographics";
 import { makeCwDataLink, makeLinksHistory, makeCwPatientData } from "./cw-patient-data";
+import { makePatient } from "../../../domain/medical/__tests__/patient";
 import { mockStartTransaction } from "../../../models/__tests__/transaction";
 import { CwLink } from "../cw-patient-data";
 import { createOrUpdateCwPatientData } from "../command/cw-patient-data/create-cw-data";
+import { updateCwPatientData } from "../command/cw-patient-data/update-cw-data";
 
-let cqPatientModel_findOne: jest.SpyInstance;
-let cqPatientModel_update: jest.SpyInstance;
+let cwPatientModel_findOne: jest.SpyInstance;
+let cwPatientModel_create: jest.SpyInstance;
+let cwPatientModel_update: jest.SpyInstance;
 
 const mockUpdate = jest.fn();
 
 beforeEach(() => {
   mockStartTransaction();
-  cqPatientModel_findOne = jest.spyOn(CwPatientDataModel, "findOne");
-  cqPatientModel_update = mockUpdate.mockImplementation(async () => [1]);
+  cwPatientModel_findOne = jest.spyOn(CwPatientDataModel, "findOne");
+  cwPatientModel_create = jest
+    .spyOn(CwPatientDataModel, "create")
+    .mockImplementation(async () => [1]);
+  cwPatientModel_update = mockUpdate.mockImplementation(async () => [1]);
 });
 
 afterEach(() => {
   jest.clearAllMocks();
 });
+
+const checkPatientCreateeWith = ({
+  newLinks,
+  newLinksDemographicsHistory,
+}: {
+  newLinks: CwLink[];
+  newLinksDemographicsHistory?: LinkDemographicsHistory;
+}) => {
+  expect(cwPatientModel_create).toHaveBeenCalledWith(
+    expect.objectContaining({
+      data: expect.objectContaining({
+        links: expect.objectContaining(newLinks),
+        ...(newLinksDemographicsHistory && {
+          linkDemographicsHistory: expect.objectContaining(newLinksDemographicsHistory),
+        }),
+      }),
+    })
+  );
+};
 
 const checkPatientUpdateWith = ({
   newLinks,
@@ -28,7 +53,7 @@ const checkPatientUpdateWith = ({
   newLinks: CwLink[];
   newLinksDemographicsHistory: LinkDemographicsHistory;
 }) => {
-  expect(cqPatientModel_update).toHaveBeenCalledWith(
+  expect(cwPatientModel_update).toHaveBeenCalledWith(
     expect.objectContaining({
       data: expect.objectContaining({
         links: expect.objectContaining(newLinks),
@@ -39,15 +64,52 @@ const checkPatientUpdateWith = ({
   );
 };
 
-describe("update cq data", () => {
-  it("update patient with no link demographics", async () => {
-    const cqPatientData = makeCwPatientData({
-      data: {
-        links: [],
+describe("create or update cw data", () => {
+  it("create patient data (links only)", async () => {
+    const patient = makePatient();
+    cwPatientModel_findOne.mockResolvedValueOnce(undefined);
+    const newLinks = [makeCwDataLink()];
+    await createOrUpdateCwPatientData({
+      id: patient.id,
+      cxId: patient.cxId,
+      cwLinks: newLinks,
+    });
+    checkPatientCreateeWith({
+      newLinks,
+    });
+  });
+  it("create patient data (links and links demographics)", async () => {
+    const patient = makePatient();
+    cwPatientModel_findOne.mockResolvedValueOnce(undefined);
+    const newLinks = [makeCwDataLink()];
+    const newLinksDemographicsHistory = makeLinksHistory();
+    const requestId = Object.keys(newLinksDemographicsHistory)[0];
+    const linksDemographics = Object.values(newLinksDemographicsHistory)[0];
+    await createOrUpdateCwPatientData({
+      id: patient.id,
+      cxId: patient.cxId,
+      cwLinks: newLinks,
+      requestLinksDemographics: {
+        requestId,
+        linksDemographics,
       },
     });
-    cqPatientModel_findOne.mockResolvedValueOnce({
-      ...cqPatientData,
+    checkPatientCreateeWith({
+      newLinks,
+      newLinksDemographicsHistory,
+    });
+  });
+  it("update patient with existing initial link demographics (createOrUpdate)", async () => {
+    const existingCwLinks = [makeCwDataLink()];
+    const existingLinksDemographicsHistory = makeLinksHistory();
+    const cwPatientData = makeCwPatientData({
+      data: {
+        links: existingCwLinks,
+        linkDemographicsHistory: existingLinksDemographicsHistory,
+      },
+    });
+    cwPatientModel_findOne.mockResolvedValueOnce({
+      ...cwPatientData,
       update: mockUpdate,
     });
     const newLinks = [makeCwDataLink()];
@@ -55,8 +117,8 @@ describe("update cq data", () => {
     const requestId = Object.keys(newLinksDemographicsHistory)[0];
     const linksDemographics = Object.values(newLinksDemographicsHistory)[0];
     await createOrUpdateCwPatientData({
-      id: cqPatientData.id,
-      cxId: cqPatientData.cxId,
+      id: cwPatientData.id,
+      cxId: cwPatientData.cxId,
       cwLinks: newLinks,
       requestLinksDemographics: {
         requestId,
@@ -64,30 +126,33 @@ describe("update cq data", () => {
       },
     });
     checkPatientUpdateWith({
-      newLinks,
-      newLinksDemographicsHistory,
+      newLinks: [...existingCwLinks, ...newLinks],
+      newLinksDemographicsHistory: {
+        ...existingLinksDemographicsHistory,
+        ...newLinksDemographicsHistory,
+      },
     });
   });
-  it("update patient with existing initial link demographics", async () => {
+  it("update patient with existing initial link demographics (update)", async () => {
     const existingCwLinks = [makeCwDataLink()];
     const existingLinksDemographicsHistory = makeLinksHistory();
-    const cqPatientData = makeCwPatientData({
+    const cwPatientData = makeCwPatientData({
       data: {
         links: existingCwLinks,
         linkDemographicsHistory: existingLinksDemographicsHistory,
       },
     });
-    cqPatientModel_findOne.mockResolvedValueOnce({
-      ...cqPatientData,
+    cwPatientModel_findOne.mockResolvedValueOnce({
+      ...cwPatientData,
       update: mockUpdate,
     });
     const newLinks = [makeCwDataLink()];
     const newLinksDemographicsHistory = makeLinksHistory();
     const requestId = Object.keys(newLinksDemographicsHistory)[0];
     const linksDemographics = Object.values(newLinksDemographicsHistory)[0];
-    await createOrUpdateCwPatientData({
-      id: cqPatientData.id,
-      cxId: cqPatientData.cxId,
+    await updateCwPatientData({
+      id: cwPatientData.id,
+      cxId: cwPatientData.cxId,
       cwLinks: newLinks,
       requestLinksDemographics: {
         requestId,
