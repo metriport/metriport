2b656c823bd93daa3aab4443f7a4836f6e9233df
Ramil Garipov <garipovramil93@hotmail.ca>
2024-07-26 11:37:43 -0700

    fix(core): fixed structure body inputs
    refs. metriport/metriport-internal#1951
    
    Signed-off-by: Ramil Garipov <garipovramil93@hotmail.ca>

--- packages/core/src/fhir-to-cda/cda-templates/clinical-document/structured-body.ts
+++ packages/core/src/fhir-to-cda/cda-templates/clinical-document/structured-body.ts
@@ -1,34 +1,29 @@
 import { Bundle } from "@medplum/fhirtypes";
-// import { buildAllergies } from "../components/allergies";
-// import { buildEncounters } from "../components/encounters";
-// import { buildFamilyHistory } from "../components/family-history";
-// import { buildImmunizations } from "../components/immunizations";
-// import { buildMedications } from "../components/medications";
-// import { buildMentalStatus } from "../components/mental-status";
-// import { buildNotes } from "../components/notes";
-// import { buildProblems } from "../components/problems";
-// import { buildResult } from "../components/results";
-// import { buildSocialHistory } from "../components/social-history";
-// import { buildVitalSigns } from "../components/vital-signs";
+import { buildAllergies } from "../components/allergies";
+import { buildEncounters } from "../components/encounters";
+import { buildFamilyHistory } from "../components/family-history";
+import { buildImmunizations } from "../components/immunizations";
+import { buildMedications } from "../components/medications";
+import { buildMentalStatus } from "../components/mental-status";
+import { buildProblems } from "../components/problems";
+import { buildSocialHistory } from "../components/social-history";
+import { buildVitalSigns } from "../components/vital-signs";
 import { buildAssessmentAndPlan } from "../components/assessment-and-plan";
 import { buildVariousNotesAndResults } from "../components/notes-and-results";
 
 export function buildStructuredBody(fhirBundle: Bundle): unknown {
-  // const notes = buildNotes(fhirBundle);
   const variousNotes = buildVariousNotesAndResults(fhirBundle);
   const structuredBodySections = [
-    // buildResult(fhirBundle),
-    // buildSocialHistory(fhirBundle),
-    // buildMentalStatus(fhirBundle),
-    // buildMedications(fhirBundle),
-    // buildProblems(fhirBundle),
-    // buildAllergies(fhirBundle),
-    // buildEncounters(fhirBundle),
-    // buildImmunizations(fhirBundle),
-    // buildVitalSigns(fhirBundle),
-    // buildFamilyHistory(fhirBundle),
+    buildSocialHistory(fhirBundle),
+    buildMentalStatus(fhirBundle),
+    buildMedications(fhirBundle),
+    buildProblems(fhirBundle),
+    buildAllergies(fhirBundle),
+    buildEncounters(fhirBundle),
+    buildImmunizations(fhirBundle),
+    buildVitalSigns(fhirBundle),
+    buildFamilyHistory(fhirBundle),
     buildAssessmentAndPlan(),
-    // ...(notes ? notes : []),
     ...(variousNotes ? variousNotes : []),
   ];
 
