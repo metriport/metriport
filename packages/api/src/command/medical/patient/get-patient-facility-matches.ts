f49f00c9eb0faee19fa67846ad9007d96f7463c1
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2024-07-26 12:07:05 -0400

    fix(api): update get facility matches
    Ref. metriport/metriport#2057
    
    Signed-off-by: Jorge Orta <36802074+Orta21@users.noreply.github.com>

--- packages/api/src/command/medical/patient/get-patient-facility-matches.ts
+++ packages/api/src/command/medical/patient/get-patient-facility-matches.ts
@@ -1,36 +1,20 @@
 import { Address } from "@metriport/core/domain/address";
 import { USState } from "@metriport/api-sdk";
-import { PatientNetworkLink } from "@metriport/commonwell-sdk";
-import { ISO_DATE } from "@metriport/shared/common/date";
-import { toTitleCase } from "@metriport/shared/common/string";
-import dayjs from "dayjs";
-import { PatientResource } from "@metriport/ihe-gateway-sdk";
+import { LinkDemographics } from "@metriport/core/domain/patient-demographics";
 import { CQLink } from "../../../external/carequality/cq-patient-data";
 import { CwLink } from "../../../external/commonwell/cw-patient-data";
 import { CQPatientDataModel } from "../../../external/carequality/models/cq-patient-data";
 import { CwPatientDataModel } from "../../../external/commonwell/models/cw-patient-data";
 import { CQDirectoryEntryModel } from "../../../external/carequality/models/cq-directory";
 import { orgDirectory } from "../../../external/commonwell/org-directory";
-import { mapGenderAtBirthFromCw } from "../../../external/commonwell/patient-conversion";
-import { mapGenderAtBirthFromCq } from "../../../external/carequality/patient-demographics";
-import { getCwPatientContactType } from "../../../external/commonwell/shared";
-import { getCqPatientContactType } from "../../../external/carequality/shared";
+import { patientResourceToNormalizedLinkDemographics as cqPatientResourceToNormalizedLinkDemographics } from "../../../external/carequality/patient-demographics";
+import { patientNetworkLinkToNormalizedLinkDemographics as cwPatientResourceToNormalizedLinkDemographics } from "../../../external/commonwell/patient-demographics";
 
 type PatientFacilityMatch = {
   name: string;
   oid: string;
   address: Partial<Address>;
-  patient?: PatientFacilityMatchDemo;
-};
-
-type PatientFacilityMatchDemo = {
-  firstName: string;
-  lastName: string;
-  dob: string;
-  gender: string;
-  address: Partial<Address>[];
-  emails?: string[];
-  phones?: string[];
+  patient?: LinkDemographics;
 };
 
 export async function getPatientFacilityMatches({
@@ -70,7 +54,7 @@ async function getCqFacilityMatches(cqLinks: CQLink[]): Promise<PatientFacilityM
       continue;
     }
 
-    const patientMatchDemo = cqPatientToPatientMatch(cqLink.patientResource);
+    const patientMatchDemo = cqPatientResourceToNormalizedLinkDemographics(cqLink.patientResource);
 
     patientFacilityMatches.push({
       name: cqFacility.name ?? "",
@@ -88,35 +72,6 @@ async function getCqFacilityMatches(cqLinks: CQLink[]): Promise<PatientFacilityM
   return patientFacilityMatches;
 }
 
-function cqPatientToPatientMatch(patient: PatientResource): PatientFacilityMatchDemo {
-  const givenNames = patient.name[0].given;
-  const firstName = givenNames.join(" ");
-
-  const familyName = patient.name[0].family;
-
-  const address = patient.address?.map(addr => ({
-    addressLine1: addr.line?.[0],
-    addressLine2: addr.line?.[1],
-    city: addr.city,
-    state: addr.state as USState,
-    zip: addr.postalCode,
-    country: addr.country,
-  }));
-
-  const emails = getCqPatientContactType(patient.telecom, "email");
-  const phones = getCqPatientContactType(patient.telecom, "phone");
-
-  return {
-    firstName: toTitleCase(firstName),
-    lastName: toTitleCase(familyName),
-    gender: mapGenderAtBirthFromCq(patient.gender),
-    dob: dayjs(patient.birthDate).format(ISO_DATE),
-    address: address ?? [],
-    emails,
-    phones,
-  };
-}
-
 async function getCwFacilityMatches(cwLinks: CwLink[]): Promise<PatientFacilityMatch[]> {
   const patientFacilityMatches = cwLinks.reduce((acc: PatientFacilityMatch[], curr) => {
     const patient = curr.patient;
@@ -131,7 +86,7 @@ async function getCwFacilityMatches(cwLinks: CwLink[]): Promise<PatientFacilityM
 
     const org = orgDirectory.find(org => org.oid === oid);
 
-    const patientMatchDemo = cwPatientToPatientMatch(patient);
+    const patientMatchDemo = cwPatientResourceToNormalizedLinkDemographics(patient);
 
     acc.push({
       name: display,
@@ -147,36 +102,4 @@ async function getCwFacilityMatches(cwLinks: CwLink[]): Promise<PatientFacilityM
   }, []);
 
   return patientFacilityMatches;
-}
-
-function cwPatientToPatientMatch(patient: PatientNetworkLink): PatientFacilityMatchDemo {
-  const givenNames = patient.details?.name[0].given;
-  const firstName = givenNames?.join(" ");
-
-  const familyName = patient.details.name[0].family;
-  const lastName = familyName.join(" ");
-
-  const address = patient.details.address.map(addr => ({
-    addressLine1: addr.line?.[0] ?? undefined,
-    addressLine2: addr.line?.[1] ?? undefined,
-    city: addr.city ?? undefined,
-    state: addr.state as USState,
-    zip: addr.zip ?? undefined,
-    country: addr.country ?? undefined,
-  }));
-
-  const emails = getCwPatientContactType(patient.details.telecom, "email");
-  const phones = getCwPatientContactType(patient.details.telecom, "phone");
-
-  return {
-    firstName: firstName ? toTitleCase(firstName) : "",
-    lastName: toTitleCase(lastName),
-    gender: patient.details.gender.display
-      ? mapGenderAtBirthFromCw(patient.details.gender.code)
-      : "UN",
-    dob: dayjs(patient.details.birthDate).format(ISO_DATE),
-    address,
-    emails,
-    phones,
-  };
 }
