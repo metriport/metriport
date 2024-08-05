0055df54bcaa60dc17d6da9ba312c165f1591338
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-08-05 16:20:14 -0500

    build: add header to `search-on-file.ts`
    refs. metriport/metriport-internal#2040
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/utils/src/search-on-file.ts
+++ packages/utils/src/search-on-file.ts
@@ -4,8 +4,18 @@ import fs from "fs";
 
 dayjs.extend(duration);
 
-const filename = "...";
+/**
+ * Utility to search for multiple strings on a single file.
+ *
+ * Useful when we have a very large file and we want to check it if contains certain values in
+ * there, like IDs, codes, etc.
+ *
+ * Set the variables below and run the script.
+ */
 
+// Full path of the file name where we want to search on.
+const filename = "...";
+// Array with the strings we're searching for.
 const whatToSearchFor: string[] = [];
 
 async function main() {
