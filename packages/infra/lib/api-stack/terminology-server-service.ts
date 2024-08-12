15f4c0219ee8c031ceac98a55e1578d71ffda8a6
Jonah Kaye <jonah.arturo@gmail.com>
2024-08-12 08:34:00 -0700

    feat(term): rw permissions + s3 bucket
    Refs: #1490
    Signed-off-by: Jonah Kaye <jonah.arturo@gmail.com>

--- packages/infra/lib/api-stack/terminology-server-service.ts
+++ packages/infra/lib/api-stack/terminology-server-service.ts
@@ -6,6 +6,7 @@ import * as ecr_assets from "aws-cdk-lib/aws-ecr-assets";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import { FargateService } from "aws-cdk-lib/aws-ecs";
 import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as s3 from "aws-cdk-lib/aws-s3";
 import { Bucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 import { EnvConfig } from "../../config/env-config";
@@ -47,15 +48,19 @@ export function createTermServerService(
 ): { service: FargateService; address: string } {
   const { cpu, memoryLimitMiB, taskCountMin, taskCountMax, maxExecutionTimeout } = settings();
 
-  // Create a new Amazon Elastic Container Service (ECS) cluster
+  const terminologyBucket = new s3.Bucket(stack, "TerminologyBucket", {
+    bucketName: "umls-terminology",
+    publicReadAccess: false,
+    encryption: s3.BucketEncryption.S3_MANAGED,
+    versioned: true,
+  });
+
   const cluster = new ecs.Cluster(stack, "TermServerCluster", { vpc, containerInsights: true });
 
-  // Create a Docker image and upload it to the Amazon Elastic Container Registry (ECR)
   const dockerImage = new ecr_assets.DockerImageAsset(stack, "TermServerImage", {
     directory: "../terminology",
   });
 
-  // Run some servers on fargate containers
   const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(
     stack,
     "TermServerFargateService",
@@ -81,6 +86,8 @@ export function createTermServerService(
     }
   );
   const serverAddress = fargateService.loadBalancer.loadBalancerDnsName;
+
+  terminologyBucket.grantReadWrite(fargateService.taskDefinition.taskRole);
 
   fargateService.loadBalancer.logAccessLogs(
     props.generalBucket,
@@ -110,22 +117,16 @@ export function createTermServerService(
   alarmAction && fargateMemoryAlarm.addAlarmAction(alarmAction);
   alarmAction && fargateMemoryAlarm.addOkAction(alarmAction);
 
-  // allow the NLB to talk to fargate
   fargateService.service.connections.allowFrom(
     ec2.Peer.ipv4(vpc.vpcCidrBlock),
     ec2.Port.allTraffic(),
     "Allow traffic from within the VPC to the service secure port"
   );
-  // TODO: #489 ain't the most secure, but the above code doesn't work as CDK complains we can't use the connections
-  // from the cluster created above, should be fine for now as it will only accept connections in the VPC
+
   fargateService.service.connections.allowFromAnyIpv4(ec2.Port.allTcp());
 
-  // This speeds up deployments so the tasks are swapped quicker.
-  // See for details: https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-target-groups.html#deregistration-delay
   fargateService.targetGroup.setAttribute("deregistration_delay.timeout_seconds", "17");
 
-  // This also speeds up deployments so the health checks have a faster turnaround.
-  // See for details: https://docs.aws.amazon.com/elasticloadbalancing/latest/network/target-group-health-checks.html
   fargateService.targetGroup.configureHealthCheck({
     healthyThresholdCount: 2,
     interval: Duration.seconds(10),
