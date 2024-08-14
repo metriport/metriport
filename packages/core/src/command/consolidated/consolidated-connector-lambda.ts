94e2aa4def796897a6ead21a9e48817ec2894fca
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-08-14 09:53:01 -0500

    Revert "Merge pull request #2603 from metriport/1319-make-consolidated-lambda--name-required"
    This reverts commit 130f8d4bb431f3f1a99c734564116d0e1b01d0e5, reversing
    changes made to 521695497b968a85a45fa909a92d2220b1e46fbc.
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/core/src/command/consolidated/consolidated-connector-lambda.ts
+++ packages/core/src/command/consolidated/consolidated-connector-lambda.ts
@@ -9,6 +9,7 @@ import {
   ConsolidatedDataResponse,
   ConsolidatedPatientDataRequest,
 } from "./consolidated-connector";
+import { ConsolidatedDataConnectorLocal } from "./consolidated-connector-local";
 
 dayjs.extend(duration);
 
@@ -20,8 +21,8 @@ export type ConsolidatedRequestLambda = ConsolidatedPatientDataRequest & {
 export const TIMEOUT_CALLING_CONVERTER_LAMBDA = dayjs.duration(15, "minutes").add(2, "seconds");
 
 export class ConsolidatedDataConnectorLambda implements ConsolidatedDataConnector {
-  readonly lambdaName: string;
-  readonly lambdaClient: AWS.Lambda;
+  lambdaName: string | undefined;
+  lambdaClient: AWS.Lambda;
   constructor() {
     const region = Config.getAWSRegion();
     this.lambdaName = Config.getFHIRtoBundleLambdaName();
@@ -31,6 +32,13 @@ export class ConsolidatedDataConnectorLambda implements ConsolidatedDataConnecto
   async execute(
     params: ConsolidatedDataRequestSync | ConsolidatedDataRequestAsync
   ): Promise<ConsolidatedDataResponse> {
+    // TODO 1319 Remove this once the first release has been shipped and the lambda name is requires
+    if (!this.lambdaName) {
+      const bucketName = Config.getMedicalDocumentsBucketName();
+      const apiURL = Config.getApiUrl();
+      const connector = new ConsolidatedDataConnectorLocal(bucketName, apiURL);
+      return connector.execute(params);
+    }
     const result = await this.lambdaClient
       .invoke({
         FunctionName: this.lambdaName,
