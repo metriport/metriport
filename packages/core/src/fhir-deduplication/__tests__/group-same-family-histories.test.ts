1c2ea4e349026b926a3ea6d3036fa6a7d9ffcc09
Ramil Garipov <garipovramil93@hotmail.ca>
2024-08-23 16:08:18 +0300

    fix(core): fixed fam mem hist tests
    refs. metriport/metriport-internal#2148
    
    Signed-off-by: Ramil Garipov <garipovramil93@hotmail.ca>

--- packages/core/src/fhir-deduplication/__tests__/group-same-family-histories.test.ts
+++ packages/core/src/fhir-deduplication/__tests__/group-same-family-histories.test.ts
@@ -24,30 +24,34 @@ describe("groupSameFamilyMemberHistories", () => {
     expect(famMemberHistsMap.size).toBe(1);
   });
 
-  it("correctly groups duplicate famMemHistorys based on relationship", () => {
+  it("does not group if dob is different or missing in one of the resources", () => {
     famMemHistory.bornDate = "1930-01-01";
-    const { famMemberHistsMap } = groupSameFamilyMemberHistories([famMemHistory, famMemHistory2]);
-    expect(famMemberHistsMap.size).toBe(1);
+    let result = groupSameFamilyMemberHistories([famMemHistory, famMemHistory2]);
+    expect(result.famMemberHistsMap.size).toBe(2);
+
+    famMemHistory.bornDate = "1930-01-01";
+    famMemHistory2.bornDate = "1932-02-02";
+    result = groupSameFamilyMemberHistories([famMemHistory, famMemHistory2]);
+    expect(result.famMemberHistsMap.size).toBe(2);
   });
 
-  it("correctly groups duplicate famMemHistorys based on relationship", () => {
-    famMemHistory.bornDate = "1930-01-01";
-
+  it("does not group the same relationship if names are different", () => {
     famMemHistory.name = "Stephen";
-    famMemHistory2.name = "Steven";
+    famMemHistory2.name = "Randy";
     const { famMemberHistsMap } = groupSameFamilyMemberHistories([famMemHistory, famMemHistory2]);
-    expect(famMemberHistsMap.size).toBe(1);
-    const masterFather = famMemberHistsMap.values().next().value as FamilyMemberHistory;
-    expect(masterFather.name).toEqual("Steven");
-    expect(masterFather.bornDate).toEqual("1930-01-01");
+    expect(famMemberHistsMap.size).toBe(2);
+    const mapValues = famMemberHistsMap.values();
+    const father1 = mapValues.next().value as FamilyMemberHistory;
+    expect(father1.name).toEqual("Stephen");
+    const father2 = mapValues.next().value as FamilyMemberHistory;
+    expect(father2.name).toEqual("Randy");
   });
 
-  it("correctly groups duplicate famMemHistorys based on relationship", () => {
-    famMemHistory.relationship = naturalBrotherCode;
+  it("does not group people with the same name if the relationship is different", () => {
     famMemHistory2.relationship = naturalBrotherCode;
 
-    famMemHistory.name = "Harry";
-    famMemHistory2.name = "Gary";
+    famMemHistory.name = "Steven";
+    famMemHistory2.name = "Steven";
 
     const { famMemberHistsMap } = groupSameFamilyMemberHistories([famMemHistory, famMemHistory2]);
     expect(famMemberHistsMap.size).toBe(2);
