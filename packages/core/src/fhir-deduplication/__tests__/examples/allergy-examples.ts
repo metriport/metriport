32c36f1c2edbfd6d27ad2fa80ce9d143ec9247d2
Ramil Garipov <garipovramil93@hotmail.ca>
2024-08-28 02:43:06 +0300

    fix(core): removed export
    refs. metriport/metriport-internal#799
    
    Signed-off-by: Ramil Garipov <garipovramil93@hotmail.ca>

--- packages/core/src/fhir-deduplication/__tests__/examples/allergy-examples.ts
+++ packages/core/src/fhir-deduplication/__tests__/examples/allergy-examples.ts
@@ -56,13 +56,3 @@ export const substanceCashew = {
     },
   ],
 };
-
-export const noKnownAllergiesSubstance = {
-  coding: [
-    {
-      system: "urn:oid:2.16.840.1.113883.4.296",
-      code: "900388",
-      display: "No Known Allergies",
-    },
-  ],
-};
