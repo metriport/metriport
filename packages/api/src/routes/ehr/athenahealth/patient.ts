32e098397592be3e6d643d8b87e37a28ae0b0a91
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2024-08-28 21:54:28 -0700

    feat(athena): adding get consolidated
    Ref: #2115
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/api/src/routes/ehr/athenahealth/patient.ts
+++ packages/api/src/routes/ehr/athenahealth/patient.ts
@@ -1,19 +1,23 @@
 import Router from "express-promise-router";
 import httpStatus from "http-status";
 import { Request, Response } from "express";
+import { getPatientOrFail } from "../../../command/medical/patient/get-patient";
+import { getConsolidatedPatientData } from "../../../command/medical/patient/consolidated-get";
 import { getPatient } from "../../../external/ehr/athenahealth/command/get-patient";
 import { requestLogger } from "../../helpers/request-logger";
 import { asyncHandler, getCxIdOrFail, getFrom } from "../../util";
 import { getAuthorizationToken } from "../../util";
+import { getResourcesQueryParam } from "../../medical/schemas/fhir";
+import { parseISODate } from "../../../shared/date";
 
 const router = Router();
 
 /**
- * GET /ehr/athena/patient/:id
+ * GET /ehr/athenahealth/patient/:id
  *
- * Retrieves the organization with the specified OID from CommonWell.
- * @param req.params.oid The OID of the organization to retrieve.
- * @returns Returns the organization with the specified OID.
+ * Tries to retrieve the matching Metriport patient
+ * @param req.params.id The ID of AthenaHealth Patient.
+ * @returns Metriport Patient if found.
  */
 router.get(
   "/:id",
@@ -28,6 +32,40 @@ router.get(
       athenaPatientId,
     });
     return res.status(httpStatus.OK).json(patient);
+  })
+);
+
+/**
+ * GET /ehr/athenahealth/patient/:id/consolidated
+ *
+ * Returns a patient's consolidated data.
+ *
+ * @param req.cxId The customer ID.
+ * @param req.param.id The  ID of the Metriport Patient whose data is to be returned.
+ * @param req.query.resources Optional comma-separated list of resources to be returned.
+ * @param req.query.dateFrom Optional start date that resources will be filtered by (inclusive).
+ * @param req.query.dateTo Optional end date that resources will be filtered by (inclusive).
+ * @return Metriport Patient's consolidated data.
+ */
+router.get(
+  "/:id/consolidated",
+  requestLogger,
+  asyncHandler(async (req: Request, res: Response) => {
+    const cxId = getCxIdOrFail(req);
+    const metriportPatientId = getFrom("params").orFail("id", req);
+    const resources = getResourcesQueryParam(req);
+    const dateFrom = parseISODate(getFrom("query").optional("dateFrom", req));
+    const dateTo = parseISODate(getFrom("query").optional("dateTo", req));
+    const patient = await getPatientOrFail({ id: metriportPatientId, cxId });
+
+    const data = await getConsolidatedPatientData({
+      patient,
+      resources,
+      dateFrom,
+      dateTo,
+    });
+
+    return res.json(data);
   })
 );
 
