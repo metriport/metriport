38b33a27c0999cef584db1a26461c42f1089b707
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-09-02 17:40:44 -0500

    feat(util): add Observation to the report dedup script +
    - add Observation to the report dedup script
    - create shared column types and functions
    
    Ref. metriport/metriport-internal#2179
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/utils/src/fhir-dedup/medication-administration.ts
+++ packages/utils/src/fhir-dedup/medication-administration.ts
@@ -2,6 +2,16 @@ import { MedicationAdministration, Resource } from "@medplum/fhirtypes";
 import fs from "fs";
 import { Dictionary } from "lodash";
 import { csvSeparator, safeCsv } from "./csv";
+import {
+  category0Columns,
+  effectiveDateTimeColumns,
+  effectivePeriodColumns,
+  getCategory,
+  getEffectiveDateTime,
+  getEffectivePeriod,
+  getNotes,
+  notesColumns,
+} from "./resource-props";
 
 // Lots of fields were not mapped, see https://www.hl7.org/fhir/R4/medicationadministration.html if you want to add them
 const columns = [
@@ -21,12 +31,8 @@ const columns = [
   "perfActorRef0_d",
   "perfActorRef1_o",
   "perfActorRef1_d",
-  "eDateTime_o",
-  "eDateTime_d",
-  "ePeriod_start_o",
-  "ePeriod_start_d",
-  "ePeriod_end_o",
-  "ePeriod_end_d",
+  ...effectiveDateTimeColumns,
+  ...effectivePeriodColumns,
   "dosage_text_o",
   "dosage_text_d",
   "dosage_route_code0_o",
@@ -43,16 +49,7 @@ const columns = [
   "dosage_dose_value_d",
   "dosage_dose_unit_o",
   "dosage_dose_unit_d",
-  "cat_code0_o",
-  "cat_code0_d",
-  "cat_disp0_o",
-  "cat_disp0_d",
-  "cat_code1_o",
-  "cat_code1_d",
-  "cat_disp1_o",
-  "cat_disp1_d",
-  "cat_text_o",
-  "cat_text_d",
+  ...category0Columns,
   "reason0_code0_o",
   "reason0_code0_d",
   "reason0_disp0_o",
@@ -77,10 +74,7 @@ const columns = [
   "reasonRef0_d",
   "reasonRef1_o",
   "reasonRef1_d",
-  "note0_txt_o",
-  "note0_txt_d",
-  "note1_txt_o",
-  "note1_txt_d",
+  ...notesColumns,
   "id_d",
 ] as const;
 type Columns = (typeof columns)[number];
@@ -137,9 +131,6 @@ function toCsv(resource: MedicationAdministration, siblings: MedicationAdministr
   const perfActorRef0_o = resource.performer?.[0]?.actor?.reference ?? "";
   const perfActorRef1_o = resource.performer?.[1]?.actor?.reference ?? "";
   const ctxtRef_o = resource.context?.reference ?? "";
-  const eDateTime_o = resource.effectiveDateTime ?? "";
-  const ePeriod_start_o = resource.effectivePeriod?.start ?? "";
-  const ePeriod_end_o = resource.effectivePeriod?.end ?? "";
   const dosage = resource.dosage;
   const dosage_text_o = dosage?.text ?? "";
   const dosage_route_code0_o = dosage?.route?.coding?.[0]?.code ?? "";
@@ -149,12 +140,6 @@ function toCsv(resource: MedicationAdministration, siblings: MedicationAdministr
   const dosage_route_text_o = dosage?.route?.text ?? "";
   const dosage_dose_value_o = dosage?.dose?.value ?? "";
   const dosage_dose_unit_o = dosage?.dose?.unit ?? "";
-  const cat = resource.category;
-  const cat_code0_o = cat?.coding?.[0].code ?? "";
-  const cat_disp0_o = cat?.coding?.[0]?.display ?? "";
-  const cat_code1_o = cat?.coding?.[1]?.code ?? "";
-  const cat_disp1_o = cat?.coding?.[1]?.display ?? "";
-  const cat_text_o = cat?.text ?? "";
   const reason0 = resource.reasonCode?.[0];
   const reason0_code0_o = reason0?.coding?.[0]?.code ?? "";
   const reason0_disp0_o = reason0?.coding?.[0]?.display ?? "";
@@ -169,8 +154,6 @@ function toCsv(resource: MedicationAdministration, siblings: MedicationAdministr
   const reason1_text_o = reason1?.text ?? "";
   const reasonRef0_o = resource.reasonReference?.[0]?.reference ?? "";
   const reasonRef1_o = resource.reasonReference?.[1]?.reference ?? "";
-  const note0_txt_o = resource.note?.[0]?.text ?? "";
-  const note1_txt_o = resource.note?.[1]?.text ?? "";
 
   const status_d = sibling?.status ?? "";
   const medRef_d = sibling?.medicationReference?.reference ?? "";
@@ -179,9 +162,6 @@ function toCsv(resource: MedicationAdministration, siblings: MedicationAdministr
   const perfActorRef0_d = sibling?.performer?.[0]?.actor?.reference ?? "";
   const perfActorRef1_d = sibling?.performer?.[1]?.actor?.reference ?? "";
   const ctxtRef_d = sibling?.context?.reference ?? "";
-  const eDateTime_d = sibling?.effectiveDateTime ?? "";
-  const ePeriod_start_d = sibling?.effectivePeriod?.start ?? "";
-  const ePeriod_end_d = sibling?.effectivePeriod?.end ?? "";
   const dosage_d = sibling?.dosage;
   const dosage_text_d = dosage_d?.text ?? "";
   const dosage_route_code0_d = dosage_d?.route?.coding?.[0]?.code ?? "";
@@ -191,11 +171,6 @@ function toCsv(resource: MedicationAdministration, siblings: MedicationAdministr
   const dosage_route_text_d = dosage_d?.route?.text ?? "";
   const dosage_dose_value_d = dosage_d?.dose?.value ?? "";
   const dosage_dose_unit_d = dosage_d?.dose?.unit ?? "";
-  const cat_code0_d = sibling?.category?.coding?.[0]?.code ?? "";
-  const cat_disp0_d = sibling?.category?.coding?.[0]?.display ?? "";
-  const cat_code1_d = sibling?.category?.coding?.[1]?.code ?? "";
-  const cat_disp1_d = sibling?.category?.coding?.[1]?.display ?? "";
-  const cat_text_d = sibling?.category?.text ?? "";
   const reason0_code0_d = sibling?.reasonCode?.[0]?.coding?.[0]?.code ?? "";
   const reason0_disp0_d = sibling?.reasonCode?.[0]?.coding?.[0]?.display ?? "";
   const reason0_code1_d = sibling?.reasonCode?.[0]?.coding?.[1]?.code ?? "";
@@ -208,8 +183,11 @@ function toCsv(resource: MedicationAdministration, siblings: MedicationAdministr
   const reason1_text_d = sibling?.reasonCode?.[1]?.text ?? "";
   const reasonRef0_d = sibling?.reasonReference?.[0]?.reference ?? "";
   const reasonRef1_d = sibling?.reasonReference?.[1]?.reference ?? "";
-  const note0_txt_d = sibling?.note?.[0]?.text ?? "";
-  const note1_txt_d = sibling?.note?.[1]?.text ?? "";
+
+  const category = getCategory(resource, sibling);
+  const effectiveDateTime = getEffectiveDateTime(resource, sibling);
+  const effectivePeriod = getEffectivePeriod(resource, sibling);
+  const notes = getNotes(resource, sibling);
 
   const res: Record<Columns, string | number | boolean> = {
     id_o: resource.id ?? "",
@@ -228,12 +206,8 @@ function toCsv(resource: MedicationAdministration, siblings: MedicationAdministr
     perfActorRef0_d,
     perfActorRef1_o,
     perfActorRef1_d,
-    eDateTime_o,
-    eDateTime_d,
-    ePeriod_start_o,
-    ePeriod_start_d,
-    ePeriod_end_o,
-    ePeriod_end_d,
+    ...effectiveDateTime,
+    ...effectivePeriod,
     dosage_text_o,
     dosage_text_d,
     dosage_route_code0_o,
@@ -250,16 +224,7 @@ function toCsv(resource: MedicationAdministration, siblings: MedicationAdministr
     dosage_dose_value_d,
     dosage_dose_unit_o,
     dosage_dose_unit_d,
-    cat_code0_o,
-    cat_code0_d,
-    cat_disp0_o,
-    cat_disp0_d,
-    cat_code1_o,
-    cat_code1_d,
-    cat_disp1_o,
-    cat_disp1_d,
-    cat_text_o,
-    cat_text_d,
+    ...category,
     reason0_code0_o,
     reason0_code0_d,
     reason0_disp0_o,
@@ -284,10 +249,7 @@ function toCsv(resource: MedicationAdministration, siblings: MedicationAdministr
     reasonRef0_d,
     reasonRef1_o,
     reasonRef1_d,
-    note0_txt_o,
-    note0_txt_d,
-    note1_txt_o,
-    note1_txt_d,
+    ...notes,
     id_d: sibling?.id ?? "",
   };
   return Object.values(res).map(safeCsv).join(csvSeparator);
