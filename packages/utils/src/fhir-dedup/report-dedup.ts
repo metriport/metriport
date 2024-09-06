8022b4197047c9d5d34142a730bda416444a0ee0
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-09-05 19:46:51 -0500

    fix(util): missing refs on dedup script to account for all refs
    Ref. metriport/metriport-internal#2179
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/utils/src/fhir-dedup/report-dedup.ts
+++ packages/utils/src/fhir-dedup/report-dedup.ts
@@ -1,11 +1,9 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 // keep that ^ on top
-import { BundleEntry, EncounterDiagnosis, Reference, Resource } from "@medplum/fhirtypes";
+import { BundleEntry, Resource } from "@medplum/fhirtypes";
 import { getEnvVarOrFail } from "@metriport/core/util/env-var";
 import { out } from "@metriport/core/util/log";
-import dayjs from "dayjs";
-import duration from "dayjs/plugin/duration";
 import fs from "fs";
 import { groupBy } from "lodash";
 import { ellapsedTimeAsStr } from "../shared/duration";
@@ -13,7 +11,6 @@ import { initRunsFolder } from "../shared/folder";
 import { processAllergyIntolerance } from "./allergy-intolerance";
 import { processCondition } from "./condition";
 import { processCoverage } from "./coverage";
-import { csvSeparator } from "./csv";
 import { processDiagnosticReport } from "./diagnostic-report";
 import { processEncounter } from "./encounter";
 import { processFamilyMemberHistory } from "./family-member-history";
@@ -33,8 +30,7 @@ import { processOrganization } from "./organization";
 import { processPractitioner } from "./practitioner";
 import { processProcedure } from "./procedure";
 import { processRelatedPerson } from "./related-person";
-
-dayjs.extend(duration);
+import { validateReferences } from "./validate-references";
 
 /**
  * Utility to report differences between two FHIR bundles.
@@ -67,7 +63,6 @@ const bucketName = getEnvVarOrFail("MEDICAL_DOCUMENTS_BUCKET_NAME");
 
 const startedAt = Date.now();
 const dirName = buildGetDirPathInside(`dedup-reports`, startedAt);
-const missingRefsFileName = "missing-refs.csv";
 
 async function main() {
   initRunsFolder();
@@ -163,114 +158,6 @@ async function main() {
   console.log(``);
 
   console.log(`>>> Done in ${ellapsedTimeAsStr(startedAt)}`);
-}
-
-/**
- * Go through each resource, find other resources it references, and check if they are present in the bundle.
- */
-function validateReferences(resources: Resource[], dirName: string): boolean {
-  const resourceMap = new Map<string, Resource>();
-  resources.forEach(r => resourceMap.set(r.id ?? "", r));
-
-  const missingRefs: { resource: Resource; missingRefs: string[] }[] = [];
-  resources.forEach(r => {
-    const localMissingRefs = findMissingRefs(r, resourceMap);
-    if (localMissingRefs.length) missingRefs.push({ resource: r, missingRefs: localMissingRefs });
-  });
-
-  if (missingRefs.length) {
-    const outputFileName = dirName + "/" + missingRefsFileName;
-    const header = ["resource", "missing-refs..."].join(csvSeparator);
-    fs.writeFileSync(outputFileName, header + "\n");
-    console.log(
-      `>>> Found ${missingRefs.length} missing references! Check the file ${outputFileName} for details.`
-    );
-    const lines = missingRefs
-      .map(entry => {
-        const resource = `${entry.resource.resourceType}/${entry.resource.id}`;
-        const missingRefs = entry.missingRefs.join(csvSeparator);
-        return resource + csvSeparator + missingRefs;
-      })
-      .join("\n");
-    fs.writeFileSync(outputFileName, lines, { flag: "a+" });
-    return false;
-  }
-  return true;
-}
-
-function findMissingRefs(resource: Resource, resourceMap: Map<string, Resource>): string[] {
-  const missingRefs: string[] = [];
-  const refs = findRefs(resource);
-  refs.forEach(ref => {
-    if (!resourceMap.has(ref.split("/")[1] ?? "")) {
-      missingRefs.push(ref);
-    }
-  });
-  return missingRefs;
-}
-
-function findRefs<T extends Resource>(resource: T): string[] {
-  if ("result" in resource && resource.result) {
-    const results = resource.result;
-    if (Array.isArray(results)) {
-      return results.flatMap(item => item.reference ?? []);
-    }
-  }
-
-  if ("diagnosis" in resource && resource.diagnosis) {
-    if (resource.resourceType === "Encounter") {
-      const diagnoses = resource.diagnosis as EncounterDiagnosis[];
-      return diagnoses.flatMap(d => d.condition?.reference ?? []);
-    }
-  }
-
-  if ("author" in resource && resource.author) {
-    if (resource.resourceType === "Composition") {
-      return resource.author.flatMap(a => a.reference ?? []);
-    }
-  }
-
-  if ("section" in resource && resource.section) {
-    return resource.section.flatMap(s => s.entry?.flatMap(e => e.reference ?? []) ?? []);
-  }
-
-  if ("location" in resource && resource.location) {
-    if (Array.isArray(resource.location)) {
-      if (resource.location.length < 1) return [];
-      //eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const withRefs = (resource.location as any[]).filter(
-        //eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (l: any) => "reference" in l
-      ) as Reference<Resource>[];
-      return withRefs.flatMap(l => l.reference ?? []);
-    }
-    if ("reference" in resource.location) {
-      return [resource.location.reference ?? ""];
-    }
-  }
-
-  if ("participant" in resource && resource.participant) {
-    if (resource.resourceType === "Encounter") {
-      return resource.participant.flatMap(p => p.individual?.reference ?? []);
-    }
-  }
-
-  if ("performer" in resource && resource.performer) {
-    if (Array.isArray(resource.performer)) {
-      return resource.performer.flatMap(p => {
-        if ("reference" in p) return p.reference ?? [];
-        if ("actor" in p) return p.actor?.reference ?? [];
-        return [];
-      });
-    }
-    return [resource.performer.reference ?? []].flat();
-  }
-
-  if ("payor" in resource && resource.payor) {
-    return resource.payor.flatMap(p => p.reference ?? []);
-  }
-
-  return [];
 }
 
 main();
