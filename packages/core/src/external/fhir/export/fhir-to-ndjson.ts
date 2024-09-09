9d0220372ec9308d05986e116cbb063d98b32b52
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-09-09 18:14:53 -0500

    feat(util): script to generate NDJSON files
    Ref. metriport/metriport-internal#2179
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

---
+++ packages/core/src/external/fhir/export/fhir-to-ndjson.ts
@@ -0,0 +1,21 @@
+import { Bundle, Resource } from "@medplum/fhirtypes";
+
+/**
+ * Converts a bundle into a NDJSON string - New line Delimited JSON.
+ * Thats essentially text file with a JSON string from each resource per line.
+ *
+ * This is used convert exiting FHIR bundles into a string that can be saved to a file
+ * and later on used to import data on FHIR servers.
+ *
+ * @see https://build.fhir.org/ig/HL7/bulk-data/export.html for info on how to obtain a data export
+ * from a FHIR server.
+ */
+export function bundleToNdjson(bundle: Bundle<Resource>): string {
+  return (
+    bundle.entry
+      ?.map(entry => {
+        return JSON.stringify(entry.resource);
+      })
+      .join("\n") ?? ""
+  );
+}
