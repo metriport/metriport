825df257c9fc692f0c4d7e4e5b3bc50dd61218e6
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2024-09-13 10:54:05 -0700

    feat(athena): wip
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/shared/src/domain/address/zip.ts
+++ packages/shared/src/domain/address/zip.ts
@@ -1,7 +1,16 @@
+const STRICT_ZIP_LENGTH = 5;
+
 function isValidZipCode(zipCode: string): boolean {
   if (!zipCode) return false;
   if (zipCode.length === 0) return false;
   if (!zipCode.match(/^[0-9-]+$/)) return false;
+  return true;
+}
+
+function isValidZipCodeStrict(zipCode: string): boolean {
+  const baseValid = isValidZipCode(zipCode);
+  if (!baseValid) return false;
+  if (zipCode.length !== STRICT_ZIP_LENGTH) return false;
   return true;
 }
 
@@ -13,15 +22,16 @@ function isValidZipCode(zipCode: string): boolean {
  * TODO: Refactor, so `normalize` simply returns a zip of a certain format and returns undefined if it cannot,
  * while `validate` would throw an error.
  */
-export function normalizeZipCode(zipCode: string): string {
-  const zipOrUndefined = normalizeZipCodeSafe(zipCode);
+export function normalizeZipCode(zipCode: string, isStrict = false): string {
+  const zipOrUndefined = normalizeZipCodeSafe(zipCode, isStrict);
   if (!zipOrUndefined) throw new Error("Invalid Zip.");
   return zipOrUndefined;
 }
 
-export function normalizeZipCodeSafe(zipCode: string): string | undefined {
+export function normalizeZipCodeSafe(zipCode: string, isStrict = false): string | undefined {
   const trimmedZip = zipCode.trim();
-  if (!isValidZipCode(trimmedZip)) return undefined;
+  const validationFunc = isStrict ? isValidZipCodeStrict : isValidZipCode;
+  if (!validationFunc(trimmedZip)) return undefined;
   if (trimmedZip.includes("-") && trimmedZip.trim().length === 9) return trimmedZip.slice(0, 4);
   if (trimmedZip.trim().length === 8) return trimmedZip.slice(0, 4);
   return trimmedZip.slice(0, 5);
