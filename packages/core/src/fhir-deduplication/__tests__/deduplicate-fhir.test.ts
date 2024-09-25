3bb7fae8362fdc06ee7f46bc325da5e5f52b4604
Jonah Kaye <jonah.arturo@gmail.com>
2024-09-25 11:30:34 -0400

    feat(dedup): adding back test
    Refs: #2569
    Signed-off-by: Jonah Kaye <jonah.arturo@gmail.com>

--- packages/core/src/fhir-deduplication/__tests__/deduplicate-fhir.test.ts
+++ packages/core/src/fhir-deduplication/__tests__/deduplicate-fhir.test.ts
@@ -336,7 +336,6 @@ describe("deduplicateFhir", () => {
 
     bundle.entry = entries;
     bundle = deduplicateFhir(bundle);
-    console.log(JSON.stringify(bundle, null, 2));
     expect(bundle.entry?.length).toBe(3);
     const resComposition = findCompositionResource(bundle);
     expect(resComposition).not.toEqual(undefined);
