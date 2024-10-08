a93bd5fefeafc7181879848eb221b956ad52f73f
Thomas Yopes <thomasyopes@Thomass-MacBook-Pro.local>
2024-10-08 14:36:44 -0700

    feat(patient-import): more locations
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MacBook-Pro.local>

--- packages/lambdas/src/cw-session-management.ts
+++ packages/lambdas/src/cw-session-management.ts
@@ -1,4 +1,5 @@
 import { getSecret } from "@aws-lambda-powertools/parameters/secrets";
+import { MetriportError, AdditionalInfo } from "@metriport/shared";
 import { CodeChallengeFromSecretManager } from "@metriport/core/domain/auth/code-challenge/code-challenge-on-secrets";
 import { CookieManagerOnSecrets } from "@metriport/core/domain/auth/cookie-management/cookie-manager-on-secrets";
 import { makeS3Client } from "@metriport/core/external/aws/s3";
@@ -8,7 +9,6 @@ import {
   SessionManagementConfig,
 } from "@metriport/core/external/commonwell/management/session";
 import { base64ToBuffer } from "@metriport/core/util/base64";
-import { AdditionalInfo, MetriportError } from "@metriport/core/util/error/metriport-error";
 import * as Sentry from "@sentry/serverless";
 
 import * as playwright from "playwright-aws-lambda";
