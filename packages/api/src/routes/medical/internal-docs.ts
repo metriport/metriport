ae8f0857a1a0f0199eecfbaa0d50af1448aa68d1
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2024-10-09 16:13:23 -0700

    feat(patient-import): typo
    Ref: #2330
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/api/src/routes/medical/internal-docs.ts
+++ packages/api/src/routes/medical/internal-docs.ts
@@ -358,7 +358,7 @@ router.post(
     const facilityId = getFrom("query").optional("facilityId", req);
     const forcePatientDiscovery = getFromQueryAsBoolean("forcePatientDiscovery", req);
     const cqManagingOrgName = getFrom("query").optional("cqManagingOrgName", req);
-    const triggerConsolidated = getFromQueryAsBoolean("forcePatientDiscovery", req);
+    const triggerConsolidated = getFromQueryAsBoolean("triggerConsolidated", req);
     const cxDocumentRequestMetadata = cxRequestMetadataSchema.parse(req.body);
 
     const docQueryProgress = await queryDocumentsAcrossHIEs({
