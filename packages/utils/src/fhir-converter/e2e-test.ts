19ec83f8dd210ba926453c5d999e9f0e63c3f3d4
Jonah Kaye <jonah.arturo@gmail.com>
2024-10-23 16:09:10 -0400

    feat(fhir): nit
    Refs: #1925
    Signed-off-by: Jonah Kaye <jonah.arturo@gmail.com>

--- packages/utils/src/fhir-converter/e2e-test.ts
+++ packages/utils/src/fhir-converter/e2e-test.ts
@@ -55,7 +55,7 @@ dayjs.extend(duration);
  */
 
 const cdaLocation = ``;
-const converterBaseUrl = "http://localhost:8080";
+const converterBaseUrl = "http://localhost:8777";
 const fhirBaseUrl = "http://localhost:8889";
 const parallelConversions = 10;
 // Execute 1 batch at a time to avoid concurrency when upserting resources (resulting in 409/Conflict), which
