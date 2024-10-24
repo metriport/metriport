62c0961f878d4adbc03148ddcf204fab3468a86c
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2024-10-24 16:49:40 -0700

    feat(patient-import): export on api
    Ref: #2330
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/api-sdk/src/medical/models/common/us-data.ts
+++ packages/api-sdk/src/medical/models/common/us-data.ts
@@ -3,6 +3,7 @@ import {
   USTerritory as USTerritoryShared,
   usStateSchema as usStateSchemaShared,
   usTerritorySchema as usTerritorySchemaShared,
+  usStateForAddressSchema as usStateForAddressSchemaShared,
 } from "@metriport/shared";
 
 export const USState = USStateShared;
@@ -12,3 +13,5 @@ export const usStateSchema = usStateSchemaShared;
 export const USTerritory = USTerritoryShared;
 
 export const usTerritorySchema = usTerritorySchemaShared;
+
+export const usStateForAddressSchema = usStateForAddressSchemaShared;
