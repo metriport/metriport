b47bc00895b786135ae88031fa81f30d653f0b9d
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2024-10-29 13:36:08 -0700

    feat(patient-import): patch patient routes
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/api/src/routes/ehr/athenahealth/index.ts
+++ packages/api/src/routes/ehr/athenahealth/index.ts
@@ -1,7 +1,9 @@
 import Router from "express-promise-router";
+import { patientAuthorization } from "../../middlewares/patient-authorization";
 import { processPatientRoute, processDocumentRoute } from "./auth/middleware";
 import patient from "./patient";
 import chart from "./chart";
+import medicalPatientRoot from "../../medical/patient-root";
 import medicalPatient from "../../medical/patient";
 import medicalDocument from "../../medical/document";
 import settings from "../../settings";
@@ -10,7 +12,13 @@ const routes = Router();
 
 routes.use("/patient", patient);
 routes.use("/chart", chart);
-routes.use("/medical/v1/patient", processPatientRoute, medicalPatient);
+routes.use("/medical/v1/patient", processPatientRoute, medicalPatientRoot);
+routes.use(
+  "/medical/v1/patient/:id",
+  processPatientRoute,
+  patientAuthorization("params"),
+  medicalPatient
+);
 routes.use("/medical/v1/document", processDocumentRoute, medicalDocument);
 routes.use("/settings", settings);
 
