b75635fb402093c4b47ca6b7c99ac67b7cd89964
Ramil Garipov <garipovramil93@hotmail.ca>
2024-10-31 20:31:01 -0700

    feat(api): wh not sent for disabled wh calls
    refs. metriport/metriport-internal#858
    
    Signed-off-by: Ramil Garipov <garipovramil93@hotmail.ca>

--- packages/api/src/command/medical/document/document-webhook.ts
+++ packages/api/src/command/medical/document/document-webhook.ts
@@ -87,6 +87,7 @@ export const processPatientDocumentRequest = async (
         metadata
       );
     } else {
+      log(`WH disabled. Not sending it`);
       // TODO 858 indicate this was not really sent to the customer
       await createWebhookRequest({
         cxId,
