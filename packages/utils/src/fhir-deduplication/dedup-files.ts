bc18c6a616734a20a8e74c93a36520731cefbde6
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2024-11-06 10:56:46 -0800

    feat(dedup): generate bundle in testing script
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/utils/src/fhir-deduplication/dedup-files.ts
+++ packages/utils/src/fhir-deduplication/dedup-files.ts
@@ -2,23 +2,42 @@ import * as dotenv from "dotenv";
 dotenv.config();
 // keep that ^ on top
 import { Bundle, Resource } from "@medplum/fhirtypes";
+import { MetriportMedicalApi } from "@metriport/api-sdk";
+import {
+  buildConsolidatedBundle,
+  merge,
+} from "@metriport/core/command/consolidated/consolidated-create";
+import { toFHIR as patientToFhir } from "@metriport/core/external/fhir/patient/conversion";
+import { buildBundleEntry } from "@metriport/core/external/fhir/shared/bundle";
 import { deduplicateFhir } from "@metriport/core/fhir-deduplication/deduplicate-fhir";
 import { executeAsynchronously } from "@metriport/core/util/concurrency";
+import { getFileContents, getFileNames, makeDir } from "@metriport/core/util/fs";
 import { elapsedTimeFromNow } from "@metriport/shared/common/date";
+import { getEnvVarOrFail } from "@metriport/shared/common/env-var";
+import { parseFhirBundle } from "@metriport/shared/medical";
 import dayjs from "dayjs";
 import fs from "fs";
-import { getFileContents, getFileNames, makeDir } from "../shared/fs";
-import { validateReferences } from "./report/validate-references";
 import { v4 as uuidv4 } from "uuid";
+import { validateReferences } from "./report/validate-references";
 
 /**
- * Folder with consolidated files/bundles.
+ * Folder with consolidated files/bundles. If the files need to be combined into a bundle
+ * set createBundle, existingPatientId, and auth stuff.
  *
  * WARNING: this will overwrite the *_deduped.json files!!!
  */
 const samplesFolderPath = ``;
 
 const suffix = "_deduped";
+
+const createBundle = false;
+const existingPatientId = ``;
+// auth stuff
+const apiKey = getEnvVarOrFail("API_KEY");
+const apiUrl = getEnvVarOrFail("API_URL");
+const metriportAPI = new MetriportMedicalApi(apiKey, {
+  baseAddress: apiUrl,
+});
 
 /**
  * Read FHIR bundles from 'samplesFolderPath' and deduplicates the resources inside those bundles.
@@ -30,13 +49,10 @@ const suffix = "_deduped";
  * WARNING: it will override the *_deduped.json files from the source folder!!!
  */
 async function main() {
-  const bundleFileNames = getFileNames({
-    folder: samplesFolderPath,
-    recursive: true,
-    extension: "json",
-  });
-  const filteredBundleFileNames = bundleFileNames.filter(f => !f.includes(suffix));
-
+  const filteredBundleFileNames = await createOrGetBundles(
+    createBundle,
+    createBundle ? existingPatientId : undefined
+  );
   const timestamp = dayjs().toISOString();
   const logsFolderName = `runs/dedup/${timestamp}`;
 
@@ -73,6 +89,53 @@ async function main() {
     fs.writeFileSync(`./${logsFolderName}/${fileNameWithExtension}`, output);
     fs.writeFileSync(`${samplesFolderPath}/${fileNameWithExtension}`, output);
   });
+}
+
+// Taken from core/command/consolidated/consolidated-create.ts
+async function createOrGetBundles(createBundle: boolean, patientId?: string) {
+  const fileNames = getFileNames({
+    folder: samplesFolderPath,
+    recursive: true,
+    extension: "json",
+  });
+  if (createBundle) {
+    if (!patientId || patientId === "")
+      throw new Error("Patient ID is required when creating a bundle");
+    const patient = await metriportAPI.getPatient(patientId);
+
+    const fhirPatient = patientToFhir({
+      id: patient.id,
+      data: {
+        ...patient,
+        personalIdentifiers: [], // Typing fix -- not relevant for script
+        address: [], // Typing fix -- not relevant for script
+        contact: [], // Typing fix -- not relevant for script
+      },
+    });
+    const patientEntry = buildBundleEntry(fhirPatient);
+
+    const mergedBundle = buildConsolidatedBundle();
+    await executeAsynchronously(fileNames, async filePath => {
+      console.log(`Getting conversion bundle from fileNames ${filePath}`);
+      const contents = getFileContents(filePath);
+      console.log(`Merging bundle ${filePath} into the consolidated one`);
+      const singleConversion = parseFhirBundle(contents);
+      if (!singleConversion) {
+        console.log(`No valid bundle found in ${filePath}, skipping`);
+        return;
+      }
+      merge(singleConversion).into(mergedBundle);
+    });
+
+    const conversions = mergedBundle.entry ?? [];
+    const withDups = buildConsolidatedBundle();
+    withDups.entry = [...conversions, patientEntry]; // Missing ...docRefs.map(buildBundleEntry)
+    withDups.total = withDups.entry.length;
+    const bundleFileName = `${samplesFolderPath}/generatedBundle${patientId}`;
+    fs.writeFileSync(bundleFileName, JSON.stringify(withDups));
+    return [bundleFileName];
+  }
+  return fileNames.filter(f => !f.includes(suffix));
 }
 
 main();
