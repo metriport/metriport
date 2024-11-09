ac5f811ca2390d910dd2e581a9b2f302a424d4b5
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2024-11-08 16:27:57 -0800

    feat(rate-limiting): logging
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/api/src/routes/middlewares/rate-limitng.ts
+++ packages/api/src/routes/middlewares/rate-limitng.ts
@@ -1,5 +1,7 @@
 import { checkRateLimit as checkRateLimitCore } from "@metriport/core/command/rate-limiting/rate-limiting";
-import { RateLimit, RateLimitOperation, TooManyRequestsError } from "@metriport/shared";
+import { RateLimit, RateLimitOperation } from "@metriport/shared";
+import { out } from "@metriport/core/util/log";
+import { capture } from "@metriport/core/util/notifications";
 import { NextFunction, Request, Response } from "express";
 import { getDB } from "../../models/db";
 import { getCxIdOrFail } from "../util";
@@ -19,6 +21,9 @@ export function checkRateLimit(
 ): (req: Request, res: Response, next: NextFunction) => Promise<void> {
   return async (req: Request, _: Response, next: NextFunction): Promise<void> => {
     const cxId = getCxIdOrFail(req);
+    const { log } = out(
+      `checkRateLimit - cxId ${cxId} operation ${operation} rateLimit ${rateLimit}`
+    );
 
     const allowed = await checkRateLimitCore({
       cxId,
@@ -28,9 +33,18 @@ export function checkRateLimit(
     });
 
     if (!allowed) {
-      throw new TooManyRequestsError(
-        `Too many requests for ${routeMapForError[operation]} - please reduce your request rate for this operation`
-      );
+      const msg = `Too many requests for ${routeMapForError[operation]} - please reduce your request rate for this operation`;
+      log(msg);
+      capture.message(msg, {
+        extra: {
+          cxId,
+          operation,
+          rateLimit,
+        },
+        level: "info",
+      });
+      // TODO Enable error after monitoring
+      //throw new TooManyRequestsError(msg);
     }
 
     next();
