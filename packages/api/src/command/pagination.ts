4aa471e7eb473cccc008914d935d2a10f9f3951b
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-11-20 15:00:48 -0300

    refactor: move repo pagination functions to own file +
    - move repo pagination functions to own file
    - make pagination url params compile time safe
    
    Ref. metriport/metriport-internal#483
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/api/src/command/pagination.ts
+++ packages/api/src/command/pagination.ts
@@ -1,17 +1,42 @@
-/**
- * One of "toItem" or "fromItem" are expected to be empty.
- * - fromItem indicates the minimum item to be included in the response, inclusive.
- * - toItem indicates the maximum item to be included in the response, inclusive.
- * - count indicates the number of items to be included in the response.
- */
-export type Pagination =
-  | {
-      fromItem?: string;
-      toItem?: never;
-      count?: number;
-    }
-  | {
-      fromItem?: never;
-      toItem?: string;
-      count?: number;
-    };
+import { FindOptions, Op, OrderItem } from "sequelize";
+import { UnionToIntersection, XOR } from "ts-essentials";
+
+export type Pagination = XOR<PaginationFromItem, PaginationToItem> & {
+  /** indicates the number of items to be included in the response */
+  count: number;
+};
+
+export type PaginationFromItem = {
+  /** indicates the minimum item to be included in the response, inclusive */
+  fromItem?: string | undefined;
+};
+export type PaginationToItem = {
+  /** indicates the maximum item to be included in the response, inclusive */
+  toItem?: string | undefined;
+};
+export type PaginationItem = UnionToIntersection<PaginationFromItem | PaginationToItem>;
+
+export function getPaginationFilters(pagination: Pagination | undefined) {
+  const { toItem, fromItem } = pagination ?? {};
+  return {
+    ...(toItem ? { id: { [Op.lte]: toItem } } : undefined),
+    ...(fromItem ? { id: { [Op.gte]: fromItem } } : undefined),
+  };
+}
+
+export function getPaginationLimits(
+  pagination: Pagination | undefined
+): Pick<FindOptions, "limit"> | undefined {
+  const { count } = pagination ?? {};
+  return count ? { limit: count } : undefined;
+}
+
+export function getPaginationSorting(pagination: Pagination | undefined): OrderItem {
+  const { toItem } = pagination ?? {};
+  return ["id", toItem ? "DESC" : "ASC"];
+}
+
+export function sortForPagination<T>(items: T[], pagination: Pagination | undefined): T[] {
+  const { toItem } = pagination ?? {};
+  return toItem ? items.reverse() : items;
+}
