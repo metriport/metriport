2b3d697fe23e2cfb6dfca06e80b4af48e4494747
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2024-11-21 08:52:17 -0500

    fix(api): add ehr endpoint
    Ref. metriport/metriport-internal#2475
    
    Signed-off-by: Jorge Orta <36802074+Orta21@users.noreply.github.com>

--- packages/api/src/routes/ehr/athenahealth/patient.ts
+++ packages/api/src/routes/ehr/athenahealth/patient.ts
@@ -1,11 +1,14 @@
 import Router from "express-promise-router";
 import httpStatus from "http-status";
 import { Request, Response } from "express";
+import { isTrue } from "@metriport/shared/common/boolean";
 import { getPatientIdOrFail as getPatientIdFromAthenaPatientOrFail } from "../../../external/ehr/athenahealth/command/get-patient";
 import { requestLogger } from "../../helpers/request-logger";
 import { asyncHandler, getCxIdOrFail, getFrom, getFromQueryOrFail } from "../../util";
 import { getAuthorizationToken } from "../../util";
 import { handleParams } from "../../helpers/handle-params";
+import { getHieOptOut, setHieOptOut } from "../../../command/medical/patient/update-hie-opt-out";
+import { PatientHieOptOutResponse } from "../../medical/schemas/patient";
 
 const router = Router();
 
@@ -32,6 +35,82 @@ router.get(
       accessToken,
     });
     return res.status(httpStatus.OK).json(patientId);
+  })
+);
+
+/** ---------------------------------------------------------------------------
+ * PUT /patient/:id/hie-opt-out
+ *
+ * Returns whether the patient is opted out of data pulling and sharing.
+ *
+ * @param req.cxId The customer ID.
+ * @param req.param.patientId The ID of the patient whose data is to be returned.
+ * @param req.query.hieOptOut Boolean value to opt patient out or in.
+ */
+router.put(
+  "/:id//hie-opt-out",
+  handleParams,
+  requestLogger,
+  asyncHandler(async (req: Request, res: Response) => {
+    const accessToken = getAuthorizationToken(req);
+    const cxId = getCxIdOrFail(req);
+    const athenaPatientId = getFrom("params").orFail("id", req);
+    const athenaPracticeId = getFromQueryOrFail("practiceId", req);
+    const patientId = await getPatientIdFromAthenaPatientOrFail({
+      cxId,
+      athenaPracticeId,
+      athenaPatientId,
+      accessToken,
+    });
+
+    const hieOptOut = isTrue(getFrom("query").orFail("hieOptOut", req));
+
+    const result = await setHieOptOut({ patientId, cxId, hieOptOut });
+
+    const respPayload: PatientHieOptOutResponse = {
+      id: result.id,
+      hieOptOut: result.hieOptOut ?? false,
+      message: `Patient has been opted ${result.hieOptOut ? "out from" : "in to"} the networks`,
+    };
+
+    return res.status(httpStatus.OK).json(respPayload);
+  })
+);
+
+// TODO #2475 expose this on the patient
+/** ---------------------------------------------------------------------------
+ * GET /patient/:id/hie-opt-out
+ *
+ * Returns whether the patient is opted out of data pulling and sharing.
+ *
+ * @param req.cxId The customer ID.
+ * @param req.param.patientId The ID of the patient whose data is to be returned.
+ */
+router.get(
+  "/:id//hie-opt-out",
+  handleParams,
+  requestLogger,
+  asyncHandler(async (req: Request, res: Response) => {
+    const accessToken = getAuthorizationToken(req);
+    const cxId = getCxIdOrFail(req);
+    const athenaPatientId = getFrom("params").orFail("id", req);
+    const athenaPracticeId = getFromQueryOrFail("practiceId", req);
+    const patientId = await getPatientIdFromAthenaPatientOrFail({
+      cxId,
+      athenaPracticeId,
+      athenaPatientId,
+      accessToken,
+    });
+
+    const hieOptOut = await getHieOptOut({ cxId, patientId });
+
+    const respPayload: PatientHieOptOutResponse = {
+      id: patientId,
+      hieOptOut: hieOptOut,
+      message: `Patient has opted ${hieOptOut ? "out from" : "in to"} the networks`,
+    };
+
+    return res.status(httpStatus.OK).json(respPayload);
   })
 );
 
