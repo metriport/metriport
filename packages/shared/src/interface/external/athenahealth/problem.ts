b813950f52a1f609e618d9c2dfda6b45f56049ec
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2024-12-12 17:27:38 -0800

    feat(ehr): zod to string
    Ref: #2529
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/shared/src/interface/external/athenahealth/problem.ts
+++ packages/shared/src/interface/external/athenahealth/problem.ts
@@ -3,9 +3,6 @@ import { z } from "zod";
 export const problemCreateResponseSchema = z.object({
   success: z.boolean(),
   errormessage: z.string().optional(),
-  problemid: z
-    .number()
-    .transform(val => `${val}`)
-    .optional(),
+  problemid: z.string().optional(),
 });
 export type ProblemCreateResponse = z.infer<typeof problemCreateResponseSchema>;
