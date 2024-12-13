e9cc781d98f9448d72336c0e3643546f8af2bbcb
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2024-12-13 18:55:23 -0500

    fix(api): update infra
    Ref. metriport/metriport-internal#2526
    
    Signed-off-by: Jorge Orta <36802074+Orta21@users.noreply.github.com>

--- packages/infra/lib/api-stack.ts
+++ packages/infra/lib/api-stack.ts
@@ -336,6 +336,9 @@ export class APIStack extends Stack {
       medicalDocumentsBucket,
       sandboxSeedDataBucket,
       alarmAction: slackNotification?.alarmAction,
+      // TEMP LOGIC
+      dashUrl: isSandbox(props.config) ? "" : props.config.dashUrl,
+      bedrock: props.config.bedrock,
       appConfigEnvVars: {
         appId: appConfigAppId,
         configId: appConfigConfigId,
@@ -423,7 +426,6 @@ export class APIStack extends Stack {
           appId: appConfigAppId,
           configId: appConfigConfigId,
         },
-        bedrock: props.config.bedrock,
         ...props.config.fhirToMedicalLambda,
       });
     }
@@ -1311,7 +1313,6 @@ export class APIStack extends Stack {
       appId: string;
       configId: string;
     };
-    bedrock: { modelId: string; region: string; anthropicVersion: string } | undefined;
   }): Lambda {
     const {
       nodeRuntimeArn,
@@ -1319,11 +1320,9 @@ export class APIStack extends Stack {
       vpc,
       sentryDsn,
       envType,
-      dashUrl,
       alarmAction,
       medicalDocumentsBucket,
       appConfigEnvVars,
-      bedrock,
     } = ownProps;
 
     const lambdaTimeout = MAXIMUM_LAMBDA_TIMEOUT.minus(Duration.seconds(5));
@@ -1343,13 +1342,6 @@ export class APIStack extends Stack {
         PDF_CONVERT_TIMEOUT_MS: CDA_TO_VIS_TIMEOUT.toMilliseconds().toString(),
         APPCONFIG_APPLICATION_ID: appConfigEnvVars.appId,
         APPCONFIG_CONFIGURATION_ID: appConfigEnvVars.configId,
-        ...(bedrock && {
-          // API_URL set on the api-stack after the OSS API is created
-          DASH_URL: dashUrl,
-          BEDROCK_REGION: bedrock?.region,
-          BEDROCK_VERSION: bedrock?.anthropicVersion,
-          AI_BRIEF_MODEL_ID: bedrock?.modelId,
-        }),
         ...(sentryDsn ? { SENTRY_DSN: sentryDsn } : {}),
       },
       layers: [
