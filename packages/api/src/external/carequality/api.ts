875686a22aad6b0463d07001b75d97a063e37c5e
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-12-14 00:01:57 -0300

    refactor(cq-sdk): remove old API impl and interface
    Ref. metriport/metriport-internal#1040
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/api/src/external/carequality/api.ts
+++ packages/api/src/external/carequality/api.ts
@@ -1,8 +1,6 @@
 import {
   APIMode as CQAPIMode,
-  CarequalityManagementAPI,
   CarequalityManagementAPIFhir,
-  CarequalityManagementAPIImpl,
   CarequalityManagementAPIImplFhir,
 } from "@metriport/carequality-sdk";
 import { Config } from "../../shared/config";
@@ -12,28 +10,6 @@ const cqApiMode = Config.isProdEnv()
   : Config.isStaging()
   ? CQAPIMode.staging
   : CQAPIMode.dev;
-
-/**
- * Creates a new instance of the Carequality Management API client.
- *
- * @returns Carequality API.
- */
-export function makeCarequalityManagementAPI(): CarequalityManagementAPI {
-  if (Config.isSandbox()) throw new Error("Carequality API not initialized");
-
-  const cqManagementApiKey = Config.getCQManagementApiKey();
-  const cqOrgCert = Config.getCQOrgCertificate();
-  const cqOrgPrivateKey = Config.getCQOrgPrivateKey();
-  const cqPrivateKeyPassword = Config.getCQOrgPrivateKeyPassword();
-
-  return new CarequalityManagementAPIImpl({
-    apiKey: cqManagementApiKey,
-    apiMode: cqApiMode,
-    orgCert: cqOrgCert,
-    rsaPrivateKey: cqOrgPrivateKey,
-    rsaPrivateKeyPassword: cqPrivateKeyPassword,
-  });
-}
 
 export function makeCarequalityManagementAPIFhir(): CarequalityManagementAPIFhir {
   if (Config.isSandbox()) throw new Error("Carequality Management API not initialized");
