e447d1fca0b75ee319431adb8f5bce6547ae53b3
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2024-12-16 15:43:09 -0500

    fix(api): add logs
    Ref. metriport/metriport-internal#2526
    
    Signed-off-by: Jorge Orta <36802074+Orta21@users.noreply.github.com>

--- packages/core/src/command/ai-brief/ai-brief-shared.ts
+++ packages/core/src/command/ai-brief/ai-brief-shared.ts
@@ -24,9 +24,12 @@ export async function generateAiBriefFhirResource(
 }
 
 export function getAiBriefContentFromBundle(bundle: Bundle): string | undefined {
+  console.log("binaryResource", bundle.entry);
   const binaryResource = bundle.entry?.find(entry => entry.resource?.resourceType === "Binary") as
     | Binary
     | undefined;
+
+  console.log("binaryResource", binaryResource);
 
   if (!binaryResource || !binaryResource.data) return undefined;
 
