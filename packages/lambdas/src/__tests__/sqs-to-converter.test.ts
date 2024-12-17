9cb2e060be3bd79bd3750bc3668b2c859c464413
Ramil Garipov <garipovramil93@hotmail.ca>
2024-12-17 00:15:32 -0800

    fix(lambda): minor fixes
    refs. metriport/metriport-internal#2525
    
    Signed-off-by: Ramil Garipov <garipovramil93@hotmail.ca>

--- packages/lambdas/src/__tests__/sqs-to-converter.test.ts
+++ packages/lambdas/src/__tests__/sqs-to-converter.test.ts
@@ -1,4 +1,4 @@
-import { cleanUpPayload } from "../sqs-to-converter/cleanup";
+import { cleanUpPayload } from "@metriport/core/domain/consolidated/cleanup";
 
 describe("sqs-to-converter", () => {
   describe("cleanUpPayload", () => {
