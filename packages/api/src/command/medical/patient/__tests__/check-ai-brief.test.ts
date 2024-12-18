4e3b19974173e122fdbae4e69da2eb0accbcaffe
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2024-12-18 14:44:39 -0500

    fix(api): ai docs
    Ref. metriport/metriport-internal#2526
    
    Signed-off-by: Jorge Orta <36802074+Orta21@users.noreply.github.com>

--- packages/api/src/command/medical/patient/__tests__/check-ai-brief.test.ts
+++ packages/api/src/command/medical/patient/__tests__/check-ai-brief.test.ts
@@ -13,67 +13,59 @@
 //   jest.restoreAllMocks();
 // });
 
-// describe("checkAiBriefEnabled", () => {
-//   const cxId = uuidv4();
-
-//   describe("isAiBriefEnabledForCx is false", () => {
-//     beforeEach(() => {
-//       isAiBriefEnabledForCx_mock.mockReturnValue(false);
-//     });
-
-//     it("returns false when generateAiBrief is false", async () => {
-//       const resp = await checkAiBriefEnabled({
-//         cxId,
-//         generateAiBrief: false,
-//       });
-//       expect(resp).toEqual(false);
-//     });
-
-//     it("returns false when generateAiBrief is undefined", async () => {
-//       const resp = await checkAiBriefEnabled({
-//         cxId,
-//         generateAiBrief: undefined,
-//       });
-//       expect(resp).toEqual(false);
-//     });
-
-//     it("throws when generateAiBrief is true", async () => {
-//       await expect(() =>
-//         checkAiBriefEnabled({
-//           cxId,
-//           generateAiBrief: true,
-//         })
-//       ).rejects.toThrow("Contact Metriport to enable the AI Brief feature.");
-//     });
-//   });
-
-//   describe("isAiBriefEnabledForCx is true", () => {
-//     beforeEach(() => {
-//       isAiBriefEnabledForCx_mock.mockReturnValue(true);
-//     });
-
-//     it("returns false when generateAiBrief is false", async () => {
-//       const resp = await checkAiBriefEnabled({
-//         cxId,
-//         generateAiBrief: false,
-//       });
-//       expect(resp).toEqual(false);
-//     });
-
-//     it("returns true when generateAiBrief is undefined", async () => {
-//       const resp = await checkAiBriefEnabled({
-//         cxId,
-//         generateAiBrief: undefined,
-//       });
-//       expect(resp).toEqual(true);
-//     });
-
-//     it("returns true when generateAiBrief is true", async () => {
-//       const resp = await checkAiBriefEnabled({
-//         cxId,
-//         generateAiBrief: true,
-//       });
-//       expect(resp).toEqual(true);
-//     });
-//   });
-// });
+describe("checkAiBriefEnabled", () => {
+  //   const cxId = uuidv4();
+  //   describe("isAiBriefEnabledForCx is false", () => {
+  //     beforeEach(() => {
+  //       isAiBriefEnabledForCx_mock.mockReturnValue(false);
+  //     });
+  //     it("returns false when generateAiBrief is false", async () => {
+  //       const resp = await checkAiBriefEnabled({
+  //         cxId,
+  //         generateAiBrief: false,
+  //       });
+  //       expect(resp).toEqual(false);
+  //     });
+  //     it("returns false when generateAiBrief is undefined", async () => {
+  //       const resp = await checkAiBriefEnabled({
+  //         cxId,
+  //         generateAiBrief: undefined,
+  //       });
+  //       expect(resp).toEqual(false);
+  //     });
+  //     it("throws when generateAiBrief is true", async () => {
+  //       await expect(() =>
+  //         checkAiBriefEnabled({
+  //           cxId,
+  //           generateAiBrief: true,
+  //         })
+  //       ).rejects.toThrow("Contact Metriport to enable the AI Brief feature.");
+  //     });
+  //   });
+  //   describe("isAiBriefEnabledForCx is true", () => {
+  //     beforeEach(() => {
+  //       isAiBriefEnabledForCx_mock.mockReturnValue(true);
+  //     });
+  //     it("returns false when generateAiBrief is false", async () => {
+  //       const resp = await checkAiBriefEnabled({
+  //         cxId,
+  //         generateAiBrief: false,
+  //       });
+  //       expect(resp).toEqual(false);
+  //     });
+  //     it("returns true when generateAiBrief is undefined", async () => {
+  //       const resp = await checkAiBriefEnabled({
+  //         cxId,
+  //         generateAiBrief: undefined,
+  //       });
+  //       expect(resp).toEqual(true);
+  //     });
+  //     it("returns true when generateAiBrief is true", async () => {
+  //       const resp = await checkAiBriefEnabled({
+  //         cxId,
+  //         generateAiBrief: true,
+  //       });
+  //       expect(resp).toEqual(true);
+  //     });
+  //   });
+});
