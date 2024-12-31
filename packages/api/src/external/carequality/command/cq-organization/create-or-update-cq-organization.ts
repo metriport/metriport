356fd51d37f67ba346c031fb30d1133c90f43ea7
Rafael Leite <2132564+leite08@users.noreply.github.com>
2024-12-31 00:52:31 -0300

    refactor(api): cache orgs while rebuilding CQ dir
    Ref. metriport/metriport-internal#2553
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/api/src/external/carequality/command/cq-organization/create-or-update-cq-organization.ts
+++ packages/api/src/external/carequality/command/cq-organization/create-or-update-cq-organization.ts
@@ -5,7 +5,7 @@ import { errorToString } from "@metriport/shared";
 import { makeCarequalityManagementAPI } from "../../api";
 import { CQDirectoryEntryData2 } from "../../cq-directory";
 import { CQOrgDetails, CQOrgDetailsWithUrls, getCqAddress, getCqOrgUrls } from "../../shared";
-import { getCqOrg } from "./get-cq-organization";
+import { CqOrgLoaderImpl } from "./get-cq-organization";
 import { getOrganizationFhirTemplate } from "./organization-template";
 import { parseCQOrganization } from "./parse-cq-organization";
 
@@ -25,8 +25,9 @@ export type CreateOrUpdateCqOrganizationCmd = Omit<
 export async function createOrUpdateCqOrganization(
   cmd: CreateOrUpdateCqOrganizationCmd
 ): Promise<CQDirectoryEntryData2> {
+  const cqOrgLoader = new CqOrgLoaderImpl();
   const [cqOrg, orgDetailsWithUrls] = await Promise.all([
-    getCqOrg(cmd.oid),
+    cqOrgLoader.getCqOrg(cmd.oid),
     cmdToCqOrgDetails(cmd),
   ]);
   if (cqOrg) return await updateCqOrganization(orgDetailsWithUrls);
@@ -64,7 +65,7 @@ async function updateCqOrganization(
   try {
     const resp = await cq.updateOrganization(carequalityOrg);
     debug(`resp updateOrganization: `, () => JSON.stringify(resp));
-    return parseCQOrganization(resp);
+    return parseCQOrganization(resp, new CqOrgLoaderImpl());
     //eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     const extra = {
@@ -97,7 +98,7 @@ async function createCqOrganization(
   try {
     const resp = await cq.registerOrganization(carequalityOrg);
     debug(`resp registerOrganization: `, () => JSON.stringify(resp));
-    return parseCQOrganization(resp);
+    return parseCQOrganization(resp, new CqOrgLoaderImpl());
   } catch (error) {
     const msg = `Failure while registering org @ CQ`;
     log(`${msg}. Org OID: ${orgDetails.oid}. Cause: ${errorToString(error)}`);
