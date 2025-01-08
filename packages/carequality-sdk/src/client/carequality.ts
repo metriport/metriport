51579053c3e4dc651f8608a5e28e738cb2c19cf7
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-01-08 00:32:59 -0300

    refactor(api): cq dir rebuild with view and 3 backup tables
    Ref. metriport/metriport-internal#2553
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/carequality-sdk/src/client/carequality.ts
+++ packages/carequality-sdk/src/client/carequality.ts
@@ -30,7 +30,8 @@ export interface CarequalityManagementApi {
    * @param count Optional, number of organizations to fetch. Defaults to 1000.
    * @param start Optional, the index of the directory to start querying from. Defaults to 0.
    * @param oid Optional, the OID of the organization to fetch.
-   * @param active Optional, indicates whether to list active or inactive organizations. Defaults to true.
+   * @param active Optional, indicates whether to list active or inactive organizations. If not
+   *               provided, includes both active and inactive entries.
    * @returns
    */
   listOrganizations(params?: ListOrganizationsParams | undefined): Promise<OrganizationWithId[]>;
@@ -39,7 +40,8 @@ export interface CarequalityManagementApi {
    * Registers an organization with the Carequality directory.
    *
    * @param org string containing the organization resource (in XML format)
-   * @returns an XML string containing an OperationOutcome resource - see Carequality documentation for details - https://carequality.org/healthcare-directory/OperationOutcome-create-success-example2.xml.html
+   * @returns an XML string containing an OperationOutcome resource - see Carequality documentation
+   * for details - https://carequality.org/healthcare-directory/OperationOutcome-create-success-example2.xml.html
    */
   registerOrganization(org: OrganizationWithId): Promise<OrganizationWithId>;
 
@@ -48,7 +50,8 @@ export interface CarequalityManagementApi {
    *
    * @param org string containing the organization resource (in XML format) - the organization must
    *            have an id
-   * @returns an XML string containing an OperationOutcome resource - see Carequality documentation for details - https://carequality.org/healthcare-directory/OperationOutcome-create-success-example2.xml.html
+   * @returns an XML string containing an OperationOutcome resource - see Carequality documentation
+   * for details - https://carequality.org/healthcare-directory/OperationOutcome-create-success-example2.xml.html
    */
   updateOrganization(org: OrganizationWithId): Promise<OrganizationWithId>;
 
