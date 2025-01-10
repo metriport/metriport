90b354af8cf61d53c55ed58e0a93de719be516c7
Ramil Garipov <garipovramil93@hotmail.ca>
2025-01-09 23:34:00 -0800

    feat(core): added interpretations and unit tests
    refs. metriportmetriport-internal#2551
    
    Signed-off-by: Ramil Garipov <garipovramil93@hotmail.ca>

--- packages/core/src/external/fhir/normalization/resources/__tests__/observation.test.ts
+++ packages/core/src/external/fhir/normalization/resources/__tests__/observation.test.ts
@@ -11,7 +11,7 @@ import {
   valueQuantityWeightLb,
 } from "../../../../../fhir-deduplication/__tests__/examples/observation-examples";
 import { makeObservation } from "../../../../../fhir-to-cda/cda-templates/components/__tests__/make-observation";
-import { normalizeObservations } from "../observation";
+import { calculateInterpretationCode, normalizeObservations } from "../observation";
 
 describe("normalizeObservations", () => {
   it("correctly handle temperature celsius", () => {
@@ -143,5 +143,101 @@ describe("normalizeObservations", () => {
 
     expect(rangeResult[0]?.low?.unit).toBe("g/dL");
     expect(rangeResult[0]?.high?.unit).toBe("g/dL");
+  });
+
+  it("correctly calculates interpretation to be normal based on value and reference range", () => {
+    const observation = makeObservation({
+      valueQuantity: valueQuantityHemoglobin,
+      referenceRange: referenceRangeHemoglobin,
+    });
+
+    const normalized = normalizeObservations([observation]);
+    expect(normalized.length).toBe(1);
+    const obs = normalized[0];
+    expect(obs).toBeTruthy();
+    if (!obs) throw new Error("Expected result undefined");
+
+    expect(obs.interpretation?.[0]?.text).toEqual("Normal");
+    expect(obs.interpretation?.[0]?.coding?.[0]?.code).toEqual("N");
+  });
+
+  it("correctly calculates interpretation to be low based on value and reference range", () => {
+    const observation = makeObservation({
+      valueQuantity: { ...valueQuantityHemoglobin, value: 1.0 },
+      referenceRange: referenceRangeHemoglobin,
+    });
+
+    const normalized = normalizeObservations([observation]);
+    expect(normalized.length).toBe(1);
+    const obs = normalized[0];
+    expect(obs).toBeTruthy();
+    if (!obs) throw new Error("Expected result undefined");
+
+    expect(obs.interpretation?.[0]?.text).toEqual("Low");
+    expect(obs.interpretation?.[0]?.coding?.[0]?.code).toEqual("L");
+  });
+
+  it("correctly calculates interpretation to be high based on value and reference range", () => {
+    const observation = makeObservation({
+      valueQuantity: { ...valueQuantityHemoglobin, value: 20 },
+      referenceRange: referenceRangeHemoglobin,
+    });
+
+    const normalized = normalizeObservations([observation]);
+    expect(normalized.length).toBe(1);
+    const obs = normalized[0];
+    expect(obs).toBeTruthy();
+    if (!obs) throw new Error("Expected result undefined");
+
+    expect(obs.interpretation?.[0]?.text).toEqual("High");
+    expect(obs.interpretation?.[0]?.coding?.[0]?.code).toEqual("H");
+  });
+
+  it("creates a correct interpretation based on valueString", () => {
+    const observation = makeObservation({
+      valueString: "Normal",
+    });
+
+    const normalized = normalizeObservations([observation]);
+    expect(normalized.length).toBe(1);
+    const obs = normalized[0];
+    expect(obs).toBeTruthy();
+    if (!obs) throw new Error("Expected result undefined");
+
+    expect(obs.interpretation?.[0]?.text).toEqual("Normal");
+    expect(obs.interpretation?.[0]?.coding?.[0]?.code).toEqual("N");
+  });
+
+  describe("calculateInterpretationCode", () => {
+    it("returns correct code for a value containing `normal`", () => {
+      const value = "normal";
+      const result = calculateInterpretationCode(undefined, value, undefined);
+      expect(result).toEqual("N");
+    });
+    it("returns correct code for a value containing `not detected`", () => {
+      const value = "not detected";
+      const result = calculateInterpretationCode(undefined, value, undefined);
+      expect(result).toEqual("N");
+    });
+    it("returns correct code for a value containing `negative`", () => {
+      const value = "negative";
+      const result = calculateInterpretationCode(undefined, value, undefined);
+      expect(result).toEqual("N");
+    });
+    it("returns correct code for a value containing `abnormal`", () => {
+      const value = "abnormal";
+      const result = calculateInterpretationCode(undefined, value, undefined);
+      expect(result).toEqual("A");
+    });
+    it("returns correct code for a value containing `low`", () => {
+      const value = "low";
+      const result = calculateInterpretationCode(undefined, value, undefined);
+      expect(result).toEqual("L");
+    });
+    it("returns correct code for a value containing `high`", () => {
+      const value = "high";
+      const result = calculateInterpretationCode(undefined, value, undefined);
+      expect(result).toEqual("H");
+    });
   });
 });
--- packages/core/src/external/fhir/normalization/resources/observation.ts
+++ packages/core/src/external/fhir/normalization/resources/observation.ts
@@ -1,10 +1,23 @@
-import { Observation, Quantity, ObservationReferenceRange } from "@medplum/fhirtypes";
+import {
+  CodeableConcept,
+  Observation,
+  ObservationReferenceRange,
+  Quantity,
+} from "@medplum/fhirtypes";
 import convert, { Unit } from "convert-units";
 
 type UnitComplex = {
   unit: string;
   code?: string;
 };
+
+type ReferenceRange = {
+  low: number | undefined;
+  high: number | undefined;
+  unit: string | undefined;
+  text?: string | undefined;
+};
+
 /**
  * This map is used to normalize unconventional unit names to standard unit names. i.e cel -> C, millimeter to m, etc.
  */
@@ -25,15 +38,39 @@ const unitConversionMap = new Map<string, UnitComplex>([
   ["in", { unit: "cm" }], // https://hl7.org/fhir/R4/valueset-ucum-bodylength.html
 ]);
 
-export function normalizeObservations(observations: Observation[]): Observation[] {
-  observations.map(o => {
-    if (o.valueQuantity) {
-      const initialValueUnit = processValueQuantity(o.valueQuantity);
+const blacklistedValues = ["see below", "see text", "see comments", "see note"];
 
-      if (o.referenceRange) {
-        processReferenceRanges(o.referenceRange, initialValueUnit);
+const INTERPRETATION_LOW = "L";
+const INTERPRETATION_NORMAL = "N";
+const INTERPRETATION_HIGH = "H";
+const INTERPRETATION_ABNORMAL = "A";
+
+const interpretationMap = new Map<string, string>([
+  [INTERPRETATION_LOW, "Low"],
+  [INTERPRETATION_HIGH, "High"],
+  [INTERPRETATION_NORMAL, "Normal"],
+  [INTERPRETATION_ABNORMAL, "Abnormal"],
+]);
+
+const hl7ObservationInterpretationSystem =
+  "http://terminology.hl7.org/CodeSystem/v3-ObservationInterpretation";
+
+const highInterpretations = ["high", "critical"];
+const lowInterpretations = ["low"];
+const normalInterpretations = ["normal", "negative", "none seen", "not detected", "neg"];
+const abnormalInterpretations = ["abnormal", "positive"];
+
+export function normalizeObservations(observations: Observation[]): Observation[] {
+  observations.map(obs => {
+    if (obs.valueQuantity) {
+      const initialValueUnit = processValueQuantity(obs.valueQuantity);
+
+      if (obs.referenceRange) {
+        processReferenceRanges(obs.referenceRange, initialValueUnit);
       }
     }
+    const interpretation = buildObservationInterpretation(obs);
+    if (interpretation) obs.interpretation = interpretation;
   });
 
   return observations;
@@ -126,4 +163,121 @@ function normalizeUnit(unit?: string): string | undefined {
     : unitConversionMap.has(trimmedUnit.toUpperCase())
     ? trimmedUnit.toUpperCase()
     : unitNormalizationMap.get(trimmedUnit.toLowerCase());
+}
+
+export function buildObservationInterpretation(obs: Observation): CodeableConcept[] | undefined {
+  const firstReference = obs.referenceRange?.[0];
+
+  const referenceRange: ReferenceRange = {
+    low: firstReference?.low?.value,
+    high: firstReference?.high?.value,
+    unit: firstReference?.low?.unit?.toString() ?? firstReference?.high?.unit?.toString(),
+    text: firstReference?.text?.toLowerCase().trim(),
+  };
+
+  const value = getValueFromObservation(obs);
+
+  const explicitInterpretation = getExplicitInterpretation(obs);
+  const interpretationString = calculateInterpretationCode(
+    explicitInterpretation,
+    value,
+    referenceRange
+  );
+  console.log("interpretationString IS", interpretationString);
+  return buildInterpretationFromString(interpretationString);
+}
+
+function getValueFromObservation(obs: Observation): string | number | undefined {
+  let value: number | string | undefined;
+  if (obs.valueQuantity) {
+    value = obs.valueQuantity.value;
+  } else if (obs.valueCodeableConcept) {
+    value = obs.valueCodeableConcept.text;
+  } else if (obs.valueString) {
+    const parsedNumber = parseFloat(obs.valueString);
+    value = isNaN(parsedNumber) ? obs.valueString : parsedNumber;
+    if (blacklistedValues.includes(value?.toString().toLowerCase().trim())) value = undefined;
+  }
+
+  return value;
+}
+
+export function getExplicitInterpretation(obs: Observation): string | undefined {
+  const interpretationText =
+    obs.interpretation?.[0]?.text === "unknown" ? undefined : obs.interpretation?.[0]?.text;
+
+  return (
+    interpretationText ??
+    obs.interpretation?.[0]?.coding?.[0]?.display ??
+    obs.interpretation?.[0]?.coding?.[0]?.code
+  );
+}
+
+export function calculateInterpretationCode(
+  explicitInterpretation: string | undefined,
+  value: number | string | undefined,
+  referenceRange: ReferenceRange | undefined
+): string | undefined {
+  if (explicitInterpretation) return normalizeInterpretationStringToCode(explicitInterpretation);
+
+  if (typeof value === "number" && referenceRange) {
+    const low = referenceRange.low;
+    const high = referenceRange.high;
+
+    console.log(low, high, "and value", value);
+    if (low != undefined && high != undefined) {
+      if (value >= low && value <= high) {
+        return INTERPRETATION_NORMAL;
+      } else if (value < low) {
+        return INTERPRETATION_LOW;
+      } else if (value > high) {
+        return INTERPRETATION_HIGH;
+      }
+    } else if (low != undefined) {
+      if (value < low) {
+        return INTERPRETATION_LOW;
+      } else {
+        return INTERPRETATION_NORMAL;
+      }
+    } else if (high != undefined) {
+      if (value > high) {
+        return INTERPRETATION_HIGH;
+      } else {
+        return INTERPRETATION_NORMAL;
+      }
+    }
+  } else if (typeof value === "string") {
+    return normalizeInterpretationStringToCode(value);
+  }
+
+  return undefined;
+}
+
+function buildInterpretationFromString(
+  interpretation: string | undefined
+): CodeableConcept[] | undefined {
+  if (!interpretation) return undefined;
+  const interpretationText = interpretationMap.get(interpretation);
+  return [
+    {
+      ...(interpretationText ? { text: interpretationText } : undefined),
+      coding: [
+        {
+          code: interpretation,
+          system: hl7ObservationInterpretationSystem,
+        },
+      ],
+    },
+  ];
+}
+
+export function normalizeInterpretationStringToCode(interpretation: string): string {
+  const normalized = interpretation.toLowerCase().trim();
+
+  if (highInterpretations.includes(normalized)) return INTERPRETATION_HIGH;
+  if (lowInterpretations.includes(normalized)) return INTERPRETATION_LOW;
+  if (normalInterpretations.includes(normalized)) return INTERPRETATION_NORMAL;
+  if (abnormalInterpretations.includes(normalized)) return INTERPRETATION_ABNORMAL;
+
+  return interpretation;
 }
