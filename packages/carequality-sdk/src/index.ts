edcab4892726d4db3972e2d23c896d56af20c03f
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-01-16 01:08:55 -0300

    refactor(cq-sdk): remove old STU3 based models
    Ref. metriport/metriport-internal#2553
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/carequality-sdk/src/index.ts
+++ packages/carequality-sdk/src/index.ts
@@ -4,7 +4,3 @@ export {
   CarequalityManagementApi as CarequalityManagementAPI,
 } from "./client/carequality";
 export { CarequalityManagementApiFhir } from "./client/carequality-fhir";
-export { Address } from "./models/address";
-export { Contact } from "./models/contact";
-export { Contained } from "./models/contained";
-export { Organization } from "./models/organization";
