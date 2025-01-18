47c36567c58c59e1afd4d66112b0eaf48a0acf81
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-01-17 18:06:46 -0800

    feat(api): create network domain type + dto fn
    refs. metriport/metriport#1772
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/api/src/routes/medical/network.ts
+++ packages/api/src/routes/medical/network.ts
@@ -4,6 +4,8 @@ import { requestLogger } from "../helpers/request-logger";
 import { asyncHandler } from "../util";
 import status from "http-status";
 import { networkGetSchema } from "./schemas/network";
+import { NetworkDTO } from "./dtos/networkDTO";
+import { USState } from "@metriport/shared";
 
 const router = Router();
 
@@ -12,7 +14,40 @@ router.get(
   requestLogger,
   asyncHandler(async (req: Request, res: Response) => {
     const params = networkGetSchema.parse(req.query);
-    return res.status(status.OK).json({ params });
+    const networks: NetworkDTO[] = [
+      {
+        id: "1",
+        eTag: "1",
+        name: "Network 1",
+        oid: "1",
+        zip: "12345",
+        state: USState.CA,
+        managingOrg: "Org 1",
+        managingOrgOid: "1",
+      },
+      {
+        id: "2",
+        eTag: "2",
+        name: "Network 2",
+        oid: "2",
+        zip: "12345",
+        state: USState.NJ,
+        managingOrg: "Org 2",
+        managingOrgOid: "2",
+      },
+      {
+        id: "3",
+        eTag: "3",
+        name: "Network 3",
+        oid: "3",
+        zip: "12345",
+        state: USState.FL,
+        managingOrg: "Org 3",
+        managingOrgOid: "3",
+      },
+    ];
+
+    return res.status(status.OK).json({ networks, params });
   })
 );
 
