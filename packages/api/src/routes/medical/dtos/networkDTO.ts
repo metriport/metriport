6db6dfc257c17c71d309beaf261860e793c19e2c
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-01-20 13:26:56 -0800

    feat(api): remove unnecessary fields from dto
    refs. metriport/metriport-internal#1772
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/api/src/routes/medical/dtos/networkDTO.ts
+++ packages/api/src/routes/medical/dtos/networkDTO.ts
@@ -1,14 +1,11 @@
 import { normalizeState } from "@metriport/shared";
 import { Network } from "../../../domain/medical/network";
 import { CQDirectoryEntry2 } from "../../../external/carequality/cq-directory";
-import { BaseDTO } from "./baseDTO";
 
-export type NetworkDTO = BaseDTO & Network;
+export type NetworkDTO = Network;
 
 export function dtoFromHieDirectoryEntry(network: CQDirectoryEntry2): NetworkDTO {
   return {
-    id: network.id,
-    eTag: "1",
     name: network.name,
     oid: network.id,
     zip: network.zip,
