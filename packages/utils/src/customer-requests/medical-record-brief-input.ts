3504ef62e44df6f49dba3191e8e1154ec2449ade
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-01-22 15:28:33 -0300

    build: util script that generates MR local uses wkhtmltopdf
    Ref. metriport/metriport-internal#2510
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/utils/src/customer-requests/medical-record-brief-input.ts
+++ packages/utils/src/customer-requests/medical-record-brief-input.ts
@@ -1,10 +1,10 @@
 /* eslint-disable */
-// @ts-nocheck
 import * as dotenv from "dotenv";
 dotenv.config();
 // keep that ^ on top
 import { Bundle, Resource } from "@medplum/fhirtypes";
-import { summarizeFilteredBundleWithAI } from "@metriport/core/command/ai-brief/create";
+import { Brief } from "@metriport/core/command/ai-brief/create";
+import { getAiBriefContentFromBundle } from "@metriport/core/command/ai-brief/shared";
 import { bundleToHtml } from "@metriport/core/external/aws/lambda-logic/bundle-to-html";
 import { wkHtmlToPdf, WkOptions } from "@metriport/core/external/wk-html-to-pdf/index";
 import { sleep } from "@metriport/shared";
@@ -18,43 +18,31 @@ import { uuidv7 } from "../shared/uuid-v7";
  *
  * To use it:
  * 1. Set the variables:
- *  - SOURCE_DIR: the directory where the FHIR bundle is located
- *  - CX_ID: the customer ID
- *  - SOURCE_PATIENT_ID: the patient ID
- *  - SOURCE_BUNDLE_FILE: the name of the FHIR bundle file
+ *  - SOURCE_BUNDLE_FILE: the full path to the consolidated (FHIR) bundle file
  * 2. Run the script with:
- *  - `ts-node src/customer-requests/medical-record-brief-input.ts`
+ *  - `ts-node src/customer-requests/medical-record-brief-input`
  */
 
 // Update this to staging or local URL if you want to test the brief link
-const dashUrl = "http://dash.metriport.com";
+const dashURL = "http://dash.metriport.com";
 
-const SOURCE_DIR = "";
-const CX_ID = ""; // OPTIONAL - used for logs and analytics in the summarizeFilteredBundleWithAI function
-const SOURCE_PATIENT_ID = "";
-const SOURCE_BUNDLE_FILE = `${SOURCE_PATIENT_ID}.json`;
+const SOURCE_BUNDLE_FILE = ``;
 
 async function main() {
   await sleep(50); // Give some time to avoid mixing logs w/ Node's
   const startedAt = Date.now();
   console.log(`############## Started at ${new Date(startedAt).toISOString()} ##############`);
 
-  const fileName = `${SOURCE_DIR}/${SOURCE_BUNDLE_FILE}`;
-  const bundleStr = fs.readFileSync(fileName, { encoding: "utf8" });
+  const bundleStr = fs.readFileSync(SOURCE_BUNDLE_FILE, { encoding: "utf8" });
   const bundle = JSON.parse(bundleStr) as Bundle<Resource>;
 
-  const briefStartedAt = Date.now();
-  const brief = await summarizeFilteredBundleWithAI(bundle, CX_ID, SOURCE_PATIENT_ID);
-  const briefDuration = Date.now() - briefStartedAt;
-  const briefId = uuidv7();
+  const aiBriefContent = getAiBriefContentFromBundle(bundle);
+  const aiBrief = prepareBriefToBundle({ aiBrief: aiBriefContent });
 
   console.log(`Converting to HTML...`);
   const htmlStartedAt = Date.now();
-  const html = bundleToHtml(
-    bundle,
-    brief ? { content: brief, id: briefId, link: `${dashUrl}/feedback/${briefId}` } : undefined
-  );
-  fs.writeFileSync(`${SOURCE_DIR}/output_${SOURCE_BUNDLE_FILE}.html`, html);
+  const html = bundleToHtml(bundle, aiBrief);
+  fs.writeFileSync(`${SOURCE_BUNDLE_FILE}_output.html`, html);
   const htmlDuration = Date.now() - htmlStartedAt;
   console.log(`Converting to PDF...`);
   const pdfStartedAt = Date.now();
@@ -64,14 +52,24 @@ async function main() {
   };
   const stream = Readable.from(Buffer.from(html));
   const pdfData = await wkHtmlToPdf(options, stream, console.log);
-  fs.writeFileSync(`${SOURCE_DIR}/output_${SOURCE_BUNDLE_FILE}.pdf`, pdfData);
+  fs.writeFileSync(`${SOURCE_BUNDLE_FILE}_output.pdf`, pdfData);
   const pdfDuration = Date.now() - pdfStartedAt;
 
   console.log(
-    `>>> Done in ${elapsedTimeAsStr(
-      startedAt
-    )}, Brief in ${briefDuration}ms, HTML in ${htmlDuration}ms, PDF in ${pdfDuration}ms`
+    `>>> Done in ${elapsedTimeAsStr(startedAt)}, HTML in ${htmlDuration}ms, PDF in ${pdfDuration}ms`
   );
+}
+
+// Copied from fhir-to-medical-record.ts
+function prepareBriefToBundle({ aiBrief }: { aiBrief: string | undefined }): Brief | undefined {
+  if (!aiBrief) return undefined;
+  const feedbackId = uuidv7();
+  const feedbackLink = `${dashURL}/feedback/${feedbackId}`;
+  return {
+    id: feedbackId,
+    content: aiBrief,
+    link: feedbackLink,
+  };
 }
 
 main();
