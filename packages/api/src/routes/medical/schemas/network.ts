f270fd14aa56fcbda2f56984bdda0910ccffaaca
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-01-22 14:55:13 -0800

    feat(api): add default page size and tsdoc above route
    refs. metriport/metriport#1772
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/api/src/routes/medical/schemas/network.ts
+++ packages/api/src/routes/medical/schemas/network.ts
@@ -1,10 +1,15 @@
 import { z } from "zod";
 import { queryMetaSchema } from "../../pagination";
 
+const defaultPageSize = 100;
 export const networkGetSchema = z
   .object({
     filter: z.string().optional(),
   })
-  .and(queryMetaSchema);
+  .and(queryMetaSchema)
+  .transform(data => ({
+    ...data,
+    count: data.count ?? defaultPageSize,
+  }));
 
 export type NetworkGetParams = z.infer<typeof networkGetSchema>;
