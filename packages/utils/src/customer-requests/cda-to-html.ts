2871ed069b906cdbbd2b41654e35c1495316af32
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-01-30 12:36:44 -0300

    fix(util): update cda-to-html script w/ typos/wrong names
    Ref. metriport/metriport-internal#1040
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/utils/src/customer-requests/cda-to-html.ts
+++ packages/utils/src/customer-requests/cda-to-html.ts
@@ -11,16 +11,16 @@ import { elapsedTimeAsStr } from "../shared/duration";
 const styleSheetText = require("../../../lambdas/src/cda-to-visualization/stylesheet.js");
 
 /**
- * Script to convert a CDA bundle to HTML.
+ * Script to convert a CDA document to HTML.
  *
  * To use it:
  * 1. Set the variables:
- *  - SOURCE_BUNDLE_FILE: the full path to the CDA/XML file
+ *  - SOURCE_FILE: the full path to the CDA/XML file
  * 2. Run the script with:
  *  - `npm run cda-to-html`
  */
 
-const SOURCE_BUNDLE_FILE = ``;
+const SOURCE_FILE = ``;
 
 let cda10: unknown;
 let narrative: unknown;
@@ -31,23 +31,22 @@ async function main() {
   const startedAt = Date.now();
   console.log(`############## Started at ${new Date(startedAt).toISOString()} ##############`);
 
-  const bundleStr = fs.readFileSync(SOURCE_BUNDLE_FILE, { encoding: "utf8" });
+  const document = fs.readFileSync(SOURCE_FILE, { encoding: "utf8" });
 
   // https://metriport-inc.sentry.io/issues/5641255225/?alert_rule_id=14442454&alert_type=issue&environment=production&notification_uuid=24c13683-7ce2-43bf-b620-7fc31eca8502&project=4505252341153792&referrer=slack
-  const normalizedDocument = normalizeDocument(bundleStr);
+  const normalizedDocument = normalizeDocument(document);
 
   console.log(`Converting to HTML...`);
   const htmlStartedAt = Date.now();
   const html = await convertToHtml(normalizedDocument, console.log);
   const htmlDuration = Date.now() - htmlStartedAt;
-  fs.writeFileSync(`${SOURCE_BUNDLE_FILE}_output.html`, html);
+  fs.writeFileSync(`${SOURCE_FILE}_output.html`, html);
 
   console.log(`>>> Done in ${elapsedTimeAsStr(startedAt)}, HTML in ${htmlDuration}ms`);
 }
 
 // Based on packages/lambdas/src/cda-to-visualization.ts
 function normalizeDocument(document: string): string {
-  // return document;
   const normalizedDocument = document.replace(/\s&\s/g, " &amp; ");
   return normalizedDocument;
 }
