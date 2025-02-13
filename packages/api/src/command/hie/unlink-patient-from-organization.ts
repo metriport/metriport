b147e1bd95d5f1d5dac57f7ff0948effe40f4718
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2025-02-13 17:50:14 -0500

    fix(api): wip
    Ref. metriport/metriport-internal#2641
    
    Signed-off-by: Jorge Orta <36802074+Orta21@users.noreply.github.com>

--- packages/api/src/command/hie/unlink-patient-from-organization.ts
+++ packages/api/src/command/hie/unlink-patient-from-organization.ts
@@ -1,11 +1,13 @@
 import { getDocuments } from "@metriport/core/external/fhir/document/get-documents";
 import { out } from "@metriport/core/util/log";
+import { DocumentReferenceWithId } from "@metriport/core/external/fhir/document/document-reference";
 import { isOrganization, isPatient } from "@metriport/core/external/fhir/shared";
 import { hasCommonwellContent } from "@metriport/core/external/commonwell/extension";
 import { hasCarequalityContent } from "@metriport/core/external/carequality/extension";
 import { getCQPatientData } from "../../external/carequality/command/cq-patient-data/get-cq-data";
 import { getCwPatientData } from "../../external/commonwell/command/cw-patient-data/get-cw-data";
-import { DocumentReferenceWithId } from "@metriport/core/external/fhir/document/document-reference";
+import { CQData } from "../../external/carequality/cq-patient-data";
+import { CwData } from "../../external/commonwell/cw-patient-data";
 
 type UnlinkPatientFromOrganizationParams = {
   cxId: string;
@@ -26,6 +28,30 @@ export async function unlinkPatientFromOrganization({
   const documents = await getDocuments({ cxId, patientId });
 
   const documentsWithOid = getDocumentsWithOid(documents, oid);
+
+  for (const document of documentsWithOid) {
+    const fileName = getS3FileNameFromDocument(document);
+    if (!fileName) continue;
+  }
+}
+
+function findCwLinkWithOid(cwPatientData: CwData, oid: string): string | undefined {
+  const cwLinks = cwPatientData.links;
+
+  for (const cwLink of cwLinks) {
+    const patient = cwLink.patient;
+    if (!patient) continue;
+
+    const identifier = patient.details.identifier?.find(
+      identifier => identifier.assigner !== "Commonwell" && identifier.system === oid
+    );
+
+    if (identifier) {
+      return cwLink;
+    }
+  }
+
+  return undefined;
 }
 
 function getDocumentsWithOid(
@@ -59,4 +85,16 @@ function getDocumentsWithOid(
   }
 
   return matchingDocumentRefs;
+}
+
+function getS3FileNameFromDocument(document: DocumentReferenceWithId): string | undefined {
+  const s3Attachment = document.content?.find(content => content.attachment?.url)?.attachment;
+  const fileName = s3Attachment?.title;
+
+  if (!fileName) {
+    console.error(`No file name found in document ${document.id}`);
+    return undefined;
+  }
+
+  return fileName;
 }
