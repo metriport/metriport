045cafa9282c813c552189ad73da5714d8a6cc79
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-02-14 12:21:56 -0800

    feat(canvas): canvas app code
    Ref: #2188
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

---
+++ canvas-plugins/metriport-patient-app/README.md
@@ -0,0 +1,22 @@
+# metriport-patient-app
+
+## Description
+
+An embeddable application that can load the Metriport patient data into the canvas patient view.
+
+### Installation
+
+Requirments:
+
+1. [Canvas CLI](https://docs.canvasmedical.com/sdk/canvas_cli/) set up
+2. Metriport Token
+
+Please reach out to support@metriport.com to get a Metriport Token if not already provided.
+
+1. Install the plugin: `canvas install metriport-patient-app`
+2. Set `METRIPORT_TOKEN` in the plugin settings to be the token from Requirements (2).
+
+### Usage
+
+1. Navigate to a patient in Canvas and click the Metriport Patient app icon.
+2. The Metriport Patient app will load the patient data into the canvas patient view.
---
+++ canvas-plugins/metriport-patient-app/applications/metriport-patient-app.py
@@ -0,0 +1,20 @@
+from canvas_sdk.effects import Effect
+from canvas_sdk.effects.launch_modal import LaunchModalEffect
+from canvas_sdk.handlers.application import Application
+
+
+class MetriportPatientApp(Application):
+    """An embeddable application that can load the Metriport patient data into the canvas patient modal."""
+
+    def on_open(self) -> Effect:
+        """Handle the on_open event."""
+        # To be used once Ouath flow is implemented
+        #oauthClientKey = self.secrets['WEBHOOK_ID']
+        #canvasDomain = self.secrets['CANVAS_DOMAIN']
+        metriportToken = self.secrets['METRIPORT_TOKEN']
+        if (metriportToken is None):
+          raise Exception("Metriport token not set")
+        if (metriportToken == ""):
+          raise Exception("Metriport token is empty")
+        # Implement this method to handle the application on_open event.
+        return LaunchModalEffect(url=f"https://ehr.metriport.com/canvas/app#patient={self.context['patient']['id']}&access_token={metriportToken}", target=LaunchModalEffect.TargetType.DEFAULT_MODAL).apply()
