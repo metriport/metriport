d899f56123bc8272cfa8c43142740248f67a16e3
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-02-21 01:27:55 +0000

    refactor: bulk patient import step create runs 10 pts/min
    Ref. metriport/metriport-internal#2330
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/infra/lib/patient-import-nested-stack.ts
+++ packages/infra/lib/patient-import-nested-stack.ts
@@ -13,7 +13,7 @@ import { createLambda } from "./shared/lambda";
 import { LambdaLayers } from "./shared/lambda-layers";
 import { createQueue } from "./shared/sqs";
 
-const waitTimePatientCreate = Duration.seconds(15);
+const waitTimePatientCreate = Duration.seconds(6); // 10 patients/min
 const waitTimePatientQuery = Duration.seconds(0);
 
 function settings() {
