d8a5b0046e979e6f555d28ab9f944c6f49da2b58
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-02-21 12:15:50 -0800

    feat(infra): make alarm scale with num of ACUs
    refs. metriport/metriport-internal#2716
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/infra/lib/shared/rds.ts
+++ packages/infra/lib/shared/rds.ts
@@ -2,19 +2,28 @@ import * as cloudwatch from "aws-cdk-lib/aws-cloudwatch";
 import { SnsAction } from "aws-cdk-lib/aws-cloudwatch-actions";
 import * as rds from "aws-cdk-lib/aws-rds";
 import { Construct } from "constructs";
-import { RDSAlarmThresholds } from "../../config/aws/rds";
 import { mbToBytes } from "../shared/util";
-
+import { EnvConfig } from "../../config/env-config";
 const DEFAULT_MIN_LOCAL_STORAGE_MB_ALARM = 10_000;
+
+const getMaxPostgresConnections = (maxAcu: number): number => {
+  if (maxAcu < 4) return 189;
+  if (maxAcu < 8) return 823;
+  if (maxAcu < 16) return 1_669;
+  if (maxAcu < 32) return 3_360;
+
+  // 32+ ACUs all have 5000 max connections
+  return 5_000;
+};
 
 export function addDBClusterPerformanceAlarms(
   scope: Construct,
   dbCluster: rds.DatabaseCluster,
   dbClusterName: string,
-  thresholds?: RDSAlarmThresholds,
+  dbConfig: EnvConfig["apiDatabase"],
   alarmAction?: SnsAction
 ) {
-  if (!thresholds) return;
+  if (!dbConfig.alarmThresholds) return;
   const createAlarm = ({
     name,
     metric,
@@ -44,7 +53,7 @@ export function addDBClusterPerformanceAlarms(
   createAlarm({
     metric: dbCluster.metricFreeableMemory(),
     name: "FreeableMemoryAlarm",
-    threshold: mbToBytes(thresholds.freeableMemoryMb),
+    threshold: mbToBytes(dbConfig.alarmThresholds.freeableMemoryMb),
     evaluationPeriods: 1,
     comparisonOperator: cloudwatch.ComparisonOperator.LESS_THAN_OR_EQUAL_TO_THRESHOLD,
     treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
@@ -53,7 +62,7 @@ export function addDBClusterPerformanceAlarms(
   createAlarm({
     metric: dbCluster.metricCPUUtilization(),
     name: "CPUUtilizationAlarm",
-    threshold: thresholds.cpuUtilizationPct,
+    threshold: dbConfig.alarmThresholds.cpuUtilizationPct,
     evaluationPeriods: 1,
     treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
   });
@@ -61,7 +70,7 @@ export function addDBClusterPerformanceAlarms(
   createAlarm({
     metric: dbCluster.metricVolumeReadIOPs(),
     name: "VolumeReadIOPsAlarm",
-    threshold: thresholds.volumeReadIops,
+    threshold: dbConfig.alarmThresholds.volumeReadIops,
     evaluationPeriods: 1,
     treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
   });
@@ -69,7 +78,7 @@ export function addDBClusterPerformanceAlarms(
   createAlarm({
     metric: dbCluster.metricVolumeWriteIOPs(),
     name: "VolumeWriteIOPsAlarm",
-    threshold: thresholds.volumeWriteIops,
+    threshold: dbConfig.alarmThresholds.volumeWriteIops,
     evaluationPeriods: 1,
     treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
   });
@@ -77,7 +86,7 @@ export function addDBClusterPerformanceAlarms(
   createAlarm({
     metric: dbCluster.metricACUUtilization(),
     name: "ACUUtilizationAlarm",
-    threshold: thresholds.acuUtilizationPct,
+    threshold: dbConfig.alarmThresholds.acuUtilizationPct,
     evaluationPeriods: 1,
     treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
   });
@@ -85,7 +94,7 @@ export function addDBClusterPerformanceAlarms(
   createAlarm({
     metric: dbCluster.metricDatabaseConnections(),
     name: "DatabaseConnectionsAlarm",
-    threshold: thresholds.databaseConnectionsCount,
+    threshold: getMaxPostgresConnections(dbConfig.maxCapacity),
     evaluationPeriods: 1,
     comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
     treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
@@ -99,7 +108,9 @@ export function addDBClusterPerformanceAlarms(
   createAlarm({
     metric: dbCluster.metricFreeLocalStorage(),
     name: "FreeLocalStorageAlarm",
-    threshold: mbToBytes(thresholds.freeLocalStorageMb ?? DEFAULT_MIN_LOCAL_STORAGE_MB_ALARM),
+    threshold: mbToBytes(
+      dbConfig.alarmThresholds.freeLocalStorageMb ?? DEFAULT_MIN_LOCAL_STORAGE_MB_ALARM
+    ),
     evaluationPeriods: 1,
     comparisonOperator: cloudwatch.ComparisonOperator.LESS_THAN_OR_EQUAL_TO_THRESHOLD,
     treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
