a19cbd80b1ad4bd6cbe80f96c55dea7f0a7901b0
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-02-21 10:24:37 -0800

    feat(ehr): revert to exact id matching
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/api/src/command/medical/patient/get-patient.ts
+++ packages/api/src/command/medical/patient/get-patient.ts
@@ -142,9 +142,9 @@ function getPatientsSharedQueryUntilFTS(
       ? ` AND (
         search_criteria @@ websearch_to_tsquery('english', :filters) 
         OR external_id ilike '%' || :filters || '%'
-        OR id ilike '%' || :filters || '%'
+        OR id = :filters
         OR id IN (SELECT patient_id FROM patient_mapping WHERE cx_id = :cxId and external_id ilike '%' || :filters || '%')
-        OR facility_ids && (SELECT array_agg(id) FROM facility WHERE cx_id = :cxId and (data->>'name' ilike '%' || :filters || '%' or id ilike '%' || :filters || '%'))
+        OR facility_ids && (SELECT array_agg(id) FROM facility WHERE cx_id = :cxId and (data->>'name' ilike '%' || :filters || '%' or id = :filters))
       )`
       : "");
 
