0ac1ffb17ecedf2ec6b3761e1e802c1eb3e9e2c5
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-02-21 12:26:30 -0800

    feat(infra): undo change to config type
    refs. metriport/metriport-internal#2716
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/infra/config/aws/rds.ts
+++ packages/infra/config/aws/rds.ts
@@ -5,10 +5,6 @@ export type RDSAlarmThresholds = {
   volumeReadIops: number;
   volumeWriteIops: number;
   /**
-   * Threshold for database connections count
-   */
-  databaseConnectionsCount: number;
-  /**
    * The amount of available storage in MB. Defaults to 10GB.
    */
   freeLocalStorageMb?: number;
