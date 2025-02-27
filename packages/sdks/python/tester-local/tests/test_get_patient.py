983189a62a84e5bd40808f0d165b1c86f443a5dd
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-02-27 11:45:24 -0800

    feat(fern): update fern spec to support new paginated list patients and deprecated list patients
    refs. metriport/metriport-internal#2706
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/sdks/python/tester-local/tests/test_get_patient.py
+++ packages/sdks/python/tester-local/tests/test_get_patient.py
@@ -11,8 +11,17 @@ api_key = os.environ.get("API_KEY")
 patient_id = os.environ.get("PATIENT_ID")
 base_url = os.environ.get("BASE_URL")
 
+def test_get_all_patients_paginated() -> None:
+    """
+    The function `get_all_patients` retrieves a list of all patients from a medical API and prints their
+    IDs.
+    """
+    metriport = Metriport(api_key=api_key, base_url=base_url)
+    response = metriport.medical.patient.list(count=5)
+    for patient in response.patients:
+        print(f"Received patient with ID: {patient.id}")
 
-def test_get_all_patients() -> None:
+def test_get_all_patients_unpaginated() -> None:
     """
     The function `get_all_patients` retrieves a list of all patients from a medical API and prints their
     IDs.
@@ -27,6 +36,6 @@ def test_get_specific_patient() -> None:
     The function `get_patient` retrieves a patient from a medical API and prints their
     ID.
     """
-    metriport = Metriport(api_key=api_key, base_url="http://localhost:8080")
+    metriport = Metriport(api_key=api_key, base_url=base_url)
     response = metriport.medical.patient.get(id=patient_id)
     print(f"Received specific patient with ID: {response.id}")
