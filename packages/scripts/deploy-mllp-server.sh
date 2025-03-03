a82b32474f0e99f9ecd8675c1a386c20c3edc23d
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-03-03 08:33:13 -0800

    feat(infra): add gh actions workflows and applicable deploy scripts for mllp server
    refs. metriport/metriport-internal#2706
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/scripts/deploy-mllp-server.sh
+++ packages/scripts/deploy-mllp-server.sh
@@ -19,12 +19,11 @@ if [[ -z "${ECS_SERVICE}" ]]; then
   exit 1
 fi
 
-
 # Build mllp-server tarball
-source ./packages/scripts/generate-tarball.sh api
+source ./packages/scripts/generate-tarball.sh mllp-server
 
 # Build and push Docker images
-source ./packages/scripts/push-image.sh api
+source ./packages/scripts/push-image.sh mllp-server
 
 # Restart ECS service
 source ./packages/scripts/restart-ecs.sh
---
+++ packages/scripts/generate-tarball.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Run from the root of the repository.
+
+if [[ $# -eq 0 ]]; then
+  echo "Error: Package name is required as first argument"
+  echo "Usage: $0 <package-name>"
+  exit 1
+fi
+
+if [[ -z "${AWS_REGION}" ]]; then
+  echo "AWS_REGION is missing"
+  exit 1
+fi
+if [[ -z "${ECR_REPO_URI}" ]]; then
+  echo "ECR_REPO_URI is missing"
+  exit 1
+fi
+
+# Fail on error
+set -e
+
+# Echo commands
+set -x
+
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REPO_URI
+
+GITHUB_SHA=$(git rev-parse --short HEAD)
+
+PACKAGE_NAME=$1
+PACKAGE_FOLDER=packages/$PACKAGE_NAME
+TARBALL_NAME=metriport-$PACKAGE_NAME.tar.gz
+PACKAGE_TARBALL=$PACKAGE_FOLDER/$TARBALL_NAME
+
+rm -rf ${PACKAGE_TARBALL}
+
+# Build server tarball
+tar \
+  --exclude='**/*.ts' \
+  --exclude='**/__tests__' \
+  --exclude='**/*.spec*' \
+  --exclude='**/*.test*' \
+  --exclude="**/${TARBALL_NAME}" \
+  -czf ${PACKAGE_TARBALL} \
+  package.json \
+  package-lock.json \
+  packages/shared/package.json \
+  packages/shared/dist \
+  packages/core/package.json \
+  packages/core/dist \
+  ${PACKAGE_FOLDER}/package.json \
+  ${PACKAGE_FOLDER}/dist
