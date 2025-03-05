b4ec564ff9f90002463e525c52ea65db1d84f807
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-03-05 14:27:51 -0800

    feat(ehr): unit test for new mapping method
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

---
+++ packages/api/src/command/__tests__/cx-mapping.test.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { getCxMappingSourceFromJwtTokenSource } from "../../command/mapping/cx";
+
+describe("cx-mapping command", () => {
+  describe("getCxMappingSourceFromJwtTokenSource", () => {
+    const validSources = [
+      "canvas",
+      "canvas-client",
+      "canvas-webhook",
+      "athenahealth",
+      "athenahealth-client",
+      "elation-client",
+      "elation-webhook",
+    ];
+
+    for (const source of validSources) {
+      it(`returns the correct source for ${source}`, () => {
+        const result = getCxMappingSourceFromJwtTokenSource(source);
+        expect(result).toEqual(source.split("-")[0]);
+      });
+    }
+
+    const invalidSources = ["bogus", "bogus-client", "bogus-webhook"];
+
+    for (const source of invalidSources) {
+      it(`throws an error for ${source}`, () => {
+        expect(() => getCxMappingSourceFromJwtTokenSource(source)).toThrow();
+      });
+    }
+  });
+});
