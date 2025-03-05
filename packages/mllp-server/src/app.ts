93b5d7e002947ca2408cfad18097e32b15208b6c
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-03-05 14:59:20 -0800

    fix(mllp): improve mllp tcp conn logs
    refs. metriport/metriport-internal#2759
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/mllp-server/src/app.ts
+++ packages/mllp-server/src/app.ts
@@ -14,7 +14,8 @@ const MLLP_DEFAULT_PORT = 2575;
 
 async function createHl7Server(logger: Logger): Promise<Hl7Server> {
   const server = new Hl7Server(connection => {
-    logger.log("Connection received");
+    const clientId = `${connection.socket.remoteAddress}:${connection.socket.remotePort}`;
+    logger.log(`Connection received from ${clientId}`);
 
     connection.addEventListener("message", ({ message }) => {
       logger.log(
@@ -27,15 +28,15 @@ async function createHl7Server(logger: Logger): Promise<Hl7Server> {
 
     connection.addEventListener("error", error => {
       if (error instanceof Error) {
-        logger.log("Connection error:", error);
+        logger.log(`Connection error from ${clientId}:`, error);
         Sentry.captureException(error);
       } else {
-        logger.log("Connection terminated by client");
+        logger.log(`Connection terminated by client ${clientId}`);
       }
     });
 
     connection.addEventListener("close", () => {
-      logger.log("Connection closed");
+      logger.log(`Connection closed by ${clientId}`);
     });
   });
 
@@ -44,8 +45,9 @@ async function createHl7Server(logger: Logger): Promise<Hl7Server> {
 
 async function main() {
   const logger = out("MLLP Server");
-  const server = await createHl7Server(logger);
+  logger.log(`[bootup] Starting server on port ${MLLP_DEFAULT_PORT}`);
 
+  const server = await createHl7Server(logger);
   server.start(MLLP_DEFAULT_PORT);
   if (!server.server) {
     throw new Error("Error starting server");
