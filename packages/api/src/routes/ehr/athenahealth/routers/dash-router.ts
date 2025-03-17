eab53f84a16c0edf52b5afd5045e1bd5c7d2abe5
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-03-17 10:43:52 -0700

    feat(ehr): adding back in handle params
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/api/src/routes/ehr/athenahealth/routers/dash-router.ts
+++ packages/api/src/routes/ehr/athenahealth/routers/dash-router.ts
@@ -1,4 +1,5 @@
 import Router from "express-promise-router";
+import { handleParams } from "../../../helpers/handle-params";
 import medicalDocument from "../../../medical/document";
 import medicalPatient from "../../../medical/patient";
 import { patientAuthorization } from "../../../middlewares/patient-authorization";
@@ -13,6 +14,7 @@ routes.use("/patient", patient);
 routes.use("/chart", chart);
 routes.use(
   "/medical/v1/patient/:id",
+  handleParams,
   processPatientRoute,
   patientAuthorization("query"),
   medicalPatient
