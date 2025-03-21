96d37c98cd0ffdd157be54f7082db459ac96701b
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-03-21 09:24:45 -0700

    feat(ehr): adding process patient boolean
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/api/src/routes/ehr/elation/patient-webhook.ts
+++ packages/api/src/routes/ehr/elation/patient-webhook.ts
@@ -1,8 +1,13 @@
+import { buildEhrSyncPatientHandler } from "@metriport/core/external/ehr/sync-patient/ehr-sync-patient-factory";
 import { out } from "@metriport/core/util/log";
+import { MetriportError } from "@metriport/shared";
+import { ElationSecondaryMappings } from "@metriport/shared/interface/external/ehr/elation/cx-mapping";
 import { elationPatientEventSchema } from "@metriport/shared/interface/external/ehr/elation/event";
+import { EhrSources } from "@metriport/shared/interface/external/ehr/source";
 import { Request, Response } from "express";
 import Router from "express-promise-router";
 import httpStatus from "http-status";
+import { getCxMappingOrFail } from "../../../command/mapping/cx";
 import { createOrUpdateElationPatientMetadata } from "../../../external/ehr/elation/command/sync-patient";
 import { handleParams } from "../../helpers/handle-params";
 import { requestLogger } from "../../helpers/request-logger";
@@ -33,11 +38,32 @@ router.post(
       log(`Patient event is not a created event for patient ${event.data.id}`);
       return res.sendStatus(httpStatus.OK);
     }
+    const cxMapping = await getCxMappingOrFail({
+      externalId: elationPracticeId,
+      source: EhrSources.elation,
+    });
+    if (!cxMapping.secondaryMappings) {
+      throw new MetriportError("Elation secondary mappings not found", undefined, {
+        externalId: elationPracticeId,
+        source: EhrSources.elation,
+      });
+    }
+    const secondaryMappings = cxMapping.secondaryMappings as ElationSecondaryMappings;
     await createOrUpdateElationPatientMetadata({
       cxId,
       elationPracticeId,
       elationPatientId: event.data.id,
     });
+    if (secondaryMappings.webhookPatientPatientProcessingEnabled) {
+      const handler = buildEhrSyncPatientHandler();
+      await handler.processSyncPatient({
+        ehr: EhrSources.elation,
+        cxId,
+        practiceId: elationPracticeId,
+        patientId: event.data.id,
+        triggerDq: true,
+      });
+    }
     return res.sendStatus(httpStatus.OK);
   })
 );
