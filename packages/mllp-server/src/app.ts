d931de4ed0ba293d92ca11570e98525dc162e0a7
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-03-24 11:41:05 -0700

    feat(mllp-server): write request logs to s3
    refs. metriport/metriport-internal#2757
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/mllp-server/src/app.ts
+++ packages/mllp-server/src/app.ts
@@ -4,23 +4,45 @@ import { out } from "@metriport/core/util/log";
 import * as dotenv from "dotenv";
 import * as Sentry from "@sentry/node";
 import { initSentry } from "./sentry";
+import { S3Utils } from "@metriport/core/src/external/aws/s3";
+import { Config } from "@metriport/core/src/util/config";
 
 dotenv.config();
 
 initSentry();
 
 const MLLP_DEFAULT_PORT = 2575;
+const s3Utils = new S3Utils(Config.getAWSRegion());
+
+function generateS3KeyPrefix(message: Hl7Message) {
+  const messageType = message.get("MSH.9.1");
+  const triggerEvent = message.get("MSH.9.2");
+
+  const sendingFacility = message.get("MSH.4.1");
+
+  const timestampRaw = message.get("MSH.7.1");
+  const timestampDate = timestampRaw.substring(0, 8); // YYYYMMDD
+  const timestampHour = timestampRaw.substring(8, 12); // HHMM
+
+  const controlId = message.get("MSH.10");
+
+  return `${messageType}/${triggerEvent}/${sendingFacility}/${timestampDate}_${timestampHour}_${controlId}.hl7`;
+}
 
 async function createHl7Server(logger: Logger): Promise<Hl7Server> {
   const server = new Hl7Server(connection => {
     logger.log("Connection received");
 
-    connection.addEventListener("message", ({ message }) => {
+    connection.addEventListener("message", async ({ message }) => {
       logger.log(
         `## New Message from ${connection.socket.remoteAddress}:${connection.socket.remotePort} ##`
       );
-      // TODO(lucas|2757|2025-03-05): Write request log to S3
       // TODO(lucas|2758|2025-03-05): Enqueue message for pickup
+      await s3Utils.uploadFile({
+        bucket: "metriport-hl7v2-messages",
+        key: generateS3KeyPrefix(message),
+        file: Buffer.from(message.toString()),
+      });
       connection.send(message.buildAck());
     });
 
