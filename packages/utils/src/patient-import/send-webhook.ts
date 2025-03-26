bd0b40781dabdf2e936286924285a21edd49a2ed
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-03-26 17:33:13 -0300

    fix(util): send WH script using env vars
    Ref. metriport/metriport-internal#2330
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/utils/src/patient-import/send-webhook.ts
+++ packages/utils/src/patient-import/send-webhook.ts
@@ -2,35 +2,35 @@ import * as dotenv from "dotenv";
 dotenv.config();
 // keep that ^ on top
 import { S3Utils } from "@metriport/core/external/aws/s3";
-import { sleep } from "@metriport/shared";
+import { getEnvVarOrFail, sleep } from "@metriport/shared";
+import { PatientImportStatus } from "@metriport/shared/domain/patient/patient-import/types";
 import { WebhookBulkPatientImportPayload, WebhookMetadata } from "@metriport/shared/medical";
 import dayjs from "dayjs";
 import { v4 as uuidv4 } from "uuid";
 import { sendPayload } from "../../../api/src/command/webhook/webhook";
 import { CreateWebhookRequestCommand } from "../../../api/src/command/webhook/webhook-request";
 import { elapsedTimeAsStr } from "../shared/duration";
-import { PatientImportStatus } from "@metriport/shared/domain/patient/patient-import/types";
 
 /**
  * Temporary script to send a webhook request as the result of a bulk patient import.
  * This is a temporary script to be used until the bulk patient import is fully integrated with the webhook.
  */
 
-// Update these
-const cxId = "";
-const jobId = "";
-const webhookUrl = "";
-const webhookKey = "";
 // Update to "processing" if you want to send the webhook when the import is initialized
 const bulkPatientCreateStatus: PatientImportStatus = "completed";
 
+const cxId = getEnvVarOrFail("CX_ID");
+const jobId = getEnvVarOrFail("PATIENT_IMPORT_JOB_ID");
+const webhookUrl = getEnvVarOrFail("CX_WH_URL");
+const webhookKey = getEnvVarOrFail("METRIPORT_WH_KEY");
+const s3BucketName = getEnvVarOrFail("PATIENT_IMPORT_BUCKET_NAME");
+const awsRegion = getEnvVarOrFail("AWS_REGION");
+
 // Update if needed
 const resultFileKeyName = `patient-import/cxid=${cxId}/jobid=${jobId}/files/result.csv`;
-const s3BucketName = "metriport-patient-import-production";
-const awsRegion = "us-west-1";
 
-const presignedResultUrlDuration = dayjs.duration(3, "minutes");
-
+const presignedResultUrlDuration = dayjs.duration(10, "minutes");
+const confirmationTime = dayjs.duration(10, "seconds");
 const s3Utils = new S3Utils(awsRegion);
 
 async function main() {
@@ -76,6 +76,11 @@ async function main() {
     meta,
     ...payload,
   };
+
+  console.log(`Will send WH in ${confirmationTime.asSeconds()} seconds...`);
+  console.log(`- URL: ${webhookUrl}`);
+  console.log(`- Payload to send: ${JSON.stringify(fullPayload, undefined, 2)}`);
+  await sleep(confirmationTime.asMilliseconds());
 
   const res = await sendPayload(fullPayload, webhookUrl, webhookKey);
   console.log(`>>> res: ${JSON.stringify(res, undefined, 2)}`);
