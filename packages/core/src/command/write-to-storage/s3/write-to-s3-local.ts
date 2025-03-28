07e667aaf458fbb4f5b5bc73ad5ea058ad7c0ee9
Thomas Yopes <thomasyopes@Thomass-MacBook-Pro.local>
2025-03-28 15:58:23 -0700

    feat(utils): remove captures
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MacBook-Pro.local>

--- packages/core/src/command/write-to-storage/s3/write-to-s3-local.ts
+++ packages/core/src/command/write-to-storage/s3/write-to-s3-local.ts
@@ -1,8 +1,5 @@
-import { errorToString } from "@metriport/shared";
 import { S3Utils } from "../../../external/aws/s3";
 import { Config } from "../../../util/config";
-import { out } from "../../../util/log";
-import { capture } from "../../../util/notifications";
 import { uuidv7 } from "../../../util/uuid-v7";
 import { ProcessWriteToS3Handler, ProcessWriteToS3Request } from "./write-to-s3";
 
@@ -11,51 +8,13 @@ const s3Utils = new S3Utils(region);
 
 export class ProcessWriteToS3Local implements ProcessWriteToS3Handler {
   async processWriteToS3(params: ProcessWriteToS3Request): Promise<void> {
-    const { serviceId, bucket, filePath, key, contentType, metadata, payload } = params;
-    const { log } = out(
-      `processWriteToS3.cloud - serviceId ${serviceId} destination ${`${bucket}/${key}`}`
-    );
-    const file = createFile(serviceId, payload);
-    if (!file) return;
-    try {
-      await s3Utils.uploadFile({
-        bucket,
-        key: `${filePath}/${key ?? `${uuidv7()}.json`}`,
-        file,
-        ...(contentType ? { contentType } : undefined),
-        ...(metadata ? { metadata } : undefined),
-      });
-    } catch (error) {
-      const msg = `Failure while processing patient create @ PatientImport`;
-      log(`${msg}. Cause: ${errorToString(error)}`);
-      capture.error(msg, {
-        extra: {
-          serviceId,
-          bucket,
-          key,
-          context: "write-to-s3-local.processWriteToS3",
-          error,
-        },
-      });
-      throw error;
-    }
-  }
-}
-
-export function createFile(serviceId: string, payload: string): Buffer | undefined {
-  const { log } = out(`createFile.cloud - serviceId ${serviceId}`);
-  try {
-    return Buffer.from(payload);
-  } catch (error) {
-    const msg = `Failure while processing patient create @ PatientImport`;
-    log(`${msg}. Cause: ${errorToString(error)}`);
-    capture.error(msg, {
-      extra: {
-        serviceId,
-        context: "processWriteToS3Bulk.cloud",
-        error,
-      },
+    const { bucket, filePath, key, contentType, metadata, payload } = params;
+    await s3Utils.uploadFile({
+      bucket,
+      key: `${filePath}/${key ?? `${uuidv7()}.json`}`,
+      file: Buffer.from(payload),
+      ...(contentType ? { contentType } : undefined),
+      ...(metadata ? { metadata } : undefined),
     });
-    return undefined;
   }
 }
