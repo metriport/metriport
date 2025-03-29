e873f0f578953136eb5cc6d76c4f5274730a1364
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-03-29 13:14:32 -0700

    feat(api): remove all fhir server admin code
    refs. metriport/metriport-internal#2842
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/core/src/external/fhir/api/api-factory.ts
+++ packages/core/src/external/fhir/api/api-factory.ts
@@ -1,5 +1,5 @@
-import { FhirAdminClient, FhirClient } from "./api";
-import { HapiFhirAdminClient, HapiFhirClient } from "./api-hapi";
+import { FhirClient } from "./api";
+import { HapiFhirClient } from "./api-hapi";
 
 /**
  * Return an instance of the FHIR API client configured to access the respective
@@ -7,12 +7,3 @@ import { HapiFhirAdminClient, HapiFhirClient } from "./api-hapi";
  */
 export const makeFhirApi = (cxId: string, baseUrl: string): FhirClient =>
   new HapiFhirClient(cxId, baseUrl);
-
-/**
- * WARNING: THIS IS FOR TENANT MANAGEMENT ONLY!
- *
- * Returns an instance of the FHIR API client configured to access the
- * default tenant, which can be used to manage tenants on the FHIR server.
- */
-export const makeFhirAdminApi = (baseUrl: string): FhirAdminClient =>
-  new HapiFhirAdminClient(baseUrl);
