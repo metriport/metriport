499f4f43995ddcbe8fe2334f1d868b97adbb636d
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-03-31 11:10:57 -0700

    feat(utils:): adding sqs client to constructor
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/core/src/command/write-to-storage/s3/write-to-s3-cloud.ts
+++ packages/core/src/command/write-to-storage/s3/write-to-s3-cloud.ts
@@ -3,20 +3,22 @@ import { SQSClient } from "../../../external/aws/sqs";
 import { Config } from "../../../util/config";
 import { S3Writer, WriteToS3Request } from "./write-to-s3";
 
-const region = Config.getAWSRegion();
-const sqsClient = new SQSClient({ region });
-
 /** ---------------------------------------------------------------------------
  * This class is used to write to S3 in a cloud environment via SQS. The max
  * payload size is 256KB.
  */
 export class S3WriterCloud implements S3Writer {
-  constructor(private readonly writeToS3QueueUrl: string) {}
+  private readonly sqsClient: SQSClient;
+
+  constructor(private readonly writeToS3QueueUrl: string) {
+    const region = Config.getAWSRegion();
+    this.sqsClient = new SQSClient({ region });
+  }
 
   async writeToS3(params: WriteToS3Request): Promise<void> {
     await Promise.all(
       params.map(p => {
-        sqsClient.sendMessageToQueue(this.writeToS3QueueUrl, p.payload, {
+        this.sqsClient.sendMessageToQueue(this.writeToS3QueueUrl, p.payload, {
           fifo: true,
           messageDeduplicationId: createUuidFromText(p.payload),
           messageGroupId: p.serviceId,
