09d0d2f97203816f55dc9dc5a7c8cca92998f0b9
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-03-31 14:34:08 -0700

    feat(utils:): params for factory
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/core/src/command/write-to-storage/s3/write-to-s3-cloud.ts
+++ packages/core/src/command/write-to-storage/s3/write-to-s3-cloud.ts
@@ -10,9 +10,12 @@ import { S3Writer, WriteToS3Request } from "./write-to-s3";
 export class S3WriterCloud implements S3Writer {
   private readonly sqsClient: SQSClient;
 
-  constructor(private readonly writeToS3QueueUrl: string, region?: string) {
-    const clientRegion = region ?? Config.getAWSRegion();
-    this.sqsClient = new SQSClient({ region: clientRegion });
+  constructor(private readonly writeToS3QueueUrl: string, region?: string, sqsClient?: SQSClient) {
+    if (!sqsClient) {
+      this.sqsClient = new SQSClient({ region: region ?? Config.getAWSRegion() });
+    } else {
+      this.sqsClient = sqsClient;
+    }
   }
 
   async writeToS3(params: WriteToS3Request): Promise<void> {
