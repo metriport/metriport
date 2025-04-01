046a183c9a5b458c9867c2434e856e7fd1a910e9
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-04-01 10:46:01 -0300

    build: run each package's test in parallel
    Ref. metriport/metriport-internal#1040
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

---
+++ packages/scripts/run-tests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Don't fail on error
+set +e
+# Don't echo commands
+set +x
+
+# Read workspaces from package.json and populate packages array
+packages=($(cat package.json | grep -o '"packages/[^"]*"' | sed 's/"packages\///g' | sed 's/"//g'))
+
+set -x
+# Run tests for each package
+parallel --halt never,fail=1 --keep-order --line-buffer 'FORCE_COLOR=true npm run test -w packages/{}' ::: "${packages[@]}"
+set +x
+
+if [ $? -eq 0 ]; then
+  echo -e "\n\033[1;32mTests passed\033[0m"
+else
+  echo -e "\n\033[1;31m>>> Tests failed <<<\\033[0m"
+  exit 1
+fi
