a92db12d439d3df3b0974efce30b4c8c0c886130
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-04-04 14:18:19 -0700

    feat(canvas): going to single dedup flow
    Ref: #2850
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/core/src/external/ehr/resource-diff/utils.ts
+++ packages/core/src/external/ehr/resource-diff/utils.ts
@@ -17,7 +17,7 @@ import {
   RelatedPerson,
   Resource,
 } from "@medplum/fhirtypes";
-import { MetriportError } from "@metriport/shared";
+import { BadRequestError } from "@metriport/shared";
 import { deduplicateAllergyIntolerances } from "../../../fhir-deduplication/resources/allergy-intolerance";
 import { deduplicateCompositions } from "../../../fhir-deduplication/resources/composition";
 import { deduplicateConditions } from "../../../fhir-deduplication/resources/condition";
@@ -34,6 +34,7 @@ import { deduplicateOrganizations } from "../../../fhir-deduplication/resources/
 import { deduplicatePractitioners } from "../../../fhir-deduplication/resources/practitioner";
 import { deduplicateProcedures } from "../../../fhir-deduplication/resources/procedure";
 import { deduplicateRelatedPersons } from "../../../fhir-deduplication/resources/related-person";
+import { artifactRelatedArtifactUrl } from "../../../fhir-deduplication/shared";
 import { ResourceWithId } from "./compute/ehr-compute-resource-diff";
 
 export function computeResourceDiff({
@@ -43,132 +44,114 @@ export function computeResourceDiff({
   existingResources: ResourceWithId[];
   newResource: ResourceWithId;
 }): string[] {
-  const resourceType = newResource.resourceType;
-  const resourceAlreadyExists = existingResources.find(resource => resource.id === newResource.id);
-  if (resourceAlreadyExists) return [];
+  const mewResourceType = newResource.resourceType;
+  const invalidExistingResources = existingResources.filter(
+    resource => resource.resourceType !== mewResourceType
+  );
+  if (invalidExistingResources.length > 0) {
+    throw new BadRequestError("Invalid existing resource types", undefined, {
+      invalidExistingResourceIds: invalidExistingResources.map(resource => resource.id).join(","),
+      invalidExistingResourceTypes: invalidExistingResources
+        .map(resource => resource.resourceType)
+        .join(","),
+    });
+  }
+  const newResourceAlreadyExists = existingResources.find(
+    resource => resource.id === newResource.id
+  );
+  if (newResourceAlreadyExists) return [];
+
   const resources = existingResources.concat([newResource]);
-  let deduplicatedResourceInitial: Resource[];
   let deduplicatedResources: Resource[];
-  switch (resourceType) {
+  switch (mewResourceType) {
     case "AllergyIntolerance":
-      deduplicatedResourceInitial = deduplicateAllergyIntolerances(
-        existingResources as AllergyIntolerance[]
-      ).combinedResources;
       deduplicatedResources = deduplicateAllergyIntolerances(
         resources as AllergyIntolerance[]
       ).combinedResources;
       break;
     case "Composition":
-      deduplicatedResourceInitial = deduplicateCompositions(
-        existingResources as Composition[]
-      ).combinedResources;
       deduplicatedResources = deduplicateCompositions(resources as Composition[]).combinedResources;
       break;
     case "Condition":
-      deduplicatedResourceInitial = deduplicateConditions(
-        existingResources as Condition[]
-      ).combinedResources;
       deduplicatedResources = deduplicateConditions(resources as Condition[]).combinedResources;
       break;
     case "Encounter":
-      deduplicatedResourceInitial = deduplicateEncounters(
-        existingResources as Encounter[]
-      ).combinedResources;
       deduplicatedResources = deduplicateEncounters(resources as Encounter[]).combinedResources;
       break;
     case "FamilyMemberHistory":
-      deduplicatedResourceInitial = deduplicateFamilyMemberHistories(
-        existingResources as FamilyMemberHistory[]
-      ).combinedResources;
       deduplicatedResources = deduplicateFamilyMemberHistories(
         resources as FamilyMemberHistory[]
       ).combinedResources;
       break;
     case "Immunization":
-      deduplicatedResourceInitial = deduplicateImmunizations(
-        existingResources as Immunization[]
-      ).combinedResources;
       deduplicatedResources = deduplicateImmunizations(
         resources as Immunization[]
       ).combinedResources;
       break;
     case "Location":
-      deduplicatedResourceInitial = deduplicateLocations(
-        existingResources as Location[]
-      ).combinedResources;
       deduplicatedResources = deduplicateLocations(resources as Location[]).combinedResources;
       break;
     case "Medication":
-      deduplicatedResourceInitial = deduplicateMedications(
-        existingResources as Medication[]
-      ).combinedResources;
       deduplicatedResources = deduplicateMedications(resources as Medication[]).combinedResources;
       break;
     case "MedicationAdministration":
-      deduplicatedResourceInitial = deduplicateMedAdmins(
-        existingResources as MedicationAdministration[]
-      ).combinedResources;
       deduplicatedResources = deduplicateMedAdmins(
         resources as MedicationAdministration[]
       ).combinedResources;
       break;
     case "MedicationRequest":
-      deduplicatedResourceInitial = deduplicateMedRequests(
-        existingResources as MedicationRequest[]
-      ).combinedResources;
       deduplicatedResources = deduplicateMedRequests(
         resources as MedicationRequest[]
       ).combinedResources;
       break;
     case "MedicationStatement":
-      deduplicatedResourceInitial = deduplicateMedStatements(
-        existingResources as MedicationStatement[]
-      ).combinedResources;
       deduplicatedResources = deduplicateMedStatements(
         resources as MedicationStatement[]
       ).combinedResources;
       break;
     case "Observation":
-      deduplicatedResourceInitial = deduplicateObservations(
-        existingResources as Observation[]
-      ).combinedResources;
       deduplicatedResources = deduplicateObservations(resources as Observation[]).combinedResources;
       break;
     case "Organization":
-      deduplicatedResourceInitial = deduplicateOrganizations(
-        existingResources as Organization[]
-      ).combinedResources;
       deduplicatedResources = deduplicateOrganizations(
         resources as Organization[]
       ).combinedResources;
       break;
     case "Practitioner":
-      deduplicatedResourceInitial = deduplicatePractitioners(
-        existingResources as Practitioner[]
-      ).combinedResources;
       deduplicatedResources = deduplicatePractitioners(
         resources as Practitioner[]
       ).combinedResources;
       break;
     case "Procedure":
-      deduplicatedResourceInitial = deduplicateProcedures(
-        existingResources as Procedure[]
-      ).combinedResources;
       deduplicatedResources = deduplicateProcedures(resources as Procedure[]).combinedResources;
       break;
     case "RelatedPerson":
-      deduplicatedResourceInitial = deduplicateRelatedPersons(
-        existingResources as RelatedPerson[]
-      ).combinedResources;
       deduplicatedResources = deduplicateRelatedPersons(
         resources as RelatedPerson[]
       ).combinedResources;
       break;
     default:
-      throw new MetriportError(`Unsupported resource type: ${resourceType}`, undefined, {
-        resourceType,
+      throw new BadRequestError(`Unsupported resource type: ${mewResourceType}`, undefined, {
+        resourceType: mewResourceType,
       });
   }
-  if (deduplicatedResources.length > deduplicatedResourceInitial.length) return [];
-  return existingResources.map(resource => resource.id);
+
+  const existingResourceIds = existingResources.map(resource => resource.id);
+  const newResourceInDeduplicatedResources = deduplicatedResources.find(
+    resource => resource.id === newResource.id
+  );
+  if (!newResourceInDeduplicatedResources) return existingResourceIds;
+  const newResourceIsDerivedFromExistingResource = resourceIsDerivedFromExistingResource(
+    newResourceInDeduplicatedResources
+  );
+  if (newResourceIsDerivedFromExistingResource) return existingResourceIds;
+  return [];
+}
+
+function resourceIsDerivedFromExistingResource(resource: Resource): boolean {
+  if (!("extension" in resource)) return false;
+  const derivedFrom = resource.extension.find(
+    extension => extension.url === artifactRelatedArtifactUrl
+  );
+  return derivedFrom !== undefined;
 }
