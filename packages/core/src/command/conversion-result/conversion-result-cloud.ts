2b6e8d31f1bdd821d2a206a0e71ebd16872fa0d9
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-04-04 21:39:22 -0300

    refactor: remove error handling from ConversionResultCloud
    Ref. metriport/metriport-internal#1040
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/core/src/command/conversion-result/conversion-result-cloud.ts
+++ packages/core/src/command/conversion-result/conversion-result-cloud.ts
@@ -1,7 +1,4 @@
-import { errorToString } from "@metriport/shared";
 import { SQSClient } from "../../external/aws/sqs";
-import { out } from "../../util/log";
-import { capture } from "../../util/notifications";
 import { ConversionResult, ConversionResultHandler } from "./types";
 
 export class ConversionResultCloud implements ConversionResultHandler {
@@ -11,26 +8,8 @@ export class ConversionResultCloud implements ConversionResultHandler {
     this.sqsClient = new SQSClient({ region });
   }
 
-  async notifyApi(params: ConversionResult, logParam?: typeof console.log): Promise<void> {
-    const { cxId, jobId } = params;
-    const { log } = logParam
-      ? { log: logParam }
-      : out(`notifyApi.cloud - cxId ${cxId} jobId ${jobId}`);
-    try {
-      const payload = JSON.stringify(params);
-      await this.sqsClient.sendMessageToQueue(this.conversionResultQueueUrl, payload);
-    } catch (error) {
-      const msg = `Failure while processing conversion result @ ConversionResult`;
-      log(`${msg}. Cause: ${errorToString(error)}`);
-      capture.error(msg, {
-        extra: {
-          cxId,
-          jobId,
-          context: "conversion-result-cloud.notifyApi",
-          error,
-        },
-      });
-      throw error;
-    }
+  async notifyApi(params: ConversionResult): Promise<void> {
+    const payload = JSON.stringify(params);
+    await this.sqsClient.sendMessageToQueue(this.conversionResultQueueUrl, payload);
   }
 }
