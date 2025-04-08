c701a6255e1c67be276b2dc89f7aaec7011b96d9
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-04-08 14:36:55 -0700

    feat(infra): catch errors in handlers
    refs. metriport/metriport-internal#2879
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/mllp-server/src/utils.ts
+++ packages/mllp-server/src/utils.ts
@@ -4,6 +4,8 @@ dotenv.config();
 import { unpackUuid } from "@metriport/core/util/pack-uuid";
 import { Base64Scrambler } from "@metriport/core/util/base64-scrambler";
 import { Config } from "@metriport/core/util/config";
+import * as Sentry from "@sentry/node";
+import { Logger } from "@metriport/core/util/log";
 
 const crypto = new Base64Scrambler(Config.getHl7Base64ScramblerSeed());
 
@@ -35,3 +37,17 @@ export function buildS3Key({
 }) {
   return `${cxId}/${patientId}/${timestamp}_${messageType}_${messageCode}.hl7`;
 }
+
+export const withErrorHandling = <T>(
+  handler: (data: T) => void,
+  logger: Logger
+): ((data: T) => Promise<void>) => {
+  return async (data: T) => {
+    try {
+      await handler(data);
+    } catch (error) {
+      logger.log(`Error in handler: ${error}`);
+      Sentry.captureException(error);
+    }
+  };
+};
