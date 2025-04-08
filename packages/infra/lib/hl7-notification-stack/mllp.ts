c988aa5728f0b5a2ef6292f96db8b95620967d65
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-04-08 13:30:18 -0700

    feat(infra): rename
    refs. metriport/metriport-internal#2879
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/infra/lib/hl7-notification-stack/mllp.ts
+++ packages/infra/lib/hl7-notification-stack/mllp.ts
@@ -7,7 +7,7 @@ import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import { Construct } from "constructs";
 import { EnvConfigNonSandbox } from "../../config/env-config";
 import { buildSecrets, secretsToECS } from "../shared/secrets";
-import { MLLP_DEFAULT_PORT, MLLP_SERVER_NLB_PRIVATE_IP } from "./constants";
+import { MLLP_DEFAULT_PORT, MLLP_SERVER_NLB_INTERNAL_IP } from "./constants";
 
 interface MllpStackProps extends cdk.StackProps {
   config: EnvConfigNonSandbox;
@@ -63,7 +63,7 @@ export class MllpStack extends cdk.NestedStack {
     (nlb.node.defaultChild as elbv2.CfnLoadBalancer).subnetMappings = [
       {
         subnetId: privateSubnet.subnetId,
-        privateIPv4Address: MLLP_SERVER_NLB_PRIVATE_IP,
+        privateIPv4Address: MLLP_SERVER_NLB_INTERNAL_IP,
       },
     ];
 
