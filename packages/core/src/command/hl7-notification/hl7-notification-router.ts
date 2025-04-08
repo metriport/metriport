eeb2c299ab1ddb94f7635769b8203ca0a45c710c
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-04-07 17:04:57 -0700

    feat(core): add hl7 notification lambda boilerplate
    refs. metriport/metriport-internal#2758
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

---
+++ packages/core/src/command/hl7-notification/hl7-notification-router.ts
@@ -0,0 +1,9 @@
+export type Hl7Notification = {
+  cxId: string;
+  patientId: string;
+  message: string;
+};
+
+export interface Hl7NotificationRouter {
+  execute(request: Hl7Notification): Promise<void>;
+}
