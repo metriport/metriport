8963244b4c8703398bf8723db53ae3a8b7a77f1c
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-04-10 10:06:19 -0700

    fix(core): fix import
    refs. metriport/metriport-internal#2758
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/core/src/command/hl7-notification/hl7-notification-router-factory.ts
+++ packages/core/src/command/hl7-notification/hl7-notification-router-factory.ts
@@ -1,7 +1,7 @@
 import { Config } from "../../util/config";
 import { Hl7NotificationRouter } from "./hl7-notification-router";
 import { Hl7NotificationRouterCloud } from "./hl7-notification-router-cloud";
-import { Hl7NotificationRouterDirect } from "./hl7-notification-router-local";
+import { Hl7NotificationRouterDirect } from "./hl7-notification-router-direct";
 
 export function buildHl7NotificationRouter(): Hl7NotificationRouter {
   if (Config.isDev()) return new Hl7NotificationRouterDirect();
