470f5c75bdf3e4b5851a093182ea074ab8622ba9
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-04-10 09:54:22 -0700

    fix(core): rename local to direct
    refs. metriport/metriport-internal#2758
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/lambdas/src/hl7-notification-router.ts
+++ packages/lambdas/src/hl7-notification-router.ts
@@ -1,5 +1,5 @@
 import { capture } from "./shared/capture";
-import { Hl7NotificationRouterLocal } from "@metriport/core/command/hl7-notification/hl7-notification-router-local";
+import { Hl7NotificationRouterDirect } from "@metriport/core/command/hl7-notification/hl7-notification-router-direct";
 import { Hl7Notification } from "@metriport/core/command/hl7-notification/hl7-notification-router";
 
 // Keep this as early on the file as possible
@@ -13,5 +13,5 @@ export async function handler(params: Hl7Notification): Promise<void> {
     context: "hl7-notification-router-cloud.execute",
   });
 
-  new Hl7NotificationRouterLocal().execute(params);
+  new Hl7NotificationRouterDirect().execute(params);
 }
