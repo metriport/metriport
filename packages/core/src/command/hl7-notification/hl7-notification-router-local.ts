75c3de45b3c95db110ce91a21980f80f98c67a8a
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-04-10 07:27:20 -0700

    feat(infra): add 'messageReceivedTimestamp' to queue
    refs. metriport/metriport-internal#2758
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

---
+++ packages/core/src/command/hl7-notification/hl7-notification-router-local.ts
@@ -0,0 +1,20 @@
+import { out } from "../../util/log";
+import { Hl7Notification, Hl7NotificationRouter } from "./hl7-notification-router";
+
+export class Hl7NotificationRouterLocal implements Hl7NotificationRouter {
+  private readonly lambdaName = "hl7-notification-router-local";
+  private readonly log;
+
+  constructor() {
+    const { log } = out(this.lambdaName);
+    this.log = log;
+  }
+
+  async execute(params: Hl7Notification): Promise<void> {
+    const { cxId, patientId, messageReceivedTimestamp } = params;
+
+    this.log(
+      `[${messageReceivedTimestamp}] Invoking execute for cxId ${cxId} + patientId ${patientId}`
+    );
+  }
+}
