5c48acddf367c0b4b71b293949cf624111c2cd9f
Syed Husain <syed@metriport.com>
2025-04-12 17:52:14 -0700

    feat:  adding error handling + modifying the utility function to better handle < 1900 years
    refs. metriport/metriport-internal#1853
    
    Signed-off-by: Syed Husain <syed@metriport.com>

--- packages/api/src/command/medical/patient/shared.ts
+++ packages/api/src/command/medical/patient/shared.ts
@@ -8,6 +8,7 @@ import { cloneDeep } from "lodash";
 import { PatientCreateCmd } from "./create-patient";
 import { PatientMatchCmd } from "./get-patient";
 import { PatientUpdateCmd } from "./update-patient";
+import { BadRequestError } from "@metriport/shared/dist/error/bad-request";
 
 export function sanitize<T extends PatientCreateCmd | PatientUpdateCmd | PatientMatchCmd>(
   patient: T
@@ -22,7 +23,14 @@ export function validate<T extends PatientCreateCmd | PatientUpdateCmd | Patient
 ): boolean {
   if (!patient.address || patient.address.length < 1) return false;
   patient.personalIdentifiers?.forEach(pid => pid.period && validatePeriod(pid.period));
-  validateDateOfBirth(patient.dob);
+
+  if (patient.dob) {
+    try {
+      if (!validateDateOfBirth(patient.dob)) return false;
+    } catch (error) {
+      throw new BadRequestError(`Invalid date of birth`);
+    }
+  }
   return true;
 }
 
