c7f9e5c0da88d94a7a9a61a555a00b2317531d78
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-04-15 08:19:28 -0700

    feat(shared): add timer
    refs. metriport/metriport-internal#2758
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/shared/src/common/timer.ts
+++ packages/shared/src/common/timer.ts
@@ -1,5 +1,18 @@
 import { elapsedTimeFromNow } from "./date";
 
+/**
+ * Creates a timer object that tracks elapsed time from initialization.
+ * @returns An object with methods to:
+ * - Get the start time (`getStartedAt`)
+ * - Get the elapsed time since initialization (`getElapsedTime`)
+ *
+ * @example
+ * ```ts
+ * const timer = initTimer();
+ * // ... do some work ...
+ * console.log(`Operation took ${timer.getElapsedTime()}ms`);
+ * ```
+ */
 export function initTimer() {
   const startedAt = new Date();
   return {
