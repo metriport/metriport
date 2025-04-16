73d0b23e0fb193c094a8216d511792dc0b6e1ec1
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-04-15 23:09:49 -0300

    refactor: rename update-status to tracking +
    - rename update-status to tracking
    - document disableWH applies to all WHs
    
    Ref. metriport/metriport-internal#2330
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/api/src/routes/internal/medical/patient.ts
+++ packages/api/src/routes/internal/medical/patient.ts
@@ -69,7 +69,7 @@ import {
   CreatePatientImportMappingCmd,
 } from "../../../command/medical/patient/patient-import/mapping/create";
 import { updatePatientImportParams } from "../../../command/medical/patient/patient-import/update-params";
-import { updatePatientImportTracking } from "../../../command/medical/patient/patient-import/update-status";
+import { updatePatientImportTracking } from "../../../command/medical/patient/patient-import/update-tracking";
 import {
   PatientUpdateCmd,
   updatePatientWithoutHIEs,
@@ -1033,7 +1033,8 @@ router.post(
  *        on this PD run.
  * @param req.query.triggerConsolidated - Optional; Whether to force get consolidated PDF on
  *        conversion finish.
- * @param req.query.disableWebhooks Optional: Indicates whether send webhooks.
+ * @param req.query.disableWebhooks Optional: Indicates whether send webhooks, applies to both
+ *        the data pipeline and the bulk import webhooks.
  * @param req.query.dryRun Whether to simply validate or run the assessment.
  *
  */
@@ -1075,7 +1076,8 @@ router.post(
  * @param req.query.cxId The customer ID.
  * @param req.query.rerunPdOnNewDemographics Optional: Indicates whether to use demo augmentation on this PD run.
  * @param req.query.triggerConsolidated - Optional; Whether to force get consolidated PDF on conversion finish.
- * @param req.query.disableWebhooks Optional: Indicates whether send webhooks.
+ * @param req.query.disableWebhooks Optional: Indicates whether send webhooks, applies to both
+ *        the data pipeline and the bulk import webhooks.
  * @param req.query.forceStatusUpdate Optional: Indicates whether to bypass the job status validation (state machine).
  * @param req.query.dryRun Whether to simply validate or run the assessment, overrides the cx
  *                         provided one (optional, defaults to cx provided or false if none provides it).
@@ -1125,7 +1127,8 @@ router.post(
  *
  * @param req.params.id The patient import job ID.
  * @param req.query.cxId The customer ID.
- * @param req.query.disableWebhooks Optional: Indicates whether send webhooks.
+ * @param req.query.disableWebhooks Optional: Indicates whether send webhooks, applies to both
+ *        the data pipeline and the bulk import webhooks.
  */
 router.post(
   "/bulk/:id/done",
