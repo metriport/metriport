e4d33d6fc3f5336437a391264a5775b6275efa72
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-04-21 17:26:05 -0700

    feat(core): initial fix for undefined message
    refs. metriport/metriport-internal#2758
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/lambdas/src/hl7-notification-webhook-sender.ts
+++ packages/lambdas/src/hl7-notification-webhook-sender.ts
@@ -1,20 +1,49 @@
 import { Hl7Notification } from "@metriport/core/command/hl7-notification/hl7-notification-webhook-sender";
+import { SQSEvent } from "aws-lambda";
 import { Hl7NotificationWebhookSenderDirect } from "@metriport/core/command/hl7-notification/hl7-notification-webhook-sender-direct";
 import { capture } from "./shared/capture";
 import * as Sentry from "@sentry/serverless";
+import { getSingleMessageOrFail } from "./shared/sqs";
+import { getEnvOrFail } from "./shared/env";
+import { z } from "zod";
+import { prefixedLog } from "./shared/log";
 
 // Keep this as early on the file as possible
 capture.init();
 
-export const handler = Sentry.AWSLambda.wrapHandler(
-  async (params: Hl7Notification): Promise<void> => {
-    capture.setExtra({
-      cxId: params.cxId,
-      patientId: params.patientId,
-      payload: params.message,
-      context: "hl7-notification-webhook-sender-cloud.execute",
-    });
+// Automatically set by AWS
+const lambdaName = getEnvOrFail("AWS_LAMBDA_FUNCTION_NAME");
 
-    await new Hl7NotificationWebhookSenderDirect().execute(params);
+export const handler = Sentry.AWSLambda.wrapHandler(async (event: SQSEvent): Promise<void> => {
+  const params = getSingleMessageOrFail(event.Records, lambdaName);
+  if (!params) return;
+
+  const log = prefixedLog(lambdaName);
+  log(`Parsing body: ${params.body}`);
+  const parsedBody = parseBody(params.body);
+  const { cxId, patientId } = parsedBody;
+
+  capture.setExtra({
+    cxId,
+    patientId,
+    context: "hl7-notification-webhook-sender-cloud.execute",
+  });
+
+  await new Hl7NotificationWebhookSenderDirect().execute(parsedBody);
+});
+
+const parseBody = (body: string): Hl7Notification => {
+  const schema = z.object({
+    cxId: z.string().uuid(),
+    patientId: z.string().uuid(),
+    message: z.string(),
+    messageReceivedTimestamp: z.string(),
+  });
+
+  const parsed = schema.safeParse(JSON.parse(body));
+  if (!parsed.success) {
+    throw new Error(`Invalid HL7 notification payload: ${parsed.error.message}`);
   }
-);
+
+  return parsed.data;
+};
