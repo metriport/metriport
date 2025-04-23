f324397a407e85fc52506fc70b83b0501d4507d1
Ramil Garipov <ramil@metriport.com>
2025-04-22 22:54:34 -0700

    fix(core): small fixes
    Part of ENG-89
    
    Signed-off-by: Ramil Garipov <ramil@metriport.com>

--- packages/utils/src/hl7v2-notifications/convert-adt-to-fhir-local-example.ts
+++ packages/utils/src/hl7v2-notifications/convert-adt-to-fhir-local-example.ts
@@ -12,6 +12,7 @@ import { getCxIdAndPatientIdOrFail } from "@metriport/core/command/hl7v2-subscri
 import { errorToString } from "@metriport/shared";
 import fs from "fs";
 import { buildGetDirPathInside, initRunsFolder } from "../shared/folder";
+import { getFileNames } from "@metriport/core/util/fs";
 
 /**
  * Converts HL7v2 ADT messages to FHIR Bundle and saves them to a file.
@@ -29,12 +30,10 @@ import { buildGetDirPathInside, initRunsFolder } from "../shared/folder";
  *
  * Usage:
  * 1. Place your HL7v2 ADT messages file in the input folder
- * 2. Set the inputFilePath and fileName constants below
- * 3. Run the script with ts-node src/hl7v2-notifications/convert-adt-to-fhir-local-example.ts
+ * 2. Run the script with ts-node src/hl7v2-notifications/convert-adt-to-fhir-local-example.ts
  */
 
-const inputFilePath = "";
-const fileName = "";
+const filePath = "";
 const getDirPath = buildGetDirPathInside("hl7v2-conversion");
 
 async function convertAdtToFhir() {
@@ -42,41 +41,52 @@ async function convertAdtToFhir() {
   const outputFolder = getDirPath("converted");
   const errorsFolder = getDirPath("errors");
 
-  const hl7Text = fs.readFileSync(`${inputFilePath}/${fileName}`, "utf-8");
-  const chunks = hl7Text.split(/(?=^MSH\|)/m);
+  const hl7FileNames = getFileNames({
+    folder: filePath,
+    recursive: true,
+    extension: "hl7",
+  });
+  // .filter(n => n.includes("many_diagnoses.hl7"));
 
   const errors: unknown[] = [];
-  chunks.forEach((msg, index) => {
-    const hl7Message = Hl7Message.parse(msg);
 
-    console.log(
-      new Date().toISOString().split(".")[0].replace(/-/g, "").replace("T", "").replace(/:/g, "")
-    );
-    const timestamp = getOrCreateMessageDatetime(hl7Message);
-    const messageId = getMessageUniqueIdentifier(hl7Message);
+  let fileIndex = 0;
+  for (const fileName of hl7FileNames) {
+    fileIndex++;
+    const hl7Text = fs.readFileSync(fileName, "utf-8");
+    const chunks = hl7Text.split(/(?=^MSH\|)/m);
 
-    try {
-      const { cxId, patientId } = getCxIdAndPatientIdOrFail(hl7Message);
-      const bundle = convertHl7v2MessageToFhir({
-        hl7Message,
-        cxId,
-        patientId,
-        messageId,
-        timestampString: timestamp,
-      });
+    chunks.forEach((msg, index) => {
+      const hl7Message = Hl7Message.parse(msg);
+      const timestamp = getOrCreateMessageDatetime(hl7Message);
+      const messageId = getMessageUniqueIdentifier(hl7Message);
 
-      if (!fs.existsSync(outputFolder)) {
-        fs.mkdirSync(outputFolder, { recursive: true });
+      try {
+        const { cxId, patientId } = getCxIdAndPatientIdOrFail(hl7Message);
+        const bundle = convertHl7v2MessageToFhir({
+          hl7Message,
+          cxId,
+          patientId,
+          messageId,
+          timestampString: timestamp,
+        });
+
+        if (!fs.existsSync(outputFolder)) {
+          fs.mkdirSync(outputFolder, { recursive: true });
+        }
+        fs.writeFileSync(
+          `${outputFolder}/${fileIndex}_${index}.json`,
+          JSON.stringify(bundle, null, 2)
+        );
+      } catch (err) {
+        console.log("ERROR IS", errorToString(err));
+        errors.push({
+          err,
+          msg: errorToString(err),
+        });
       }
-      fs.writeFileSync(`${outputFolder}/${index}.json`, JSON.stringify(bundle, null, 2));
-    } catch (err) {
-      console.log("ERROR IS", errorToString(err));
-      errors.push({
-        err,
-        msg: errorToString(err),
-      });
-    }
-  });
+    });
+  }
 
   if (!fs.existsSync(errorsFolder)) {
     fs.mkdirSync(errorsFolder, { recursive: true });
