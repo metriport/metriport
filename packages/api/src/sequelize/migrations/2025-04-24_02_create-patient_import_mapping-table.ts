b3a593f72ef0e79d544ade0e916a03aa8edca04d
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-04-23 22:49:36 -0300

    refactor(api): add unique constraint to patient_import_mapping
    Ref. metriport/metriport-internal#2330
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/api/src/sequelize/migrations/2025-04-24_02_create-patient_import_mapping-table.ts
+++ packages/api/src/sequelize/migrations/2025-04-24_02_create-patient_import_mapping-table.ts
@@ -3,8 +3,10 @@ import type { Migration } from "..";
 import * as shared from "../migrations-shared";
 
 const tableName = "patient_import_mapping";
-const indexName = "patient_import_mapping_cxid_patientid_idx";
+const indexName = "patient_import_mapping_patientid_idx";
 const indexFieldNames = ["patient_id"];
+const constraintName = "patient_import_mapping_unique";
+const constraintFieldNames = ["job_id", "row_number", "data_pipeline_request_id"];
 
 // Use 'Promise.all' when changes are independent of each other
 export const up: Migration = async ({ context: queryInterface }) => {
@@ -46,6 +48,12 @@ export const up: Migration = async ({ context: queryInterface }) => {
       },
       { transaction, addVersion: true }
     );
+    await queryInterface.addConstraint(tableName, {
+      name: constraintName,
+      fields: constraintFieldNames,
+      type: "unique",
+      transaction,
+    });
     await queryInterface.addIndex(tableName, {
       name: indexName,
       fields: indexFieldNames,
@@ -57,6 +65,7 @@ export const up: Migration = async ({ context: queryInterface }) => {
 export const down: Migration = ({ context: queryInterface }) => {
   return queryInterface.sequelize.transaction(async transaction => {
     await queryInterface.removeIndex(tableName, indexName, { transaction });
+    await queryInterface.removeConstraint(tableName, constraintName, { transaction });
     await queryInterface.dropTable(tableName, { transaction });
   });
 };
