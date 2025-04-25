b1956e3f9081699d88e9abda1ed7d1dd50582b58
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2025-04-24 20:29:17 -0400

    Revert "build: update prompt"
    This reverts commit bd02e95bb765ec8e59a69c450ca4e8ae39f87a06.

--- packages/core/src/command/ai-brief/custom-prompt.ts
+++ packages/core/src/command/ai-brief/custom-prompt.ts
@@ -6,61 +6,46 @@ export const documentVariableName = "text";
 
 const instructions = `
 Instructions:
-1. Review the patient medical records and format the output as follows:
-    Most recent Primary Care Provider (PCP) visit (Date: [date]):
-    PCP Name: [name], [credentials] [specialty]
-
-    Medical / Problem List: [comma separated list of conditions]
-    Medication List: [comma separated list of medications]
-    Note: Only include MD, DO, NP, PA visits to the specialty of Family Medicine, Internal Medicine or OB/GYN.
-    If PCP visit isn't available, use History of Present Illness (HPI) visit instead
-
-    Most recent height (Date: [date]): [value] in
-    Most recent weight (Date: [date]): [value] lbs
-    Most Recent BMI ([date]): [value]
-
-    Qualifying Conditions: [Comma separated list of conditions with their most recent diagnosis date]
-    Disqualifying Conditions: [Comma separated list of conditions with their most recent diagnosis date]
-    Qualifying Medications: [Comma separated list of medications with their recent fill date]
-    Disqualifying Medications: [Comma separated list of medications with their recent fill date]
-
-List of qualifying conditions:
-- High blood pressure or hypertension
-- High cholesterol or Hyperlipidemia/Dyslipidemia
-- Type 2 Diabetes Sleep Apnea or OSA
-- Polycystic ovarian syndrome (PCOS)
-- Fatty Liver, Hepatic Steatosis, or Non-alcoholic fatty liver disease (NAFLD)*
-- Prediabetes Insulin Resistance*
-- Coronary Artery Disease (Heart Disease, History of MI/Heart Attack > 6 months, etc.)*
-- Infertility
-
-List of disqualifying conditions:
-- Current pregnancy or currently breastfeeding
-- Active cancer or cancer treatment in the last 6 months Active drug or alcohol abuse
-- CKD Stage 4 or higher (eGFR <29) or kidney transplant
-- Active hepatitis or liver disease (fatty liver does not apply)
-- Heart attack / stroke / any heart condition that limits daily activity in last 6 months
-- Serious uncontrolled mental health conditions: mental health conditions if there is evidence of uncontrolled symptoms or inpatient hospitalization
-
-List of qualifying medications:
-- Alli or Xenical (orlistat)
-- Bupropion Contrave (naltrexone-bupropion)
-- Diethylpropion
-- Metformin
-- Mounjaro (tirzepatide)
-- Naltrexone
-- Ozempic (semaglutide)
-- Phentermine (Lomaira)
-- Qsymia (phentermine + topiramate ER)
-- Rybelsus (oral semaglutide)
-- Saxenda (liraglutide)
-- Topiramate Trulicity (dulaglutide)
-- Victoza (liraglutide)
-- Wegovy (semaglutide)
-- Zonisamide
-
-List of disqualifying medications:
-- Daily use of oral steroid meds equivalent or higher than prednisone 20 mg twice daily (not including inhalers)
+1. Review the patient medical records:
+2. Extract the following information
+    a. Most recent Primary Care Provider (PCP) visit: include the PCP name, credentials, specialty, date, medical list/problem list, medication list. Only include MD, DO, NP, PA visits to the specialty of Family Medicine, Internal Medicine or OB/GYN.
+        i. If PCP visit isn't available, use History of Present Illness (HPI) visit instead
+    b. Most recent height, weight and date.
+    c. Most recent BMI and date.
+    d. Indicate if a qualifying condition is present and include only that name and the most recent diagnosis date in the response. Here is the list of qualifying conditions:
+        i. High blood pressure or hypertension
+        ii. High cholesterol or Hyperlipidemia/Dyslipidemia
+        iii. Type 2 Diabetes Sleep Apnea or OSA
+        iv. Polycystic ovarian syndrome (PCOS)
+        v. Fatty Liver, Hepatic Steatosis, or Non-alcoholic fatty liver disease (NAFLD)*
+        vi. Prediabetes Insulin Resistance*
+        vii. Coronary Artery Disease (Heart Disease, History of MI/Heart Attack > 6 months, etc.)*
+        viii. Infertility
+    e. Indicate if a disqualifying condition is present and include only that name and the most recent diagnosis date in the response. Here is the list of disqualifying conditions:
+        i. Current pregnancy or currently breastfeeding
+        ii. Active cancer or cancer treatment in the last 6 months Active drug or alcohol abuse
+        iii. CKD Stage 4 or higher (eGFR <29) or kidney transplant
+        iv. Active hepatitis or liver disease (fatty liver does not apply)
+        v. Heart attack / stroke / any heart condition that limits daily activity in last 6 months
+        vi. Serious uncontrolled mental health conditions: mental health conditions if there is evidence of uncontrolled symptoms or inpatient hospitalization.
+    f. Indicate if a patient is on a qualifying medication and has a fill date in the last 60 days. Your response should include the medication name and recent fill date.
+        i. Alli or Xenical (orlistat)
+        ii. Bupropion Contrave (naltrexone-bupropion)
+        iii. Diethylpropion
+        iv. Metformin
+        v. Mounjaro (tirzepatide)
+        vi. Naltrexone
+        vii. Ozempic (semaglutide)
+        viii. Phentermine (Lomaira)
+        ix. Qsymia (phentermine + topiramate ER)
+        x. Rybelsus (oral semaglutide)
+        xi. Saxenda (liraglutide)
+        xii. Topiramate Trulicity (dulaglutide)
+        xiii. Victoza (liraglutide)
+        xiv. Wegovy (semaglutide)
+        xv. Zonisamide
+    g. Indicate if a patient is on a disqualifying medication and has a fill date in the last 60 days. Your response should include the medication name and recent fill date.
+        i. Daily use of oral steroid meds equivalent or higher than prednisone 20 mg twice daily (not including inhalers)
 `;
 
 export const mainSummaryPrompt = `
@@ -88,7 +73,7 @@ Here are the previous summaries written by you of sections of the patient's medi
 {${documentVariableName}}
 --------
 
-Combine these summaries into a single, comprehensive summary.
+Combine these summaries into a single, comprehensive summary. 
 Use these ${instructions}.
 
 Don't tell me that you are writing a summary, just write the summary. Also, don't tell me about any limitations of the information provided.
