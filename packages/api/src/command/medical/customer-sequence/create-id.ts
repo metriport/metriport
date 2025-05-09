6791ea44bfaf231ebbaca8c512c36b7c7161449b
Keshav Saharia <keshav@metriport.com>
2025-05-08 19:18:36 -0700

    feat(api): allow global offset of organization ID from environment variable
    metriport/metriport-internal#1040
    
    refs. ENG-266
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/api/src/command/medical/customer-sequence/create-id.ts
+++ packages/api/src/command/medical/customer-sequence/create-id.ts
@@ -1,8 +1,16 @@
+import { getEnvVar } from "@metriport/shared";
 import { OrganizationModel } from "../../../models/medical/organization";
 import { makeOrganizationOID } from "../../../shared/oid";
 
 export async function createOrganizationId(): Promise<{ oid: string; organizationNumber: number }> {
-  const maxOrgNumber = Number(await OrganizationModel.max("organizationNumber"));
+  let maxOrgNumber = Number(await OrganizationModel.max("organizationNumber"));
+
+  // If the environment variable is set, add it to the max org number
+  const organizationNumberOffset = parseInt(getEnvVar("ORGANIZATION_NUMBER_OFFSET") ?? "0");
+  if (isFinite(organizationNumberOffset)) {
+    maxOrgNumber += organizationNumberOffset;
+  }
+
   const organizationNumber = maxOrgNumber + 1;
   const oid = makeOrganizationOID(organizationNumber);
   return { oid, organizationNumber };
