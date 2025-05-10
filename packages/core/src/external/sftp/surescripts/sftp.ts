6bf8bef6baf68cf3bb49e4b1e691ff4596cf3709
Keshav Saharia <keshav@metriport.com>
2025-05-09 18:54:24 -0700

    feat(api): created auto-incrementing ID generator which does not pollute global scope
    metriport/metriport-internal#1040
    
    refs. ENG-194
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/external/sftp/surescripts/sftp.ts
+++ packages/core/src/external/sftp/surescripts/sftp.ts
@@ -1,8 +1,11 @@
 import Client from "ssh2-sftp-client";
-// import { Config } from '../../../util/config';
+import { Config } from "../../../util/config";
 import { createWritableBuffer, SftpConfig } from "../shared";
+import { IdGenerator, createIdGenerator } from "../shared";
 
-interface SurescriptsSftpConfig extends Partial<SftpConfig> {
+interface SurescriptsSftpConfig extends Partial<SftpConfig>, TransmissionConfig {}
+
+interface TransmissionConfig {
   senderId: string;
   senderPassword: string;
   receiverId?: string;
@@ -29,44 +32,64 @@ interface Transmission<T extends TransmissionType> {
 
 const surescriptsHost = "sftp.surescripts.com";
 const surescriptsPort = 22;
-// const surescriptsReceiverId = 'S00000000000006';
-// const surescriptsVersion = '2.0';
+const surescriptsReceiverId = "S00000000000006";
+const surescriptsVersion = "2.0";
 
 class SurescriptsSftpClient implements SftpClient {
   private client: Client;
+  private idGenerator: IdGenerator;
 
   private host: string;
   private port: number;
   private username: string;
   private password: string;
-  // private senderId: string;
-  // private senderPassword: string;
-  // private receiverId: string;
-  // private version: string;
+  private senderId: string;
+  private senderPassword: string;
+  private receiverId: string;
+  private version: string;
 
   constructor(config: SurescriptsSftpConfig) {
     this.client = new Client();
+    this.idGenerator = createIdGenerator(10);
+
     this.host = config.host ?? surescriptsHost;
     this.port = config.port ?? surescriptsPort;
     this.username = config.username ?? "P00000000023456";
     this.password = config.password ?? "M94WJ7CW6H";
-    // this.senderId = config.senderId;
-    // this.senderPassword = config.senderPassword;
+    this.senderId = config.senderId;
+    this.senderPassword = config.senderPassword;
 
-    // this.receiverId = config.receiverId ??
-    //   Config.getSurescriptsSftpReceiverId() ??
-    //   surescriptsReceiverId;
-    // this.version = config.version ??
-    //   Config.getSurescriptsSftpVersion() ??
-    //   surescriptsVersion;
+    this.receiverId =
+      config.receiverId ?? Config.getSurescriptsSftpReceiverId() ?? surescriptsReceiverId;
+    this.version = config.version ?? Config.getSurescriptsSftpVersion() ?? surescriptsVersion;
   }
 
   createTransmission<T extends TransmissionType>(type: T): Transmission<T> {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, "0");
+    const date = now.getDate().toString().padStart(2, "0");
+    const hour = now.getHours().toString().padStart(2, "0");
+    const minute = now.getMinutes().toString().padStart(2, "0");
+    const second = now.getSeconds().toString().padStart(2, "0");
+    const centisecond = Math.round(now.getMilliseconds() / 10)
+      .toString()
+      .padStart(2, "0");
+
     return {
       type,
-      id: "TODO",
-      date: "TODO",
-      time: "TODO",
+      id: this.idGenerator().toString("ascii"),
+      date: [year, month, date].join(""),
+      time: [hour, minute, second, centisecond].join(""),
+    };
+  }
+
+  getTransmissionHeader(): TransmissionConfig {
+    return {
+      senderId: this.senderId,
+      senderPassword: this.senderPassword,
+      receiverId: this.receiverId,
+      version: this.version,
     };
   }
 
