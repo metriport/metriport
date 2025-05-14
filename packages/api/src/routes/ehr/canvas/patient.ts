7e301e4a4e4a8ef16282784b76227d6c5f9381f0
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-05-14 04:46:04 -0700

    feat(ehr): small fixes
    Ref: ENG-199
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/api/src/routes/ehr/canvas/patient.ts
+++ packages/api/src/routes/ehr/canvas/patient.ts
@@ -94,6 +94,37 @@ router.post(
 );
 
 /**
+ * POST /ehr/canvas/patient/:id/condition
+ *
+ * Creates a condition
+ * @param req.params.id The ID of Canvas Patient.
+ * @param req.query.practiceId The ID of Canvas Practice.
+ * @param req.query.practitionerId The ID of Canvas Practitioner.
+ * @param req.body The FHIR Resource payload
+ * @returns Canvas API response
+ */
+router.post(
+  "/:id/condition",
+  handleParams,
+  requestLogger,
+  asyncHandler(async (req: Request, res: Response) => {
+    const cxId = getCxIdOrFail(req);
+    const canvasPatientId = getFrom("params").orFail("id", req);
+    const canvasPracticeId = getFromQueryOrFail("practiceId", req);
+    const canvasPractitionerId = getFromQueryOrFail("practitionerId", req);
+    const payload = req.body; // TODO Parse body https://github.com/metriport/metriport-internal/issues/2170
+    const conditionDetails = await createCondition({
+      cxId,
+      canvasPatientId,
+      canvasPracticeId,
+      canvasPractitionerId,
+      condition: payload,
+    });
+    return res.status(httpStatus.OK).json(conditionDetails);
+  })
+);
+
+/**
  * GET /ehr/canvas/patient/:id/resource/diff/latest
  *
  * Retrieves the latest resource diff job and pre-signed URLs for the bundles if completed
@@ -147,37 +178,6 @@ router.get(
       bundleType: BundleType.RESOURCE_DIFF_METRIPORT_ONLY,
     });
     return res.status(httpStatus.OK).json(bundle);
-  })
-);
-
-/**
- * POST /ehr/canvas/patient/:id/condition
- *
- * Creates a condition
- * @param req.params.id The ID of Canvas Patient.
- * @param req.query.practiceId The ID of Canvas Practice.
- * @param req.query.practitionerId The ID of Canvas Practitioner.
- * @param req.body The FHIR Resource payload
- * @returns Canvas API response
- */
-router.post(
-  "/:id/condition",
-  handleParams,
-  requestLogger,
-  asyncHandler(async (req: Request, res: Response) => {
-    const cxId = getCxIdOrFail(req);
-    const canvasPatientId = getFrom("params").orFail("id", req);
-    const canvasPracticeId = getFromQueryOrFail("practiceId", req);
-    const canvasPractitionerId = getFromQueryOrFail("practitionerId", req);
-    const payload = req.body; // TODO Parse body https://github.com/metriport/metriport-internal/issues/2170
-    const conditionDetails = await createCondition({
-      cxId,
-      canvasPatientId,
-      canvasPracticeId,
-      canvasPractitionerId,
-      condition: payload,
-    });
-    return res.status(httpStatus.OK).json(conditionDetails);
   })
 );
 
