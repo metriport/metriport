d75ac45171f76c447d66758008cca0ba06a72be3
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-05-20 09:04:46 -0700

    feat(ecw): fixes after merge
    Ref: ENG-198
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/api/src/external/ehr/eclinicalworks/command/sync-patient.ts
+++ packages/api/src/external/ehr/eclinicalworks/command/sync-patient.ts
@@ -10,7 +10,7 @@ import { getPatientOrFail } from "../../../../command/medical/patient/get-patien
 import {
   createMetriportPatientDemosFhir,
   getOrCreateMetriportPatientFhir,
-} from "../../shared-fhir";
+} from "../../shared/utils/fhir";
 import { createEClinicalWorksClient } from "../shared";
 
 export type SyncEclinicalworksPatientIntoMetriportParams = {
