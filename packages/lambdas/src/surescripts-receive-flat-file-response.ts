c402e82328b5d5a2da42a4d9b8734cefe201b611
Keshav Saharia <keshav@metriport.com>
2025-05-20 11:36:05 -0700

    refactor(infra): fix coderabbit message
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/lambdas/src/surescripts-receive-flat-file-response.ts
+++ packages/lambdas/src/surescripts-receive-flat-file-response.ts
@@ -1,8 +1,11 @@
 import { capture } from "./shared/capture";
+import { prefixedLog } from "./shared/log";
 
 capture.init();
 
+const log = prefixedLog("surescripts");
+
 // Stub which will be integrated with Surescripts commands
 export const handler = capture.wrapHandler(async () => {
-  console.log(`This is a stub for the Surescripts SFTP integration`);
+  log(`This is a stub for the Surescripts SFTP integration`);
 });
