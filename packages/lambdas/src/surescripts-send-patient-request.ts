5762193aa5276c9ff66ea52e6c5d3dad096c6c7c
Keshav Saharia <keshav@metriport.com>
2025-05-21 14:18:33 -0700

    build(lambdas): added verification that bucket is available to Lambdas
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/lambdas/src/surescripts-send-patient-request.ts
+++ packages/lambdas/src/surescripts-send-patient-request.ts
@@ -1,3 +1,5 @@
+import { Config } from "@metriport/core/util/config";
+import { S3Utils } from "@metriport/core/external/aws/s3";
 import { capture } from "./shared/capture";
 import { prefixedLog } from "./shared/log";
 
@@ -7,5 +9,19 @@ const log = prefixedLog("surescripts");
 
 // Stub which will be integrated with Surescripts commands
 export const handler = capture.wrapHandler(async () => {
-  log(`This is a stub for the Surescripts Send Patient Request integration`);
+  const s3Utils = new S3Utils(Config.getAWSRegion());
+  const bucketName = process.env.SURESCRIPTS_REPLICA_BUCKET_NAME;
+  if (!bucketName) throw new Error("Missing bucket name");
+
+  s3Utils.uploadFile({
+    bucket: bucketName,
+    key: "mock_surescripts/plf.txt",
+    file: Buffer.from("test PLF"),
+    contentType: "text/plain",
+    metadata: {
+      "x-surescript-sent": "2025-05-01T00:00:00.000Z",
+    },
+  });
+
+  log(`Uploaded test file to ${bucketName}`);
 });
