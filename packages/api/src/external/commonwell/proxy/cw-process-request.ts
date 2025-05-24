92dee9e0f9b46f724896c5c3d18067a3fed63052
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-05-24 15:01:22 -0700

    feat(core): fix build errs
    metriport/metriport-internal#1040
    
    Ref: ENG-257
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/api/src/external/commonwell/proxy/cw-process-request.ts
+++ packages/api/src/external/commonwell/proxy/cw-process-request.ts
@@ -132,13 +132,13 @@ function getAllowedSearchParams(searchParams: URLSearchParams): URLSearchParams 
 export function prepareBundle(resources: Resource[], params: URLSearchParams): Bundle<Resource> {
   const { documentReferences, otherResources } = splitResources(resources);
   const filteredDocRefs = applyFilterParams(documentReferences, params);
-  if (filteredDocRefs.length < 1) return buildBundle();
+  if (filteredDocRefs.length < 1) return buildBundle({ type: "searchset" });
 
   const updatedDocRefs = adjustAttachmentURLs(filteredDocRefs);
   const consolidatedResources = [...updatedDocRefs, ...otherResources];
   const uniqueResources = uniqBy(consolidatedResources, r => r.id);
   const bundleEntries: BundleEntry[] = uniqueResources.map(r => ({ resource: r }));
-  const bundle = buildBundle({ entries: bundleEntries });
+  const bundle = buildBundle({ entries: bundleEntries, type: "searchset" });
   return bundle;
 }
 
