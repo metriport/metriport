59bd487449e7891d3dd17849c2d4ac73ae294ddc
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-05-24 17:07:44 -0700

    feat(api): move query params to body + clean up commands
    metriport/metriport-internal#1040
    
    Ref: ENG-350
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/api-sdk/src/medical/models/patient-settings.ts
+++ packages/api-sdk/src/medical/models/patient-settings.ts
@@ -6,6 +6,25 @@ const subscriptionsSchema = z.object({
 
 export type PatientSubscriptions = z.infer<typeof subscriptionsSchema>;
 
-export const patientSettingsSchema = z.object({
-  subscriptions: subscriptionsSchema.optional(),
+export const upsertPatientSettingsBaseSchema = z.object({
+  cxId: z.string(),
+  settings: z.object({
+    subscriptions: subscriptionsSchema.optional(),
+  }),
 });
+
+export const upsertPatientSettingsSchema = upsertPatientSettingsBaseSchema.and(
+  z.discriminatedUnion("type", [
+    z.object({
+      type: z.literal("patientList"),
+      patientIds: z.array(z.string()),
+    }),
+    z.object({
+      type: z.literal("facility"),
+      facilityId: z.string(),
+    }),
+  ])
+);
+
+export type UpsertPatientSettingsBase = z.infer<typeof upsertPatientSettingsBaseSchema>;
+export type UpsertPatientSettings = z.infer<typeof upsertPatientSettingsSchema>;
