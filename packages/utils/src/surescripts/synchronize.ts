89c4cfeccd5681c5484e8aa58a437024f40a5146
Keshav Saharia <keshav@metriport.com>
2025-05-28 10:32:07 -0700

    fix(utils): update synchronize script to use Lambda implementation
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/utils/src/surescripts/synchronize.ts
+++ packages/utils/src/surescripts/synchronize.ts
@@ -9,25 +9,22 @@ const program = new Command();
 program
   .name("synchronize")
   .option("-d, --dry-run", "Dry run the synchronization")
-  .option("-a, --all", "Synchronize all files")
-  .option("-t, --transmission <id>", "Synchronize a specific transmission")
+  .option("--from-surescripts", "Synchronize all incoming files")
+  .option("--to-surescripts", "Synchronize all outgoing files")
   .description(
     "Ensure one or all files are synchronized between the Surescripts SFTP server and the S3 bucket"
   )
   .showHelpAfterError()
   .version("1.0.0")
   .action(async () => {
-    const { dryRun, all, file } = program.opts();
+    const { dryRun, fromSurescripts, toSurescripts } = program.opts();
 
-    console.log(all, file);
     const replica = new SurescriptsReplica();
-    if (dryRun) {
-      await replica.synchronize(dryRun);
-    } else if (all) {
-      await replica.synchronize(true);
-    } else {
-      console.log("File", file);
-    }
+    await replica.synchronize({
+      dryRun,
+      fromSurescripts,
+      toSurescripts,
+    });
   });
 
 export default program;
