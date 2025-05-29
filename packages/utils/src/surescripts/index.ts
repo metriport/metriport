ecc981e3b041ebb39a9c590a0974c1d8e2a68a3b
Keshav Saharia <keshav@metriport.com>
2025-05-29 11:29:00 -0700

    refactor(utils): imports at top of file
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/utils/src/surescripts/index.ts
+++ packages/utils/src/surescripts/index.ts
@@ -1,13 +1,11 @@
 import { Command } from "commander";
-
-const program = new Command();
-
 import connect from "./connect";
 import synchronize from "./synchronize";
 import generatePatientLoadFile from "./generate-patient-load-file";
 import receiveFlatFileResponse from "./receive-flat-file-response";
 import receiveVerificationResponse from "./receive-verification-response";
 
+const program = new Command();
 program.addCommand(connect);
 program.addCommand(synchronize);
 program.addCommand(generatePatientLoadFile);
