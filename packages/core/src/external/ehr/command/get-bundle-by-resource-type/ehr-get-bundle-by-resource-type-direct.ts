aab2bd34653b627207b10d703e6e294ebd82913a
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-05-29 10:16:34 -0700

    feat(ehr): fix
    Ref: ENG-199
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/core/src/external/ehr/command/get-bundle-by-resource-type/ehr-get-bundle-by-resource-type-direct.ts
+++ packages/core/src/external/ehr/command/get-bundle-by-resource-type/ehr-get-bundle-by-resource-type-direct.ts
@@ -20,12 +20,9 @@ export class EhrGetBundleByResourceTypeDirect implements EhrGetBundleByResourceT
 
 type GetBundleByResourceType = (params: GetBundleByResourceTypeClientRequest) => Promise<Bundle>;
 
-export type GetBundleByResourceTypeMethodsMap = Record<
-  EhrSource,
-  GetBundleByResourceType | undefined
->;
+type GetBundleByResourceTypeMethodsMap = Record<EhrSource, GetBundleByResourceType | undefined>;
 
-export const ehrGetBundleByResourceTypeMap: GetBundleByResourceTypeMethodsMap = {
+const ehrGetBundleByResourceTypeMap: GetBundleByResourceTypeMethodsMap = {
   [EhrSources.canvas]: getBundleByResourceTypeCanvas,
   [EhrSources.athena]: undefined,
   [EhrSources.elation]: undefined,
@@ -33,7 +30,7 @@ export const ehrGetBundleByResourceTypeMap: GetBundleByResourceTypeMethodsMap = 
   [EhrSources.eclinicalworks]: undefined,
 };
 
-export function getEhrGetBundleByResourceTypeHandler(ehr: EhrSource): GetBundleByResourceType {
+function getEhrGetBundleByResourceTypeHandler(ehr: EhrSource): GetBundleByResourceType {
   const handler = ehrGetBundleByResourceTypeMap[ehr];
   if (!handler) {
     throw new BadRequestError("No get bundle by resource type handler found", undefined, {
