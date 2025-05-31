1900118b3ec8f1d56433671bcd430327f908c50a
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-05-30 18:34:34 -0700

    feat(infra): remove / detach some nlbs
    metriport/metriport-internal#1040
    
    Ref: ENG-394
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/infra/lib/hl7-notification-stack/constants.ts
+++ packages/infra/lib/hl7-notification-stack/constants.ts
@@ -1,6 +1,8 @@
 export const MLLP_DEFAULT_PORT = 2575;
 export const HL7_NOTIFICATION_VPC_CIDR = "10.1.0.0/16";
 export const PROBLEMATIC_IPSEC_CHARACTERS = "!@#$%^&*()_+-=[]{}|;:,.<>?~`'\"\\/";
-export const MLLP_SERVER_NLB_INTERNAL_IP_A = "10.1.1.20";
-export const MLLP_SERVER_NLB_INTERNAL_IP_B = "10.1.1.21";
+export const MLLP_SERVER_NLB_PROD_INTERNAL_IP_A = "10.1.1.20";
+export const MLLP_SERVER_NLB_PROD_INTERNAL_IP_B = "10.1.1.21";
+export const MLLP_SERVER_NLB_STAGING_INTERNAL_IP_A = "10.1.1.22";
+export const MLLP_SERVER_NLB_STAGING_INTERNAL_IP_B = "10.1.1.23";
 export const MLLP_SERVER_CONTAINER_NAME = "MllpServer";
