18ea344daf7327f008dd4537dbb92dd6a6f51efa
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-06-04 04:36:34 -0700

    feat(ehr): fixing imports
    Ref: ENG-199
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/api/src/external/ehr/shared/command/clients/get-two-legged-client.ts
+++ packages/api/src/external/ehr/shared/command/clients/get-two-legged-client.ts
@@ -1,9 +1,13 @@
+import {
+  EhrClientTwoLeggedAuth,
+  EhrEnv,
+  EhrPerPracticeParams,
+} from "@metriport/core/external/ehr/environment";
 import { BadRequestError } from "@metriport/shared";
 import { EhrSources } from "@metriport/shared/interface/external/ehr/source";
 import { createAthenaClientWithTokenIdAndEnvironment } from "../../../athenahealth/shared";
 import { createCanvasClientWithTokenIdAndEnvironment } from "../../../canvas/shared";
 import { createElationClientWithTokenIdAndEnvironment } from "../../../elation/shared";
-import { EhrClientTwoLeggedClient, EhrEnv, EhrPerPracticeParams } from "../../utils/client";
 
 /**
  * Get the client with token id and environment for the EHRs that support two-legged auth
@@ -17,7 +21,7 @@ export async function getTwoLeggedClientWithTokenIdAndEnvironment({
   cxId,
   practiceId,
 }: EhrPerPracticeParams & { ehr: TwoLeggedEhrSource }): Promise<{
-  client: EhrClientTwoLeggedClient;
+  client: EhrClientTwoLeggedAuth;
   tokenId: string;
   environment: EhrEnv;
 }> {
@@ -31,13 +35,13 @@ export function isTwoLeggedEhrSource(ehr: string): ehr is TwoLeggedEhrSource {
   return twoLeggedEhrSources.includes(ehr as TwoLeggedEhrSource);
 }
 
-type GetClientWithTokenIdAndEnvironment<T extends EhrClientTwoLeggedClient, Env extends EhrEnv> = (
+type GetClientWithTokenIdAndEnvironment<T extends EhrClientTwoLeggedAuth, Env extends EhrEnv> = (
   params: EhrPerPracticeParams
 ) => Promise<{ client: T; tokenId: string; environment: Env }>;
 
 type ClientWithTokenIdAndEnvironmentMethodMap = Record<
   TwoLeggedEhrSource,
-  GetClientWithTokenIdAndEnvironment<EhrClientTwoLeggedClient, EhrEnv> | undefined
+  GetClientWithTokenIdAndEnvironment<EhrClientTwoLeggedAuth, EhrEnv> | undefined
 >;
 
 const clientWithTokenIdAndEnvironmentMethodsBy: ClientWithTokenIdAndEnvironmentMethodMap = {
@@ -48,7 +52,7 @@ const clientWithTokenIdAndEnvironmentMethodsBy: ClientWithTokenIdAndEnvironmentM
 
 function getClientWithTokenIdAndEnvironmentHandler(
   ehr: TwoLeggedEhrSource
-): GetClientWithTokenIdAndEnvironment<EhrClientTwoLeggedClient, EhrEnv> {
+): GetClientWithTokenIdAndEnvironment<EhrClientTwoLeggedAuth, EhrEnv> {
   const handler = clientWithTokenIdAndEnvironmentMethodsBy[ehr];
   if (!handler) {
     throw new BadRequestError("No client with token id and environment handler found", undefined, {
