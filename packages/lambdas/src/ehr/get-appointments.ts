a64ac524ef6267cd40b672bb0ebe7666190c8daf
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-06-05 06:36:43 -0700

    feat(ehr): fixing lambda
    Ref: ENG-199
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/lambdas/src/ehr/get-appointments.ts
+++ packages/lambdas/src/ehr/get-appointments.ts
@@ -1,12 +1,8 @@
 import { GetAppointmentsRequest } from "@metriport/core/external/ehr/command/get-appointments/ehr-get-appointments";
 import { EhrGetAppointmentsDirect } from "@metriport/core/external/ehr/command/get-appointments/ehr-get-appointments-direct";
-import { MetriportError } from "@metriport/shared";
-import { SQSEvent } from "aws-lambda";
 import { capture } from "../shared/capture";
-import { ehrGetAppointmentsSchema } from "../shared/ehr";
 import { getEnvOrFail } from "../shared/env";
 import { prefixedLog } from "../shared/log";
-import { getSingleMessageOrFail } from "../shared/sqs";
 
 // Keep this as early on the file as possible
 capture.init();
@@ -14,33 +10,35 @@ capture.init();
 // Automatically set by AWS
 const lambdaName = getEnvOrFail("AWS_LAMBDA_FUNCTION_NAME");
 
-export const handler = capture.wrapHandler(async (event: SQSEvent) => {
+type GetAppointmentsRequestInLambda = Omit<GetAppointmentsRequest, "fromDate" | "toDate"> & {
+  fromDate?: string;
+  toDate?: string;
+};
+
+export const handler = capture.wrapHandler(async (params: GetAppointmentsRequestInLambda) => {
   capture.setExtra({ event, context: lambdaName });
+  const { cxId, practiceId, method } = params;
 
   const startedAt = new Date().getTime();
-  const message = getSingleMessageOrFail(event.Records, lambdaName);
-  if (!message) return;
-
-  const parsedBody = parseBody(message.body);
-  const { method, cxId, practiceId } = parsedBody;
 
   const log = prefixedLog(`method ${method}, cxId ${cxId}, practiceId ${practiceId}`);
 
   const ehrGetAppointmentsHandler = new EhrGetAppointmentsDirect();
-  const appointments = await ehrGetAppointmentsHandler.getAppointments(parsedBody);
+  const appointments = await ehrGetAppointmentsHandler.getAppointments(
+    convertToGetAppointmentsRequest(params)
+  );
 
   const finishedAt = new Date().getTime();
   log(`Done local duration: ${finishedAt - startedAt}ms`);
   return appointments;
 });
 
-function parseBody(body?: unknown): GetAppointmentsRequest {
-  if (!body) throw new MetriportError(`Missing message body`);
-
-  const bodyString = typeof body === "string" ? (body as string) : undefined;
-  if (!bodyString) throw new MetriportError(`Invalid body`);
-
-  const bodyAsJson = JSON.parse(bodyString);
-
-  return ehrGetAppointmentsSchema.parse(bodyAsJson);
+function convertToGetAppointmentsRequest(
+  params: GetAppointmentsRequestInLambda
+): GetAppointmentsRequest {
+  return {
+    ...params,
+    fromDate: params.fromDate ? new Date(params.fromDate) : undefined,
+    toDate: params.toDate ? new Date(params.toDate) : undefined,
+  };
 }
