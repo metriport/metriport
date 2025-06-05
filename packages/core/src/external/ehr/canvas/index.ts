fe49db60d2f570d55662554702b24d17371c268e
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-06-05 06:57:32 -0700

    feat(ehr): fix canvas
    Ref: ENG-199
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/core/src/external/ehr/canvas/index.ts
+++ packages/core/src/external/ehr/canvas/index.ts
@@ -863,13 +863,10 @@ class CanvasApi {
     toDate: Date;
   }): Promise<SlimBookedAppointment[]> {
     const { debug } = out(`Canvas getAppointments - cxId ${cxId} practiceId ${this.practiceId}`);
-    const params = {
-      status: "booked",
-      ...(fromDate && { date: `ge${this.formatDate(fromDate.toISOString()) ?? ""}` }),
-      ...(toDate && { date: `lt${this.formatDate(toDate.toISOString()) ?? ""}` }),
-      _count: "1000",
-    };
+    const params = { status: "booked", _count: "1000" };
     const urlParams = new URLSearchParams(params);
+    urlParams.append("date", `ge${this.formatDate(fromDate.toISOString())}`);
+    urlParams.append("date", `lt${this.formatDate(toDate.toISOString())}`);
     const appointmentUrl = `/Appointment?${urlParams.toString()}`;
     const additionalInfo = {
       cxId,
