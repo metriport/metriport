ac6157bdc8eba571e292b5f1dcc9b8756b3780ac
Ramil Garipov <ramil@metriport.com>
2025-06-06 16:45:56 -0700

    fix(api): minor changes
    Part of ENG-420
    
    Signed-off-by: Ramil Garipov <ramil@metriport.com>

--- packages/api/src/models/medical/cohort.ts
+++ packages/api/src/models/medical/cohort.ts
@@ -1,14 +1,14 @@
 import { Cohort, CohortAssignment, MonitoringSettings } from "@metriport/core/domain/cohort";
 import { DataTypes, Sequelize } from "sequelize";
-import { BaseModel, ModelSetup } from "../_default";
+import { BaseModel, BaseModelNoId, ModelSetup } from "../_default";
 
 export class CohortModel extends BaseModel<CohortModel> implements Cohort {
   static NAME = "cohort";
 
   declare cxId: string;
   declare name: string;
-  declare monitoring: MonitoringSettings;
-  declare otherSettings: Record<string, unknown>;
+  declare monitoring?: MonitoringSettings;
+  declare otherSettings?: Record<string, unknown>;
 
   static override attributes() {
     return {
@@ -57,7 +57,7 @@ export class CohortModel extends BaseModel<CohortModel> implements Cohort {
 }
 
 export class CohortAssignmentModel
-  extends BaseModel<CohortAssignmentModel>
+  extends BaseModelNoId<CohortAssignmentModel>
   implements CohortAssignment
 {
   static NAME = "patient_cohort";
@@ -67,10 +67,11 @@ export class CohortAssignmentModel
 
   static override attributes() {
     return {
-      ...super.attributes(),
       patientId: {
         type: DataTypes.UUID,
         allowNull: false,
+        field: "patient_id",
+        primaryKey: true,
         references: {
           model: "patient",
           key: "id",
@@ -80,12 +81,15 @@ export class CohortAssignmentModel
       cohortId: {
         type: DataTypes.UUID,
         allowNull: false,
+        field: "cohort_id",
+        primaryKey: true,
         references: {
           model: "cohort",
           key: "id",
         },
         onDelete: "CASCADE",
       },
+      ...BaseModelNoId.attributes(),
     };
   }
 
--- packages/core/src/domain/cohort.ts
+++ packages/core/src/domain/cohort.ts
@@ -1,21 +1,25 @@
-import { BaseDomain, BaseDomainCreate } from "./base-domain";
+import { BaseDomain, BaseDomainCreate, BaseDomainNoId } from "./base-domain";
 
 export type MonitoringSettings = {
-  adt: boolean;
+  adt?: boolean;
 };
 
-export interface CohortCreate extends Omit<BaseDomainCreate, "id"> {
+export interface CohortCreate extends BaseDomainCreate {
   cxId: string;
   name: string;
-  monitoring: MonitoringSettings;
-  otherSettings: Record<string, unknown>;
+  monitoring?: MonitoringSettings;
+  otherSettings?: Record<string, unknown>;
 }
 
-export interface Cohort extends BaseDomain, Required<CohortCreate> {}
+export interface Cohort extends BaseDomain, CohortCreate {}
 
-export interface CohortAssignmentCreate extends Omit<BaseDomainCreate, "id"> {
+export interface CohortAssignmentData {
   patientId: string;
   cohortId: string;
 }
 
-export interface CohortAssignment extends BaseDomain, Required<CohortAssignmentCreate> {}
+export interface CohortAssignmentCreate extends CohortAssignmentData {
+  cxId: string;
+}
+
+export interface CohortAssignment extends BaseDomainNoId, Required<CohortAssignmentData> {}
