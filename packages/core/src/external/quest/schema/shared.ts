4e25ceeffe3f988a21f5c1249715e19b9d67083f
Keshav Saharia <keshav@metriport.com>
2025-06-07 13:57:17 -0700

    feat(core): created request schema
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/external/quest/schema/shared.ts
+++ packages/core/src/external/quest/schema/shared.ts
@@ -1,10 +1,11 @@
 import { MetriportError } from "@metriport/shared";
+import { convertDateToString } from "@metriport/shared/common/date";
 
 /**
  * Describes a single field mapping from an object to a column in a space-padded
  */
 interface FileField<T extends object, K extends keyof T = keyof T> {
-  field: number;
+  field: number; // 1 indexed to match specification
   length: number; // in bytes
   key?: K;
   description?: string;
@@ -15,6 +16,7 @@ interface FieldOption {
   optional?: boolean;
   minLength?: number;
   truncate?: boolean;
+  defaultValue?: string;
 }
 
 /**
@@ -93,7 +95,7 @@ export function toQuestEnum<T extends object>(
     } else if (option.optional && value == null) {
       return fillEmptyByteLength(byteLength);
     } else {
-      throw new MetriportError(`Invalid value: ${value}`, "to_quest_enum", {
+      throw new MetriportError(`Invalid value: ${value}`, undefined, {
         value: String(value),
         options: enumerated.join(","),
       });
@@ -112,7 +114,7 @@ export function fromQuestEnum<T extends string, O extends FieldOption>(
     } else if (value === "" && option.optional) {
       return undefined as FieldTypeFromQuest<T, O>;
     } else {
-      throw new MetriportError(`Invalid value: ${value}`, "from_quest_enum", {
+      throw new MetriportError(`Invalid value: ${value}`, undefined, {
         value,
         options: enumerated.join(","),
       });
@@ -128,7 +130,7 @@ export function toQuestUnused<T extends object>(): ConvertFieldToQuest<T> {
 
 export function toQuestString<T extends object>(
   key: keyof T,
-  { optional = false, minLength = 0, truncate = false }: FieldOption = {}
+  { optional = false, minLength = 0, truncate = false, defaultValue = "" }: FieldOption = {}
 ) {
   return function (sourceObject: T, byteLength: number): string {
     const value = sourceObject[key];
@@ -138,9 +140,9 @@ export function toQuestString<T extends object>(
       }
       if (minLength != null && value.length < minLength) {
         if (optional) {
-          return fillEmptyByteLength(byteLength);
+          return fillByteLength(defaultValue, byteLength);
         }
-        throw new MetriportError(`Value is too short: ${value}`, "to_quest_string", {
+        throw new MetriportError(`Value is too short: ${value}`, undefined, {
           value: String(value),
           key: String(key),
           minLength,
@@ -148,9 +150,9 @@ export function toQuestString<T extends object>(
       }
       return fillByteLength(value, byteLength);
     } else if (optional && value == null) {
-      return fillEmptyByteLength(byteLength);
+      return fillByteLength(defaultValue, byteLength);
     } else {
-      throw new MetriportError(`Invalid value: ${value}`, "to_quest_string", {
+      throw new MetriportError(`Invalid value: ${value}`, undefined, {
         value: String(value),
         key: String(key),
       });
@@ -164,6 +166,64 @@ export function fromQuestString<O extends FieldOption>(option: O = {} as O) {
       return undefined as FieldTypeFromQuest<string, O>;
     } else {
       return value;
+    }
+  };
+}
+
+export function toQuestInteger<T extends object>(
+  key: keyof T,
+  { optional = false }: { optional?: boolean } = {}
+) {
+  return function (sourceObject: T, byteLength: number): string {
+    const value = sourceObject[key];
+    if (typeof value === "number" && Number.isFinite(value)) {
+      return fillByteLength(value.toFixed(0), byteLength);
+    } else if (optional && value == null) {
+      return fillEmptyByteLength(byteLength);
+    } else {
+      throw new MetriportError(`Invalid value: ${value}`, undefined, {
+        value: String(value),
+        key: String(key),
+      });
+    }
+  };
+}
+
+export function fromQuestDate<O extends FieldOption>(option: O = {} as O) {
+  return function (value: string): FieldTypeFromQuest<Date, O> {
+    if (value.length !== 8) {
+      if (option.optional && value.length === 0) {
+        return undefined as FieldTypeFromQuest<Date, O>;
+      } else {
+        throw new MetriportError(`Invalid date: ${value}`);
+      }
+    }
+    const date = new Date();
+    date.setUTCFullYear(parseInt(value.substring(0, 4), 10));
+    date.setUTCMonth(parseInt(value.substring(4, 6), 10) - 1);
+    date.setUTCDate(parseInt(value.substring(6, 8), 10));
+    date.setUTCHours(0, 0, 0, 0);
+    return date;
+  };
+}
+
+export function toQuestDate<T extends object>(
+  key: keyof T,
+  { optional = false, useUtc = true }: { optional?: boolean; useUtc?: boolean } = {}
+) {
+  return function (sourceObject: T): string {
+    const value = sourceObject[key];
+    if (value instanceof Date) {
+      return convertDateToString(value, { useUtc });
+    } else if (typeof value === "string") {
+      return value.replace(/-/g, "");
+    } else if (optional && value == null) {
+      return "";
+    } else {
+      throw new MetriportError(`Invalid value: ${value}`, undefined, {
+        value: String(value),
+        key: String(key),
+      });
     }
   };
 }
