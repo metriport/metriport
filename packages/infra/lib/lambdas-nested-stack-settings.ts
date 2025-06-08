301afd2844abaf0309051a4dc9a08778861e6b19
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-06-07 20:04:02 -0700

    build: increase RAM on ingestion lambda
    Ref eng-403
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/infra/lib/lambdas-nested-stack-settings.ts
+++ packages/infra/lib/lambdas-nested-stack-settings.ts
@@ -1,8 +1,10 @@
 import { Duration } from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 
+// Single timeout for both lambdas b/c ingestion needs more time, and currently search might also ingest
+const lambdaTimeout = Duration.minutes(15).minus(Duration.seconds(5));
+
 export function getConsolidatedIngestionConnectorSettings() {
-  const lambdaTimeout = Duration.minutes(15).minus(Duration.seconds(5));
   return {
     name: "ConsolidatedIngestion",
     queue: {
@@ -16,7 +18,7 @@ export function getConsolidatedIngestionConnectorSettings() {
     },
     lambda: {
       runtime: lambda.Runtime.NODEJS_20_X,
-      memory: 2048,
+      memory: 4096,
       timeout: lambdaTimeout,
     },
     eventSource: {
@@ -24,6 +26,17 @@ export function getConsolidatedIngestionConnectorSettings() {
       maxConcurrency: 5, // how many lambdas can hit the OpenSearch service at once
       // Partial batch response: https://docs.aws.amazon.com/prescriptive-guidance/latest/lambda-event-filtering-partial-batch-responses-for-sqs/welcome.html
       reportBatchItemFailures: false,
+    },
+  };
+}
+
+export function getConsolidatedSearchConnectorSettings() {
+  return {
+    name: "ConsolidatedSearch",
+    lambda: {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      memory: 4096,
+      timeout: lambdaTimeout,
     },
   };
 }
