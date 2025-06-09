18f5a67035e65cdd120eea7f34245531755e2de5
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-06-09 10:55:44 -0700

    build: wip cw v2
    Ref eng-200
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/commonwell-cert-runner/src/index.ts
+++ packages/commonwell-cert-runner/src/index.ts
@@ -1,14 +1,18 @@
-#!/usr/bin/env node
+import * as dotenv from "dotenv";
+// keep that ^ on top
 import { APIMode, CommonWell, RequestMetadata } from "@metriport/commonwell-sdk";
 import { PurposeOfUse } from "@metriport/shared";
 import { Command } from "commander";
-import * as dotenv from "dotenv";
-import { documentConsumption } from "./document-consumption";
-import { documentContribution } from "./document-contribution";
-import { linkManagement } from "./link-management";
+import {
+  memberCertificateString,
+  memberId,
+  memberName,
+  memberPrivateKeyString,
+  orgCertificateString,
+  orgPrivateKeyString,
+} from "./env";
 import { orgManagement } from "./org-management";
 import { patientManagement } from "./patient-management";
-import { personManagement } from "./person-management";
 import { getEnvOrFail } from "./util";
 
 function metriportBanner(): string {
@@ -39,21 +43,7 @@ program
   .description("Tool to run through Edge System CommonWell certification test cases.")
   .requiredOption(
     `--env-file <file-path>`,
-    `Absolute path to the .env file containing required config. Example required file contents:
-
-COMMONWELL_ORG_NAME=Metriport
-COMMONWELL_OID=2.16.840.1.113883.3.9621
-COMMONWELL_SANDBOX_ORG_NAME=Metriport-OrgA-1617
-COMMONWELL_SANDBOX_OID=2.16.840.1.113883.3.3330.8889429.1617.1
-COMMONWELL_ORG_PRIVATE_KEY="-----BEGIN PRIVATE KEY-----
-fkadsjhfhdsakjfhdsakhfkdsahfadshfkhdsfhdsakfdhafkashdfkjhalsdkjf
------END PRIVATE KEY-----
-"
-COMMONWELL_ORG_CERTIFICATE="-----BEGIN CERTIFICATE-----
-asdlkfjladsjflkjdaslkfjdsafjadslfjasdlkfjdsaklfjdkalfjdslfjalkjs
------END CERTIFICATE-----
-"
-    `
+    `Absolute path to the .env file containing required config. Example required file contents on README.md`
   )
   .addHelpText("before", metriportBanner())
   .showHelpAfterError()
@@ -65,62 +55,65 @@ async function main() {
   const options = program.opts();
   dotenv.config({ path: options.envFile });
 
-  // Main Account Org
-  const commonwellOrgPrivateKey = getEnvOrFail("COMMONWELL_ORG_PRIVATE_KEY");
-  const commonwellOrgCert = getEnvOrFail("COMMONWELL_ORG_CERTIFICATE");
-  const commonwellOID = getEnvOrFail("COMMONWELL_OID");
-  const commonwellOrgName = getEnvOrFail("COMMONWELL_ORG_NAME");
-
-  const commonWell = new CommonWell(
-    commonwellOrgCert,
-    commonwellOrgPrivateKey,
-    commonwellOrgName,
-    commonwellOID,
-    APIMode.integration
-  );
-
-  // Sandbox Account Org
-  const commonwellSandboxOID = getEnvOrFail("COMMONWELL_SANDBOX_OID");
-  const commonwellSandboxOrgName = getEnvOrFail("COMMONWELL_SANDBOX_ORG_NAME");
-
-  const commonWellSandbox = new CommonWell(
-    commonwellOrgCert,
-    commonwellOrgPrivateKey,
-    commonwellSandboxOrgName,
-    commonwellSandboxOID,
-    APIMode.integration
-  );
-
-  // Member Account Org
-  const commonwellMemberOID = getEnvOrFail("COMMONWELL_MEMBER_OID");
-  const commonwellMemberPrivateKey = getEnvOrFail("COMMONWELL_MEMBER_PRIVATE_KEY");
-  const commonwellMemberCert = getEnvOrFail("COMMONWELL_MEMBER_CERTIFICATE");
-
-  const commonWellMember = new CommonWell(
-    commonwellMemberCert,
-    commonwellMemberPrivateKey,
-    commonwellOrgName,
-    commonwellMemberOID,
-    APIMode.integration
-  );
-
-  const queryMeta: RequestMetadata = {
-    purposeOfUse: PurposeOfUse.TREATMENT,
-    role: "ict",
-    subjectId: "admin",
-  };
+  // // Sandbox Account Org
+  const commonwellSandboxOID = getEnvOrFail("CW_SANDBOX_ORG_OID");
+  const commonwellSandboxOrgName = getEnvOrFail("CW_SANDBOX_ORG_NAME");
+  // const commonWellSandbox = new CommonWell(
+  //   commonwellOrgCert,
+  //   commonwellOrgPrivateKey,
+  //   commonwellSandboxOrgName,
+  //   commonwellSandboxOID,
+  //   APIMode.integration
+  // );
 
   // Run through the CommonWell certification test cases
 
-  await orgManagement(commonWellMember, queryMeta);
-  await personManagement(commonWell, queryMeta);
-  await patientManagement(commonWell, commonWellSandbox, queryMeta);
-  await linkManagement(commonWell, queryMeta);
-  await documentConsumption(commonWell, queryMeta);
-  await documentContribution({ memberManagementApi: commonWellMember, api: commonWell, queryMeta });
+  try {
+    const memberQueryMeta: RequestMetadata = {
+      purposeOfUse: PurposeOfUse.TREATMENT,
+      role: "ict",
+      subjectId: "admin",
+    };
+    const commonWellMember = new CommonWell(
+      memberCertificateString,
+      memberPrivateKeyString,
+      memberName,
+      memberId,
+      APIMode.integration
+    );
+    const org = await orgManagement(commonWellMember, memberQueryMeta);
 
-  // Issue #425Ø
-  // await patientLinksWithStrongIds(commonWell, queryMeta);
+    // const orgQueryMeta = memberQueryMeta;
+    // const commonWell = commonWellMember;
+    const orgQueryMeta: RequestMetadata = {
+      purposeOfUse: PurposeOfUse.TREATMENT,
+      role: "ict",
+      subjectId: `${org.name} System User`,
+    };
+    const commonWell = new CommonWell(
+      orgCertificateString,
+      orgPrivateKeyString,
+      org.name,
+      org.organizationId,
+      // "invisible-black-hamster",
+      // "2.16.840.1.113883.3.9621.5.556874",
+      APIMode.integration
+    );
+    await patientManagement(commonWell, orgQueryMeta);
+    // await personManagement(commonWell, queryMeta);
+    // await patientManagement(commonWell, commonWellSandbox, queryMeta);
+    // await linkManagement(commonWell, queryMeta);
+    // await documentConsumption(commonWell, queryMeta);
+    // await documentContribution({ memberManagementApi: commonWellMember, api: commonWell, queryMeta });
+
+    // Issue #425Ø
+    // await patientLinksWithStrongIds(commonWell, queryMeta);
+  } catch (error: any) {
+    console.error(`Error (${error.response?.status}): ${error.message}`);
+    console.error(JSON.stringify(error.response?.data ?? error, null, 2));
+    // console.error(error);
+    process.exit(1);
+  }
 }
 
 main();
