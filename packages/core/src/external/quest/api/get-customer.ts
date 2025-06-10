91b9b7b43b9eff4ba046d4230b220b6581ea422c
Keshav Saharia <keshav@metriport.com>
2025-06-09 17:32:06 -0700

    feat(core): add API class for Quest integration
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

---
+++ packages/core/src/external/quest/api/get-customer.ts
@@ -0,0 +1,45 @@
+import { errorToString, executeWithNetworkRetries, MetriportError } from "@metriport/shared";
+import { CustomerData, customerDataSchema } from "@metriport/shared/domain/customer";
+import axios, { AxiosInstance } from "axios";
+import { Config } from "../../../util/config";
+import { out } from "../../../util/log";
+import { validateAndLogResponse } from "./shared";
+
+export interface GetCustomerParams {
+  cxId: string;
+}
+
+/**
+ * Sends an API request to cx-data, which returns the customer's data (facilities)
+ *
+ * @param cxId - The CX ID
+ * @returns The customer's data
+ */
+export async function getCustomerData(
+  { cxId }: GetCustomerParams,
+  axiosInstance?: AxiosInstance
+): Promise<CustomerData> {
+  const { log, debug } = out(`Quest getCustomer - cxId ${cxId}`);
+  const api = axiosInstance ?? axios.create({ baseURL: Config.getApiUrl() });
+  const queryParams = new URLSearchParams({ cxId });
+  const getCustomerUrl = `/internal/cx-data?${queryParams.toString()}`;
+  try {
+    const response = await executeWithNetworkRetries(async () => {
+      return api.get(getCustomerUrl);
+    });
+    return validateAndLogResponse<CustomerData>({
+      url: getCustomerUrl,
+      response,
+      schema: customerDataSchema,
+      debug,
+    });
+  } catch (error) {
+    const msg = "Failure while getting customer data @ Api";
+    log(`${msg}. Cause: ${errorToString(error)}`);
+    throw new MetriportError(msg, error, {
+      cxId,
+      url: getCustomerUrl,
+      context: "quest.getCustomer",
+    });
+  }
+}
---
+++ packages/core/src/external/quest/api/get-patient-ids.ts
@@ -0,0 +1,48 @@
+import { errorToString, executeWithNetworkRetries, MetriportError } from "@metriport/shared";
+import axios, { AxiosInstance } from "axios";
+import { Config } from "../../../util/config";
+import { out } from "../../../util/log";
+import { validateAndLogResponse } from "./shared";
+import { PatientIdsResponse, patientIdsSchema } from "@metriport/shared/domain/patient";
+
+interface GetPatientIdsParams {
+  cxId: string;
+  facilityId?: string | undefined;
+}
+
+/**
+ * Retrieves an array of patient IDs for a given customer and facility.
+ *
+ * @param cxId - The customer ID.
+ * @param facilityId - The facility ID
+ * @returns {GetPatientIdsResponse} contains an array of patient IDs
+ */
+export async function getPatientIds(
+  { cxId, facilityId }: GetPatientIdsParams,
+  axiosInstance?: AxiosInstance
+): Promise<PatientIdsResponse> {
+  const { log, debug } = out(`Quest getPatientIds - cxId ${cxId}`);
+  const api = axiosInstance ?? axios.create({ baseURL: Config.getApiUrl() });
+  const queryParams = new URLSearchParams({ cxId, ...(facilityId ? { facilityId } : {}) });
+  const getPatientsUrl = `/internal/patient/ids?${queryParams.toString()}`;
+  try {
+    const response = await executeWithNetworkRetries(async () => {
+      return api.get(getPatientsUrl);
+    });
+    return validateAndLogResponse<PatientIdsResponse>({
+      url: getPatientsUrl,
+      response,
+      schema: patientIdsSchema,
+      debug,
+    });
+  } catch (error) {
+    const msg = "Failure while getting patient IDs @ Api";
+    log(`${msg}. Cause: ${errorToString(error)}`);
+    throw new MetriportError(msg, error, {
+      cxId,
+      facilityId,
+      url: getPatientsUrl,
+      context: "quest.getPatientIds",
+    });
+  }
+}
