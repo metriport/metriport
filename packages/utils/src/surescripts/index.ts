9a082d03f22d1706ebcf95322d2fb30d813a0562
Keshav Saharia <keshav@metriport.com>
2025-06-13 13:39:49 -0700

    fix(utils): link verify request to utils
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/utils/src/surescripts/index.ts
+++ packages/utils/src/surescripts/index.ts
@@ -2,6 +2,7 @@ import { Command } from "commander";
 import sftpAction from "./sftp-action";
 import sendPatientRequest from "./send-patient-request";
 import sendBatchRequest from "./send-batch-request";
+import verifyRequest from "./verify-request";
 import receiveResponse from "./receive-response";
 import receiveVerification from "./receive-verification";
 
@@ -9,6 +10,7 @@ const program = new Command();
 program.addCommand(sftpAction);
 program.addCommand(sendPatientRequest);
 program.addCommand(sendBatchRequest);
+program.addCommand(verifyRequest);
 program.addCommand(receiveResponse);
 program.addCommand(receiveVerification);
 program.parse();
