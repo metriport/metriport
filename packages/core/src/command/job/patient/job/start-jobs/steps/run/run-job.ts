7b087e3e489ac16162ef0e3932dd0c0f7c691e2f
Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>
2025-06-17 08:20:41 -0700

    feat(jobs): better typing
    Ref: ENG-433
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MBP.attlocal.net>

--- packages/core/src/command/job/patient/job/start-jobs/steps/run/run-job.ts
+++ packages/core/src/command/job/patient/job/start-jobs/steps/run/run-job.ts
@@ -1,6 +1,8 @@
 import { PatientJob } from "@metriport/shared/domain/job/patient-job";
 
-export type RunJobRequest = Pick<PatientJob, "id" | "cxId" | "runUrl">;
+export type RunJobRequest = Pick<PatientJob, "id" | "cxId"> & {
+  runUrl: string;
+};
 
 export interface RunJobHandler {
   runJob(request: RunJobRequest): Promise<void>;
