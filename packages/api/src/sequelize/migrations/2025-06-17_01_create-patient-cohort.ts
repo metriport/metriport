6cb9671a0bbcf296bff54895a2b9602571ad3aae
Ramil Garipov <ramil@metriport.com>
2025-06-17 14:06:19 -0700

    fix(api): upd to patient cohort table migration
    Part of ENG-420
    
    Signed-off-by: Ramil Garipov <ramil@metriport.com>

--- packages/api/src/sequelize/migrations/2025-06-17_01_create-patient-cohort.ts
+++ packages/api/src/sequelize/migrations/2025-06-17_01_create-patient-cohort.ts
@@ -5,7 +5,11 @@ import * as shared from "../migrations-shared";
 const patientTableName = "patient";
 const cohortTableName = "cohort";
 const patientCohortTableName = "patient_cohort";
-const pkName = `${patientCohortTableName}_patient_id_cohort_id_pkey`;
+
+const cohortIdColumn = "cohort_id";
+const patientIdColumn = "patient_id";
+const cohortIdIndex = `${patientCohortTableName}_${cohortIdColumn}_idx`;
+// const patientCohortIndex = `${patientCohortTableName}_${cohortIdColumn}_${patientIdColumn}_idx`;
 
 export const up: Migration = async ({ context: queryInterface }) => {
   await queryInterface.sequelize.transaction(async transaction => {
@@ -13,21 +17,30 @@ export const up: Migration = async ({ context: queryInterface }) => {
       queryInterface,
       patientCohortTableName,
       {
-        patient_id: {
+        id: {
+          type: DataTypes.UUID,
+          primaryKey: true,
+          allowNull: false,
+        },
+        patientId: {
           type: DataTypes.STRING,
           allowNull: false,
+          field: "patient_id",
           references: {
             model: patientTableName,
             key: "id",
           },
+          onDelete: "CASCADE",
         },
-        cohort_id: {
+        cohortId: {
           type: DataTypes.UUID,
           allowNull: false,
+          field: "cohort_id",
           references: {
             model: cohortTableName,
             key: "id",
           },
+          onDelete: "CASCADE",
         },
       },
       {
@@ -37,10 +50,18 @@ export const up: Migration = async ({ context: queryInterface }) => {
     );
 
     await queryInterface.addConstraint(patientCohortTableName, {
-      fields: ["cohort_id", "patient_id"],
-      type: "primary key",
-      name: pkName,
+      fields: [patientIdColumn, cohortIdColumn],
+      type: "unique",
+      name: `${patientCohortTableName}_${patientIdColumn}_${cohortIdColumn}_unique`,
       transaction,
+    });
+
+    return queryInterface.sequelize.transaction(async transaction => {
+      await queryInterface.addIndex(patientCohortTableName, {
+        name: cohortIdIndex,
+        fields: [cohortIdColumn],
+        transaction,
+      });
     });
   });
 };
