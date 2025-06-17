c0a3765f06a23100916f8434586f1bf84585e601
Keshav Saharia <keshav@metriport.com>
2025-06-17 11:07:31 -0700

    fix(core): fix global variable in cloud handler
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/external/surescripts/command/convert-batch-response/convert-batch-response-cloud.ts
+++ packages/core/src/external/surescripts/command/convert-batch-response/convert-batch-response-cloud.ts
@@ -3,20 +3,19 @@ import { executeWithNetworkRetries } from "@metriport/shared";
 import { getLambdaResultPayload, makeLambdaClient } from "../../../aws/lambda";
 import { SurescriptsConversionBundle, SurescriptsJob } from "../../types";
 import { SurescriptsConvertBatchResponseHandler } from "./convert-batch-response";
-
-const region = Config.getAWSRegion();
-const lambdaClient = makeLambdaClient(region);
-
 export class SurescriptsConvertBatchResponseHandlerCloud
   implements SurescriptsConvertBatchResponseHandler
 {
-  constructor(private readonly surescriptsConvertBatchResponseLambdaName: string) {}
+  constructor(
+    private readonly surescriptsConvertBatchResponseLambdaName: string,
+    private readonly lambdaClient = makeLambdaClient(Config.getAWSRegion())
+  ) {}
 
   async convertBatchResponse(job: SurescriptsJob): Promise<SurescriptsConversionBundle[]> {
     const payload = JSON.stringify(job);
     let resultPayload: string | undefined;
     await executeWithNetworkRetries(async () => {
-      const result = await lambdaClient
+      const result = await this.lambdaClient
         .invoke({
           FunctionName: this.surescriptsConvertBatchResponseLambdaName,
           InvocationType: "RequestResponse",
