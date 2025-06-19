3b883cef5726f970cd7f0afcccf926428c38cdfa
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-06-19 12:14:12 -0700

    build: 3rd wip cw v2
    Ref eng-200
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/commonwell-cert-runner/src/env.ts
+++ packages/commonwell-cert-runner/src/env.ts
@@ -8,6 +8,18 @@ export const memberPrivateKeyString = getEnvOrFail("CW_MEMBER_PRIVATE_KEY");
 
 export const orgCertificateString = getEnvOrFail("CW_ORG_CERTIFICATE");
 export const orgPrivateKeyString = getEnvOrFail("CW_ORG_PRIVATE_KEY");
+export const orgGatewayEndpoint = getEnvOrFail("CW_ORG_GATEWAY_ENDPOINT");
+export const orgGatewayAuthorizationServerEndpoint = getEnvOrFail(
+  "CW_ORG_GATEWAY_AUTHORIZATION_SERVER_ENDPOINT"
+);
+export const orgGatewayAuthorizationClientId = getEnvOrFail(
+  "CW_ORG_GATEWAY_AUTHORIZATION_CLIENT_ID"
+);
+export const orgGatewayAuthorizationClientSecret = getEnvOrFail(
+  "CW_ORG_GATEWAY_AUTHORIZATION_CLIENT_SECRET"
+);
+/** If set, the cert runner will use this org and not try to create a new one. */
+export const existingOrgId = getEnv("CW_ORG_ID");
 
 export const commonwellSandboxOID = getEnvOrFail("CW_SANDBOX_ORG_OID");
 export const commonwellSandboxOrgName = getEnvOrFail("CW_SANDBOX_ORG_NAME");
