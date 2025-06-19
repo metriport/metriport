b2e45beffa2eb025425c579917bde1ea878c805a
Keshav Saharia <keshav@metriport.com>
2025-06-19 10:52:32 -0700

    feat(utils): added quest utils scripts
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

---
+++ packages/utils/src/quest/index.ts
@@ -0,0 +1,20 @@
+import { Command } from "commander";
+import sendPatientRequest from "./send-patient-request";
+import sendBatchRequest from "./send-batch-request";
+import receiveResponse from "./receive-response";
+import { buildSftpAction } from "../shared/sftp-action";
+import { QuestSftpClient } from "@metriport/core/external/quest/client";
+
+const program = new Command();
+program.addCommand(sendPatientRequest);
+program.addCommand(sendBatchRequest);
+program.addCommand(receiveResponse);
+
+const sftpAction = buildSftpAction(
+  new QuestSftpClient({
+    logLevel: "debug",
+  })
+);
+program.addCommand(sftpAction);
+
+program.parse();
