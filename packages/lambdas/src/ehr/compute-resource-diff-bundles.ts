5d3210ebb143367a65332d20f307cc60ed5e03c9
Thomas Yopes <thomasyopes@thomass-mbp.lan>
2025-06-20 13:23:50 -0700

    feat(ehr): fixing lambda logging
    Ref: ENG-72
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@thomass-mbp.lan>

--- packages/lambdas/src/ehr/compute-resource-diff-bundles.ts
+++ packages/lambdas/src/ehr/compute-resource-diff-bundles.ts
@@ -1,11 +1,10 @@
-import { ComputeResourceDiffBundlesRequest } from "@metriport/core/external/ehr/job/create-resource-diff-bundles/steps/compute/ehr-compute-resource-diff-bundles";
 import { EhrComputeResourceDiffBundlesDirect } from "@metriport/core/external/ehr/job/create-resource-diff-bundles/steps/compute/ehr-compute-resource-diff-bundles-direct";
-import { MetriportError } from "@metriport/shared";
 import { SQSEvent } from "aws-lambda";
 import { capture } from "../shared/capture";
 import { ehrCreateResourceDiffBundlesSchema } from "../shared/ehr";
 import { getEnvOrFail } from "../shared/env";
 import { prefixedLog } from "../shared/log";
+import { parseBody } from "../shared/parse-body";
 import { getSingleMessageOrFail } from "../shared/sqs";
 
 // Keep this as early on the file as possible
@@ -26,14 +25,12 @@ export const handler = capture.wrapHandler(async (event: SQSEvent) => {
   const message = getSingleMessageOrFail(event.Records, lambdaName);
   if (!message) return;
 
-  console.log(`Running with unparsed body: ${message.body}`);
-  const parsedBody = parseBody(message.body);
+  const parsedBody = parseBody(ehrCreateResourceDiffBundlesSchema, message.body);
   const { metriportPatientId, ehrPatientId, jobId } = parsedBody;
 
   const log = prefixedLog(
     `metriportPatientId ${metriportPatientId}, ehrPatientId ${ehrPatientId}, jobId ${jobId}`
   );
-  log(`Parsed: ${JSON.stringify(parsedBody)}, waitTimeInMillis ${waitTimeInMillis}`);
 
   const receiveCount = parseInt(message.attributes.ApproximateReceiveCount);
   const reportError = receiveCount >= maxAttempts;
@@ -45,14 +42,3 @@ export const handler = capture.wrapHandler(async (event: SQSEvent) => {
   const finishedAt = new Date().getTime();
   log(`Done local duration: ${finishedAt - startedAt}ms`);
 });
-
-function parseBody(body?: unknown): ComputeResourceDiffBundlesRequest {
-  if (!body) throw new MetriportError(`Missing message body`);
-
-  const bodyString = typeof body === "string" ? (body as string) : undefined;
-  if (!bodyString) throw new MetriportError(`Invalid body`);
-
-  const bodyAsJson = JSON.parse(bodyString);
-
-  return ehrCreateResourceDiffBundlesSchema.parse(bodyAsJson);
-}
