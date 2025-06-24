8a85b59b3adce268bebe0c058b877d5b044d18aa
Ramil Garipov <ramil@metriport.com>
2025-06-24 16:51:55 -0700

    fix(api): adjusting routes
    Part of ENG-432
    
    Signed-off-by: Ramil Garipov <ramil@metriport.com>

--- packages/api/src/routes/internal/medical/monitoring/index.ts
+++ packages/api/src/routes/internal/medical/monitoring/index.ts
@@ -3,6 +3,6 @@ import dischargeRequery from "./discharge-requery";
 
 const routes = Router();
 
-routes.use("/discharge-requery/", dischargeRequery);
+routes.use("/job/discharge-requery/", dischargeRequery);
 
 export default routes;
