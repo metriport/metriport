d7f42612d6cb1d91fd82ca864957b281d702e360
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-06-23 18:27:42 -0700

    feat(api): add @metriport/eslint-rules to api
    metriport/metriport-internal#1040
    
    Ref: ENG-503
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/api/.eslintrc.js
+++ packages/api/.eslintrc.js
@@ -1,7 +1,18 @@
 /*global module*/
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "@metriport/eslint-rules"],
   root: true,
+  rules: {
+    "@metriport/eslint-rules/no-named-arrow-functions": "warn",
+  },
+  overrides: [
+    {
+      files: ["src/sequelize/migrations/**/*"],
+      rules: {
+        "@metriport/eslint-rules/no-named-arrow-functions": "off",
+      },
+    },
+  ],
 };
