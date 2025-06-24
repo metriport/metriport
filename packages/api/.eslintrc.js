9c72523d4ea5c4cc0beda1de499cd7d4353ab70c
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-06-24 08:07:30 -0700

    feat(core): enable linting rules
    metriport/metriport-internal#1040
    
    Ref: ENG-503
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/api/.eslintrc.js
+++ packages/api/.eslintrc.js
@@ -1,12 +1,9 @@
 /*global module*/
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:@metriport/eslint-rules/recommended"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "@metriport/eslint-rules"],
   root: true,
-  rules: {
-    "@metriport/eslint-rules/no-named-arrow-functions": "warn",
-  },
   overrides: [
     {
       files: ["src/sequelize/migrations/**/*"],
