44df8b6ec3273c8f9d322f3a995ad07b6759f3db
Keshav Saharia <keshav@metriport.com>
2025-07-07 23:04:52 -0700

    test(core): added bedrock agent tests
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

---
+++ packages/core/src/external/bedrock/__tests__/bedrock-tool.test.ts
@@ -0,0 +1,43 @@
+import { z } from "zod";
+import { BedrockTool } from "../agent/tool";
+
+describe("BedrockTool", () => {
+  it("should be defined", () => {
+    expect(BedrockTool).toBeDefined();
+  });
+
+  it("should get a tool invocation", () => {
+    type GetWeatherInput = { city: string };
+    class GetWeatherTool extends BedrockTool<GetWeatherInput> {
+      constructor() {
+        super({
+          name: "get_weather",
+          description: "Get the weather for a given city",
+          inputSchema: z.object({ city: z.string() }),
+        });
+      }
+      async execute(): Promise<{ temperature: number }> {
+        return { temperature: 70 };
+      }
+    }
+
+    const tool = new GetWeatherTool();
+
+    expect(tool.getInvocation()).toEqual({
+      type: "custom",
+      name: "get_weather",
+      description: "Get the weather for a given city",
+      input_schema: {
+        $schema: "http://json-schema.org/draft-07/schema#",
+        additionalProperties: false,
+        type: "object",
+        properties: {
+          city: {
+            type: "string",
+          },
+        },
+        required: ["city"],
+      },
+    });
+  });
+});
