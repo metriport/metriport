66f41cb293e6330e4571dbd9f3b8a80460960927
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-07-10 23:27:05 -0700

    feat(core): remove unused schema
    metriport/metriport-internal#1040
    
    Ref: ENG-615
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/core/src/domain/patient-settings.ts
+++ packages/core/src/domain/patient-settings.ts
@@ -1,6 +1,6 @@
+import { queryMetaSchema } from "@metriport/shared";
 import { z } from "zod";
 import { BaseDomain, BaseDomainCreate } from "./base-domain";
-import { queryMetaSchema } from "@metriport/shared";
 
 export type Subscriptions = {
   adt?: boolean;
@@ -22,31 +22,8 @@ export const validHl7v2Subscriptions = ["adt"] as const;
 export const hl7v2SubscriptionSchema = z.enum(validHl7v2Subscriptions);
 export type Hl7v2Subscription = z.infer<typeof hl7v2SubscriptionSchema>;
 
-export const hl7v2SubscriptionRequestSchema = z.object({
-  subscriptions: z.array(z.string()).transform(subs => {
-    const validSubs: Hl7v2Subscription[] = [];
-    const invalidSubs: string[] = [];
-
-    subs.forEach(sub => {
-      const result = hl7v2SubscriptionSchema.safeParse(sub.toLowerCase());
-      if (result.success) {
-        validSubs.push(result.data);
-      } else {
-        invalidSubs.push(sub);
-      }
-    });
-
-    return { validSubscriptions: validSubs, invalidSubscriptions: invalidSubs };
-  }),
-});
-
 export const hl7v2SubscribersQuerySchema = z
   .object({
     hie: z.string(),
   })
   .and(queryMetaSchema);
-
-export const hl7v2SubscriptionRequestNewSchema = z.object({
-  subscriptions: z.array(hl7v2SubscriptionSchema),
-});
-export type Hl7v2SubscriptionRequestNew = z.infer<typeof hl7v2SubscriptionRequestNewSchema>;
