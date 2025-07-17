19752cdc85968b30ac7d776be54e1f4e1355f466
Thomas Yopes <thomasyopes@Mac.attlocal.net>
2025-07-17 11:05:55 -0700

    feat(ap): routes
    Ref: ENG-602
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Mac.attlocal.net>

--- packages/api/src/routes/internal/analytics-platform/index.ts
+++ packages/api/src/routes/internal/analytics-platform/index.ts
@@ -1,11 +1,10 @@
-import { startCsvToMetricsBatchJob } from "@metriport/core/command/analytics-platform/csv-to-metrics";
 import { startFhirToCsvBatchJob } from "@metriport/core/command/analytics-platform/fhir-to-csv";
 import { jobRunBodySchema } from "@metriport/shared/domain/job/types";
 import { Request, Response } from "express";
 import Router from "express-promise-router";
 import httpStatus from "http-status";
 import { requestLogger } from "../../helpers/request-logger";
-import { asyncHandler } from "../../util";
+import { asyncHandler, getFromQuery } from "../../util";
 
 const router = Router();
 
@@ -22,25 +21,9 @@ router.post(
   requestLogger,
   asyncHandler(async (req: Request, res: Response) => {
     const { cxId, jobId } = jobRunBodySchema.parse(req.body);
-    await startFhirToCsvBatchJob({ cxId, jobId });
-    return res.sendStatus(httpStatus.OK);
-  })
-);
-
-/**
- * POST /internal/analytics-platform/csv-to-metrics/run
- *
- * Runs the create resource diff bundles job.
- * @param req.body.cxId - The CX ID.
- * @param req.body.jobId - The job ID.
- * @returns 200 OK
- */
-router.post(
-  "/csv-to-metrics/run",
-  requestLogger,
-  asyncHandler(async (req: Request, res: Response) => {
-    const { cxId, jobId } = jobRunBodySchema.parse(req.body);
-    await startCsvToMetricsBatchJob({ cxId, jobId });
+    const patientId = getFromQuery("patientId", req);
+    const bundlesToAppend = getFromQuery("bundlesToAppend", req);
+    await startFhirToCsvBatchJob({ cxId, jobId, patientId, bundlesToAppend });
     return res.sendStatus(httpStatus.OK);
   })
 );
