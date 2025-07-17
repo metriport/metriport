4d20cf91e90c5b98a7aab5986b6ad150da11a97f
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-07-17 16:42:21 -0300

    refactor(api): org and facility internal routes to cw v2
    Ref eng-513
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

---
+++ packages/api/src/external/commonwell-v2/command/organization/organization.ts
@@ -0,0 +1,201 @@
+import {
+  isOrgInitiatorAndResponder,
+  Organization as CwSdkOrganization,
+} from "@metriport/commonwell-sdk";
+import { OID_PREFIX } from "@metriport/core/domain/oid";
+import { OrganizationData } from "@metriport/core/domain/organization";
+import { out } from "@metriport/core/util/log";
+import { capture } from "@metriport/core/util/notifications";
+import {
+  errorToString,
+  getEnvVarOrFail,
+  MetriportError,
+  NotFoundError,
+  TreatmentType,
+  USState,
+} from "@metriport/shared";
+import { Config } from "../../../../shared/config";
+import { getCertificate, makeCommonWellMemberAPI } from "../../api";
+
+const technicalContact = {
+  name: getEnvVarOrFail("CW_TECHNICAL_CONTACT_NAME"),
+  title: getEnvVarOrFail("CW_TECHNICAL_CONTACT_TITLE"),
+  email: getEnvVarOrFail("CW_TECHNICAL_CONTACT_EMAIL"),
+  phone: getEnvVarOrFail("CW_TECHNICAL_CONTACT_PHONE"),
+};
+
+export type CwOrgOrFacility = {
+  oid: string;
+  data: OrganizationData;
+  active: boolean;
+  isInitiatorAndResponder: boolean;
+};
+
+type CwSdkOrganizationWithOrgId = Omit<CwSdkOrganization, "organizationId"> &
+  Required<Pick<CwSdkOrganization, "organizationId">>;
+
+function cwOrgOrFacilityToSdk(org: CwOrgOrFacility): CwSdkOrganizationWithOrgId {
+  const cwId = OID_PREFIX.concat(org.oid);
+  const cwOrg: CwSdkOrganizationWithOrgId = {
+    name: org.data.name,
+    type: org.data.type,
+    locations: [
+      {
+        address1: org.data.location.addressLine1,
+        ...(org.data.location.addressLine2
+          ? { address2: org.data.location.addressLine2 }
+          : undefined),
+        city: org.data.location.city,
+        state: org.data.location.state,
+        postalCode: org.data.location.zip,
+        country: org.data.location.country,
+      },
+    ],
+    // NOTE: IN STAGING IF THE ID ALREADY EXISTS IT WILL SAY INVALID ORG WHEN CREATING
+    organizationId: cwId,
+    homeCommunityId: cwId,
+    patientIdAssignAuthority: cwId,
+    displayName: org.data.name,
+    memberName: Config.getCWMemberOrgName(),
+    securityTokenKeyType: "BEARER",
+    isActive: org.active,
+    technicalContacts: [technicalContact],
+    networks: [
+      {
+        type: "CommonWell",
+        purposeOfUse: [],
+      },
+    ],
+  };
+  if (org.isInitiatorAndResponder) {
+    cwOrg.authorizationInformation = {
+      authorizationServerEndpoint: Config.getGatewayAuthorizationServerEndpoint(),
+      clientId: Config.getGatewayAuthorizationClientId(),
+      clientSecret: Config.getGatewayAuthorizationClientSecret(),
+      documentReferenceScope: "fhir/document",
+      binaryScope: "fhir/document",
+    };
+    cwOrg.gateways = [
+      {
+        serviceType: "R4_Base",
+        gatewayType: "FHIR",
+        endpointLocation: Config.getGatewayEndpoint(),
+      },
+    ];
+  }
+  return cwOrg;
+}
+
+export async function get(orgOid: string): Promise<CwSdkOrganization | undefined> {
+  const { log, debug } = out(`CW.v2 get Org - CW Org OID ${orgOid}`);
+  const cwId = OID_PREFIX.concat(orgOid);
+
+  const commonWell = makeCommonWellMemberAPI(Config.getCWMemberOrgName(), Config.getCWMemberOID());
+  try {
+    const resp = await commonWell.getOneOrg(cwId);
+    debug(`resp getOneOrg: `, JSON.stringify(resp));
+    return resp;
+  } catch (error) {
+    const msg = `Failure while getting Org @ CW`;
+    const cwRef = commonWell.lastTransactionId;
+    log(`${msg}. Org OID: ${orgOid}. Cause: ${errorToString(error)}. CW Reference: ${cwRef}`);
+    capture.error(msg, {
+      extra: {
+        orgOid,
+        cwId,
+        cwReference: cwRef,
+        context: `cw.org.get`,
+        error,
+      },
+    });
+    throw error;
+  }
+}
+
+export async function create(cxId: string, org: CwOrgOrFacility): Promise<void> {
+  const { log, debug } = out(`CW.v2 create Org - cx ${cxId}, CW Org OID ${org.oid}`);
+
+  const sdkOrg = cwOrgOrFacilityToSdk(org);
+  const commonWell = makeCommonWellMemberAPI(Config.getCWMemberOrgName(), Config.getCWMemberOID());
+  try {
+    const respCreate = await commonWell.createOrg(sdkOrg);
+    debug(`resp createOrg: `, JSON.stringify(respCreate));
+    const respAddCert = await commonWell.addCertificateToOrg(getCertificate(), org.oid);
+    debug(`resp addCertificateToOrg: `, JSON.stringify(respAddCert));
+  } catch (error) {
+    const msg = `Failure while creating org @ CW`;
+    const cwRef = commonWell.lastTransactionId;
+    log(`${msg}. Org OID: ${org.oid}. Cause: ${errorToString(error)}. CW Reference: ${cwRef}`);
+    capture.error(msg, {
+      extra: {
+        orgOid: org.oid,
+        cwReference: cwRef,
+        context: `cw.org.create`,
+        commonwellOrg: sdkOrg,
+        error,
+      },
+    });
+    throw error;
+  }
+}
+
+export async function update(cxId: string, org: CwOrgOrFacility): Promise<void> {
+  const { log, debug } = out(`CW.v2 update Org - cx ${cxId}, CW Org OID ${org.oid}`);
+
+  const sdkOrg = cwOrgOrFacilityToSdk(org);
+  const commonWell = makeCommonWellMemberAPI(Config.getCWMemberOrgName(), Config.getCWMemberOID());
+  try {
+    const resp = await commonWell.updateOrg(sdkOrg);
+    debug(`resp updateOrg: `, JSON.stringify(resp));
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    const cwRef = commonWell.lastTransactionId;
+    const extra = {
+      orgOid: org.oid,
+      cwReference: cwRef,
+      context: `cw.org.update`,
+      commonwellOrg: sdkOrg,
+      error,
+    };
+    if (error.response?.status === 404) {
+      capture.message("Got 404 while updating Org @ CW, creating it", { extra });
+      await create(cxId, org);
+    }
+    const msg = `Failure while updating org @ CW`;
+    log(`${msg}. Org OID: ${org.oid}. Cause: ${errorToString(error)}. CW Reference: ${cwRef}`);
+    capture.error(msg, { extra });
+    throw error;
+  }
+}
+
+export function parseCWEntry(org: CwSdkOrganization): CwOrgOrFacility {
+  const location = org.locations[0];
+  if (!location)
+    throw new MetriportError("Missing location on CW Organization", undefined, {
+      orgOid: org.organizationId,
+      orgName: org.name,
+    });
+  return {
+    data: {
+      name: org.name,
+      location: {
+        addressLine1: location.address1,
+        addressLine2: location.address2 ? location.address2 : undefined,
+        city: location.city,
+        state: location.state as USState,
+        zip: location.postalCode,
+        country: location.country,
+      },
+      type: org.type as TreatmentType,
+    },
+    oid: org.organizationId.replace(OID_PREFIX, ""),
+    active: org.isActive,
+    isInitiatorAndResponder: isOrgInitiatorAndResponder(org),
+  };
+}
+
+export async function getParsedOrgOrFail(oid: string): Promise<CwOrgOrFacility> {
+  const resp = await get(oid);
+  if (!resp) throw new NotFoundError("Organization not found", undefined, { orgOid: oid });
+  return parseCWEntry(resp);
+}
--- packages/api/src/routes/internal/hie/commonwell.ts
+++ packages/api/src/routes/internal/hie/commonwell.ts
@@ -1,8 +1,9 @@
+import { isCommonwellV2EnabledForCx } from "@metriport/core/command/feature-flags/domain-ffs";
+import { uuidv7 } from "@metriport/core/util/uuid-v7";
 import { NotFoundError } from "@metriport/shared";
 import { Request, Response } from "express";
 import Router from "express-promise-router";
 import httpStatus from "http-status";
-import { uuidv7 } from "@metriport/core/util/uuid-v7";
 import { getFacilityByOidOrFail } from "../../../command/medical/facility/get-facility";
 import {
   verifyCxAccessToSendFacilityToHies,
@@ -12,15 +13,16 @@ import {
   getOrganizationByOidOrFail,
   getOrganizationOrFail,
 } from "../../../command/medical/organization/get-organization";
+import { getPatientOrFail } from "../../../command/medical/patient/get-patient";
 import { getAndUpdateCWOrgAndMetriportOrg } from "../../../external/commonwell-v1/command/create-or-update-cw-organization";
+import { runOrScheduleCwPatientDiscovery } from "../../../external/commonwell-v1/command/run-or-schedule-patient-discovery";
 import { getParsedOrgOrFail } from "../../../external/commonwell-v1/organization";
 import { cwOrgActiveSchema } from "../../../external/commonwell-v1/shared";
+import { getAndUpdateCWOrgAndMetriportOrgV2 } from "../../../external/commonwell-v2/command/organization/create-or-update-cw-organization";
 import { handleParams } from "../../helpers/handle-params";
 import { requestLogger } from "../../helpers/request-logger";
 import { getUUIDFrom } from "../../schemas/uuid";
 import { asyncHandler, getFrom, getFromQueryAsBoolean } from "../../util";
-import { runOrScheduleCwPatientDiscovery } from "../../../external/commonwell-v1/command/run-or-schedule-patient-discovery";
-import { getPatientOrFail } from "../../../command/medical/patient/get-patient";
 
 const router = Router();
 
@@ -62,18 +64,28 @@ router.put(
   asyncHandler(async (req: Request, res: Response) => {
     const cxId = getUUIDFrom("query", req, "cxId").orFail();
     const oid = getFrom("params").orFail("oid", req);
+
     await verifyCxAccessToSendOrgToHies(cxId);
 
     const org = await getOrganizationByOidOrFail({ cxId, oid });
     if (!org.cwApproved) throw new NotFoundError("CW not approved");
 
     const orgActive = cwOrgActiveSchema.parse(req.body);
-    await getAndUpdateCWOrgAndMetriportOrg({
-      cxId,
-      oid,
-      active: orgActive.active,
-      org,
-    });
+    if (await isCommonwellV2EnabledForCx(cxId)) {
+      await getAndUpdateCWOrgAndMetriportOrgV2({
+        cxId,
+        oid,
+        active: orgActive.active,
+        org,
+      });
+    } else {
+      await getAndUpdateCWOrgAndMetriportOrg({
+        cxId,
+        oid,
+        active: orgActive.active,
+        org,
+      });
+    }
     return res.sendStatus(httpStatus.OK);
   })
 );
@@ -91,7 +103,12 @@ router.put(
   asyncHandler(async (req: Request, res: Response) => {
     const cxId = getUUIDFrom("query", req, "cxId").orFail();
     const facilityId = getFrom("query").orFail("facilityId", req);
+    // TODO this should be refactored: (1) the oid is already in the facility; (2) we should have a fn for org
+    // and one for facility (the impl of getAndUpdateCWOrgAndMetriportOrg uses facility of org based on whether
+    // facility is set or not); (3) we're loading models on the route and passing those around; (4) the route
+    // has part of the logic to implement the endpoint (it should be in a command).
     const oid = getFrom("params").orFail("oid", req);
+
     await verifyCxAccessToSendFacilityToHies(cxId);
 
     const org = await getOrganizationOrFail({ cxId });
@@ -99,13 +116,23 @@ router.put(
     if (!facility.cwApproved) throw new NotFoundError("CW not approved");
 
     const facilityActive = cwOrgActiveSchema.parse(req.body);
-    await getAndUpdateCWOrgAndMetriportOrg({
-      cxId,
-      oid,
-      active: facilityActive.active,
-      org,
-      facility,
-    });
+    if (await isCommonwellV2EnabledForCx(cxId)) {
+      await getAndUpdateCWOrgAndMetriportOrgV2({
+        cxId,
+        oid,
+        active: facilityActive.active,
+        org,
+        facility,
+      });
+    } else {
+      await getAndUpdateCWOrgAndMetriportOrg({
+        cxId,
+        oid,
+        active: facilityActive.active,
+        org,
+        facility,
+      });
+    }
     return res.sendStatus(httpStatus.OK);
   })
 );
