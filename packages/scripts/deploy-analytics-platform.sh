9d8af5bbf8715d9e4fc0bdb4518def602f6d32b7
Thomas Yopes <thomasyopes@Mac.attlocal.net>
2025-07-17 11:46:46 -0700

    feat(ap): remove dependecies
    Ref: ENG-602
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Mac.attlocal.net>

--- packages/scripts/deploy-analytics-platform.sh
+++ packages/scripts/deploy-analytics-platform.sh
@@ -11,15 +11,5 @@ if [[ -z "${ECR_REPO_URI}" ]]; then
   exit 1
 fi
 
-# Define extra dependencies needed for API
-API_EXTRA_DEPS="packages/api-sdk/package.json \
-  packages/api-sdk/dist \
-  packages/commonwell-sdk/package.json \
-  packages/commonwell-sdk/dist \
-  packages/carequality-sdk/package.json \
-  packages/carequality-sdk/dist \
-  packages/ihe-gateway-sdk/package.json \
-  packages/ihe-gateway-sdk/dist"
-
-source ./packages/scripts/generate-tarball.sh data-transformation/fhir-to-csv "$API_EXTRA_DEPS"
-source ./packages/scripts/push-image.sh data-transformation/fhir-to-csv "fhir-to-csv"
+source ./packages/scripts/generate-tarball.sh data-transformation/fhir-to-csv
+source ./packages/scripts/push-image.sh data-transformation/fhir-to-csv fhir-to-csv
