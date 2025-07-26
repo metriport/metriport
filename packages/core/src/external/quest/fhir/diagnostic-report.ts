739242e88eb748ef5894f59791f3c97e2a311586
Keshav Saharia <keshav@metriport.com>
2025-07-25 17:30:17 -0700

    feat(core): diagnostic report
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

---
+++ packages/core/src/external/quest/fhir/diagnostic-report.ts
@@ -0,0 +1,58 @@
+import { DiagnosticReport, Identifier, Specimen } from "@medplum/fhirtypes";
+import { ResponseDetail } from "../schema/response";
+
+export function buildDiagnosticReport(
+  detail: ResponseDetail,
+  { specimen }: { specimen?: Specimen }
+): DiagnosticReport {
+  const effectiveDateTime = getEffectiveDateTime(detail);
+  const identifier = getIdentifier(detail);
+
+  return {
+    resourceType: "DiagnosticReport",
+    status: "final",
+    effectiveDateTime,
+    identifier,
+    specimen: [
+      {
+        reference: `Specimen/${specimen?.id}`,
+      },
+    ],
+    category: [
+      {
+        coding: [
+          {
+            system: "http://questdiagnostics.com/lpc",
+            code: detail.localProfileCode ?? "",
+            display: detail.profileName ?? "",
+          },
+          {
+            system: "http://questdiagnostics.com/spc",
+            code: detail.standardProfileCode ?? "",
+            display: detail.profileName ?? "",
+          },
+        ],
+      },
+    ],
+  };
+}
+
+function getEffectiveDateTime(detail: ResponseDetail): string {
+  return detail.dateOfService.toISOString();
+}
+
+function getIdentifier(detail: ResponseDetail): Identifier[] {
+  const identifier: Identifier[] = [
+    {
+      system: "quest-accession",
+      value: detail.accessionNumber,
+    },
+  ];
+  if (detail.requisitionNumber) {
+    identifier.push({
+      system: "quest-requisition",
+      value: detail.requisitionNumber,
+    });
+  }
+  return identifier;
+}
