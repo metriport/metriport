3e0fa2544f5fe3d79ff2b33488fe33c2255da9e4
Rafael Leite <2132564+leite08@users.noreply.github.com>
2025-07-26 21:29:57 -0300

    refactor(data-trans): clean-up tmp files + log using print
    Ref eng-722
    
    Signed-off-by: Rafael Leite <2132564+leite08@users.noreply.github.com>

--- packages/data-transformation/fhir-to-csv/main.py
+++ packages/data-transformation/fhir-to-csv/main.py
@@ -1,6 +1,6 @@
 import boto3
 import os
-import logging
+import shutil
 import json
 import ndjson
 import snowflake.connector
@@ -21,11 +21,6 @@ from src.utils.database import (
 from src.utils.environment import Environment
 from src.utils.dwh import DWH
 from src.utils.file import create_consolidated_key, create_output_file_prefix
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(levelname)s - %(message)s'
-)
 
 transform_name = 'fhir-to-csv'
 
@@ -48,19 +43,19 @@ def transform_and_upload_data(
     else:
         bundle_key = create_consolidated_key(cx_id, patient_id)
 
-    local_cx_path = f"/tmp/output/{cx_id}"
+    local_cx_path = f"/tmp/{transform_name}/output/{cx_id}"
     os.makedirs(local_cx_path, exist_ok=True)
     local_patient_path = f"{local_cx_path}/{patient_id}"
     os.makedirs(local_patient_path, exist_ok=True)
     local_bundle_key = f"{local_patient_path}/bundle_{bundle_key.replace('/', '_')}.json"
     with open(local_bundle_key, "wb") as f:
         try:
-            logging.info(f"Downloading bundle {bundle_key} from {input_bucket} to {local_bundle_key}")
+            print(f"Downloading bundle {bundle_key} from {input_bucket} to {local_bundle_key}")
             s3_client.download_file(input_bucket, bundle_key, local_bundle_key)
-            logging.info(f"Downloaded bundle {bundle_key} from {input_bucket} to {local_bundle_key}")
+            print(f"Downloaded bundle {bundle_key} from {input_bucket} to {local_bundle_key}")
         except s3_client.exceptions.ClientError as e:
             if e.response['Error']['Code'] == '404':
-                logging.warning(f"Bundle {bundle_key} not found in input bucket {input_bucket}")
+                print(f"Bundle {bundle_key} not found in input bucket {input_bucket}")
                 raise ValueError("Bundle not found") from e
             else:
                 raise e
@@ -68,12 +63,12 @@ def transform_and_upload_data(
         bundle = json.load(f)
         entries = bundle["entry"]
         if entries is None or len(entries) < 1:
-            logging.warning(f"Bundle {bundle_key} has no entries")
+            print(f"Bundle {bundle_key} has no entries")
             return []
         local_ndjson_bundle_key = local_bundle_key.replace(".json", ".ndjson")
         with open(local_ndjson_bundle_key, "w") as f:
             ndjson.dump(entries, f)
-        logging.info(f"Parsing bundle {local_ndjson_bundle_key} to {local_patient_path}")
+        print(f"Parsing bundle {local_ndjson_bundle_key} to {local_patient_path}")
     local_output_files = parseNdjsonBundle.parse(local_ndjson_bundle_key, local_patient_path)
 
     output_bucket_and_file_keys_and_table_names = []
@@ -82,11 +77,14 @@ def transform_and_upload_data(
         output_file_key = f"{output_file_prefix}/{file.replace('/', '_')}"
         table_name = file.split("/")[-1].replace(".csv", "")
         with open(file, "rb") as f:
-            logging.info(f"Uploading file {file} to {output_bucket}/{output_file_key}")
+            print(f"Uploading file {file} to {output_bucket}/{output_file_key}")
             s3_client.upload_fileobj(f, output_bucket, output_file_key)
         output_bucket_and_file_keys_and_table_names.append((output_bucket, output_file_key, table_name))
 
-    logging.info(f"Done transform_and_upload_data for patient_id {patient_id}")
+    print(f"Cleaning up local files in {local_cx_path}")
+    shutil.rmtree(local_cx_path)
+
+    print(f"Done transform_and_upload_data for patient_id {patient_id}")
     return output_bucket_and_file_keys_and_table_names
 
 def handler(event: dict, context: dict):
@@ -112,7 +110,7 @@ def handler(event: dict, context: dict):
         raise ValueError("SNOWFLAKE_CREDS is not set")
     snowflake_creds = json.loads(snowflake_creds) if isinstance(snowflake_creds, str) else snowflake_creds
 
-    logging.info(f">>> Parsing data and uploading it to S3 for Snowflake - {cx_id}, patient_id {patient_id}, job_id {job_id}")
+    print(f">>> Parsing data and uploading it to S3 for Snowflake - {cx_id}, patient_id {patient_id}, job_id {job_id}")
     output_bucket_and_file_keys_and_table_names = transform_and_upload_data(
         input_bucket,
         output_bucket,
@@ -123,12 +121,12 @@ def handler(event: dict, context: dict):
     )
 
     if len(output_bucket_and_file_keys_and_table_names) < 1:
-        logging.info("No files were uploaded")
+        print("No files were uploaded")
         exit(0)
 
     table_defs = generate_table_names_and_create_table_statements(patient_id, job_id)
     
-    logging.info(f">>> Uploading data into Snowflake - {cx_id}, patient_id {patient_id}, job_id {job_id}")
+    print(f">>> Uploading data into Snowflake - {cx_id}, patient_id {patient_id}, job_id {job_id}")
     with snowflake.connector.connect(**get_snowflake_credentials(snowflake_creds)) as snowflake_conn:
         database_name = format_database_name(cx_id)
         snowflake_conn.cursor().execute(f"CREATE DATABASE IF NOT EXISTS {database_name}")
@@ -148,7 +146,7 @@ def handler(event: dict, context: dict):
         copy_into_resource_table(snowflake_conn, cx_id, patient_id, job_id, table_defs, rebuild_patient)
 
         set_patient_status(snowflake_conn, cx_id, patient_id, "completed")
-    logging.info(f">>> Done processing {cx_id}, patient_id {patient_id}, job_id {job_id}")
+    print(f">>> Done processing {cx_id}, patient_id {patient_id}, job_id {job_id}")
 
 if __name__ == "__main__":
     handler({}, {})
