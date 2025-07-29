a0dcefc45259dfec64c81200c7ceb284bc8669be
RadmirGari <radmirgaripovrss@gmail.com>
2025-07-29 11:25:31 -0700

    feat(api): moved endpoint to correct plce
    metriport/metriport-internal#1040
    
    Signed-off-by: RadmirGari <radmirgaripovrss@gmail.com>

--- packages/api/src/routes/internal/index.ts
+++ packages/api/src/routes/internal/index.ts
@@ -346,45 +346,6 @@ router.post(
 );
 
 /**
- * POST /internal/cx-mapping/update-secondary-mappings
- *
- * Update secondary mapping in a cx mapping.
- *
- * @param req.query.cxId - The customer's ID.
- * @param req.query.id - The cx mapping ID.
- * @param req.query.source - the mapping source.
- *
- * @return status 200 with the newly updated CxMapping object.
- */
-router.post(
-  "/cx-mapping/update-secondary-mappings",
-  requestLogger,
-  asyncHandler(async (req: Request, res: Response) => {
-    const cxId = getUUIDFrom("query", req, "cxId").orFail();
-    const id = getFrom("query").orFail("id", req);
-    const source = getFromQueryOrFail("source", req);
-    const secondaryMappingsSchema = secondaryMappingsSchemaMap[source as CxMappingSource];
-    const secondaryMappings = secondaryMappingsSchema
-      ? secondaryMappingsSchema.parse(req.body)
-      : null;
-    if (!secondaryMappings) {
-      throw new BadRequestError(`Invalid secondaryMappings for cx mapping`, undefined, {
-        cxId,
-        id,
-        source,
-        secondaryMappings,
-      });
-    }
-    const newCxMapping = await setSecondaryMappingsOnCxMappingById({
-      cxId,
-      id,
-      secondaryMappings,
-    });
-    return res.status(httpStatus.OK).json(newCxMapping);
-  })
-);
-
-/**
  * GET /internal/cx-mapping
  *
  * Get cx mappings for customer.
@@ -451,6 +412,45 @@ router.delete(
       id,
     });
     return res.sendStatus(httpStatus.NO_CONTENT);
+  })
+);
+
+/**
+ * PUT /internal/cx-mapping/update-secondary-mapping
+ *
+ * Update secondary mapping in a cx mapping.
+ *
+ * @param req.query.cxId - The customer's ID.
+ * @param req.query.id - The cx mapping ID.
+ * @param req.query.source - the mapping source.
+ *
+ * @return status 200 with the newly updated CxMapping object.
+ */
+router.put(
+  "/cx-mapping/:id/update-secondary-mapping",
+  requestLogger,
+  asyncHandler(async (req: Request, res: Response) => {
+    const cxId = getUUIDFrom("query", req, "cxId").orFail();
+    const id = getFrom("query").orFail("id", req);
+    const source = getFromQueryOrFail("source", req);
+    const secondaryMappingsSchema = secondaryMappingsSchemaMap[source as CxMappingSource];
+    const secondaryMappings = secondaryMappingsSchema
+      ? secondaryMappingsSchema.parse(req.body)
+      : undefined;
+    if (!secondaryMappings) {
+      throw new BadRequestError(`Invalid secondaryMappings for cx mapping`, undefined, {
+        cxId,
+        id,
+        source,
+        secondaryMappings,
+      });
+    }
+    const newCxMapping = await setSecondaryMappingsOnCxMappingById({
+      cxId,
+      id,
+      secondaryMappings,
+    });
+    return res.status(httpStatus.OK).json(newCxMapping);
   })
 );
 
