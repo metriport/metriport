b373c4232fcacf21b4c8d839ba22bbd7d27dd0db
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-07-31 16:54:42 -0700

    feat(core): fix naming + tie schema to type
    metriport/metriport-internal#1040
    
    Ref: ENG-258
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/core/src/command/hl7-notification/hl7-notification-webhook-sender.ts
+++ packages/core/src/command/hl7-notification/hl7-notification-webhook-sender.ts
@@ -1,12 +1,16 @@
-export type Hl7Notification = {
-  cxId: string;
-  patientId: string;
-  message: string;
-  messageReceivedTimestamp: string;
-  sourceTimestamp: string;
-  rawDataFileKey: string;
-};
+import { z } from "zod";
+
+export const hl7NotificationSenderParamsSchema = z.object({
+  cxId: z.string().uuid(),
+  patientId: z.string().uuid(),
+  message: z.string(),
+  sourceTimestamp: z.string(),
+  messageReceivedTimestamp: z.string(),
+  rawDataFileKey: z.string(),
+});
+
+export type Hl7NotificationSenderParams = z.infer<typeof hl7NotificationSenderParamsSchema>;
 
 export interface Hl7NotificationWebhookSender {
-  execute(request: Hl7Notification): Promise<void>;
+  execute(request: Hl7NotificationSenderParams): Promise<void>;
 }
