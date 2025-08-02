18f52e10325ce7267a29133ad6a444d18571cf10
RadmirGari <radmirgaripovrss@gmail.com>
2025-08-02 15:22:31 -0700

    feat(utils): added making sure file exists. Moved bucket and duration into env
    refs. metriport/metriport#1440
    
    Signed-off-by: RadmirGari <radmirgaripovrss@gmail.com>

--- packages/utils/src/preview-consolidated.ts
+++ packages/utils/src/preview-consolidated.ts
@@ -1,16 +1,30 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+// keep that ^ on top
 import { getS3UtilsInstance } from "@metriport/core/external/ehr/bundle/bundle-shared";
 import { exec } from "child_process";
+import { getEnvVarOrFail, MetriportError } from "@metriport/shared";
 
 async function main(cxId: string, ptId: string) {
   const S3Utils = getS3UtilsInstance();
   const fileName = `${cxId}/${ptId}/${cxId}_${ptId}_CONSOLIDATED_DATA.json`;
-  const durationSeconds = 1 * 60 * 60; //1 hour
-  const bucketName = "metriport-medical-documents";
+  const durationSeconds = getEnvVarOrFail("DURATION_SECONDS");
+  const bucketName = getEnvVarOrFail("MEDICAL_BUCKET_NAME");
+
+  const existsFile = await S3Utils.fileExists(bucketName, fileName);
+
+  if (!existsFile) {
+    throw new MetriportError(
+      `Could not find file. Make sure your cxId and ptId are correct.`,
+      undefined,
+      { cxId, ptId }
+    );
+  }
 
   const presignedUrl = await S3Utils.getSignedUrl({
     bucketName,
     fileName,
-    durationSeconds,
+    durationSeconds: Number(durationSeconds),
   });
 
   const encoded = encodeURIComponent(presignedUrl);
