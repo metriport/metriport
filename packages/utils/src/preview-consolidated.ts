3bbd9588e96ae3c49ac2cc3469fa4b8602c8ba82
RadmirGari <radmirgaripovrss@gmail.com>
2025-08-02 16:51:48 -0700

    feat(api): added program instead of argv, changed to logging instead of opening for non-mac users
    metriport/metriport#1440
    
    Signed-off-by: RadmirGari <radmirgaripovrss@gmail.com>

--- packages/utils/src/preview-consolidated.ts
+++ packages/utils/src/preview-consolidated.ts
@@ -2,22 +2,24 @@ import * as dotenv from "dotenv";
 dotenv.config();
 // keep that ^ on top
 import { getS3UtilsInstance } from "@metriport/core/external/ehr/bundle/bundle-shared";
-import { exec } from "child_process";
-import { getEnvVarOrFail, MetriportError } from "@metriport/shared";
+import { getEnvVarOrFail, MetriportError } from "../../shared/dist";
+import { Command } from "commander";
 
 async function main(cxId: string, ptId: string) {
   const S3Utils = getS3UtilsInstance();
-  const fileName = `${cxId}/${ptId}/${cxId}_${ptId}_CONSOLIDATED_DATA.json`;
+  const trimmedCxId = cxId.trim();
+  const trimmedPtId = ptId.trim();
+  const fileName = `${trimmedCxId}/${trimmedPtId}/${trimmedCxId}_${trimmedPtId}_CONSOLIDATED_DATA.json`;
   const durationSeconds = getEnvVarOrFail("DURATION_SECONDS");
   const bucketName = getEnvVarOrFail("MEDICAL_BUCKET_NAME");
 
-  const existsFile = await S3Utils.fileExists(bucketName, fileName);
+  const fileExists = await S3Utils.fileExists(bucketName, fileName);
 
-  if (!existsFile) {
+  if (!fileExists) {
     throw new MetriportError(
-      `Could not find file. Make sure your cxId and ptId are correct.`,
+      `File does not exist. Make sure your cxId and ptId are correct.`,
       undefined,
-      { cxId, ptId }
+      { cxId, ptId, bucketName, durationSeconds }
     );
   }
 
@@ -31,13 +33,20 @@ async function main(cxId: string, ptId: string) {
 
   const url = `https://preview.metriport.com?url=${encoded}`;
 
-  exec(`open -a "Google Chrome" "${url}"`);
+  console.log(`Open this url: ${url}`);
 }
 
-const args = process.argv.slice(2);
-const [cxId, ptId] = args;
+const program = new Command();
 
-main(cxId, ptId).catch(error => {
-  console.error("Error: ", error);
-  process.exit(1);
-});
+program
+  .name("preview-consolidated")
+  .option("--cx-id <cxId>", "The customer ID")
+  .option("--pt-id <ptId>", "The patient ID")
+  .description("Previews a patients consolidated bundle")
+  .showHelpAfterError()
+  .version("1.0.0")
+  .action(async function ({ cxId, ptId }) {
+    await main(cxId, ptId);
+  });
+
+export default program;
