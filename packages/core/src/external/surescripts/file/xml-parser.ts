49b46e42dc14a15d7502e10816ba7a6d0c6c416e
Keshav Saharia <keshav@metriport.com>
2025-08-02 17:51:45 -0700

    feat(core): add ncpdp todo marker
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/external/surescripts/file/xml-parser.ts
+++ packages/core/src/external/surescripts/file/xml-parser.ts
@@ -1,5 +1,6 @@
 import { parseStringPromise } from "xml2js";
 
+// TODO: ENG-484 - Parse NCPDP script format from Surescripts
 export function parseXmlResponse(responseFileContent: Buffer): Promise<unknown> {
   const xml = responseFileContent.toString();
   const result = parseStringPromise(xml);
