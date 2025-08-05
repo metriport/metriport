736955b5966cbde7b56d95e241eaee7d80665345
Keshav Saharia <keshav@metriport.com>
2025-08-04 19:52:40 -0700

    revert(core): remove comprehend client
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/util/config.ts
+++ packages/core/src/util/config.ts
@@ -334,11 +334,4 @@ export class Config {
   static getDischargeNotificationSlackUrl(): string {
     return getEnvVarOrFail("DISCHARGE_NOTIFICATION_SLACK_URL");
   }
-
-  static getAWSComprehendRegion(): string {
-    return getEnvVarOrFail("AWS_COMPREHEND_REGION");
-  }
-  static getExtractionBucketName(): string {
-    return getEnvVarOrFail("EXTRACTION_BUCKET_NAME");
-  }
 }
