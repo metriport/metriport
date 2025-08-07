27960ed3db9a94d5e3d788630d6c50da75542482
Keshav Saharia <keshav@metriport.com>
2025-08-06 19:56:30 -0700

    feat(shared): clean up schema and add organization schema to customer response
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/shared/src/domain/customer.ts
+++ packages/shared/src/domain/customer.ts
@@ -1,16 +1,27 @@
 import { z } from "zod";
 
+export const orgDataSchema = z.object({
+  id: z.string(),
+  oid: z.string(),
+  cxId: z.string(),
+  name: z.string(),
+  type: z.string(),
+  businessType: z.string(),
+});
+
+export const facilityDataSchema = z.object({
+  id: z.string(),
+  oid: z.string(),
+  name: z.string(),
+  npi: z.string(),
+});
+
 export const customerDataSchema = z.object({
   cxId: z.string(),
-  facilities: z.array(
-    z.object({
-      id: z.string(),
-      oid: z.string(),
-      name: z.string(),
-      npi: z.string(),
-    })
-  ),
+  org: orgDataSchema,
+  facilities: z.array(facilityDataSchema),
 });
 
 export type CustomerData = z.infer<typeof customerDataSchema>;
-export type FacilityData = CustomerData["facilities"][number];
+export type OrganizationData = z.infer<typeof orgDataSchema>;
+export type FacilityData = z.infer<typeof facilityDataSchema>;
