d431b09813de57a471deb1d926daf08784fa6ad9
Ramil Garipov <ramil@metriport.com>
2025-08-11 16:17:29 -0700

    fix(core): minor fixes
    Part of ENG-836
    
    Signed-off-by: Ramil Garipov <ramil@metriport.com>

--- packages/core/src/domain/document/filename.ts
+++ packages/core/src/domain/document/filename.ts
@@ -1,4 +1,5 @@
 import { getFileExtension } from "../../util/mime";
+import { ConversionType, validConversionTypes } from "../conversion/cda-to-html-pdf";
 import { createFilePath } from "../filename";
 
 export function createDocumentFileName(docId: string, mimeType?: string | undefined): string {
@@ -16,13 +17,11 @@ export function createDocumentFilePath(
   return createFilePath(cxId, patientId, createDocumentFileName(docId, mimeType));
 }
 
-function createDocumentRenderFilePath(filePath: string, renderType: "html" | "pdf"): string {
+function createDocumentRenderFilePath(filePath: string, renderType: ConversionType): string {
   const extension = renderType === "html" ? ".html" : ".pdf";
   return filePath.concat(extension);
 }
 
 export function createDocumentRenderFilePaths(filePath: string): string[] {
-  return ["html", "pdf"].map(renderType =>
-    createDocumentRenderFilePath(filePath, renderType as "html" | "pdf")
-  );
+  return validConversionTypes.map(renderType => createDocumentRenderFilePath(filePath, renderType));
 }
