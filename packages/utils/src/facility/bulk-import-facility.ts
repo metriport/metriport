05dbed1bd8c1ed5a3d7dbc30d7da9209a2edf0e3
RadmirGari <radmirgaripovrss@gmail.com>
2025-08-13 12:20:52 -0700

    fix(utils): moved to local script instead of script from s3
    Part of ENG-542
    
    Signed-off-by: RadmirGari <radmirgaripovrss@gmail.com>

--- packages/utils/src/facility/bulk-import-facility.ts
+++ packages/utils/src/facility/bulk-import-facility.ts
@@ -3,19 +3,21 @@ dotenv.config();
 // keep that ^ on top
 import { Facility } from "@metriport/api-sdk";
 import { FacilityInternalDetails } from "@metriport/core/domain/facility";
-import { getS3UtilsInstance } from "@metriport/core/external/ehr/bundle/bundle-shared";
 import {
   getFacilityByNpiOrFail,
   translateNpiFacilityToMetriportFacility,
 } from "@metriport/core/external/npi-registry/npi-registry";
 import { errorToString, getEnvVarOrFail, MetriportError, sleep } from "@metriport/shared";
+import { buildDayjs } from "@metriport/shared/common/date";
 import axios from "axios";
 import { Command } from "commander";
 import csvParser from "csv-parser";
 import fs from "fs/promises";
+import { createReadStream, constants as FS } from "node:fs";
+import { access } from "node:fs/promises";
+import { Writable } from "node:stream";
+import { pipeline } from "node:stream/promises";
 import path from "path";
-import { Readable } from "stream";
-import { pipeline } from "stream/promises";
 import { z } from "zod";
 
 /*
@@ -25,7 +27,7 @@ import { z } from "zod";
  *
  * CqOboOid and CwOboOid are both optional if type is 'non-obo'
  *
- * It outputs the result of processing in the same S3 folder as well as runs/import-facility/timestamp with the name inputted and _result appended.
+ * It outputs the result of processing into runs/import-facility/timestamp with the name inputted and _result appended.
  * - facility-creates.json: is created under runs/import-facility/timestamp it contains the list of facilities that were sent (would have been sent if dryrun) for creation.
  *
  * Format of the .csv file:
@@ -35,18 +37,15 @@ import { z } from "zod";
  * Either set the env vars below on the OS or create a .env file in the root folder of this package.
  *
  * Execute this with:
- * $ ts-node src/facility/bulk-import-facility --cx-id <cxId> --timestamp <timestamp> --name <name> --dryrun
- * $ ts-node src/facility/bulk-import-facility --cx-id <cxId> --timestamp <timestamp> --name <name>
+ * $ ts-node src/facility/bulk-import-facility --input-path <inputpath> --cx-id <cxId> --dryrun
+ * $ ts-node src/facility/bulk-import-facility --input-path <inputpath> --cx-id <cxId>
  */
-
-const bucket = getEnvVarOrFail("FACILITY_IMPORT_BUCKET");
 
 const internalUrl = getEnvVarOrFail("API_URL");
 
 interface FacilityImportParams {
   cxId: string;
-  name: string;
-  timestamp: string;
+  inputPath: string;
   dryrun?: boolean;
 }
 
@@ -59,18 +58,19 @@ export const InputRowSchema = z.object({
 });
 export type InputRowFacilityImport = z.infer<typeof InputRowSchema>;
 
-const S3Utils = getS3UtilsInstance();
-
 const CSV_HEADER =
   ["npi", "facilityName", "facilityType", "cqOboOid", "cwOboOid", "success", "reason"].join(",") +
   "\n";
 
-async function main({ cxId, name, timestamp, dryrun }: FacilityImportParams) {
+async function main({ cxId, inputPath, dryrun }: FacilityImportParams) {
   await sleep(50); // Give some time to avoid mixing logs w/ Node's
   const isDryRun = Boolean(dryrun);
+  const currentTime = buildDayjs(new Date());
+  const outputTimeStamp = currentTime.format("YYYY-MM-DD");
+  const name = path.basename(inputPath, path.extname(inputPath));
 
   console.log(
-    `############## STARTING AT: ${new Date().toISOString()}  ${
+    `############## STARTING AT: ${currentTime.toISOString()}  ${
       isDryRun ? "[DRY RUN]" : ""
     } ##############`
   );
@@ -80,13 +80,10 @@ async function main({ cxId, name, timestamp, dryrun }: FacilityImportParams) {
     skipLines: 1,
   });
 
-  const logsFolder = `runs/import-facility/${timestamp}`;
+  const logsFolder = `runs/import-facility/${outputTimeStamp}`;
   const resultFileName = `${name}_result${isDryRun ? "_dryrun" : ""}.csv`;
-  const keyFolder = `ops/facility-import/${cxId}/${timestamp}`;
 
-  const key = `${keyFolder}/${name}`;
   const logsFilePath = `${logsFolder}/${resultFileName}`;
-  const resultKey = `${keyFolder}/${resultFileName}`;
   const payloadCreatesFilePath = `${logsFolder}/${name}_facility-creates${
     isDryRun ? "_dryrun" : ""
   }.json`;
@@ -141,14 +138,11 @@ async function main({ cxId, name, timestamp, dryrun }: FacilityImportParams) {
   await new Promise<void>((resolve, reject) => {
     parser.once("end", resolve);
     parser.once("error", reject);
-    readFileFromS3(cxId, name, bucket, key, parser).catch(reject);
+    readFileFromLocal(inputPath, parser).catch(reject);
   });
 
   await Promise.all(rowPromises);
 
-  if (!isDryRun) {
-    await uploadToS3(bucket, resultKey, logsFilePath);
-  }
   await fs.writeFile(payloadCreatesFilePath, JSON.stringify(createdFacilities, null, 2), "utf8");
 
   console.log(
@@ -158,27 +152,18 @@ async function main({ cxId, name, timestamp, dryrun }: FacilityImportParams) {
   );
 }
 
-async function readFileFromS3(
-  cxId: string,
-  name: string,
-  bucket: string,
-  key: string,
-  parser: NodeJS.WritableStream
-): Promise<void> {
-  const fileExists = await S3Utils.fileExists(bucket, `${key}.csv`);
-  if (!fileExists) {
-    throw new MetriportError(
-      "File does not exist. Make sure you inputted the correct cxId and name.",
-      undefined,
-      { cxId, name, bucket, key }
-    );
+export async function readFileFromLocal(inputPath: string, parser: Writable): Promise<void> {
+  const filePath = path.resolve(inputPath);
+
+  try {
+    await access(filePath, FS.R_OK);
+  } catch {
+    throw new MetriportError("File does not exist or is not readable.", undefined, {
+      inputPath: filePath,
+    });
   }
 
-  const fileBuffer = await S3Utils.downloadFile({ bucket, key: `${key}.csv` });
-
-  const readable = Readable.from([fileBuffer]);
-
-  await pipeline(readable, parser);
+  await pipeline(createReadStream(filePath), parser);
 }
 
 async function createFacility(
@@ -193,17 +178,6 @@ async function createFacility(
     },
   });
   return response.data;
-}
-
-async function uploadToS3(bucket: string, key: string, filePath: string): Promise<void> {
-  const fileBuffer = await fs.readFile(filePath);
-
-  await S3Utils.uploadFile({
-    bucket,
-    key,
-    file: fileBuffer,
-    contentType: "text/csv",
-  });
 }
 
 export async function createCsv(filePath: string, csvHeader: string): Promise<void> {
@@ -247,12 +221,8 @@ const program = new Command();
 
 program
   .name("bulk-import-facility")
+  .requiredOption("--input-path <inputpath>", "The path to the input csv file")
   .requiredOption("--cx-id <cxId>", "The customer ID for the facilities to be created under.")
-  .requiredOption(
-    "--timestamp <timestamp>",
-    `The timestamp (YYYY-MM-DD) of when the file was created.`
-  )
-  .requiredOption("--name <name>", `The name of the file. Do not add .csv`)
   .option(
     "--dryrun",
     "Writes to a local JSON file all the facilities it would of tried to create. Does not upload to S3 or add Facilities to the DB"
