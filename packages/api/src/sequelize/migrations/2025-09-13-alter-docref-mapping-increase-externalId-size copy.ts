e10daed69bc08dc70074f56560459f5645fa36ec
RamilGaripov <ramil@metriport.com>
2025-08-13 14:02:48 -0700

    fix(api): added a migration to increase sife of docref mapping external id
    Part of ENG-000
    
    Signed-off-by: RamilGaripov <ramil@metriport.com>

---
+++ packages/api/src/sequelize/migrations/2025-09-13-alter-docref-mapping-increase-externalId-size copy.ts
@@ -0,0 +1,26 @@
+import { DataTypes } from "sequelize";
+import type { Migration } from "..";
+
+const tableName = "docref_mapping";
+const externalIdColumn = "external_id";
+
+// Use 'Promise.all' when changes are independent of each other
+export const up: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.sequelize.transaction(async transaction => {
+    await queryInterface.changeColumn(
+      tableName,
+      externalIdColumn,
+      {
+        type: DataTypes.STRING(319),
+        allowNull: false,
+      },
+      { transaction }
+    );
+  });
+};
+
+export const down: Migration = () => {
+  // No rollback for column size increase due to large data volume
+  // Rolling back would require table rewrite which is risky with 10+ GB of data
+  return Promise.resolve();
+};
