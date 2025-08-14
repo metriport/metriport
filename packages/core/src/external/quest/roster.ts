2c3a3253c1c8fbd8dbe3725a9aed879a1e6b048e
Keshav Saharia <keshav@metriport.com>
2025-08-14 11:02:11 -0700

    refactor(core): move roster types to shared file
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/external/quest/roster.ts
+++ packages/core/src/external/quest/roster.ts
@@ -1,36 +1,6 @@
 import { Config } from "../../util/config";
 import axios from "axios";
-import { z } from "zod";
-
-const questPatientSchema = z.object({
-  id: z.string(),
-  cxId: z.string(),
-  facilityIds: z.array(z.string()),
-  data: z.object({
-    firstName: z.string(),
-    lastName: z.string(),
-    dob: z.string(),
-    genderAtBirth: z.string(),
-    address: z.array(
-      z.object({
-        addressLine1: z.string(),
-        addressLine2: z.string().optional(),
-        city: z.string(),
-        state: z.string(),
-        zip: z.string(),
-        country: z.string().optional(),
-      })
-    ),
-  }),
-});
-
-const questRosterResponseSchema = z.object({
-  patients: z.array(questPatientSchema),
-  meta: z.object({
-    itemsInTotal: z.number(),
-    itemsOnPage: z.number(),
-  }),
-});
+import { questRosterResponseSchema } from "./types";
 
 export async function generateQuestRoster() {
   const internalApi = axios.create({
--- packages/core/src/external/quest/types.ts
+++ packages/core/src/external/quest/types.ts
@@ -1,4 +1,5 @@
 import { SftpConfig } from "../sftp/types";
+import { z } from "zod";
 
 export interface QuestSftpConfig extends Partial<SftpConfig> {
   port?: number;
@@ -9,3 +10,37 @@ export interface QuestSftpConfig extends Partial<SftpConfig> {
   outgoingDirectory?: string;
   incomingDirectory?: string;
 }
+
+export const questPatientSchema = z.object({
+  id: z.string(),
+  cxId: z.string(),
+  facilityIds: z.array(z.string()),
+  data: z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    dob: z.string(),
+    genderAtBirth: z.string(),
+    address: z.array(
+      z.object({
+        addressLine1: z.string(),
+        addressLine2: z.string().optional(),
+        city: z.string(),
+        state: z.string(),
+        zip: z.string(),
+        country: z.string().optional(),
+      })
+    ),
+  }),
+});
+
+export type QuestPatient = z.infer<typeof questPatientSchema>;
+
+export const questRosterResponseSchema = z.object({
+  patients: z.array(questPatientSchema),
+  meta: z.object({
+    itemsInTotal: z.number(),
+    itemsOnPage: z.number(),
+  }),
+});
+
+export type QuestRosterResponse = z.infer<typeof questRosterResponseSchema>;
