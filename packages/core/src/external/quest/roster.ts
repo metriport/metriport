34ec59ca09a3a839724c8f5c71e9dee02d6bf4ca
Keshav Saharia <keshav@metriport.com>
2025-08-14 16:40:06 -0700

    feat(core): added network retries to get request
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/external/quest/roster.ts
+++ packages/core/src/external/quest/roster.ts
@@ -1,18 +1,23 @@
 import axios from "axios";
+import dayjs from "dayjs";
+import { executeWithNetworkRetries } from "@metriport/shared";
 import { Config } from "../../util/config";
 import { Patient } from "@metriport/shared/domain/patient";
 import { questRosterResponseSchema } from "./types";
 import { buildRosterFile } from "./file/file-generator";
-import { out } from "../../util";
+import { out, LogFunction } from "../../util";
 
 const QUEST_ROSTER_ROUTE = "/internal/quest/roster";
+const NUMBER_OF_ATTEMPTS = 3;
+const BASE_DELAY = dayjs.duration({ milliseconds: 100 });
 
 export async function generateQuestRoster(): Promise<Buffer> {
   const { log } = out("QuestRoster");
   let currentUrl: string | undefined = `${Config.getApiUrl()}/${QUEST_ROSTER_ROUTE}`;
+
   const enrolledPatients: Patient[] = [];
   while (currentUrl) {
-    const response = await axios.get(currentUrl);
+    const response = await getWithRetries(currentUrl, log);
     const rosterPage = questRosterResponseSchema.parse(response.data);
     enrolledPatients.push(...rosterPage.patients);
     currentUrl = rosterPage.meta.nextPage;
@@ -20,4 +25,12 @@ export async function generateQuestRoster(): Promise<Buffer> {
   const rosterFile = buildRosterFile(enrolledPatients);
   log(`Generated roster file with ${enrolledPatients.length} patients`);
   return rosterFile;
+}
+
+async function getWithRetries(url: string, log: LogFunction) {
+  return await executeWithNetworkRetries(() => axios.get(url), {
+    maxAttempts: NUMBER_OF_ATTEMPTS,
+    initialDelay: BASE_DELAY.asMilliseconds(),
+    log,
+  });
 }
