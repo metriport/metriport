a7a548bebb6334b12d94985ed43b3550e86b9485
Keshav Saharia <keshav@metriport.com>
2025-08-14 16:57:54 -0700

    feat(shared): add validation for quest external ID
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/shared/src/interface/external/quest/source.ts
+++ packages/shared/src/interface/external/quest/source.ts
@@ -2,4 +2,7 @@ import { z } from "zod";
 
 export const questSource = "quest";
 
-export const questMappingRequestSchema = z.object({ externalId: z.string() });
+export const QUEST_EXTERNAL_ID_REGEX = /^[A-Z0-9]{15}$/;
+export const questMappingRequestSchema = z.object({
+  externalId: z.string().regex(QUEST_EXTERNAL_ID_REGEX),
+});
