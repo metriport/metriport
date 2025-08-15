02a1996653e3c81af56ee630d16089a9529fe51d
Alexey Sergeev <alexey@metriport.com>
2025-08-14 17:37:57 -0700

    feat(api): create endpoint to setup a new cx in canvas
    metriport/metriport-internal#1040
    
    Ref:
    Signed-off-by: Alexey Sergeev <alexey@metriport.com>

--- packages/api/src/routes/internal/ehr/canvas/setup.ts
+++ packages/api/src/routes/internal/ehr/canvas/setup.ts
@@ -1,31 +1,97 @@
 import { Request, Response } from "express";
 import Router from "express-promise-router";
 import httpStatus from "http-status";
+import z from "zod";
 import { updateCustomerBillingToPointToParent } from "../../../../command/internal-server/update-customer";
+import { findOrCreateFacilityMapping } from "../../../../command/mapping/facility";
 import { requestLogger } from "../../../helpers/request-logger";
 import { getUUIDFrom } from "../../../schemas/uuid";
-import { asyncHandler, getFromQueryOrFail } from "../../../util";
+import { asyncHandler, getFromQuery, getFromQueryOrFail } from "../../../util";
+import { findOrCreateCxMapping } from "../../../../command/mapping/cx";
+import { saveJwtToken } from "../../../../external/ehr/shared/utils/jwt-token";
+import {
+  canvasDashSource,
+  canvasWebhookSource,
+} from "@metriport/shared/interface/external/ehr/canvas/jwt-token";
+import ForbiddenError from "../../../../errors/forbidden";
 
 const router = Router();
+
+const setupCanvasSchema = z.object({
+  dashToken: z.object({
+    token: z.string(),
+    exp: z.number(),
+  }),
+  webhookToken: z.object({
+    token: z.string(),
+    exp: z.number(),
+  }),
+});
 
 /**
  * POST /internal/ehr/canvas/setup
  *
- * Updates customer billing to point to parent organization
- * @param req.query.parentName - The parent customer's name
+ * Setup a new cx in Canvas
  * @param req.query.childCxId - The child customer's ID
+ * @param req.query.facilityId - The facility ID
+ * @param req.query.externalId - The external ID
+ * @param req.query.state - The state of the facility
+ * @param req.body - The JWT token data
  * @returns 200 OK
  */
 router.post(
   "/",
   requestLogger,
   asyncHandler(async (req: Request, res: Response) => {
-    const parentName = getFromQueryOrFail("parentName", req);
     const childCxId = getUUIDFrom("query", req, "childCxId").orFail();
+    const facilityId = getUUIDFrom("query", req, "facilityId").orFail();
+    const externalId = getFromQueryOrFail("externalId", req);
+    const state = getFromQuery("state", req);
 
-    await updateCustomerBillingToPointToParent({ parentName, childCxId });
+    const token = setupCanvasSchema.parse(req.body).dashToken;
+    const webhookToken = setupCanvasSchema.parse(req.body).webhookToken;
 
-    // ALEXEY TODO: the rest of the setup here...
+    if (token.token === "" || webhookToken.token === "") throw new ForbiddenError();
+
+    await updateCustomerBillingToPointToParent({ parentName: canvasDashSource, childCxId });
+
+    const externalIdWithState = state ? `${externalId}-${state}` : externalId;
+
+    await findOrCreateFacilityMapping({
+      cxId: childCxId,
+      facilityId: facilityId,
+      externalId: externalIdWithState,
+      source: canvasDashSource,
+    });
+
+    await findOrCreateCxMapping({
+      cxId: childCxId,
+      source: canvasDashSource,
+      externalId: externalId,
+      secondaryMappings: null,
+    });
+
+    await Promise.all([
+      saveJwtToken({
+        token: token.token,
+        source: canvasDashSource,
+        exp: token.exp,
+        data: {
+          source: canvasDashSource,
+          practiceId: externalId,
+        },
+      }),
+      saveJwtToken({
+        token: webhookToken.token,
+        source: canvasWebhookSource,
+        exp: webhookToken.exp,
+        data: {
+          cxId: childCxId,
+          source: canvasWebhookSource,
+          practiceId: externalId,
+        },
+      }),
+    ]);
 
     return res.sendStatus(httpStatus.OK);
   })
