adee579b2b8b8039ba64d4e89a95ecd1f8c3d85d
Keshav Saharia <keshav@metriport.com>
2025-08-18 17:11:02 -0700

    feat(core): set up tool calling for agentic workflow
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

---
+++ packages/core/src/external/bedrock/agent/anthropic/tool.ts
@@ -0,0 +1,58 @@
+import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
+import { AnthropicToolConfig } from "../../model/anthropic/tools";
+
+/**
+ * Represents a tool that can be executed by an Anthropic agent.
+ * @param I - The type of the input to the tool.
+ * @param O - The type of the output from the tool.
+ */
+export class AnthropicTool<I = unknown, O = unknown> {
+  private name: string;
+  private description: string;
+  private inputSchema: z.ZodSchema;
+  private outputSchema: z.ZodSchema;
+  private handler: (input: I) => Promise<O>;
+
+  constructor({
+    name,
+    description,
+    inputSchema,
+    outputSchema,
+    handler,
+  }: Pick<AnthropicToolConfig, "name" | "description"> & {
+    inputSchema: z.ZodSchema<I>;
+    outputSchema?: z.ZodSchema<O>;
+    handler: (input: I) => Promise<O>;
+  }) {
+    this.name = name;
+    this.description = description;
+    this.inputSchema = inputSchema;
+    this.outputSchema = outputSchema ?? z.unknown();
+    this.handler = handler;
+  }
+
+  canExecute(input: I): boolean {
+    return this.inputSchema.safeParse(input).success;
+  }
+
+  async execute(input: I): Promise<O> {
+    const validatedInput = this.inputSchema.parse(input);
+    const result = await this.handler(validatedInput);
+    const validatedResult = this.outputSchema.parse(result);
+    return validatedResult;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getConfig(): AnthropicToolConfig {
+    return {
+      type: "custom",
+      name: this.name,
+      description: this.description,
+      input_schema: zodToJsonSchema(this.inputSchema),
+    };
+  }
+}
