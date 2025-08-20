44e795cf37fd2836b3f7a5b4ec8a061f11e79c91
Keshav Saharia <keshav@metriport.com>
2025-08-20 16:48:11 -0700

    feat(lambdas): implement lambda for updating a roster
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/lambdas/src/quest/upload-roster.ts
+++ packages/lambdas/src/quest/upload-roster.ts
@@ -1,6 +1,7 @@
 import { capture } from "../shared/capture";
+import { prefixedLog } from "../shared/log";
 import { getEnvVarOrFail } from "@metriport/shared/common/env-var";
-import { buildQuestClient } from "./shared";
+import { QuestUploadRosterHandlerDirect } from "@metriport/core/external/quest/command/upload-roster/upload-roster-direct";
 
 capture.init();
 
@@ -9,7 +10,9 @@ const lambdaName = getEnvVarOrFail("AWS_LAMBDA_FUNCTION_NAME");
 
 export const handler = capture.wrapHandler(async () => {
   capture.setExtra({ context: lambdaName });
-  const client = await buildQuestClient();
-  await client.connect();
-  await client.disconnect();
+  const log = prefixedLog("quest.upload-roster");
+  log("Starting upload of Quest roster");
+  const handler = new QuestUploadRosterHandlerDirect();
+  await handler.generateAndUploadLatestQuestRoster();
+  log("Upload of Quest roster completed");
 });
