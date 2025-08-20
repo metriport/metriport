0b1e9f054546c0f80f7f53203312259e104cc906
Keshav Saharia <keshav@metriport.com>
2025-08-20 16:45:05 -0700

    feat(lambdas): set up download response lambda
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/lambdas/src/quest/download-response.ts
+++ packages/lambdas/src/quest/download-response.ts
@@ -1,6 +1,7 @@
 import { capture } from "../shared/capture";
+import { prefixedLog } from "../shared/log";
 import { getEnvVarOrFail } from "@metriport/shared/common/env-var";
-import { buildQuestClient } from "./shared";
+import { DownloadResponseHandlerDirect } from "@metriport/core/external/quest/command/download-response/download-response-direct";
 
 capture.init();
 
@@ -9,7 +10,9 @@ const lambdaName = getEnvVarOrFail("AWS_LAMBDA_FUNCTION_NAME");
 
 export const handler = capture.wrapHandler(async () => {
   capture.setExtra({ context: lambdaName });
-  const client = await buildQuestClient();
-  await client.connect();
-  await client.disconnect();
+  const log = prefixedLog("quest.download-response");
+  log("Starting download of Quest responses");
+  const handler = new DownloadResponseHandlerDirect();
+  await handler.downloadAllQuestResponses();
+  log("Download of all new Quest responses completed");
 });
