e998e9028ad1407916b7bc8b8a75f5030a03fb51
Keshav Saharia <keshav@metriport.com>
2025-08-20 16:47:22 -0700

    feat(lambdas): implement stub action for fhir converter
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/lambdas/src/quest/fhir-converter.ts
+++ packages/lambdas/src/quest/fhir-converter.ts
@@ -1,9 +1,14 @@
 import { capture } from "../shared/capture";
 import { getEnvVarOrFail } from "@metriport/shared/common/env-var";
 import { getSingleMessageOrFail } from "../shared/sqs";
+import { prefixedLog } from "../shared/log";
 import { parseBody } from "../shared/parse-body";
 import { SQSEvent } from "aws-lambda";
-import { z } from "zod";
+import { QuestFhirConverterCommandDirect } from "@metriport/core/external/quest/command/fhir-converter/fhir-converter-direct";
+import {
+  QuestFhirConversionRequest,
+  questFhirConversionRequestSchema,
+} from "@metriport/core/external/quest/types";
 
 capture.init();
 
@@ -11,14 +16,17 @@ capture.init();
 const lambdaName = getEnvVarOrFail("AWS_LAMBDA_FUNCTION_NAME");
 
 export const handler = capture.wrapHandler(async (event: SQSEvent) => {
+  const log = prefixedLog("quest.fhir-converter");
   capture.setExtra({ context: lambdaName });
   const message = getSingleMessageOrFail(event.Records, lambdaName);
   if (!message) return;
 
-  parseBody(questFhirConverterSchema, message.body);
-});
-
-const questFhirConverterSchema = z.object({
-  externalId: z.string(),
-  updateFileName: z.string(),
+  log("Starting conversion of Quest source document to FHIR bundle");
+  const request = parseBody<QuestFhirConversionRequest>(
+    questFhirConversionRequestSchema,
+    message.body
+  );
+  const handler = new QuestFhirConverterCommandDirect();
+  await handler.convertSourceDocumentToFhirBundle(request);
+  log("Conversion of Quest source document to FHIR bundle completed");
 });
