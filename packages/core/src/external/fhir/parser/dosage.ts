c06e5d029afb5933f198418c7306828162f23801
Keshav Saharia <keshav@metriport.com>
2025-08-21 23:34:57 -0700

    feat(core): added parsers for dosage period and day
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

---
+++ packages/core/src/external/fhir/parser/dosage.ts
@@ -0,0 +1,9 @@
+import { DosageDoseAndRate } from "@medplum/fhirtypes";
+// import { NCPDPNames } from "@metriport/shared/interface/external/surescripts/ncpdp";
+
+export function parseDosageAndRateFromString(
+  dosageAndRateString: string
+): DosageDoseAndRate | undefined {
+  console.log("dosageAndRateString", dosageAndRateString);
+  return undefined;
+}
