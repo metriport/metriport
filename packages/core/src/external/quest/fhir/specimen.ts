44c107d7948e310f8a1398cf0bbbc963fab53ba9
Keshav Saharia <keshav@metriport.com>
2025-08-22 18:04:27 -0700

    feat(core): specimen fhir mapping
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

---
+++ packages/core/src/external/quest/fhir/specimen.ts
@@ -0,0 +1,33 @@
+import { uuidv7 } from "@metriport/shared/util/uuid-v7";
+import { Practitioner, Reference, ServiceRequest, Specimen } from "@medplum/fhirtypes";
+import { ResponseDetail } from "../schema/response";
+import { getPractitionerReference } from "./practitioner";
+import { getQuestDataSourceExtension } from "./shared";
+import { getServiceRequestReference } from "./service-request";
+
+export function getSpecimen(
+  detail: ResponseDetail,
+  { practitioner, serviceRequest }: { practitioner: Practitioner; serviceRequest: ServiceRequest }
+): Specimen | undefined {
+  if (!detail.dateCollected) return undefined;
+  const extension = [getQuestDataSourceExtension()];
+  const collector = getPractitionerReference(practitioner);
+  const request = [getServiceRequestReference(serviceRequest)];
+
+  return {
+    resourceType: "Specimen",
+    id: uuidv7(),
+    status: "available",
+    collection: {
+      collector,
+    },
+    request,
+    extension,
+  };
+}
+
+export function getSpecimenReference(specimen: Specimen): Reference<Specimen> {
+  return {
+    reference: `Specimen/${specimen.id}`,
+  };
+}
