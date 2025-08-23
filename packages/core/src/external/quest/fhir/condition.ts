bfef25ef142355f8ab162b480c217705fe2260dc
Keshav Saharia <keshav@metriport.com>
2025-08-22 18:06:13 -0700

    feat(core): condition fhir mapping
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

---
+++ packages/core/src/external/quest/fhir/condition.ts
@@ -0,0 +1,62 @@
+import { uuidv7 } from "@metriport/shared/util/uuid-v7";
+import { Condition, Patient } from "@medplum/fhirtypes";
+import { ResponseDetail } from "../schema/response";
+import { ICD_10_URL, ICD_9_URL } from "@metriport/shared/medical";
+import { getPatientReference } from "./patient";
+import { getQuestDataSourceExtension } from "./shared";
+
+export function getConditions(
+  detail: ResponseDetail,
+  { patient }: { patient: Patient }
+): Condition[] {
+  const conditions = [
+    getCondition(patient, detail.diagnosisCode1),
+    getCondition(patient, detail.diagnosisCode2),
+    getCondition(patient, detail.diagnosisCode3),
+    getCondition(patient, detail.diagnosisCode4),
+    getCondition(patient, detail.diagnosisCode5),
+    getCondition(patient, detail.diagnosisCode6),
+    getCondition(patient, detail.diagnosisCode7),
+    getCondition(patient, detail.diagnosisCode8),
+    getCondition(patient, detail.diagnosisCode9),
+    getCondition(patient, detail.diagnosisCode10),
+  ].filter(Boolean) as Condition[];
+
+  return conditions;
+}
+
+function getCondition(patient: Patient, diagnosisCode?: string): Condition | undefined {
+  if (!diagnosisCode) return undefined;
+  const { system, code } = parseDiagnosisCode(diagnosisCode);
+  const extension = [getQuestDataSourceExtension()];
+
+  return {
+    resourceType: "Condition",
+    id: uuidv7(),
+    subject: getPatientReference(patient),
+    code: {
+      coding: [
+        {
+          system,
+          code,
+        },
+      ],
+    },
+    extension,
+  };
+}
+
+function parseDiagnosisCode(diagnosisCode: string): { system: string; code: string } {
+  if (diagnosisCode.startsWith("10^")) {
+    return { system: ICD_10_URL, code: insertPeriod(diagnosisCode.substring(3)) };
+  } else if (diagnosisCode.startsWith("09^")) {
+    return { system: ICD_9_URL, code: insertPeriod(diagnosisCode.substring(3)) };
+  } else if (diagnosisCode.startsWith("9^")) {
+    return { system: ICD_9_URL, code: insertPeriod(diagnosisCode.substring(2)) };
+  }
+  return { system: ICD_10_URL, code: diagnosisCode };
+}
+
+function insertPeriod(icd10Code: string): string {
+  return icd10Code.substring(0, 3) + "." + icd10Code.substring(3);
+}
