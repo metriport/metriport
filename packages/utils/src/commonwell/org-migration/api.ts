46a5665926effb254498a15f54465ea7f0960e3b
RamilGaripov <ramil@metriport.com>
2025-08-25 10:24:19 -0700

    fix(utils): tiny fix on import
    Part of ENG-641
    
    Signed-off-by: RamilGaripov <ramil@metriport.com>

--- packages/utils/src/commonwell/org-migration/api.ts
+++ packages/utils/src/commonwell/org-migration/api.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 dotenv.config({ path: ".env._cw_org_migration" });
 // keep that ^ on top
-import { APIMode, CommonWellMember } from "@metriport/commonwell-sdk-v1";
+import { APIMode, CommonWellMember } from "@metriport/commonwell-sdk";
 import { getEnvVarOrFail } from "@metriport/shared/common/env-var";
 
 export function makeCommonWellMemberAPI(mode: APIMode) {
