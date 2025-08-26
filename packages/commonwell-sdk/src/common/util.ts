aa000e5a413c7854a267e58bdb31bacdb1d69bf0
RamilGaripov <ramil@metriport.com>
2025-08-25 20:22:45 -0700

    feat(commonwell-sdk): updating sdk utils
    Part of ENG-513
    
    Signed-off-by: RamilGaripov <ramil@metriport.com>

--- packages/commonwell-sdk/src/common/util.ts
+++ packages/commonwell-sdk/src/common/util.ts
@@ -1,7 +1,11 @@
 import { PurposeOfUse } from "@metriport/shared";
 import { BaseRequestMetadata } from "../client/common";
-import { StrongId } from "../models/identifier";
-import { Patient, PatientCollectionItem } from "../models/patient";
+import { PatientResponseItem } from "../models/patient";
+
+export type EncodePatientIdForDocumentExchangeParams = {
+  patientId: string;
+  assignAuthority: string;
+};
 
 /**
  * Regex pattern to extract code, system, and optional assignAuthType from CommonWell patient ID
@@ -10,7 +14,16 @@ import { Patient, PatientCollectionItem } from "../models/patient";
  */
 export const CW_PATIENT_ID_REGEX = /^(.+)\^\^\^&([^&]+)(?:&(.+))?$/i;
 
-export function getPatientIdTrailingSlash(object: PatientCollectionItem): string | undefined {
+/**
+ * Get the local Patient ID from the Patient Collection Item's Self link.
+ * This ID is in the HL7 CX data type format, so it includes the system/assigninig authority's system.
+ *
+ * @param object - The Patient Collection Item.
+ * @returns The Patient ID.
+ */
+export function getCwPatientIdFromCollectionItem(
+  object: Pick<PatientResponseItem, "Links">
+): string | undefined {
   const url = object.Links?.Self;
   if (!url) return undefined;
   const isLastCharSlash = url.endsWith("/");
@@ -18,29 +31,31 @@ export function getPatientIdTrailingSlash(object: PatientCollectionItem): string
   return removeTrailingSlash.substring(removeTrailingSlash.lastIndexOf("/") + 1);
 }
 
-export function getPatientStrongIds(object: Patient): StrongId[] | undefined {
-  return object.identifier ?? undefined;
+/**
+ * Get the Edge System's patient ID from the Patient Collection Item.
+ * This is the decoded patient ID, NOT in the HL7 CX data type format.
+ *
+ * @param object - The Patient Collection Item.
+ * @returns The Edge System's patient ID.
+ * @see decodeCwPatientId
+ * @see Section "8.3.2 Get Patient" of the spec.
+ */
+export function getPatientIdFromCollectionItem(object: Pick<PatientResponseItem, "Links">): string {
+  const cwPatientId = getCwPatientIdFromCollectionItem(object);
+  if (!cwPatientId) throw new Error(`Could not get CW patient ID from collection item`);
+  const decoded = decodeCwPatientId(cwPatientId);
+  const patientId = decoded.value;
+  if (!patientId) throw new Error(`PatientId could not be decoded from ${cwPatientId}`);
+  return patientId;
 }
 
 /**
- * Converts the patient ID into subject ID, to be used during document query.
+ * Decodes the patient ID in the HL7 CX data type format into its components.
  *
- * @param {string} patientId - The patient's ID
- * @returns {string} - The subject ID as defined by the specification: [system]|[code] where 'system'
- * is the OID of the organization and 'code' is the first (numeric) part of the patient ID.
- *
- * @see {@link https://specification.commonwellalliance.org/services/data-broker/protocol-operations-data-broker#8781-find-documents|API spec}
+ * @param patientId - The patient's ID in the HL7 CX data type format.
+ * @returns The decoded patient ID, with the value, assignAuthority, and assignAuthorityType.
+ * @see Section "8.3.2 Get Patient" of the spec.
  */
-export function convertPatientIdToSubjectId(patientId: string): string | undefined {
-  const { value, assignAuthority } = decodeCwPatientId(patientId);
-  return value && assignAuthority
-    ? encodeToDocumentExchange({
-        patientId: value,
-        assignAuthority,
-      })
-    : undefined;
-}
-
 export function decodeCwPatientId(patientId: string): {
   value: string | undefined;
   assignAuthority: string | undefined;
@@ -54,7 +69,16 @@ export function decodeCwPatientId(patientId: string): {
   return { value, assignAuthority, assignAuthorityType };
 }
 
-export function encodeToCwPatientId({
+/**
+ * Encodes the patient ID into the HL7 CX data type format.
+ *
+ * @param patientId - The patient's ID.
+ * @param assignAuthority - The assign authority of the patient.
+ * @param assignAuthorityType - The assign authority type of the patient.
+ * @returns The patient ID in the HL7 CX data type format.
+ * @see Section "8.3.2 Get Patient" of the spec.
+ */
+export function encodeCwPatientId({
   patientId,
   assignAuthority,
   assignAuthorityType,
@@ -69,23 +93,41 @@ export function encodeToCwPatientId({
 /**
  * Converts the patient ID into subject ID, to be used during document query.
  *
- * @param patientId - The patient's ID.
+ * @param cwPatientId - The patient's ID in the HL7 CX data type format.
+ * @returns The subject ID as defined by the specification: [system]|[code] where 'system'
+ * is the OID of the organization and 'code' is the first part of the patient ID.
+ * @see Sectin "10.2.1 Document Query" of the spec.
+ */
+export function encodePatientIdForDocumentExchange(cwPatientId: string): string | undefined;
+
+/**
+ * Converts the patient ID into subject ID, to be used during document query.
+ *
+ * @param patientId - The patient's ID from the Edge System, unencoded.
  * @param assignAuthority - The assign authority of the patient.
  * @returns The subject ID as defined by the specification: [system]|[code] where 'system'
  * is the OID of the organization and 'code' is the first part of the patient ID.
+ * @see Sectin "10.2.1 Document Query" of the spec.
  */
-export function encodeToDocumentExchange({
+export function encodePatientIdForDocumentExchange({
   patientId,
   assignAuthority,
-}: {
-  patientId: string;
-  assignAuthority: string;
-}): string {
-  return `${assignAuthority}|${patientId}`;
-}
+}: EncodePatientIdForDocumentExchangeParams): string;
 
-export function encodeId(id: string): string {
-  return encodeURIComponent(id);
+export function encodePatientIdForDocumentExchange(
+  params: EncodePatientIdForDocumentExchangeParams | string
+): string | undefined {
+  if (typeof params === "string") {
+    const { value, assignAuthority } = decodeCwPatientId(params);
+    return value && assignAuthority
+      ? encodePatientIdForDocumentExchange({
+          patientId: value,
+          assignAuthority,
+        })
+      : undefined;
+  }
+  const { patientId, assignAuthority } = params;
+  return `${assignAuthority}|${patientId}`;
 }
 
 export function buildBaseQueryMeta(orgName: string): BaseRequestMetadata {
