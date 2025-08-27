f9cfe93a916c5a032a7a9c9311321950f7b90709
Keshav Saharia <keshav@metriport.com>
2025-08-27 13:18:12 -0700

    feat(core): add metriport error
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/external/census-geocoder/census-geocoder.ts
+++ packages/core/src/external/census-geocoder/census-geocoder.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { Address } from "../../domain/address";
-import { normalizeState } from "@metriport/shared";
+import { MetriportError, normalizeState } from "@metriport/shared";
 import {
   CensusGeocoderParams,
   censusGeocoderResponseSchema,
   CensusGeocoderResponse,
   AddressMatch,
 } from "./types";
-import { CENSUS_GEOCODER_ADDRESS_URL } from "./constants";
+import { CENSUS_GEOCODER_HEADERS, CENSUS_GEOCODER_ADDRESS_URL } from "./constants";
 
 export async function geocodeAddress(
   address: Address,
@@ -22,11 +22,18 @@ export async function geocodeAddress(
     format: "json",
   });
   const response = await axios.get<CensusGeocoderResponse>(CENSUS_GEOCODER_ADDRESS_URL, {
-    headers: { "Content-Type": "application/json", "User-Agent": "Metriport/1.0" },
+    headers: CENSUS_GEOCODER_HEADERS,
     params,
   });
-  const { result } = censusGeocoderResponseSchema.parse(response.data);
-  return result.addressMatches;
+  try {
+    const { result } = censusGeocoderResponseSchema.parse(response.data);
+    return result.addressMatches;
+  } catch (error) {
+    throw new MetriportError("Invalid response from US Census Geocoder", error, {
+      address: JSON.stringify(address),
+      response: JSON.stringify(response.data),
+    });
+  }
 }
 
 function getStreetFromAddress(address: Address): string {
--- packages/core/src/external/census-geocoder/constants.ts
+++ packages/core/src/external/census-geocoder/constants.ts
@@ -4,3 +4,9 @@ export const CENSUS_GEOCODER_ONE_LINE_ADDRESS_URL =
   "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress";
 export const CENSUS_GEOCODER_BATCH_URL =
   "https://geocoding.geo.census.gov/geocoder/locations/addressbatch";
+
+export const CENSUS_GEOCODER_USER_AGENT = "Metriport/1.0";
+export const CENSUS_GEOCODER_HEADERS: Record<string, string> = {
+  "Content-Type": "application/json",
+  "User-Agent": CENSUS_GEOCODER_USER_AGENT,
+};
