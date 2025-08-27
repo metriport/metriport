835c241accfb93a1052e5ace011440af76286817
Thomas Yopes <thomasyopes@Mac.attlocal.net>
2025-08-27 12:38:26 -0700

    feat(dedup): fixing tests
    Ref: ENG-000
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Mac.attlocal.net>

--- packages/core/src/fhir-to-cda/cda-templates/components/__tests__/make-diagnostic-report.ts
+++ packages/core/src/fhir-to-cda/cda-templates/components/__tests__/make-diagnostic-report.ts
@@ -1,5 +1,12 @@
 import { faker } from "@faker-js/faker";
-import { CodeableConcept, Coding, DiagnosticReport } from "@medplum/fhirtypes";
+import {
+  Attachment,
+  CodeableConcept,
+  Coding,
+  DiagnosticReport,
+  Observation,
+  Reference,
+} from "@medplum/fhirtypes";
 import { makeSubjectReference } from "./shared";
 
 // TODO ENG-835: Put these in a more general place, so it's available for testing other flows as well.
@@ -24,18 +31,39 @@ export function makeDiagnosticReport(
   };
 }
 
-export const presentedFormExample = [
-  {
-    contentType: "text/html",
-    data: "some b-64 encoded data",
-  },
-];
+export function makePresentedFormExample(): Attachment[] {
+  return [
+    {
+      contentType: "text/html",
+      data: "some b-64 encoded data",
+    },
+  ];
+}
 
-export const resultExample = [
-  {
-    reference: "Observation/some-obs-ID",
-  },
-];
+export function makePresentedFormExample2(): Attachment[] {
+  return [
+    {
+      contentType: "text/html",
+      data: "some other b-64 encoded data",
+    },
+  ];
+}
+
+export function makeResultExample(): Reference<Observation>[] {
+  return [
+    {
+      reference: "Observation/some-obs-ID",
+    },
+  ];
+}
+
+export function makeResultExmplae2(): Reference<Observation>[] {
+  return [
+    {
+      reference: "Observation/some-obs-ID-2",
+    },
+  ];
+}
 
 export function makeDiagnosticReportCategory(
   coding: Coding = {
@@ -49,35 +77,41 @@ export function makeDiagnosticReportCategory(
   };
 }
 
-export const a1cPanelConceptLoinc = {
-  text: "HEMOGLOBIN A1C/HEMOGLOBIN.TOTAL IN BLOOD",
-  coding: [
-    {
-      code: "4548-4",
-      display: "HEMOGLOBIN A1C/HEMOGLOBIN.TOTAL IN BLOOD",
-      system: "http://loinc.org",
-    },
-  ],
-};
+export function makeA1cConcept(): CodeableConcept {
+  return {
+    text: "HEMOGLOBIN A1C/HEMOGLOBIN.TOTAL IN BLOOD",
+    coding: [
+      {
+        code: "4548-4",
+        display: "HEMOGLOBIN A1C/HEMOGLOBIN.TOTAL IN BLOOD",
+        system: "http://loinc.org",
+      },
+    ],
+  };
+}
 
-export const metabolicPanelConceptLoinc = {
-  text: "Comprehensive metabolic 2000 panel - Serum or Plasma",
-  coding: [
-    {
-      code: "24323-8",
-      display: "Comprehensive metabolic 2000 panel - Serum or Plasma",
-      system: "http://loinc.org",
-    },
-  ],
-};
+export function makeMetabolicPanelConceptLoinc(): CodeableConcept {
+  return {
+    text: "Comprehensive metabolic 2000 panel - Serum or Plasma",
+    coding: [
+      {
+        code: "24323-8",
+        display: "Comprehensive metabolic 2000 panel - Serum or Plasma",
+        system: "http://loinc.org",
+      },
+    ],
+  };
+}
 
-export const metabolicPanelConceptOther = {
-  text: "Comprehensive metabolic 2000 panel - Serum or Plasma",
-  coding: [
-    {
-      code: "1234",
-      display: "Comprehensive metabolic 2000 panel - Serum or Plasma",
-      system: "http://terminology.hl7.org/ValueSet/v3-Unknown",
-    },
-  ],
-};
+export function makeMetabolicPanelConceptOther(): CodeableConcept {
+  return {
+    text: "Comprehensive metabolic 2000 panel - Serum or Plasma",
+    coding: [
+      {
+        code: "1234",
+        display: "Comprehensive metabolic 2000 panel - Serum or Plasma",
+        system: "http://terminology.hl7.org/ValueSet/v3-Unknown",
+      },
+    ],
+  };
+}
