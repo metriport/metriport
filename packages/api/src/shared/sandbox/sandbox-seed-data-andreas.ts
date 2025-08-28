b9329ebc766df1674f063e20dfe9b1c4a1df2b3b
RadmirGari <radmirgaripovrss@gmail.com>
2025-08-28 14:21:37 -0700

    feat(sandbox): added better data for each sandbox patient
    Part of ENG-957
    
    Signed-off-by: RadmirGari <radmirgaripovrss@gmail.com>

--- packages/api/src/shared/sandbox/sandbox-seed-data-andreas.ts
+++ packages/api/src/shared/sandbox/sandbox-seed-data-andreas.ts
@@ -4,7 +4,7 @@ export const andreasDocRefs: DataEntry[] = [
   {
     s3Info: {
       bucket,
-      key: "demo.xml",
+      key: "AndreasSmith.xml",
     },
     docRef: {
       resourceType: "DocumentReference",
@@ -12,7 +12,7 @@ export const andreasDocRefs: DataEntry[] = [
       content: [
         {
           attachment: {
-            title: "demo.xml",
+            title: "AndreasSmith.xml",
             url: "http://api.metriport.com",
             contentType: "application/xml",
             creation: "2023-06-16",
