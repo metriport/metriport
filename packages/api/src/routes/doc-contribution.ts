8bd29a492e25d0094fd43ffc97be6bbaa4174b48
RamilGaripov <ramil@metriport.com>
2025-09-01 09:53:05 -0700

    fix(api): create local logic for cw doc contribution tests
    Part of ENG-513
    
    Signed-off-by: RamilGaripov <ramil@metriport.com>

---
+++ packages/api/src/routes/doc-contribution.ts
@@ -0,0 +1,62 @@
+import { S3Utils } from "@metriport/core/external/aws/s3";
+import { BadRequestError, errorToString } from "@metriport/shared";
+import { Request, Response } from "express";
+import Router from "express-promise-router";
+import { Config } from "../shared/config";
+import { asyncHandler, getFrom } from "./util";
+
+const router = Router();
+
+/** ---------------------------------------------------------------------------------------
+ * GET /doc-contribution/commonwell
+ *
+ * This is a local development route only.
+ *
+ * Used to test the document retrieval part of the contribution logic for CommonWell.
+ *
+ * NOTE: For CW to send a request to your local env, you need to rig the flow in 2 places:
+ *   1. In `cw-process-request`, the resulting bundle should have the localhost address be replaced with your ngrok address.
+ *   2. In the CW portal, update your organization to use the <your-ngrok-address/oauth/fhir> under Gateway > FHIR R4 Endpoint.
+ *
+ * @param req.query.fileName The file name to get the signed URL for. The value for this comes from the
+ *  `oauth/fhir` route that responds to incoming document queries.
+ * @returns A redirect to the signed URL for the file.
+ */
+router.get(
+  "/commonwell",
+  asyncHandler(async (req: Request, res: Response) => {
+    {
+      if (!Config.isDev()) {
+        throw new BadRequestError("This route is only available in dev environment");
+      }
+
+      try {
+        const s3Utils = new S3Utils("us-east-2");
+
+        const fileName = getFrom("query").orFail("fileName", req);
+        if (fileName.trim().length <= 0) {
+          return res.status(400).send("Missing fileName query parameter");
+        }
+        console.log(`File name: ${fileName}`);
+
+        const key = fileName.startsWith("/") ? fileName.slice(1) : fileName;
+        if (!key || key.trim().length <= 0) {
+          return res.status(400).send("Invalid fileName query parameter");
+        }
+
+        const url = await s3Utils.getSignedUrl({
+          bucketName: Config.getMedicalDocumentsBucketName(),
+          fileName: key,
+          durationSeconds: 60,
+        });
+        return res.status(301).redirect(url);
+      } catch (error) {
+        const msg = `Error processing DR from CW`;
+        console.log(`${msg}: ${errorToString(error)}`);
+        return res.status(500).send("Internal Server Error");
+      }
+    }
+  })
+);
+
+export default router;
