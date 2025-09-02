2dceb9599e92cff8c5876ada5f2acba496d4bc61
Keshav Saharia <keshav@metriport.com>
2025-09-01 17:00:58 -0700

    feat(core): add resource identifier comparator
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

---
+++ packages/core/src/fhir-deduplication/comparators.ts
@@ -0,0 +1,25 @@
+import { Identifier, Resource } from "@medplum/fhirtypes";
+
+export function sameResourceId(resourceA: Resource, resourceB: Resource) {
+  const idOfResourceA = resourceA.id;
+  const idOfResourceB = resourceB.id;
+  if (idOfResourceA === undefined || idOfResourceB === undefined) return false;
+  return idOfResourceA === idOfResourceB;
+}
+
+export function sameResourceIdentifiers(
+  resourceA: Resource & { identifier?: Identifier[] },
+  resourceB: Resource & { identifier?: Identifier[] }
+) {
+  const identifiersOfResourceA = resourceA.identifier;
+  const identifiersOfResourceB = resourceB.identifier;
+  if (identifiersOfResourceA === undefined || identifiersOfResourceB === undefined) return false;
+  return identifiersOfResourceA.some(identifierOfResourceA => {
+    return identifiersOfResourceB.some(identifierOfResourceB => {
+      return (
+        identifierOfResourceA.system === identifierOfResourceB.system &&
+        identifierOfResourceA.value === identifierOfResourceB.value
+      );
+    });
+  });
+}
--- packages/core/src/fhir-deduplication/resources/service-request.ts
+++ packages/core/src/fhir-deduplication/resources/service-request.ts
@@ -1,4 +1,6 @@
 import { ServiceRequest } from "@medplum/fhirtypes";
+import { DisjointSetUnion } from "../disjoint-set-union";
+import { sameResourceId } from "../comparators";
 
 export function groupSameServiceRequests(serviceRequests: ServiceRequest[]): {
   serviceRequestsMap: Map<string, ServiceRequest>;
@@ -8,6 +10,9 @@ export function groupSameServiceRequests(serviceRequests: ServiceRequest[]): {
   const serviceRequestsMap = new Map<string, ServiceRequest>();
   const refReplacementMap = new Map<string, string>();
   const danglingReferences = new Set<string>();
+
+  const disjointSets = new DisjointSetUnion(serviceRequests);
+  disjointSets.deduplicate([sameResourceId]);
 
   console.log("serviceRequests", serviceRequests);
 
