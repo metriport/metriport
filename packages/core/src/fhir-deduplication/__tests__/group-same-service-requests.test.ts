1ae2787881cb5ad8c841f9af1f346628db3e9d94
Keshav Saharia <keshav@metriport.com>
2025-09-01 18:39:26 -0700

    feat(core): add initial test cases for service requests
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/fhir-deduplication/__tests__/group-same-service-requests.test.ts
+++ packages/core/src/fhir-deduplication/__tests__/group-same-service-requests.test.ts
@@ -49,4 +49,30 @@ describe("groupSameServiceRequests", () => {
     const { serviceRequestsMap } = groupSameServiceRequests([serviceRequest1, serviceRequest2]);
     expect(serviceRequestsMap.size).toBe(1);
   });
+
+  it("correctly groups serviceRequests based on requisition identifiers", () => {
+    const serviceRequest1 = makeServiceRequest({
+      id: faker.string.uuid(),
+      status: "unknown",
+      identifier: [{ system: "http://example.com", value: "1234567890" }],
+    });
+    const serviceRequest2 = makeServiceRequest({
+      id: faker.string.uuid(),
+      status: "unknown",
+      identifier: [{ system: "http://example.com", value: "1234567890" }],
+    });
+
+    const serviceRequest3 = makeServiceRequest({
+      id: faker.string.uuid(),
+      status: "unknown",
+      identifier: [{ system: "http://example.com", value: "8239482342" }],
+    });
+
+    const { serviceRequestsMap } = groupSameServiceRequests([
+      serviceRequest1,
+      serviceRequest2,
+      serviceRequest3,
+    ]);
+    expect(serviceRequestsMap.size).toBe(2);
+  });
 });
