9dff71550d3e48d2976f8297bfeba0a3cb60058b
Keshav Saharia <keshav@metriport.com>
2025-09-06 02:33:27 -0700

    feat(core): add identifier strategy
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/fhir-deduplication/merge/__tests__/merge.test.ts
+++ packages/core/src/fhir-deduplication/merge/__tests__/merge.test.ts
@@ -2,14 +2,14 @@ import { Coding, Medication } from "@medplum/fhirtypes";
 import { buildMergeFunction } from "../build-merge";
 import { mergeCodeableConcepts } from "../strategy/codeable-concept";
 
-const acetaminophenCode: Coding = {
-  system: "http://www.nlm.nih.gov/research/umls/rxnorm",
-  code: "198440",
-  display: "Acetaminophen",
-};
-
 describe("Merge test", () => {
   it("should merge two medications by order of status", () => {
+    const acetaminophenCode: Coding = {
+      system: "http://www.nlm.nih.gov/research/umls/rxnorm",
+      code: "198440",
+      display: "Acetaminophen",
+    };
+
     const testMedication1: Medication = {
       resourceType: "Medication",
       id: "1",
@@ -30,7 +30,7 @@ describe("Merge test", () => {
       },
     };
 
-    const mergeMedications = buildMergeFunction({
+    const mergeMedications = buildMergeFunction<Medication>({
       statusPrecedence: ["entered-in-error", "inactive", "active"],
       mergeStrategy: {
         code: mergeCodeableConcepts,
@@ -41,4 +41,6 @@ describe("Merge test", () => {
     const mergedInDescendingOrder = mergeMedications([testMedication2, testMedication1]);
     expect(mergedInAscendingOrder).toEqual(mergedInDescendingOrder);
   });
+
+  // it("should merge multiple medication dispenses that are exact duplicates", () => {});
 });
--- packages/core/src/fhir-deduplication/merge/strategy.ts
+++ packages/core/src/fhir-deduplication/merge/strategy.ts
@@ -1,4 +1,16 @@
-import { Identifier } from "@medplum/fhirtypes";
+export function chooseMasterOnly<T>(master: T | undefined): T | undefined {
+  return master;
+}
+
+export function chooseMasterOrHighestPrecedence<T>(
+  master: T | undefined,
+  values: T[]
+): T | undefined {
+  if (master !== undefined) return master;
+  const lastValue = values[values.length - 1];
+  if (lastValue !== undefined) return lastValue;
+  return undefined;
+}
 
 export function mergeArrays<T>(masterArray: T[] | undefined, values: T[][]): T[] | undefined {
   const mergedArray = masterArray ? [...masterArray] : [];
@@ -6,43 +18,4 @@ export function mergeArrays<T>(masterArray: T[] | undefined, values: T[][]): T[]
     mergedArray.push(...value);
   }
   return mergedArray;
-}
-
-export function mergeIdentifiers(
-  masterIdentifier: Identifier | undefined,
-  identifiers: Identifier[]
-): Identifier | undefined {
-  const firstIdentifier = masterIdentifier ? masterIdentifier : identifiers[0];
-  if (!firstIdentifier) return undefined;
-  return firstIdentifier;
-}
-
-export function mergeIdentifierArrays(
-  masterIdentifiers: Identifier[],
-  resourceIdentifiers: Identifier[][]
-): Identifier[] | undefined {
-  const systemIdentifierMap: Record<string, Record<string, Identifier>> = {};
-
-  for (const identifier of masterIdentifiers) {
-    if (!identifier.system || !identifier.value) continue;
-    let systemMap = systemIdentifierMap[identifier.system];
-    if (!systemMap) {
-      systemMap = systemIdentifierMap[identifier.system] = {};
-    }
-    systemMap[identifier.value] = identifier;
-  }
-
-  for (const identifiers of resourceIdentifiers) {
-    for (const identifier of identifiers) {
-      if (!identifier.system || !identifier.value) continue;
-      let systemMap = systemIdentifierMap[identifier.system];
-      if (!systemMap) {
-        systemMap = systemIdentifierMap[identifier.system] = {};
-      }
-      systemMap[identifier.value] = identifier;
-    }
-  }
-  return Object.entries(systemIdentifierMap).flatMap(([, values]) => {
-    return Object.values(values);
-  });
 }
