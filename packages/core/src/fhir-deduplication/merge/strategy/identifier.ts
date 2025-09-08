42a0d9128cde16e8251c5f5051154bbf2576aa46
Keshav Saharia <keshav@metriport.com>
2025-09-08 16:21:45 -0700

    feat(core): update identifier merging
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/fhir-deduplication/merge/strategy/identifier.ts
+++ packages/core/src/fhir-deduplication/merge/strategy/identifier.ts
@@ -1,40 +1,37 @@
 import { Identifier } from "@medplum/fhirtypes";
-import { lastElement } from "./util/array";
 
 type SystemIdentifierMap = Record<string, Record<string, Identifier>>;
 
-export function mergeIdentifiers(
-  masterIdentifier: Identifier | undefined,
-  identifiers: Identifier[]
-): Identifier | undefined {
-  const firstIdentifier = masterIdentifier ? masterIdentifier : lastElement(identifiers);
-  if (!firstIdentifier) return undefined;
-  return firstIdentifier;
-}
-
+/**
+ * Merges arrays of identifiers into a single array of identifiers, and removes duplicate identifiers
+ * with the same (system, value) pair.
+ * @param masterIdentifiers - the identifier array on the master resource
+ * @param resourceIdentifiers - additional identifier arrays on other equal resources
+ * @returns the merged identifier array
+ */
 export function mergeIdentifierArrays(
   masterIdentifiers: Identifier[] | undefined,
   resourceIdentifiers: Identifier[][]
 ): Identifier[] | undefined {
   const systemIdentifierMap: SystemIdentifierMap = {};
-
-  if (masterIdentifiers) {
-    addIdentifiersToSystemIdentifierMap(systemIdentifierMap, masterIdentifiers);
-  }
-
-  resourceIdentifiers.forEach(identifiers => {
+  addIdentifiersToSystemIdentifierMap(systemIdentifierMap, masterIdentifiers);
+  for (const identifiers of resourceIdentifiers) {
     addIdentifiersToSystemIdentifierMap(systemIdentifierMap, identifiers);
-  });
+  }
 
   return Object.values(systemIdentifierMap).flatMap(identifierMap => {
     return Object.values(identifierMap);
   });
 }
 
+/**
+ * Adds Identifier objects to a system identifier map, which is a map of system -> value -> identifier.
+ */
 function addIdentifiersToSystemIdentifierMap(
   systemIdentifierMap: SystemIdentifierMap,
-  identifiers: Identifier[]
+  identifiers?: Identifier[]
 ): void {
+  if (!identifiers) return;
   for (const identifier of identifiers) {
     if (!identifier.system || !identifier.value) continue;
     let systemMap = systemIdentifierMap[identifier.system];
