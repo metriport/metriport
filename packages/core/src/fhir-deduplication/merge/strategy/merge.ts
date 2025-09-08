f98f3e047ca754c46c1e561d5f6a3999cc36461b
Keshav Saharia <keshav@metriport.com>
2025-09-08 16:25:48 -0700

    feat(core): merge narratives
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/fhir-deduplication/merge/strategy/merge.ts
+++ packages/core/src/fhir-deduplication/merge/strategy/merge.ts
@@ -1,3 +1,10 @@
+/**
+ * Merges arrays of strings into a single array of strings, and removes duplicate strings.
+ *
+ * @param masterStringArray - the string array on the master resource
+ * @param stringArrays - additional string arrays on other equal resources
+ * @returns the merged string array
+ */
 export function mergeStringArrays(
   masterStringArray: string[] | undefined,
   stringArrays: string[][]
