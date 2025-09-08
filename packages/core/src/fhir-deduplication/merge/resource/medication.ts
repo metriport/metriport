ab1c1563bed5d1ee2a59f8b750f88dc772d04245
Keshav Saharia <keshav@metriport.com>
2025-09-08 11:55:47 -0700

    feat(core): merge configurations
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/fhir-deduplication/merge/resource/medication.ts
+++ packages/core/src/fhir-deduplication/merge/resource/medication.ts
@@ -3,6 +3,8 @@ import { buildMergeFunction } from "../build-merge";
 import { mergeIdentifierArrays } from "../strategy/identifier";
 import { mergeCodeableConcepts } from "../strategy/codeable-concept";
 import { chooseHighestPrecedence } from "../strategy/choose";
+import { mergeNarratives } from "../strategy/narrative";
+import { concatenateArrays } from "../strategy/concat";
 
 export function buildMedicationMergeFunction() {
   return buildMergeFunction<Medication>({
@@ -10,8 +12,18 @@ export function buildMedicationMergeFunction() {
     mergeStrategy: {
       implicitRules: chooseHighestPrecedence,
       language: chooseHighestPrecedence,
+      text: mergeNarratives,
+      contained: concatenateArrays,
+      extension: concatenateArrays,
+      modifierExtension: concatenateArrays,
       identifier: mergeIdentifierArrays,
       code: mergeCodeableConcepts,
+      status: chooseHighestPrecedence,
+      manufacturer: chooseHighestPrecedence,
+      form: mergeCodeableConcepts,
+      amount: chooseHighestPrecedence,
+      ingredient: concatenateArrays,
+      batch: chooseHighestPrecedence,
     },
   });
 }
--- packages/core/src/fhir-deduplication/merge/resource/specimen.ts
+++ packages/core/src/fhir-deduplication/merge/resource/specimen.ts
@@ -1,32 +1,34 @@
 import { Specimen } from "@medplum/fhirtypes";
-import { MergeConfig } from "../types";
 import { mergeIdentifierArrays } from "../strategy/identifier";
 import { concatenateArrays } from "../strategy/concat";
 import { chooseHighestPrecedence } from "../strategy/choose";
 import { mergeNarratives } from "../strategy/narrative";
 import { mergeCodeableConcepts, mergeCodeableConceptArrays } from "../strategy/codeable-concept";
+import { buildMergeFunction } from "../build-merge";
 
-export const specimenMergeConfig: MergeConfig<Specimen> = {
-  statusPrecedence: ["entered-in-error", "unavailable", "unsatisfactory", "available"],
-  mergeStrategy: {
-    implicitRules: chooseHighestPrecedence,
-    language: chooseHighestPrecedence,
-    text: mergeNarratives,
-    contained: concatenateArrays,
-    extension: concatenateArrays,
-    modifierExtension: concatenateArrays,
-    identifier: mergeIdentifierArrays,
-    accessionIdentifier: chooseHighestPrecedence,
-    status: chooseHighestPrecedence,
-    type: mergeCodeableConcepts,
-    subject: chooseHighestPrecedence,
-    receivedTime: chooseHighestPrecedence,
-    parent: concatenateArrays,
-    request: concatenateArrays,
-    collection: chooseHighestPrecedence,
-    processing: concatenateArrays,
-    container: concatenateArrays,
-    condition: mergeCodeableConceptArrays,
-    note: concatenateArrays,
-  },
-};
+export function buildSpecimenMergeFunction() {
+  return buildMergeFunction<Specimen>({
+    statusPrecedence: ["entered-in-error", "unavailable", "unsatisfactory", "available"],
+    mergeStrategy: {
+      implicitRules: chooseHighestPrecedence,
+      language: chooseHighestPrecedence,
+      text: mergeNarratives,
+      contained: concatenateArrays,
+      extension: concatenateArrays,
+      modifierExtension: concatenateArrays,
+      identifier: mergeIdentifierArrays,
+      accessionIdentifier: chooseHighestPrecedence,
+      status: chooseHighestPrecedence,
+      type: mergeCodeableConcepts,
+      subject: chooseHighestPrecedence,
+      receivedTime: chooseHighestPrecedence,
+      parent: concatenateArrays,
+      request: concatenateArrays,
+      collection: chooseHighestPrecedence,
+      processing: concatenateArrays,
+      container: concatenateArrays,
+      condition: mergeCodeableConceptArrays,
+      note: concatenateArrays,
+    },
+  });
+}
