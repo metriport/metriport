093a3b39effb02da4ccdc3d3085212bb3317b1d9
Keshav Saharia <keshav@metriport.com>
2025-09-08 18:24:16 -0700

    feat(core): remove test builder
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/external/comprehend/__tests__/test-builder.ts
+++
@@ -1,9 +0,0 @@
-import { ComprehendClient } from "../client";
-
-export async function buildRxNormArtifact(name: string, inputText: string) {
-  const client = new ComprehendClient();
-  const response = await client.inferRxNorm(inputText);
-  const entities = response.Entities ?? [];
-  const entity = entities.find(entity => entity.Text === inputText);
-  return entity;
-}
