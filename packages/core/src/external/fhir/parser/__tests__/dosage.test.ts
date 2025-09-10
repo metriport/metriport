91e444686afb0c0e4bdec25fd0c0dbfbbd4efaba
Keshav Saharia <keshav@metriport.com>
2025-09-10 15:22:23 -0700

    feat(core): add dosage testing
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/external/fhir/parser/__tests__/dosage.test.ts
+++ packages/core/src/external/fhir/parser/__tests__/dosage.test.ts
@@ -1,34 +1,43 @@
 import { UNIT_OF_MEASURE_URL } from "@metriport/shared/medical";
 import { parseDosage } from "../dosage";
+import { buildParserExtension } from "../extension";
 
 describe("dosage parsing tests", () => {
   it("should parse standard dosages", () => {
-    const dosage = parseDosage("150 mg");
+    const inputString = "150 mg";
+    const dosage = parseDosage(inputString);
     expect(dosage).toEqual({
       doseQuantity: { value: 150, unit: "mg", system: UNIT_OF_MEASURE_URL, code: "mg" },
+      extension: [buildParserExtension(inputString)],
     });
   });
 
   it("should parse dosages with numeric words", () => {
-    const dosage = parseDosage("eight units");
+    const inputString = "eight units";
+    const dosage = parseDosage(inputString);
     expect(dosage).toEqual({
       doseQuantity: { value: 8, unit: "U", system: UNIT_OF_MEASURE_URL, code: "U" },
+      extension: [buildParserExtension(inputString)],
     });
   });
 
   it("should parse dosage with simple rate ratio", () => {
-    const dosage = parseDosage("10 mg/mL");
+    const inputString = "10 mg/mL";
+    const dosage = parseDosage(inputString);
     expect(dosage).toEqual({
       doseQuantity: { value: 10, unit: "mg", system: UNIT_OF_MEASURE_URL, code: "mg" },
       rateQuantity: { value: 1, unit: "mL", system: UNIT_OF_MEASURE_URL, code: "mL" },
+      extension: [buildParserExtension(inputString)],
     });
   });
 
   it("should parse dosage with numerator and denominator rate ratio", () => {
-    const dosage = parseDosage("12 mmol/3 L");
+    const inputString = "12 mmol/3 L";
+    const dosage = parseDosage(inputString);
     expect(dosage).toEqual({
       doseQuantity: { value: 12, unit: "mmol", system: UNIT_OF_MEASURE_URL, code: "mmol" },
       rateQuantity: { value: 3, unit: "L", system: UNIT_OF_MEASURE_URL, code: "L" },
+      extension: [buildParserExtension(inputString)],
     });
   });
 });
--- packages/core/src/external/fhir/parser/dosage.ts
+++ packages/core/src/external/fhir/parser/dosage.ts
@@ -1,12 +1,14 @@
 import { DosageDoseAndRate } from "@medplum/fhirtypes";
 import { parseQuantity } from "./quantity-unit";
 import { parseRatio } from "./ratio";
+import { buildParserExtension } from "./extension";
 
 export function parseDosage(inputString: string): DosageDoseAndRate | undefined {
   // Parse a dosage that is expressed as a ratio
   const ratio = parseRatio(inputString);
   if (ratio && ratio.numerator && ratio.denominator) {
     return {
+      extension: [buildParserExtension(inputString)],
       doseQuantity: ratio.numerator,
       rateQuantity: ratio.denominator,
     };
@@ -17,6 +19,7 @@ export function parseDosage(inputString: string): DosageDoseAndRate | undefined 
   if (!numerator) return undefined;
 
   return {
+    extension: [buildParserExtension(inputString)],
     doseQuantity: numerator.quantity,
   };
 }
