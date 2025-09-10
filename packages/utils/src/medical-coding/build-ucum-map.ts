5b96ed7d70da5cb41d5caef4e6c71752a9fb66a3
Keshav Saharia <keshav@metriport.com>
2025-09-09 19:03:35 -0700

    feat(core): add ucum map builder
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

---
+++ packages/utils/src/medical-coding/build-ucum-map.ts
@@ -0,0 +1,43 @@
+import { Command } from "commander";
+import { readUcumSource, writeToPackage } from "./shared";
+
+/**
+ * Builds a mapping of UCUM codes to UCUM names by reading from the UCUM source file.
+ */
+const command = new Command();
+command.name("build-ucum-map");
+
+// The TypeScript code that is prepended to the generated file output.
+const ucumFunctionCode = `
+export function getValidUcumCode(inputString: string): string | undefined {
+  const unit = inputString.trim();
+  const validCode = unitToValidUcumCode[unit];
+  return validCode;
+}\n\n`;
+
+command.action(async () => {
+  const ucumRows = await readUcumSource();
+  const generated: string[] = [ucumFunctionCode];
+
+  // Generate the mapping of ICD-10 codes to HCCs
+  generated.push(`export const unitToValidUcumCode: Record<string, string> = {\n`);
+  const includedCodes = new Set<string>();
+  for (const row of ucumRows) {
+    if (includedCodes.has(row.code)) continue;
+    includedCodes.add(row.code);
+    generated.push(`\t"${row.code}": "${row.code}",\n`);
+
+    for (const synonym of row.synonym.split(",")) {
+      if (includedCodes.has(synonym)) continue;
+      includedCodes.add(synonym);
+      generated.push(`\t"${synonym.trim()}": "${row.code}",\n`);
+    }
+  }
+  generated.push(`};\n`);
+
+  // Write the output to the TypeScript source file
+  const output = generated.join("");
+  writeToPackage("core", "external/fhir/parser/ucum-unit.ts", output);
+});
+
+export default command;
