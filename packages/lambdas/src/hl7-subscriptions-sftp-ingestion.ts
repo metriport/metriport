480c92dea8ad7a2fea0d9cdcadf6fddae11b3439
RadmirGari <radmirgaripovrss@gmail.com>
2025-09-11 13:51:03 -0700

    fix(core): fix import
    Part of ENG-968
    
    Signed-off-by: RadmirGari <radmirgaripovrss@gmail.com>

--- packages/lambdas/src/hl7-subscriptions-sftp-ingestion.ts
+++ packages/lambdas/src/hl7-subscriptions-sftp-ingestion.ts
@@ -2,7 +2,7 @@ import { capture } from "./shared/capture";
 import { prefixedLog } from "./shared/log";
 import { getEnvVarOrFail } from "@metriport/shared/common/env-var";
 import { Hl7SubscriptionLaHieIngestionDirect } from "@metriport/core/command/hl7v2-subscriptions/hl7-subscriptions-sftp-ingestion/hl7-notification-sftp-ingestion-direct";
-import { LaHieSftpClient } from "@metriport/core/command/hl7v2-subscriptions/hl7-subscriptions-sftp-ingestion/hl7-subscriptions-sftp-ingestion";
+import { SftpIngestionClient } from "@metriport/core/command/hl7v2-subscriptions/hl7-subscriptions-sftp-ingestion/sftp-ingestion-client";
 
 capture.init();
 
@@ -13,7 +13,7 @@ export const handler = capture.wrapHandler(async () => {
   capture.setExtra({ context: lambdaName });
   const log = prefixedLog("LaHie-ingestion");
   log("Starting ingestion of LaHie ADTs");
-  const sftpClient = await LaHieSftpClient.create(log);
+  const sftpClient = await SftpIngestionClient.create(log);
   const handler = new Hl7SubscriptionLaHieIngestionDirect(sftpClient, log);
   await handler.execute();
   log("Finished ingestion of LaHie");
