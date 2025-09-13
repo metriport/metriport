398ab9ecc4c233f9bd88e6d94116722a105186b8
RadmirGari <radmirgaripovrss@gmail.com>
2025-09-12 17:17:57 -0700

    feat(core): move mllp timezone logic to core
    Part of ENG-968
    
    Signed-off-by: RadmirGari <radmirgaripovrss@gmail.com>

--- packages/mllp-server/src/utils.ts
+++ packages/mllp-server/src/utils.ts
@@ -1,29 +1,20 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import { Hl7Message } from "@medplum/core";
 import { Hl7Connection, Hl7ErrorEvent, Hl7MessageEvent } from "@medplum/hl7";
 import { S3Utils } from "@metriport/core/external/aws/s3";
 import { Base64Scrambler } from "@metriport/core/util/base64-scrambler";
 import { Config } from "@metriport/core/util/config";
 import { Logger } from "@metriport/core/util/log";
 import { unpackUuid } from "@metriport/core/util/pack-uuid";
-import IPCIDR from "ip-cidr";
 
-import { HieConfigDictionary } from "@metriport/core/external/hl7-notification/hie-config-dictionary";
 import { MetriportError } from "@metriport/shared";
 import * as Sentry from "@sentry/node";
+import { Hl7Message } from "@medplum/core";
 
 const crypto = new Base64Scrambler(Config.getHl7Base64ScramblerSeed());
 export const s3Utils = new S3Utils(Config.getAWSRegion());
 export const bucketName = Config.getHl7IncomingMessageBucketName();
-
-/**
- * Avoid using message.toString() as its not stringifying every segment
- */
-export function asString(message: Hl7Message) {
-  return message.segments.map(s => s.toString()).join("\n");
-}
 
 export function withErrorHandling<T extends Hl7MessageEvent | Hl7ErrorEvent>(
   connection: Hl7Connection,
@@ -83,33 +74,8 @@ export function getCleanIpAddress(address: string | undefined): string {
 }
 
 /**
- * Lookup the HIE config for a provided IP address.
- * @param hieConfigDictionary The HIE config dictionary.
- * @param ip The IP address to lookup.
- * @returns The HIE config for the given IP address.
+ * Avoid using message.toString() as its not stringifying every segment
  */
-export function lookupHieTzEntryForIp(hieConfigDictionary: HieConfigDictionary, ip: string) {
-  const hieVpnConfigRows = Object.entries(hieConfigDictionary).flatMap(keepOnlyVpnConfigs);
-  const match = hieVpnConfigRows.find(({ cidrBlocks }) =>
-    cidrBlocks.some(cidrBlock => isIpInRange(cidrBlock, ip))
-  );
-  if (!match) {
-    console.log("[mllp-server.lookupHieTzEntryForIp] Sender IP not found in any CIDR block", ip);
-    throw new MetriportError(`Sender IP not found in any CIDR block`, {
-      cause: undefined,
-      additionalInfo: { context: "mllp-server.lookupHieTzEntryForIp", ip, hieConfigDictionary },
-    });
-  }
-  return match;
-}
-
-function isIpInRange(cidrBlock: string, ip: string): boolean {
-  const cidr = new IPCIDR(cidrBlock);
-  return cidr.contains(ip);
-}
-
-function keepOnlyVpnConfigs([hieName, config]: [string, HieConfigDictionary[string]]) {
-  return "cidrBlocks" in config
-    ? [{ hieName, cidrBlocks: config.cidrBlocks, timezone: config.timezone }]
-    : [];
+export function asString(message: Hl7Message) {
+  return message.segments.map(s => s.toString()).join("\n");
 }
