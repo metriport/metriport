e475877291aeff46f29241306502a7c087febc6d
Keshav Saharia <keshav@metriport.com>
2025-09-15 12:54:14 -0700

    test(core): add more tests for service requests
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/fhir-deduplication/merge/__tests__/merge-service-requests.test.ts
+++ packages/core/src/fhir-deduplication/merge/__tests__/merge-service-requests.test.ts
@@ -1,27 +1,88 @@
-import { ServiceRequest } from "@medplum/fhirtypes";
+import { Identifier, ServiceRequest } from "@medplum/fhirtypes";
+import { ServiceRequestStatusCode } from "../../../external/fhir/resources/service-request";
 import { buildServiceRequestMergeFunction } from "../resource/service-request";
+
+function makeServiceRequest({
+  id,
+  status,
+  identifier,
+  lastUpdated,
+  source,
+  requisition,
+}: {
+  id: string;
+  status: ServiceRequestStatusCode;
+  requisition?: Identifier;
+  identifier?: Identifier[];
+  lastUpdated: string;
+  source?: string;
+}): ServiceRequest {
+  return {
+    resourceType: "ServiceRequest",
+    id,
+    status,
+    meta: {
+      lastUpdated,
+      ...(source ? { source } : {}),
+    },
+    ...(identifier ? { identifier } : {}),
+    ...(requisition ? { requisition } : {}),
+  };
+}
 
 describe("Merge service requests", () => {
   const mergeServiceRequests = buildServiceRequestMergeFunction();
 
   it("should merge service requests", () => {
-    const serviceRequest1: ServiceRequest = {
-      resourceType: "ServiceRequest",
-      meta: {
-        lastUpdated: "2025-01-01T00:00:00.000Z",
-        source: "service-request-1-source",
-      },
+    const serviceRequest1: ServiceRequest = makeServiceRequest({
       id: "1",
-    };
-    const serviceRequest2: ServiceRequest = {
-      resourceType: "ServiceRequest",
-      meta: {
-        lastUpdated: "2025-01-02T00:00:00.000Z",
-        source: "service-request-2-source",
-      },
+      status: "unknown",
+      lastUpdated: "2025-01-01T00:00:00.000Z",
+      source: "service-request-1-source",
+    });
+    const serviceRequest2: ServiceRequest = makeServiceRequest({
       id: "2",
-    };
+      status: "unknown",
+      lastUpdated: "2025-01-02T00:00:00.000Z",
+      source: "service-request-2-source",
+    });
     const result = mergeServiceRequests([serviceRequest1, serviceRequest2]);
     expect(result).toEqual(serviceRequest2);
+  });
+
+  it("should merge service requests with identifiers", () => {
+    const serviceRequest1: ServiceRequest = makeServiceRequest({
+      id: "1",
+      status: "unknown",
+      lastUpdated: "2025-01-01T00:00:00.000Z",
+      requisition: { system: "http://another-example.com", value: "ABC123" },
+      identifier: [{ system: "http://example.com", value: "1234567890" }],
+      source: "service-request-1-source",
+    });
+    const serviceRequest2: ServiceRequest = makeServiceRequest({
+      id: "2",
+      status: "unknown",
+      lastUpdated: "2025-01-02T00:00:00.000Z",
+      requisition: { system: "http://another-example.com", value: "ABC123" },
+      identifier: [{ system: "http://example.com", value: "9876543210" }],
+      source: "service-request-2-source",
+    });
+    const serviceRequest3: ServiceRequest = makeServiceRequest({
+      id: "3",
+      status: "unknown",
+      lastUpdated: "2025-01-03T00:00:00.000Z",
+      requisition: { system: "http://another-example.com", value: "ABC123" },
+      identifier: [{ system: "http://example.com", value: "6666666666" }],
+      source: "service-request-3-source",
+    });
+    const result = mergeServiceRequests([serviceRequest1, serviceRequest2, serviceRequest3]);
+    expect(result).toEqual({
+      ...serviceRequest3,
+      identifier: [
+        { system: "http://example.com", value: "1234567890" },
+        { system: "http://example.com", value: "6666666666" },
+        { system: "http://example.com", value: "9876543210" },
+      ],
+    });
   });
 });
