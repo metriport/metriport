2ea856bde00b7c72e8e9ffcf6f58246cc672c556
Keshav Saharia <keshav@metriport.com>
2025-09-15 18:31:07 -0700

    feat(core): add route to dosage
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/external/comprehend/rxnorm/attribute/dosage.ts
+++ packages/core/src/external/comprehend/rxnorm/attribute/dosage.ts
@@ -3,21 +3,23 @@ import { Dosage } from "@medplum/fhirtypes";
 import { getAttribute } from "../shared";
 import { parseTiming } from "../../../fhir/parser/timing";
 import { parseDosage } from "../../../fhir/parser/dosage";
+import { getRouteCode } from "../../../fhir/parser/route-codes";
 
 export function buildDosage(entity: RxNormEntity): Dosage | undefined {
   // -> dosage.text or dosage.doseAndRate.doseQuantity
   const dosage = getAttribute(entity, RxNormAttributeType.DOSAGE);
   const frequency = getAttribute(entity, RxNormAttributeType.FREQUENCY);
-  // const rate = getAttribute(entity, RxNormAttributeType.RATE);
+  const routeOrMode = getAttribute(entity, RxNormAttributeType.ROUTE_OR_MODE);
 
-  // if (!frequency && !rate) return undefined;
   const dosageText = dosage?.Text;
   const timing = frequency && frequency.Text ? parseTiming(frequency.Text) : undefined;
   const doseAndRate = dosageText ? parseDosage(dosageText) : undefined;
+  const route = routeOrMode && routeOrMode.Text ? getRouteCode(routeOrMode.Text) : undefined; // TODO: add route code to dosage
 
   return {
     ...(dosageText ? { text: dosageText } : undefined),
     ...(doseAndRate ? { doseAndRate: [doseAndRate] } : undefined),
     ...(timing ? { timing } : undefined),
+    ...(route ? { route } : undefined),
   };
 }
