20720a64fbcd85d021297d1706bdd67a553a4d07
Keshav Saharia <keshav@metriport.com>
2025-09-15 19:02:11 -0700

    feat(core): fix tests for route code
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/external/fhir/parser/__tests__/route-code.test.ts
+++ packages/core/src/external/fhir/parser/__tests__/route-code.test.ts
@@ -1,19 +1,14 @@
 import { getRouteCode } from "../route-codes";
 import { SNOMED_URL } from "@metriport/shared/medical";
-import { buildParserExtension } from "../extension";
 
 describe("Route code parser test", () => {
   it("should parse route code for basic phrases", () => {
-    const oralRouteCode = getRouteCode("oral");
+    const oralRouteCode = getRouteCode("oral", { includeExtension: false });
     expect(oralRouteCode).toEqual({
       coding: [{ code: "26643006", display: "Oral route", system: SNOMED_URL }],
-      extension: [buildParserExtension("oral")],
     });
 
-    const perOsRouteCode = getRouteCode("p.o.");
-    expect(perOsRouteCode).toEqual({
-      ...oralRouteCode,
-      extension: [buildParserExtension("p.o.")],
-    });
+    const perOsRouteCode = getRouteCode("p.o.", { includeExtension: false });
+    expect(perOsRouteCode).toEqual(oralRouteCode);
   });
 });
