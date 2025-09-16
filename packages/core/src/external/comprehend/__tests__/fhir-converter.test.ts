40238d503a3186916eb4fb5b780b227b7e6bdab8
Keshav Saharia <keshav@metriport.com>
2025-09-15 20:27:06 -0700

    feat(core): update tests to do comparison against stable output
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/external/comprehend/__tests__/fhir-converter.test.ts
+++ packages/core/src/external/comprehend/__tests__/fhir-converter.test.ts
@@ -1,5 +1,4 @@
-import fs from "fs";
-import path from "path";
+import { Resource } from "@medplum/fhirtypes";
 import { getFhirArtifact, getRxNormArtifact } from "./shared";
 import { getFhirResourcesFromRxNormEntities } from "../rxnorm/fhir-converter";
 import {
@@ -11,10 +10,6 @@ import {
 import { DATA_EXTRACTION_URL } from "../constants";
 
 describe("FHIR converter", () => {
-  const rxNormDir = path.join(__dirname, "artifacts", "rxnorm");
-  const rxNormArtifactIds = fs
-    .readdirSync(rxNormDir)
-    .filter(child => fs.statSync(path.join(rxNormDir, child)).isDirectory());
   const testContext = {
     patientId: TEST_PATIENT_ID,
     dateNoteWritten: TEST_DATE_NOTE_WRITTEN,
@@ -22,28 +17,49 @@ describe("FHIR converter", () => {
     extensionUrl: DATA_EXTRACTION_URL,
   };
 
-  it("should convert dosage frequency to FHIR", async () => {
-    for (const artifactId of rxNormArtifactIds) {
-      const { inputText, response } = getRxNormArtifact(artifactId);
-      const resources = getFhirResourcesFromRxNormEntities(response.Entities ?? [], {
-        confidenceThreshold: TEST_CONFIDENCE_THRESHOLD,
-        context: {
-          ...testContext,
-          originalText: inputText,
-        },
-      });
-      const expectedResources = getFhirArtifact("rxnorm", artifactId);
-      // Remove ids and references
-      for (const resource of resources) {
-        delete resource.id;
-        if ("medicationReference" in resource) {
-          delete resource.medicationReference;
-        }
-        if ("subject" in resource) {
-          delete resource.subject;
-        }
-      }
-      expect(resources).toEqual(expectedResources);
+  function removeIdsAndReferences(resource: Resource): void {
+    delete resource.id;
+    if ("medicationReference" in resource) {
+      delete resource.medicationReference;
     }
+    if ("subject" in resource) {
+      delete resource.subject;
+    }
+  }
+
+  function testArtifact(artifactId: string): void {
+    const { inputText, response } = getRxNormArtifact(artifactId);
+    const resources = getFhirResourcesFromRxNormEntities(response.Entities ?? [], {
+      confidenceThreshold: TEST_CONFIDENCE_THRESHOLD,
+      context: {
+        ...testContext,
+        originalText: inputText,
+      },
+    });
+    const expectedResources = getFhirArtifact("rxnorm", artifactId);
+    // Remove ids and references
+    for (const resource of resources) {
+      removeIdsAndReferences(resource);
+    }
+    for (const resource of expectedResources) {
+      removeIdsAndReferences(resource);
+    }
+    expect(resources).toEqual(expectedResources);
+  }
+
+  /**
+   * A simple test case with a single medication with form and strength.
+   * > Take Acetaminophen 500 mg tablet, 1-2 tablets orally every 6 hours as needed for pain, not to exceed 8 tablets per day.
+   */
+  it("should work for a simple test case with a single medication (acetaminophen)", async () => {
+    testArtifact("acetaminophen");
+  });
+
+  /**
+   * A test case with a single medication with multiple different attributes (frequency, dosage, form, strength, etc).
+   * > Administer Amoxicillin 250 mg/5 mL suspension, 10 mL by mouth three times daily for 7 days.
+   */
+  it("should work for a complex test case with a single medication (amoxicillin)", async () => {
+    testArtifact("amoxicillin");
   });
 });
--- packages/core/src/external/comprehend/__tests__/shared.ts
+++ packages/core/src/external/comprehend/__tests__/shared.ts
@@ -8,17 +8,11 @@ const ARTIFACT_DIR = path.join(__dirname, "artifacts");
 type RxNormArtifact = { inputText: string; response: InferRxNormCommandOutput };
 
 export function getRxNormArtifact(name: string): RxNormArtifact {
-  const jsonFile = fs.readFileSync(path.join(ARTIFACT_DIR, "rxnorm", name, "rxnorm.json"));
-  if (!fs.existsSync(jsonFile)) {
-    throw new Error(`RxNorm artifact ${name} not found`);
-  }
+  const jsonFile = fs.readFileSync(path.join(ARTIFACT_DIR, "rxnorm", name, "rxnorm.json"), "utf8");
   return JSON.parse(jsonFile.toString()) as RxNormArtifact;
 }
 
 export function getFhirArtifact(api: "rxnorm" | "icd10cm" | "snomedct", name: string): Resource[] {
   const jsonFile = fs.readFileSync(path.join(ARTIFACT_DIR, api, name, "fhir.json"));
-  if (!fs.existsSync(jsonFile)) {
-    throw new Error(`Fhir artifact ${name} not found`);
-  }
   return JSON.parse(jsonFile.toString()) as Resource[];
 }
