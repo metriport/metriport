6fa34e6861182888fe3ecc9caf1dd04cb3574868
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-09-16 10:19:15 -0700

    feat(core): move character sanitizer to domain
    metriport/metriport-internal#1040
    
    Ref: ENG-1061
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

---
+++ packages/core/src/domain/character-sanitizer.ts
@@ -0,0 +1,40 @@
+import { Patient } from "../../domain/patient";
+
+/**
+ * Regex to match invalid UTF-8 characters that should be stripped from patient data.
+ * Currently targets the replacement character (�) which indicates corrupted encoding.
+ */
+const badCharactersRegex = /[�]/g;
+
+/**
+ * Strips invalid UTF-8 characters from a string.
+ *
+ * @param str - The string to sanitize
+ * @returns The sanitized string with invalid characters removed, or undefined if input is falsy
+ */
+function stripBadCharactersFromString(str: string | undefined): string | undefined {
+  if (!str) return str;
+  return str.replace(badCharactersRegex, "");
+}
+
+/**
+ * Strips invalid UTF-8 characters from patient address data.
+ *
+ * @param patient - The patient whose data should be sanitized
+ * @returns A new patient object with sanitized address data
+ */
+export function stripInvalidCharactersFromPatientData(patient: Patient): Patient {
+  if (!patient.data.address) return patient;
+
+  return {
+    ...patient,
+    data: {
+      ...patient.data,
+      address: patient.data.address.map(address => ({
+        ...address,
+        addressLine1: stripBadCharactersFromString(address.addressLine1),
+        addressLine2: stripBadCharactersFromString(address.addressLine2),
+      })),
+    },
+  };
+}
