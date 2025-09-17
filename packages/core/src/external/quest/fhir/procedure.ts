8d15b6860a68cbffe5db9cdd2f99168c3b70b318
Keshav Saharia <keshav@metriport.com>
2025-09-16 20:07:14 -0700

    feat(core): add procedures to quest fhir output
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/external/quest/fhir/procedure.ts
+++ packages/core/src/external/quest/fhir/procedure.ts
@@ -1,15 +1,28 @@
-import { CodeableConcept, Procedure } from "@medplum/fhirtypes";
+import { CodeableConcept, Patient, Procedure, ServiceRequest } from "@medplum/fhirtypes";
 import { uuidv7 } from "@metriport/shared/util/uuid-v7";
+import { CPT_URL } from "@metriport/shared/medical";
 import { ResponseDetail } from "../schema/response";
+import { getPatientReference } from "./patient";
+import { getProcedureCategory } from "../../fhir/resources/procedure";
+import { getServiceRequestReference } from "./service-request";
 
-export function getProcedure(detail: ResponseDetail): Procedure {
+export function getProcedure(
+  detail: ResponseDetail,
+  { patient, serviceRequest }: { patient: Patient; serviceRequest?: ServiceRequest }
+): Procedure {
   const code = getProcedureCode(detail);
+  const subject = getPatientReference(patient);
+  const category = getProcedureCategory("Diagnostic procedure");
+  const basedOn = serviceRequest ? [getServiceRequestReference(serviceRequest)] : undefined;
 
   return {
     resourceType: "Procedure",
     id: uuidv7(),
     status: "completed",
-    ...(code ? { code } : {}),
+    subject,
+    category,
+    ...(basedOn ? { basedOn } : undefined),
+    ...(code ? { code } : undefined),
   };
 }
 
@@ -18,7 +31,7 @@ function getProcedureCode(detail: ResponseDetail): CodeableConcept | undefined {
   return {
     coding: [
       {
-        system: "http://www.ama-assn.org/go/cpt",
+        system: CPT_URL,
         code: detail.cptCode,
       },
     ],
