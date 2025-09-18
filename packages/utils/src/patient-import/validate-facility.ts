84a0f32282f65177bb35988284dc42188f9242bc
Keshav Saharia <keshav@metriport.com>
2025-09-18 14:02:18 -0700

    feat(utils): count instances in the roster where facility needs to be updated
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/utils/src/patient-import/validate-facility.ts
+++ packages/utils/src/patient-import/validate-facility.ts
@@ -2,12 +2,10 @@ import * as dotenv from "dotenv";
 dotenv.config();
 // keep that ^ on top
 
-// import fs from "fs";
-// import csv from "csv-parser";
 import { Command } from "commander";
 import { SurescriptsDataMapper as DataMapper } from "@metriport/core/external/surescripts/data-mapper";
 import { Patient } from "@metriport/shared/domain/patient";
-import { readCsv } from "../shared/csv";
+import { readCsv, streamCsv } from "../shared/csv";
 
 /**
  * This script reads a CSV roster and ensures that each patient is associated with the correct facility,
@@ -25,14 +23,38 @@ program
 
 async function validateFacility({
   cxId,
+  csvRoster,
   csvFacility,
 }: {
   cxId: string;
   csvRoster: string;
   csvFacility: string;
 }) {
-  const { facilityIdToName } = await buildFacilityMapping(csvFacility);
-  await buildExternalIdToPatientMapping(cxId, facilityIdToName);
+  const { facilityNameToId, facilityIdToName } = await buildFacilityMapping(csvFacility);
+  const externalIdToPatient = await buildExternalIdToPatientMapping(cxId, facilityIdToName);
+
+  let totalInstancesFound = 0;
+  const { rowsProcessed, errorCount } = await streamCsv<CsvRosterRow>(csvRoster, row => {
+    const { externalId, facilityName } = getExternalIdAndFacilityName(row);
+    const patient = externalIdToPatient[externalId];
+    if (!patient) {
+      throw new Error(`No patient found for external ID ${externalId}`);
+    }
+    const currentFacilityId = patient.facilityIds[0];
+    if (!currentFacilityId) {
+      throw new Error(`Patient ${externalId} has no facility ID`);
+    }
+    const expectedFacilityId = facilityNameToId[facilityName];
+    if (currentFacilityId !== expectedFacilityId) {
+      console.log(
+        `Patient ${externalId} has facility ID ${currentFacilityId} but expected ${expectedFacilityId}`
+      );
+      totalInstancesFound++;
+    }
+  });
+  console.log(`Rows processed: ${rowsProcessed}`);
+  console.log(`Error count: ${errorCount}`);
+  console.log(`Total facility changes required: ${totalInstancesFound}`);
 }
 
 async function buildFacilityMapping(csvFacility: string) {
@@ -64,7 +86,7 @@ async function buildExternalIdToPatientMapping(
     totalCount += existingPatientIds.length;
 
     // Retrieve each patient to construct a mapping of external IDs
-    const patients = await dataMapper.getEachPatientById(cxId, existingPatientIds);
+    const patients = await dataMapper.getEachPatientById(cxId, existingPatientIds, 25);
     Object.assign(
       externalIdToPatient,
       Object.fromEntries(
@@ -84,16 +106,33 @@ async function buildExternalIdToPatientMapping(
   return externalIdToPatient;
 }
 
-// async function streamRoster(csvRoster: string, handler: (row: Record<string, string>) => void): Promise<void> {
-//   fs.createReadStream(csvRoster)
-//     .pipe(csv())
-//     .on("data", handler)
-//     .on("end", () => {
-//       console.log("Roster streamed");
-//     })
-//     .on("error", (error) => {
-//       console.error("Error streaming roster", error);
-//     });
-// }
+/**
+ * Specific headers for the roster CSV that can be adapted to other CSV header formats,
+ * as long as the corresponding function below to retrieve the headers is also updated.
+ */
+interface CsvRosterRow {
+  PRACTICE_ID: string;
+  PATIENT_ID: string;
+  PATIENT_FIRST_NAME: string;
+  PATIENT_LAST_NAME: string;
+  PATIENT_DOB: string;
+  PATIENT_SEX: string;
+  PATIENT_ZIP: string;
+  PATIENT_CITY: string;
+  PATIENT_STATE: string;
+  PATIENT_ADDRESS_1: string;
+  PATIENT_ADDRESS_2: string;
+  PATIENT_HOME_PHONE: string;
+  PATIENT_WORK_PHONE: string;
+  PATIENT_MOBILE_PHONE: string;
+  PATIENT_EMAIL: string;
+}
+
+function getExternalIdAndFacilityName(row: CsvRosterRow): {
+  externalId: string;
+  facilityName: string;
+} {
+  return { externalId: row.PATIENT_ID, facilityName: row.PRACTICE_ID };
+}
 
 program.parse(process.argv);
--- packages/utils/src/shared/csv.ts
+++ packages/utils/src/shared/csv.ts
@@ -2,6 +2,8 @@ import csv from "csv-parser";
 import fs from "fs";
 import path from "path";
 
+type CsvRow = Record<string, string>;
+
 export function getCsvRunsPath(csvPath: string): string {
   if (path.isAbsolute(csvPath)) {
     return csvPath;
@@ -9,7 +11,7 @@ export function getCsvRunsPath(csvPath: string): string {
   return path.join(__dirname, "../../runs", csvPath);
 }
 
-export async function readCsv<T extends Record<string, unknown>>(csvPath: string): Promise<T[]> {
+export async function readCsv<T extends CsvRow>(csvPath: string): Promise<T[]> {
   const fullCsvPath = getCsvRunsPath(csvPath);
   return new Promise((resolve, reject) => {
     const rows: T[] = [];
@@ -20,6 +22,33 @@ export async function readCsv<T extends Record<string, unknown>>(csvPath: string
       })
       .on("end", () => {
         resolve(rows);
+      })
+      .on("error", error => {
+        reject(error);
+      });
+  });
+}
+
+export async function streamCsv<T>(
+  csvPath: string,
+  handler: (row: T) => void
+): Promise<{ rowsProcessed: number; errorCount: number }> {
+  return new Promise((resolve, reject) => {
+    let rowsProcessed = 0;
+    let errorCount = 0;
+    const fullCsvPath = getCsvRunsPath(csvPath);
+    fs.createReadStream(fullCsvPath)
+      .pipe(csv())
+      .on("data", (row: T) => {
+        rowsProcessed++;
+        try {
+          handler(row);
+        } catch (error) {
+          errorCount++;
+        }
+      })
+      .on("end", () => {
+        resolve({ rowsProcessed, errorCount });
       })
       .on("error", error => {
         reject(error);
