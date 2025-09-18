6a3c7efe0041ff1cd74ac1cd59f9938f00a2f010
RadmirGari <radmirgaripovrss@gmail.com>
2025-09-18 15:13:27 -0700

    fix(core): get address first then state for bamboo timezone
    Part of ENG-1082
    
    Signed-off-by: RadmirGari <radmirgaripovrss@gmail.com>

--- packages/core/src/command/hl7-notification/timezone.ts
+++ packages/core/src/command/hl7-notification/timezone.ts
@@ -1,11 +1,15 @@
 import { Hl7Message } from "@medplum/core";
-import { MetriportError } from "@metriport/shared/dist/error/metriport-error";
+import { MetriportError } from "@metriport/shared";
 import { normalizeState, stateToTimezone } from "@metriport/shared/domain/address/state";
 
-const ZFA_STATE_INDEX = 4;
+const ZFA_ADDRESS_INDEX = 4; // Field (1 indexed)
+const ZFA_STATE_INDEX = 3; // Arrat (0 indexed)
 
 function getTimezoneFromState(state: string): string {
+  console.log("extracted state", state);
   const stateEnum = normalizeState(state);
+  console.log("stateEnum", stateEnum);
+  console.log("stateToTimezone", stateToTimezone[stateEnum]);
   return stateToTimezone[stateEnum];
 }
 
@@ -15,8 +19,8 @@ export function getBambooTimezone(hl7Message: Hl7Message): string {
     throw new MetriportError("Custom ZFA segment was not found in a bamboo hl7 message!");
   }
 
-  const customZfaField = customZfaSegment.getField(ZFA_STATE_INDEX);
-  if (!customZfaField) {
+  const customZfaAddress = customZfaSegment.getField(ZFA_ADDRESS_INDEX);
+  if (!customZfaAddress) {
     throw new MetriportError(
       "Custom ZFA.4 field was not found in a bamboo hl7 message!",
       undefined,
@@ -25,6 +29,21 @@ export function getBambooTimezone(hl7Message: Hl7Message): string {
       }
     );
   }
-  const timezone = getTimezoneFromState(customZfaField.toString());
+
+  const addressParts = customZfaAddress.toString().split("^");
+  const state = addressParts[ZFA_STATE_INDEX];
+
+  if (!state) {
+    throw new MetriportError(
+      "State was not found in the custom ZFA.4 field of the bamboo hl7 message!",
+      undefined,
+      {
+        ZFASegment: customZfaAddress.toString(),
+        addressParts: addressParts.toString(),
+        state,
+      }
+    );
+  }
+  const timezone = getTimezoneFromState(state);
   return timezone;
 }
