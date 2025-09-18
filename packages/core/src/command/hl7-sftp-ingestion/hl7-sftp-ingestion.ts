e5b2914117bf907e40c64e5190f56902456318ef
RadmirGari <radmirgaripovrss@gmail.com>
2025-09-17 21:15:46 -0700

    fix(core): change log prefix back
    Part of ENG-1084
    
    Signed-off-by: RadmirGari <radmirgaripovrss@gmail.com>

--- packages/core/src/command/hl7-sftp-ingestion/hl7-sftp-ingestion.ts
+++ packages/core/src/command/hl7-sftp-ingestion/hl7-sftp-ingestion.ts
@@ -2,7 +2,7 @@ import { log as createLog } from "../../util/log";
 
 export const HIE_NAME = "Lahie";
 
-export const log = createLog("lahie-ingest");
+export const log = createLog("HL7-SFTP-INGESTION");
 
 export interface Hl7LahieSftpIngestion {
   execute(params: Hl7LahieSftpIngestionParams): Promise<void>;
