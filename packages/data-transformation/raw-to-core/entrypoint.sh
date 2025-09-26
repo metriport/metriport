fa6112aaa1b62c9b77ff31f6473e4c0ff6473b79
Thomas Yopes <thomasyopes@Mac.attlocal.net>
2025-09-26 10:52:01 -0700

    feat(ap): snowflake to pg
    Ref: ENG-743
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Mac.attlocal.net>

--- packages/data-transformation/raw-to-core/entrypoint.sh
+++ packages/data-transformation/raw-to-core/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-INPUT_DATABASE="${DBT_TUVA_CI_DATABASE}"
-INPUT_SCHEMA="${DBT_SNOWFLAKE_CI_SCHEMA}"
+INPUT_DATABASE="${DBT_PG_DATABASE}"
+INPUT_SCHEMA="${DBT_PG_SCHEMA}"
 dbt build --vars "{"input_database": "$INPUT_DATABASE", "input_schema": "$INPUT_SCHEMA"}"
