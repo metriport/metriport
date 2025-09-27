c676ff7b96e2ba7df572bf046ac988b5de4736f5
Thomas Yopes <thomasyopes@Mac.attlocal.net>
2025-09-27 10:50:44 -0700

    feat(ap): better errors
    Ref: ENG-954
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Mac.attlocal.net>

--- packages/data-transformation/raw-to-core/main.py
+++ packages/data-transformation/raw-to-core/main.py
@@ -3,17 +3,25 @@ import os
 
 def handler(event: dict, context: dict):
     host = event.get("HOST") or os.getenv("DBT_PG_HOST")
+    if not host:
+        raise ValueError("Missing required environment variables: HOST")
     os.environ['DBT_PG_HOST'] = host
     user = event.get("USER") or os.getenv("DBT_PG_USER")
+    if not user:
+        raise ValueError("Missing required environment variables: USER")
     os.environ['DBT_PG_USER'] = user
     password = event.get("PASSWORD") or os.getenv("DBT_PG_PASSWORD")
+    if not password:
+        raise ValueError("Missing required environment variables: PASSWORD")
     os.environ['DBT_PG_PASSWORD'] = password
     database = event.get("DATABASE") or os.getenv("DBT_PG_DATABASE")
+    if not database:
+        raise ValueError("Missing required environment variables: DATABASE")
     os.environ['DBT_PG_DATABASE'] = database
     schema = event.get("SCHEMA") or os.getenv("DBT_PG_SCHEMA")
+    if not schema:
+        raise ValueError("Missing required environment variables: SCHEMA")
     os.environ['DBT_PG_SCHEMA'] = schema
-    if (not host or not user or not password or not database or not schema):
-        raise ValueError("Missing required environment variables")
     print(f"Running DBT build with database: {database}, schema: {schema}")
     dbt_runner = dbtRunner()
     cli_args = ["build"]
