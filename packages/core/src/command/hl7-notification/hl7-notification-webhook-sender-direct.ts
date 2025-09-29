a623d2f5a18bb37a331f4ee08101a50372ed33e8
RadmirGari <radmirgaripovrss@gmail.com>
2025-09-29 11:17:22 -0700

    fix(mllp): add getsecretvar or fail
    Part of ENG-1100
    
    Signed-off-by: RadmirGari <radmirgaripovrss@gmail.com>

--- packages/core/src/command/hl7-notification/hl7-notification-webhook-sender-direct.ts
+++ packages/core/src/command/hl7-notification/hl7-notification-webhook-sender-direct.ts
@@ -42,6 +42,7 @@ import {
   SupportedTriggerEvent,
 } from "./utils";
 import { getBambooTimezone, getKonzaTimezone } from "./timezone";
+import { getSecretValueOrFail } from "../../external/aws/secret-manager";
 
 type HieConfig = { timezone: string };
 
@@ -146,17 +147,23 @@ export class Hl7NotificationWebhookSenderDirect implements Hl7NotificationWebhoo
     const encounterPeriod = getEncounterPeriod(message);
     const encounterClass = getEncounterClass(message);
     const facilityName = getFacilityName(message);
-
-    analytics({
-      distinctId: cxId,
-      event: EventTypes.hl7NotificationReceived,
-      properties: {
-        cxId,
-        patientId,
-        messageCode,
-        triggerEvent,
-      },
-    });
+    const posthogApiKeyArn = Config.getPostHogApiKey();
+    if (posthogApiKeyArn) {
+      const posthogApiKey = await getSecretValueOrFail(posthogApiKeyArn, Config.getAWSRegion());
+      analytics(
+        {
+          distinctId: cxId,
+          event: EventTypes.hl7NotificationReceived,
+          properties: {
+            cxId,
+            patientId,
+            messageCode,
+            triggerEvent,
+          },
+        },
+        posthogApiKey
+      );
+    }
 
     capture.setExtra({
       cxId,
