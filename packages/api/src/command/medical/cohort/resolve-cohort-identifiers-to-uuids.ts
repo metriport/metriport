6d42f1a0868a58ac93b066d65fe89171ba597c57
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-09-30 00:32:51 -0700

    feat(api): implement creation by name OR uuid
    metriport/metriport-internal#1040
    
    Ref: ENG-1114
    ENG-1116
    
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

---
+++ packages/api/src/command/medical/cohort/resolve-cohort-identifiers-to-uuids.ts
@@ -0,0 +1,31 @@
+import { isValidUuid } from "@metriport/core/util/uuid-v7";
+import { BadRequestError } from "@metriport/shared/dist/error/bad-request";
+import { getCohortByNameSafe } from "./get-cohort";
+
+/**
+ * Returns the cohort with the specified name.
+ * @param cxId The ID of the CX.
+ * @param name The name of the cohort.
+ * @returns The cohort with the specified name.
+ */
+export async function resolveCohortIdentifiersToUuids({
+  cxId,
+  identifiers,
+}: {
+  cxId: string;
+  identifiers: string[];
+}): Promise<string[]> {
+  return await Promise.all(
+    identifiers?.map(async cohortIdentifier => {
+      if (isValidUuid(cohortIdentifier)) {
+        return cohortIdentifier;
+      }
+
+      const cohort = await getCohortByNameSafe({ cxId, name: cohortIdentifier });
+      if (cohort === undefined) {
+        throw new BadRequestError(`Cohort not found with name ${cohortIdentifier}.`);
+      }
+      return cohort.id;
+    }) ?? []
+  );
+}
