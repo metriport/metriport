b9537906fd77d96afcd78974ea7656233ec42ba2
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-10-02 09:15:21 -0700

    feat(api): setup sse for llm inference
    metriport/metriport-internal#1040
    
    Ref: ENG-1206
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/api/src/routes/medical/inference.ts
+++ packages/api/src/routes/medical/inference.ts
@@ -1,10 +1,11 @@
+import { uuidv7 } from "@metriport/shared/util";
+import { createSession } from "better-sse";
 import { Request, Response } from "express";
 import Router from "express-promise-router";
-import status from "http-status";
 import { z } from "zod";
 import { handleParams } from "../helpers/handle-params";
 import { requestLogger } from "../helpers/request-logger";
-import { asyncHandler, getCxIdOrFail } from "../util";
+import { asyncHandler } from "../util";
 
 const router = Router();
 
@@ -22,11 +23,43 @@ router.post(
   handleParams,
   requestLogger,
   asyncHandler(async (req: Request, res: Response) => {
-    const cxId = getCxIdOrFail(req);
+    // const cxId = getCxIdOrFail(req);
     const { context } = sidePanelInferenceSchema.parse(req.body);
-    console.log(`context: ${context}, cxId: ${cxId}`);
-    const message = "Hello, world!";
-    return res.status(status.OK).json({ message });
+    const sse = await createSession(req, res, {
+      state: {
+        id: uuidv7(),
+        name: "Side Panel Inference",
+        description: "Side Panel Inference",
+        startTimeMillis: Date.now().toString(),
+        endTimeMillis: Date.now().toString(),
+      },
+    });
+
+    sse.push({
+      message: "Hello, world! Waiting 2 seconds...",
+      eventName: "side-panel-hw",
+      eventId: uuidv7(),
+    });
+
+    await new Promise(function (resolve) {
+      setTimeout(resolve, 2000);
+    });
+
+    sse.push({
+      message: "Slept, ping, waiting 2 seconds...",
+      eventName: "side-panel-ping",
+      eventId: uuidv7(),
+    });
+
+    await new Promise(function (resolve) {
+      setTimeout(resolve, 2000);
+    });
+
+    sse.push({
+      message: "Slept, pong, - your context was: " + context,
+      eventName: "side-panel-pong",
+      eventId: uuidv7(),
+    });
   })
 );
 
