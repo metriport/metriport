048354553dbb3d65c640b69f31d391ec029b3a7f
RadmirGari <radmirgaripovrss@gmail.com>
2025-10-02 18:35:56 -0700

    fix(core): remove webhook feature flag
    Part of ENG-000
    
    Signed-off-by: RadmirGari <radmirgaripovrss@gmail.com>

--- packages/core/src/command/feature-flags/domain-ffs.ts
+++ packages/core/src/command/feature-flags/domain-ffs.ts
@@ -264,13 +264,6 @@ export async function isRecentVisitAiSummaryEnabledForCx(cxId: string): Promise<
 export async function getCxsWithHl7NotificationWebhookFeatureFlag(): Promise<string[]> {
   return getCxsWithFeatureFlagEnabled("cxsWithHl7NotificationWebhookFeatureFlag");
 }
-export async function isHl7NotificationWebhookFeatureFlagEnabledForCx(
-  cxId: string
-): Promise<boolean> {
-  const cxIdsWithHl7NotificationWebhookEnabled =
-    await getCxsWithHl7NotificationWebhookFeatureFlag();
-  return cxIdsWithHl7NotificationWebhookEnabled.some(i => i === cxId);
-}
 
 // ENG-536 remove this once we automatically find the discharge summary
 export async function getCxsWithDischargeSlackNotificationFeatureFlag(): Promise<string[]> {
