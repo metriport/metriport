100444672eaabd45c326363bd6e21cd622c12de2
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-10-03 09:14:06 -0700

    feat(api): update inference endpoint
    metriport/metriport-internal#1040
    
    Ref: ENG-1206
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/api/src/routes/medical/inference.ts
+++ packages/api/src/routes/medical/inference.ts
@@ -12,8 +12,36 @@ import { AnthropicMessageText } from "@metriport/core/external/bedrock/model/ant
 const router = Router();
 
 const sidePanelInferenceSchema = z.object({
+  resourceType: z.string(),
+  resourceDisplays: z.array(z.string()),
   context: z.string(),
 });
+
+router.get(
+  "/health",
+  asyncHandler(async (req: Request, res: Response) => {
+    res.status(200).send("OK");
+  })
+);
+
+const questionsByResourceType = {
+  DiagnosticReport: [
+    "Why was this done?",
+    "Are there any notes associated?",
+    "What was the context in which this lab was taken?",
+  ],
+  Condition: [
+    "When was this diagnosed and by who?",
+    "What is the reasoning for the diagnosis",
+    "Are there any related notes on the diagnosis",
+  ],
+};
+
+const defaultQuestions = [
+  "Why is this important?",
+  "Are there any related notes?",
+  "What is the important context surrounding this?",
+];
 
 /** ---------------------------------------------------------------------------
  * POST /internal/inference/side-panel
@@ -26,7 +54,8 @@ router.post(
   requestLogger,
   asyncHandler(async (req: Request, res: Response) => {
     // const cxId = getCxIdOrFail(req);
-    const { context } = sidePanelInferenceSchema.parse(req.body);
+    const { resourceType, resourceDisplays, context } = sidePanelInferenceSchema.parse(req.body);
+    console.log(`resourceType: ${resourceType}, resourceDisplays: ${resourceDisplays.join(", ")}`);
 
     const sse = await createSession(req, res, {
       state: {
@@ -40,8 +69,8 @@ router.post(
 
     const agent = new AnthropicAgent<"claude-sonnet-3.7">({
       version: "claude-sonnet-3.7",
-      region: "us-east-1",
-      systemPrompt: `You are a helpful assistant that can answer questions about the user's context.`,
+      region: "us-west-2",
+      systemPrompt: `You are seasoned physician who answers questions given a patient's comprehensive medical record.`,
       maxTokens: 1024,
       temperature: 0,
       tools: [],
@@ -53,9 +82,24 @@ router.post(
       eventId: uuidv7(),
     });
 
-    agent.addUserMessageText(context);
-    const response = await agent.continueConversation();
+    const questions =
+      questionsByResourceType[resourceType as keyof typeof questionsByResourceType] ??
+      defaultQuestions;
 
+    agent.addUserMessageText(
+      `
+      This is about a patient - ${resourceType}: ${resourceDisplays.join(", ")}
+      Answer the following question(s):
+      ${questions.join("\n")}
+
+      In your response, please include a source for each and every claim. These sources should use markdown link syntax, but refer to the UUID of the resource that contains proof of the claim. Each reference should look like: [view](uuid-of-source-resource). Keep your answer concise, in bullet point form.
+
+      The patient's medical record is:
+      ${context}
+      `
+    );
+
+    const response = await agent.continueConversation();
     sse.push({
       message: (response.content[response.content.length - 1] as AnthropicMessageText).text,
       eventName: "side-panel-response",
