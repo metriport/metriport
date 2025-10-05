beca47aa21a5658474d8ab42e83536f59a8543f2
Lucas Della Bella <dellabella.lucas@gmail.com>
2025-10-05 11:17:23 -0700

    feat(api): remove unnecessary route
    metriport/metriport-internal#1040
    
    Ref: ENG-1206
    Signed-off-by: Lucas Della Bella <dellabella.lucas@gmail.com>

--- packages/api/src/routes/medical/dash.ts
+++ packages/api/src/routes/medical/dash.ts
@@ -1,12 +1,10 @@
 import Router from "express-promise-router";
 import medical from "./index";
 import tcmEncounter from "./tcm-encounter";
-import inference from "./inference";
 
 const routes = Router();
 
 routes.use("/", medical);
 routes.use("/tcm/encounter", tcmEncounter);
-routes.use("/inference", inference);
 
 export default routes;
