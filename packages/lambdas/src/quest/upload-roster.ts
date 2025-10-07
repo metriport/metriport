581066686501e316b615e337f74553c05e463927
Keshav Saharia <keshav@metriport.com>
2025-10-07 12:52:06 -0700

    fix(lambdas): fix roster upload lambda event parameters
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/lambdas/src/quest/upload-roster.ts
+++ packages/lambdas/src/quest/upload-roster.ts
@@ -2,17 +2,23 @@ import { capture } from "../shared/capture";
 import { prefixedLog } from "../shared/log";
 import { getEnvVarOrFail } from "@metriport/shared/common/env-var";
 import { QuestUploadRosterHandlerDirect } from "@metriport/core/external/quest/command/upload-roster/upload-roster-direct";
+import { QuestRosterType } from "@metriport/core/external/quest/types";
 
 capture.init();
 
 // Automatically set by AWS
 const lambdaName = getEnvVarOrFail("AWS_LAMBDA_FUNCTION_NAME");
 
-export const handler = capture.wrapHandler(async () => {
+interface QuestUploadRosterEvent {
+  rosterType: QuestRosterType;
+}
+
+export const handler = capture.wrapHandler(async ({ rosterType }: QuestUploadRosterEvent) => {
   capture.setExtra({ context: lambdaName });
+
   const log = prefixedLog("quest.upload-roster");
   log("Starting upload of Quest roster");
   const handler = new QuestUploadRosterHandlerDirect();
-  await handler.generateAndUploadLatestQuestRoster();
+  await handler.generateAndUploadLatestQuestRoster({ rosterType });
   log("Upload of Quest roster completed");
 });
