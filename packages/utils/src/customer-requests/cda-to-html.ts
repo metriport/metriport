34b81a4faf5decae839bdd2729236c77915babf4
Keshav Saharia <keshav@metriport.com>
2025-10-06 18:55:56 -0700

    docs(utils): improve comments for script usage
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/utils/src/customer-requests/cda-to-html.ts
+++ packages/utils/src/customer-requests/cda-to-html.ts
@@ -14,6 +14,13 @@ import { cleanUpPayload } from "@metriport/core/domain/conversion/cleanup";
 /**
  * Script to convert a CDA document to HTML. Pass an absolute or relative path to the CDA/XML file
  * as an argument - if it is a relative path, it must be relative to the utils directory.
+ *
+ * Usage:
+ * npm run cda-to-html runs/cda-to-html/example-cda.xml
+ *
+ * In the example above, a relative path is provided to the gitignored "runs" directory. You can also
+ * supply an absolute path for the XML file to convert to HTML. In the example above, the output HTML
+ * will be written to the same directory as "example-cda.html".
  */
 const program = new Command();
 program.name("cda-to-html");
@@ -22,8 +29,6 @@ program.argument("<source-file>", "The path to the CDA/XML file");
 program.showHelpAfterError();
 program.action(convertCdaToHtml);
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// Need to run from the utils directory to have a stable working directory
 const styleSheetText = require(path.join(
   __dirname,
   "../../../../../lambdas/src/cda-to-visualization/stylesheet.js"
