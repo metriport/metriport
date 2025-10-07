65837f521b8269881a2fd9cf83162d189981540a
Keshav Saharia <keshav@metriport.com>
2025-10-06 17:38:53 -0700

    test(core): added test cases for cleaning ampersand
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/domain/conversion/__tests__/cleanup.test.ts
+++ packages/core/src/domain/conversion/__tests__/cleanup.test.ts
@@ -6,6 +6,7 @@ import {
   cleanUpTranslationCode,
   replaceXmlTagChars,
   xmlTranslationCodeRegex,
+  replaceAmpersand,
 } from "../cleanup";
 
 describe("cleanUpTranslationCode", () => {
@@ -29,6 +30,46 @@ describe("cleanUpTranslationCode", () => {
     expect(result).not.toContain(`<translation code="272.4\\272.4"`);
     expect(translationCodeMatch).toBeDefined();
     expect(translationCodeMatch).toEqual(`<translation code="272.4"`);
+  });
+});
+
+describe("replaceAmpersand", () => {
+  test("should be backwards compatible", () => {
+    // The previous regex only searched for /\s&\s/g, so it would only work for cases where
+    // " & " should be replaced by " &amp; "
+    expect(replaceAmpersand(" & ")).toBe(" &amp; ");
+  });
+
+  test("should replace unescaped ampersand with escaped ampersand", () => {
+    expect(replaceAmpersand("a&b")).toBe("a&amp;b");
+  });
+
+  test("should not replace escaped hexadecimal sequence", () => {
+    expect(replaceAmpersand("yo&#xA9;yo&ox3;")).toBe("yo&#xA9;yo&amp;ox3;");
+  });
+
+  test("should not replace escaped decimal sequence", () => {
+    expect(replaceAmpersand("APPEARANCE:&#160;a&ox3.&#160;")).toBe(
+      "APPEARANCE:&#160;a&amp;ox3.&#160;"
+    );
+  });
+
+  test("should not replace the five predefined XML entities", () => {
+    expect(replaceAmpersand("&lt;")).toBe("&lt;");
+    expect(replaceAmpersand("&gt;")).toBe("&gt;");
+    expect(replaceAmpersand("&amp;")).toBe("&amp;");
+    expect(replaceAmpersand("&quot;")).toBe("&quot;");
+    expect(replaceAmpersand("&apos;")).toBe("&apos;");
+  });
+
+  test("should not replace already escaped ampersand", () => {
+    expect(replaceAmpersand("A &amp; B")).toBe("A &amp; B");
+    expect(replaceAmpersand("A &amp;amp; B")).toBe("A &amp;amp; B");
+  });
+
+  test("should work in edge cases", () => {
+    expect(replaceAmpersand("&")).toBe("&amp;");
+    expect(replaceAmpersand("&&amp;")).toBe("&amp;&amp;");
   });
 });
 
