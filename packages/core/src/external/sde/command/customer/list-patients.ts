366e4823451d64f407e19fe1d44045dafceb221d
Keshav Saharia <keshav@metriport.com>
2025-10-15 19:27:00 -0700

    feat(core): update to command that returns patient IDs
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

---
+++ packages/core/src/external/sde/command/customer/list-patients.ts
@@ -0,0 +1,32 @@
+import { out } from "../../../../util/log";
+import { S3Utils } from "../../../aws/s3";
+import { Config } from "../../../../util/config";
+import _ from "lodash";
+import { ListPatientsInput } from "../../types";
+
+export async function listPatientIdsWithDocuments({ cxId }: ListPatientsInput): Promise<string[]> {
+  const { log } = out(`sde.listPatientsWithDocuments - cx ${cxId}`);
+  log("Listing patients with documents by CX ID...");
+
+  const s3 = new S3Utils(Config.getAWSRegion());
+  const bucketName = Config.getCdaToFhirConversionBucketName();
+  if (!bucketName) {
+    log(`No cda to fhir conversion bucket name found`);
+    return [];
+  }
+
+  const objects = await s3.listObjects(bucketName, cxId);
+
+  const patientIds = _(objects)
+    .map(object => {
+      const key = object.Key;
+      if (!key) return undefined;
+      const parts = key.split("/");
+      return parts.length === 3 ? parts[1] : undefined;
+    })
+    .compact()
+    .uniq()
+    .value();
+
+  return patientIds;
+}
