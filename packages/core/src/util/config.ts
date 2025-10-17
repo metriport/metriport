95b6ba18bab3e9e1463829a74fa56e227296797e
RadmirGari <radmirgaripovrss@gmail.com>
2025-10-17 15:44:40 -0700

    feat(core): add heartbeat monitors for adts
    Part of ENG-000
    
    Signed-off-by: RadmirGari <radmirgaripovrss@gmail.com>

--- packages/core/src/util/config.ts
+++ packages/core/src/util/config.ts
@@ -514,4 +514,8 @@ export class Config {
   static getAiBriefBucketName(): string {
     return getEnvVarOrFail("AI_BRIEF_BUCKET_NAME");
   }
+
+  static getHeartBeatMonitorMap(): Record<string, string> {
+    return getEnvVarAsRecordOrFail("HEARTBEAT_MONITOR_MAP");
+  }
 }
