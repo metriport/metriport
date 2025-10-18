bf92c468b22296dc9b71b87bdf6a059d5973eb93
RadmirGari <radmirgaripovrss@gmail.com>
2025-10-18 16:36:17 -0700

    feat(core): add new mappings
    Part of ENG-000
    
    Signed-off-by: RadmirGari <radmirgaripovrss@gmail.com>

--- packages/core/src/command/hl7v2-subscriptions/types.ts
+++ packages/core/src/command/hl7v2-subscriptions/types.ts
@@ -41,6 +41,9 @@ export type RosterRowData = {
   dateMid2025NoDelimiter: string;
   emptyString: string;
   addAllCaps: "ADD";
+  address1AddressLine1SplitByTabAddress1: string;
+  address1AddressLine1SplitByTabAddress2: string;
+  firstNameWithNoParenthesis: string;
 };
 
 export type HiePatientRosterMapping = {
