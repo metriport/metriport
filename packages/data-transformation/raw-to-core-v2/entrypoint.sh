35e5da7b04cc040ca78bdc858e47d775093380cc
Thomas Yopes <thomasyopes@Mac.attlocal.net>
2025-10-19 21:34:25 -0700

    feat(ap): delete that shit
    Ref: ENG-1233
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Mac.attlocal.net>

--- packages/data-transformation/raw-to-core-v2/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-INPUT_DATABASE="${DBT_TUVA_CI_DATABASE}"
-INPUT_SCHEMA="${DBT_SNOWFLAKE_CI_SCHEMA}"
-dbt build --vars "{"input_database": "$INPUT_DATABASE", "input_schema": "$INPUT_SCHEMA"}"
