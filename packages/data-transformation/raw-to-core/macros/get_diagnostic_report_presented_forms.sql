02bf7a51ba1e5f48255cd6374612487865ccbb0f
Thomas Yopes <thomasyopes@Thomass-MacBook-Pro-2.local>
2025-10-22 10:04:01 -0700

    feat(ap): macros for leading zeros
    Ref: ENG-1233
    
    Ref: #1040
    Signed-off-by: Thomas Yopes <thomasyopes@Thomass-MacBook-Pro-2.local>

--- packages/data-transformation/raw-to-core/macros/get_diagnostic_report_presented_forms.sql
+++ packages/data-transformation/raw-to-core/macros/get_diagnostic_report_presented_forms.sql
@@ -1,15 +1,19 @@
 {% macro get_diagnostic_report_presented_forms(max_index) %}
     {% for i in range(max_index + 1) %}
+    {% set formatted_index = i|string %}
+    {% if max_index > 9 %}
+        {% set formatted_index = "%02d"|format(i) %}
+    {% endif %}
     select
             id                                         as diagnostic_report_id
-        ,   presentedform_{{i}}_data                   as data
-        ,   presentedform_{{i}}_contenttype            as content_type
-        ,   presentedform_{{i}}_creation               as creation
-        ,   presentedform_{{i}}_hash                   as hash
-        ,   presentedform_{{i}}_language               as language
-        ,   presentedform_{{i}}_size                   as size
-        ,   presentedform_{{i}}_title                  as title
-        ,   presentedform_{{i}}_url                    as url
+        ,   presentedform_{{formatted_index}}_data                   as data
+        ,   presentedform_{{formatted_index}}_contenttype            as content_type
+        ,   presentedform_{{formatted_index}}_creation               as creation
+        ,   presentedform_{{formatted_index}}_hash                   as hash
+        ,   presentedform_{{formatted_index}}_language               as language
+        ,   presentedform_{{formatted_index}}_size                   as size
+        ,   presentedform_{{formatted_index}}_title                  as title
+        ,   presentedform_{{formatted_index}}_url                    as url
     from {{ref('stage__diagnosticreport')}}
     {% if not loop.last %}union all{% endif %}
     {% endfor %}
--- packages/data-transformation/raw-to-core/macros/get_multiple_references.sql
+++ packages/data-transformation/raw-to-core/macros/get_multiple_references.sql
@@ -7,20 +7,24 @@
     reference_id_field_suffix
 )%}
     {% for i in range(max_index + 1) %}
+    {% set formatted_index = i|string %}
+    {% if max_index > 9 %}
+        {% set formatted_index = "%02d"|format(i) %}
+    {% endif %}
     select
         id                                                                                      as {{resource_id_field}}
     ,   '{{reference_property}}'                                                                as property
-    ,   right('{{reference_id_field_prefix}}_{{i}}_{{reference_id_field_suffix}}', 36)          as reference_id
+    ,   right('{{reference_id_field_prefix}}_{{formatted_index}}_{{reference_id_field_suffix}}', 36)          as reference_id
     ,   replace(
-            '{{reference_id_field_prefix}}_{{i}}_{{reference_id_field_suffix}}',
+            '{{reference_id_field_prefix}}_{{formatted_index}}_{{reference_id_field_suffix}}',
             concat(
                 '/', 
-                right('{{reference_id_field_prefix}}_{{i}}_{{reference_id_field_suffix}}', 36)
+                right('{{reference_id_field_prefix}}_{{formatted_index}}_{{reference_id_field_suffix}}', 36)
             ),
             ''
         )                                                                                       as reference_type
     from {{ref(stage_table_name)}}
-    where '{{reference_id_field_prefix}}_{{i}}_{{reference_id_field_suffix}}' != ''
+    where '{{reference_id_field_prefix}}_{{formatted_index}}_{{reference_id_field_suffix}}' != ''
     {% if not loop.last %}union all{% endif %}
     {% endfor %}
 {% endmacro %}
