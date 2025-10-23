2cca2780960eec3e532b8e469e8d978faf1c0991
Keshav Saharia <keshav@metriport.com>
2025-10-23 11:36:18 -0700

    fix(core): update exports for sde
    metriport/metriport-internal#1040
    
    Signed-off-by: Keshav Saharia <keshav@metriport.com>

--- packages/core/src/sde/index.ts
+++ packages/core/src/sde/index.ts
@@ -3,4 +3,3 @@ export * from "./types";
 export * from "./command/bundle/get-bundles";
 export * from "./command/document/download";
 export * from "./command/document/list-documents";
-export * from "./command/document/list-patients";
