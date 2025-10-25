688c415ae9645a6b51a6e1f3273a861e65a834dd
Alexey Sergeev <alexey@metriport.com>
2025-10-25 12:39:38 -0700

    feat(api): trigger patient create when patient created in canvas
    metriport/metriport-internal#1040
    
    Ref:
    Signed-off-by: Alexey Sergeev <alexey@metriport.com>

--- packages/api/src/routes/ehr/canvas/patient-webhook.ts
+++ packages/api/src/routes/ehr/canvas/patient-webhook.ts
@@ -36,4 +36,31 @@ router.post(
   })
 );
 
+/**
+ * POST /ehr/webhook/canvas/patient/:id/patient-created
+ *
+ * Tries to retrieve the matching Metriport patient on patient created
+ * @param req.params.id The ID of Canvas Patient.
+ * @returns HTTP 200 OK on successful processing.
+ */
+router.post(
+  "/:id/patient-created",
+  handleParams,
+  requestLogger,
+  asyncHandler(async (req: Request, res: Response) => {
+    const cxId = getCxIdOrFail(req);
+    const canvasPatientId = getFrom("params").orFail("id", req);
+    const canvasPracticeId = getFromQueryOrFail("practiceId", req);
+    const handler = buildEhrSyncPatientHandler();
+    await handler.processSyncPatient({
+      ehr: EhrSources.canvas,
+      cxId,
+      practiceId: canvasPracticeId,
+      patientId: canvasPatientId,
+      triggerDq: true,
+    });
+    return res.sendStatus(httpStatus.OK);
+  })
+);
+
 export default router;
