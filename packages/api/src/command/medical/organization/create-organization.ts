348f5a11fdf01138a7125f88c6b2cdfe2f3d59f0
RadmirGari <radmirgaripovrss@gmail.com>
2025-10-27 12:31:12 -0700

    fix(core): remove default cohorts
    Part of ENG-1114
    
    Signed-off-by: RadmirGari <radmirgaripovrss@gmail.com>

--- packages/api/src/command/medical/organization/create-organization.ts
+++ packages/api/src/command/medical/organization/create-organization.ts
@@ -7,7 +7,6 @@ import { UniqueConstraintError } from "sequelize";
 import { createTenantIfNotExists } from "../../../external/fhir/admin";
 import { upsertOrgToFHIRServer } from "../../../external/fhir/organization/upsert-organization";
 import { OrganizationModel } from "../../../models/medical/organization";
-import { createDefaultCohorts } from "../cohort/create-default-cohorts";
 import { createOrganizationId } from "../customer-sequence/create-id";
 import { getOrganization } from "./get-organization";
 
@@ -38,8 +37,6 @@ export async function createOrganization({
   await createTenantIfNotExists(org);
   const fhirOrg = toFHIR(org);
   await upsertOrgToFHIRServer(org.cxId, fhirOrg);
-
-  await createDefaultCohorts({ cxId });
 
   return org;
 }
