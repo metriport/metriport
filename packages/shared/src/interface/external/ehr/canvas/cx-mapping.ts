ce27dc85a04bdf0ce702cf0d2e2b7a9756ece3ac
Alexey Sergeev <alexey@metriport.com>
2025-10-27 17:34:33 -0700

    feat(shared): finalize
    metriport/metriport-internal#1040
    
    Ref:
    Signed-off-by: Alexey Sergeev <alexey@metriport.com>

--- packages/shared/src/interface/external/ehr/canvas/cx-mapping.ts
+++ packages/shared/src/interface/external/ehr/canvas/cx-mapping.ts
@@ -1,5 +1,11 @@
 import { z } from "zod";
 import { writeBackFiltersSchema } from "../shared";
 
-export const canvasSecondaryMappingsSchema = z.object({}).merge(writeBackFiltersSchema).optional();
+export const canvasSecondaryMappingsSchema = z
+  .object({
+    webhookPatientPatientLinkingDisabled: z.boolean().optional(),
+    webhookPatientPatientProcessingEnabled: z.boolean().optional(),
+  })
+  .merge(writeBackFiltersSchema)
+  .optional();
 export type CanavsSecondaryMappings = z.infer<typeof canvasSecondaryMappingsSchema>;
